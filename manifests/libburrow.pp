5b6bbed9cb996944ab4e963a9a22c87fe7a09b9c
Monty Taylor <mordred@inaugust.com>
2011-07-15 09:02:03 -0400

    Added libburrow manifest.

---
+++ manifests/libburrow.pp
@@ -0,0 +1,29 @@
+import "openstack_ci_admins_users"
+import "static_users"
+import "jenkins_slave"
+
+node default {
+  include openstack_ci_admins_users
+  include static_users
+  include jenkins_slave
+
+  package { "build-essential":
+    ensure => latest
+  }
+
+  package { "libcurl4-gnutls-dev":
+    ensure => latest
+  }
+
+  package { "libtool":
+    ensure => latest
+  }
+
+  package { "autoconf":
+    ensure => latest
+  }
+
+  package { "automake":
+    ensure => latest
+  }
+}
