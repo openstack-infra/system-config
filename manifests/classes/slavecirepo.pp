b00b7bc812e7f25b22e9da2f6b86ea2f6b324ea0
Monty Taylor <mordred@inaugust.com>
2011-07-16 13:19:00 -0400

    Put the openstack-ci repo on the slaves.

---
+++ manifests/classes/slavecirepo.pp
@@ -0,0 +1,38 @@
+define slavecirepo($ensure = present) {
+  $repo_there = "test -d /home/jenkins/openstack-ci"
+  case $ensure {
+    present: {
+      exec { "Clone openstack-ci git repo":
+        path        => "/bin:/usr/bin",
+        environment => "HOME=/home/jenkins",
+        command     => "cd /home/jenkins && git clone git://github.com/openstack/openstack-ci.git",
+        user        => "jenkins",
+        group       => "jenkins",
+        unless      => "$repo_there",
+        logoutput   => on_failure,
+      }
+      exec { "Update openstack-ci git repo":
+        path        => "/bin:/usr/bin",
+        environment => "HOME=/home/jenkins",
+        command     => "cd /home/jenkins/openstack-ci && git pull",
+        user        => "jenkins",
+        group       => "jenkins",
+        onlyif      => "$repo_there",
+        logoutput   => on_failure,
+      }
+    }
+    absent:  {
+      exec { "Remove OpenStack git repo":
+        path    => "/bin:/usr/bin",
+        environment => "HOME=/root",
+        command => "rm -rf /home/jenkins/openstack-ci",
+        user    => "root",
+        group   => "root",
+        onlyif  => "$repo_there",
+      }
+    }
+    default: {
+      fail "Invalid 'ensure' value '$ensure' for slavecirepo"
+    }
+  }
+}
---
+++ manifests/glance.pp
@@ -0,0 +1,74 @@
+import "openstack_ci_admins_users"
+import "static_users"
+import "jenkins_slave"
+
+node default {
+  include openstack_ci_admins_users
+  include static_users
+  include jenkins_slave
+
+package { "python-argparse":
+  ensure => latest
+}
+package { "python-decorator":
+  ensure => latest
+}
+package { "python-eventlet":
+  ensure => latest
+}
+package { "python-formencode":
+  ensure => latest
+}
+package { "python-greenlet":
+  ensure => latest
+}
+package { "python-migrate":
+  ensure => latest
+}
+package { "python-mox":
+  ensure => latest
+}
+package { "python-netifaces":
+  ensure => latest
+}
+package { "python-openid":
+  ensure => latest
+}
+package { "python-openssl":
+  ensure => latest
+}
+package { "python-paste":
+  ensure => latest
+}
+package { "python-pastedeploy":
+  ensure => latest
+}
+package { "python-pastescript":
+  ensure => latest
+}
+package { "python-routes":
+  ensure => latest
+}
+package { "python-scgi":
+  ensure => latest
+}
+package { "python-sqlalchemy":
+  ensure => latest
+}
+package { "python-sqlalchemy-ext":
+  ensure => latest
+}
+package { "python-swift":
+  ensure => latest
+}
+package { "python-tempita":
+  ensure => latest
+}
+package { "python-webob":
+  ensure => latest
+}
+package { "python-xattr":
+  ensure => latest
+}
+
+}
