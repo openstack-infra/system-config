bb2f047c9c45d011361d253d4444b2fd1ebd3612
James E. Blair <james.blair@rackspace.com>
2011-07-18 17:33:29 +0000

    Add puppet module for ssh that installs an sshd_config that only allows login via keys.

---
+++ modules/ssh/manifests/init.pp
@@ -0,0 +1,19 @@
+class ssh {
+    package { openssh-server: ensure => latest }
+    service { ssh: 
+      ensure          => running,
+      hasrestart      => true,
+      subscribe       => File["/etc/ssh/sshd_config"],
+    }
+    file { "/etc/ssh/sshd_config":
+      owner => 'root',
+      group => 'root',
+      mode => 444,
+      ensure => 'present',
+      source => [
+                 "puppet:///modules/ssh/sshd_config.$operatingsystem",
+                 "puppet:///modules/ssh/sshd_config"
+      ],
+      replace => 'true',
+    }
+}
