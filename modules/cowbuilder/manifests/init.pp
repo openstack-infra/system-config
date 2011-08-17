dea5cb04fa11dc2931ab920816cafd1789ed7592
Monty Taylor <mordred@inaugust.com>
2011-08-17 11:03:57 -0400

    Added debian package building slave definition.
    Change-Id: Ia4d6833339ed3975ca1dacb93f7e177caa990703

---
+++ modules/cowbuilder/manifests/init.pp
@@ -0,0 +1,52 @@
+class cowbuilder {
+    
+  $slave_packages = ["git-buildpackage",
+                     "pbuilder",
+                     "cowbuilder",
+                     "debian-archive-keyring"]
+
+  $ubuntu_releases = [ "lucid",
+                       "maverick",
+                       "natty",
+                       "oneiric" ]
+
+  $debian_releases = [ "wheezy",
+                       "squeeze" ]
+
+  package { $slave_packages:
+    ensure => "latest"
+  }
+
+  file { 'cowhookdir':
+    name => '/var/cache/pbuilder/hook.d',
+    ensure => 'directory',
+    mode => 755,
+    require => Package['pbuilder'],
+  }
+
+  file { 'cowhook':
+    name => '/var/cache/pbuilder/hook.d/E01-enable-repos',
+    owner => 'root',
+    group => 'root',
+    mode => 755,
+    ensure => 'present',
+    source => "puppet:///modules/cowbuilder/E01-enable-repos",
+    replace => 'true',
+    require => File[cowhookdir]
+  }
+
+  cowbuilder::debgpg { 'AED4B06F473041FA': }
+
+  cowbuilder::cow { $ubuntu_releases:
+    distro => 'ubuntu',
+    require => [Package[debian-archive-keyring], File[cowhook]],
+  }
+  cowbuilder::cow { $debian_releases:
+    distro => 'debian',
+    require => [ Package[debian-archive-keyring],
+                 File[cowhook],
+                 Cowbuilder::Debgpg[AED4B06F473041FA],
+               ],
+  }
+
+}
