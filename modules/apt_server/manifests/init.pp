51a19ab9382fdeceb326ed0184e34eea3957a1e4
Monty Taylor <mordred@inaugust.com>
2011-08-22 00:44:40 -0400

    Added puppet entries for packages.o.o.
    Change-Id: I3d2997b57858708257e0b7985321cb9cbe0da305
    Reviewed-on: https://review.openstack.org/303
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Tested-by: Jenkins

---
+++ modules/apt_server/manifests/init.pp
@@ -0,0 +1,63 @@
+class apt_server {
+
+  $packages = ["reprepro",
+               "nginx"]
+
+  package { $packages: ensure => "latest" }
+
+  file { "/etc/nginx/sites-available/default":
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    source => "puppet:///modules/apt_server/packages",
+    replace => 'true',
+    require => Package[nginx],
+  }
+
+  file { "/etc/nginx/sites-enabled/default":
+    ensure => link,
+    target => "/etc/nginx/sites-available/default",
+    require => Package[nginx],
+  }
+
+  file { "/srv":
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => directory,
+  }
+
+  file {"/srv/packages":
+    owner => 'root',
+    group => 'root',
+    mode => 755,
+    ensure => directory,
+    require => File["/srv"],
+  }
+
+  file {"/srv/packages/conf":
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => directory,
+    require => File["/srv/packages"],
+  }
+
+  file {"/srv/packages/conf/distributions":
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    source => "puppet:///modules/apt_server/distributions",
+  }
+
+  service { 'nginx':
+    name       => 'nginx',
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    require => Package['nginx'],
+  }
+
+}
