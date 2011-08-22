dc14d9d7ec23e15eb603675d4280c62d9efcd948
Monty Taylor <mordred@inaugust.com>
2011-08-22 12:26:23 -0400

    Restructure archive for both trunk and diablo.
    Change-Id: I9dcba819629c068ccfedaa0fc6c23b7bd631c5a9

--- modules/reprepro/manifests/init.pp
+++ modules/reprepro/manifests/init.pp
@@ -10,22 +10,54 @@ class reprepro {
   }
 
   file {"/srv/packages":
-    owner => 'jenkins',
-    group => 'jenkins',
-    mode => 755,
-    ensure => directory,
-    require => [File["/srv"], User[jenkins]]
-  }
-
-  file {"/srv/packages/conf":
     owner => 'root',
     group => 'root',
     mode => 555,
     ensure => directory,
-    require => File["/srv/packages"],
+    require => File["/srv"],
   }
 
-  file {"/srv/packages/conf/distributions":
+  file {"/srv/packages/trunk":
+    owner => 'jenkins',
+    group => 'jenkins',
+    mode => 755,
+    ensure => directory,
+    require => [File["/srv/packages"], User[jenkins]]
+  }
+
+  file {"/srv/packages/trunk/conf":
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => directory,
+    require => File["/srv/packages/trunk"],
+  }
+
+  file {"/srv/packages/trunk/conf/distributions":
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    source => "puppet:///modules/reprepro/distributions",
+  }
+
+  file {"/srv/packages/diablo":
+    owner => 'jenkins',
+    group => 'jenkins',
+    mode => 755,
+    ensure => directory,
+    require => [File["/srv/packages"], User[jenkins]]
+  }
+
+  file {"/srv/packages/diablo/conf":
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => directory,
+    require => File["/srv/packages/diablo"],
+  }
+
+  file {"/srv/packages/diablo/conf/distributions":
     owner => 'root',
     group => 'root',
     mode => 444,
