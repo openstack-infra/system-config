00059f5b2f838d132cdd550b0ebf61b1a9800f3e
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-03-06 13:37:46 +0000

    Modify puppet repo to work with stackforge
    Add stackforge manifest (can be pointed to in puppet.ini)
    Remove gerrit_installed lib (doesn't work with puppet master)
    Make jenkins_master module more generic
    Have an SSH key for different jenkins_slave setups
    
    Change-Id: Ic52f06d150210038aaf47c48aeb7c991b94c6fc8

--- modules/gerrit/manifests/init.pp
+++ modules/gerrit/manifests/init.pp
@@ -17,6 +17,21 @@ $commentlinks = [ { name => 'changeid',
                   ]
   ) {
 
+  user { "gerrit2":
+    ensure => present,
+    comment => "Gerrit",
+    home => "/home/gerrit2",
+    shell => "/bin/bash",
+    gid => "gerrit2",
+    system => true,
+    managehome => true,
+    require => Group["gerrit2"]
+  }
+
+  group { "gerrit2":
+    ensure => present
+  }
+
   package { "gitweb":
     ensure => latest
   }
@@ -33,104 +48,129 @@ $commentlinks = [ { name => 'changeid',
     require => Package[python-pip]
   }
 
-  if $gerrit_installed {
-    #notice('Gerrit is installed')
+  cron { "gerritupdateci":
+    user => gerrit2,
+    minute => "*/15",
+    command => 'sleep $((RANDOM\%60)) && cd /home/gerrit2/openstack-ci && /usr/bin/git pull -q origin master'
+  }
 
-    cron { "gerritupdateci":
-      user => gerrit2,
-      minute => "*/15",
-      command => 'sleep $((RANDOM\%60)) && cd /home/gerrit2/openstack-ci && /usr/bin/git pull -q origin master'
-    }
+  cron { "gerritsyncusers":
+    user => gerrit2,
+    minute => "*/15",
+    command => 'sleep $((RANDOM\%60+60)) && cd /home/gerrit2/openstack-ci && python gerrit/update_gerrit_users.py'
+  }
 
-    cron { "gerritsyncusers":
-      user => gerrit2,
-      minute => "*/15",
-      command => 'sleep $((RANDOM\%60+60)) && cd /home/gerrit2/openstack-ci && python gerrit/update_gerrit_users.py'
-    }
+  cron { "gerritclosepull":
+    user => gerrit2,
+    minute => "*/5",
+    command => 'sleep $((RANDOM\%60+90)) && cd /home/gerrit2/openstack-ci && python gerrit/close_pull_requests.py'
+  }
 
-    cron { "gerritclosepull":
-      user => gerrit2,
-      minute => "*/5",
-      command => 'sleep $((RANDOM\%60+90)) && cd /home/gerrit2/openstack-ci && python gerrit/close_pull_requests.py'
-    }
+  cron { "expireoldreviews":
+    user => gerrit2,
+    hour => 6,
+    minute => 3,
+    command => 'cd /home/gerrit2/openstack-ci && python gerrit/expire_old_reviews.py'
+  }
 
-    cron { "expireoldreviews":
-      user => gerrit2,
-      hour => 6,
-      minute => 3,
-      command => 'cd /home/gerrit2/openstack-ci && python gerrit/expire_old_reviews.py'
-    }
+  cron { "gerrit_repack":
+    user => gerrit2,
+    weekday => 0,
+    hour => 4,
+    minute => 7,
+    command => 'find /home/gerrit2/review_site/git/ -type d -name "*.git" -print -exec git --git-dir="{}" repack -afd \;',
+    environment => "PATH=/usr/bin:/bin:/usr/sbin:/sbin",
+  }
 
-    cron { "gerrit_repack":
-      user => gerrit2,
-      weekday => 0,
-      hour => 4,
-      minute => 7,
-      command => 'find /home/gerrit2/review_site/git/ -type d -name "*.git" -print -exec git --git-dir="{}" repack -afd \;',
-      environment => "PATH=/usr/bin:/bin:/usr/sbin:/sbin",
-    }
+  file { "/var/log/gerrit":
+    ensure => "directory",
+    owner => 'gerrit2'
+  }
 
-    file { "/var/log/gerrit":
-      ensure => "directory",
-      owner => 'gerrit2'
-    }
+# directory creation hacks until we can automate gerrit installation
 
-    file { '/home/gerrit2/github.config':
-      owner => 'root',
-      group => 'root',
-      mode => 444,
-      ensure => 'present',
-      content => template('gerrit/github.config.erb'),
-      replace => 'true',
-    }
+  file { "/home/gerrit2/review_site":
+    ensure => "directory",
+    owner => "gerrit2",
+    require => User["gerrit2"]
+  }
 
-    file { '/home/gerrit2/review_site/etc/replication.config':
-      owner => 'root',
-      group => 'root',
-      mode => 444,
-      ensure => 'present',
-      source => 'puppet:///modules/gerrit/replication.config',
-      replace => 'true',
-    }
+  file { "/home/gerrit2/review_site/etc":
+    ensure => "directory",
+    owner => "gerrit2",
+    require => File["/home/gerrit2/review_site"]
+  }
 
-    file { '/home/gerrit2/review_site/etc/gerrit.config':
-      owner => 'root',
-      group => 'root',
-      mode => 444,
-      ensure => 'present',
-      content => template('gerrit/gerrit.config.erb'),
-      replace => 'true',
-    }
+  file { "/home/gerrit2/review_site/hooks":
+    ensure => "directory",
+    owner => "gerrit2",
+    require => File["/home/gerrit2/review_site"]
+  }
 
-    file { '/home/gerrit2/review_site/hooks/change-merged':
-      owner => 'root',
-      group => 'root',
-      mode => 555,
-      ensure => 'present',
-      source => 'puppet:///modules/gerrit/change-merged',
-      replace => 'true',
-    }
+  file { "/home/gerrit2/review_site/static":
+    ensure => "directory",
+    owner => "gerrit2",
+    require => File["/home/gerrit2/review_site"]
+  }
 
-    file { '/home/gerrit2/review_site/hooks/patchset-created':
-      owner => 'root',
-      group => 'root',
-      mode => 555,
-      ensure => 'present',
-      source => 'puppet:///modules/gerrit/patchset-created',
-      replace => 'true',
-    }
+  file { '/home/gerrit2/github.config':
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    content => template('gerrit/github.config.erb'),
+    replace => 'true',
+    require => User["gerrit2"]
+  }
 
-    file { '/home/gerrit2/review_site/static/echosign-cla.html':
-      owner => 'root',
-      group => 'root',
-      mode => 444,
-      ensure => 'present',
-      source => 'puppet:///modules/gerrit/echosign-cla.html',
-      replace => 'true',
-    }
+  file { '/home/gerrit2/review_site/etc/replication.config':
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    source => 'puppet:///modules/gerrit/replication.config',
+    replace => 'true',
+    require => File["/home/gerrit2/review_site/etc"]
+  }
 
-  } else {
-    notice('Gerrit is not installed')
+  file { '/home/gerrit2/review_site/etc/gerrit.config':
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    content => template('gerrit/gerrit.config.erb'),
+    replace => 'true',
+    require => File["/home/gerrit2/review_site/etc"]
+  }
+
+  file { '/home/gerrit2/review_site/hooks/change-merged':
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => 'present',
+    source => 'puppet:///modules/gerrit/change-merged',
+    replace => 'true',
+    require => File["/home/gerrit2/review_site/hooks"]
+  }
+
+  file { '/home/gerrit2/review_site/hooks/patchset-created':
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => 'present',
+    source => 'puppet:///modules/gerrit/patchset-created',
+    replace => 'true',
+    require => File["/home/gerrit2/review_site/hooks"]
+  }
+
+  file { '/home/gerrit2/review_site/static/echosign-cla.html':
+    owner => 'root',
+    group => 'root',
+    mode => 444,
+    ensure => 'present',
+    source => 'puppet:///modules/gerrit/echosign-cla.html',
+    replace => 'true',
+    require => File["/home/gerrit2/review_site/static"]
   }
 
 }
