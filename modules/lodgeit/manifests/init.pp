863f0c862aeaa950dd8b924bf6507dd13f23caa3
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-03-22 18:04:24 +0000

    Make lodgeit module use our lodgeit git repo
    Our lodgeit repo has antispam and will have other features soon
    
    Change-Id: I12b1f8c1bfb5a2442370a55f8527196364c91ec8

--- modules/lodgeit/manifests/init.pp
+++ modules/lodgeit/manifests/init.pp
@@ -33,10 +33,11 @@ class lodgeit {
     hasrestart => true
   }
 
-# if we already have the mercurial repo the pull updates
+# if we already have the git repo the pull updates
 
   exec { "update_lodgeit":
-    command => "hg pull /tmp/lodgeit-main",
+    command => "git pull --ff-only",
+    cwd => "/tmp/lodgeit-main",
     path => "/bin:/usr/bin",
     onlyif => "test -d /tmp/lodgeit-main"
   }
@@ -44,7 +45,7 @@ class lodgeit {
 # otherwise get a new clone of it
 
   exec { "get_lodgeit":
-    command => "hg clone https://bitbucket.org/dcolish/lodgeit-main /tmp/lodgeit-main",
+    command => "git clone git://github.com/openstack-ci/lodgeit.git /tmp/lodgeit-main",
     path => "/bin:/usr/bin",
     onlyif => "test ! -d /tmp/lodgeit-main"
   }
