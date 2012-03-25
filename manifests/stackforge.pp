3f10f48f6f0cf760f56353158ce92f7b2f62bb69
Monty Taylor <mordred@inaugust.com>
2012-03-25 12:44:52 -0700

    Refactor Dynamic tcp ports.
    Change-Id: Id53f9960f0b7b5c27f3595fc6722a2a110aa88db

--- manifests/stackforge.pp
+++ manifests/stackforge.pp
@@ -3,7 +3,7 @@ import "users"
 #
 # Abstract classes:
 #
-class openstack_base {
+class openstack_base ($iptables_public_tcp_ports) {
   include openstack_project::users
   include ssh
   include snmpd
@@ -41,8 +41,10 @@ class openstack_base {
 }
 
 # A template host with no running services
-class openstack_template {
-  include openstack_base
+class openstack_template ($iptables_public_tcp_ports) {
+  class { 'openstack_base':
+    iptables_public_tcp_ports => $iptables_public_tcp_ports
+  }
   realize (
     User::Virtual::Localuser["mordred"],
     User::Virtual::Localuser["corvus"],
@@ -53,12 +55,16 @@ class openstack_template {
 }
 
 # A server that we expect to run for some time
-class openstack_server {
-  include openstack_template
+class openstack_server ($iptables_public_tcp_ports) {
+  class { 'openstack_template':
+    iptables_public_tcp_ports => $iptables_public_tcp_ports
+  }
 }
 
 class openstack_jenkins_slave {
-  include openstack_server
+  class { 'openstack_server':
+    iptables_public_tcp_ports => []
+  }
   class { 'jenkins_slave':
     ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvlHx1TM9y6Y+oWJwPQP1jDejQYLA5MaTgD2oQOgQapSAWWU3f9/xcKKF4I5cC833xrSqFCqpstuWt5FdtO6qL5KMqGeVOwTCgcH0uGHciSF/zxBVpHp2n3rHLb0Fibyz/ys2kI+9J/hD0+GlVNQ/U8h9PZPMLFoJIZz5ep5WBszLM5z4vymBZ3GeytD8hk1BW0GLYi9vYWFrwoCTH6o6xRtdKajNE/9NcRGXjkY+SW7EGvqTAfLdsQ8q23MIO2ZX6YOpnmxAmR3OyNEOMo7Y/XCWjqTGWhQ669YaFxagS65f7EGCGwhhgQPtReDwkW88yTGhU3fZjS6Rc3BymTsnx jenkins@jenkins.stackforge.org'
   }
@@ -69,20 +75,24 @@ class openstack_jenkins_slave {
 #
 
 node default {
-  include openstack_server
+  class { 'openstack_server':
+    iptables_public_tcp_ports => []
+  }
 }
 
 #
 # Long lived servers:
 #
 node "puppet.stackforge.org" {
-  $iptables_public_tcp_ports = [8140]
-  include openstack_server
+  class { 'openstack_server':
+    iptables_public_tcp_ports => [8140]
+  }
 }
 
 node "review.stackforge.org" {
-  $iptables_public_tcp_ports = [80, 443, 29418]
-  include openstack_server
+  class { 'openstack_server':
+    iptables_public_tcp_ports => [80, 443, 29418]
+  }
   class { 'gerrit':
     canonicalweburl => "https://review.stackforge.org/",
     email => "review@stackforge.org",
@@ -95,8 +105,9 @@ node "review.stackforge.org" {
 }
 
 node "jenkins.stackforge.org" {
-  $iptables_public_tcp_ports = [80, 443, 4155]
-  include openstack_server
+  class { 'openstack_server':
+    iptables_public_tcp_ports => [80, 443, 4155]
+  }
   class { 'jenkins_master':
     serveradmin => 'webmaster@stackforge.org',
     site => 'jenkins.stackforge.org',
