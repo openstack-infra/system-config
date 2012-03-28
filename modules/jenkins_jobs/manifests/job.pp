5289354f22ccd517e3e5c2cfbbb9e7f6ed67f849
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-03-28 10:57:01 +0100

    Fixes to Jenkins Job filler
    * Venv job had a bad variable, breaking its name
    * Jobs without publishers don't work, empty publisher tag needed
    * Don't set group permissions, Jenkins modifies them anyway
    * Only notify Jenkins on important files
    
    Change-Id: Idf32d25b37d60530671fa4c16fedd40b1d46047f

--- modules/jenkins_jobs/manifests/job.pp
+++ modules/jenkins_jobs/manifests/job.pp
@@ -3,42 +3,34 @@ define jenkins_jobs::job($site, $project, $job, $triggers="", $builders, $publis
   file { "/var/lib/jenkins/jobs/${project}-${job}":
     ensure => directory,
     owner => 'jenkins',
-    group => 'nogroup'
   }
 
   file { "/var/lib/jenkins/jobs/${project}-${job}/builds":
     ensure => directory,
     owner => 'jenkins',
-    group => 'nogroup',
-    require => File["/var/lib/jenkins/jobs/${project}-${job}"],
-    notify => Service["jenkins"]
+    require => File["/var/lib/jenkins/jobs/${project}-${job}"]
   }
 
   file { "/var/lib/jenkins/jobs/${project}-${job}/config-history":
     ensure => directory,
     owner => 'jenkins',
-    group => 'nogroup',
-    require => File["/var/lib/jenkins/jobs/${project}-${job}"],
-    notify => Service["jenkins"]
+    require => File["/var/lib/jenkins/jobs/${project}-${job}"]
   }
 
   file { "/var/lib/jenkins/jobs/${project}-${job}/config.xml":
     ensure => present,
     content => template("jenkins_jobs/body.xml.erb"),
     owner => 'jenkins',
-    group => 'nogroup',
     require => File["/var/lib/jenkins/jobs/${project}-${job}"],
     notify => Service["jenkins"]
   }
 
-  file { "/var/lib/jenkins/jobs/${project}-${job}/nextBuildNumer":
+  file { "/var/lib/jenkins/jobs/${project}-${job}/nextBuildNumber":
     ensure => present,
     content => "1",
     owner => 'jenkins',
-    group => 'nogroup',
     replace => false,
-    require => File["/var/lib/jenkins/jobs/${project}-${job}"],
-    notify => Service["jenkins"]
+    require => File["/var/lib/jenkins/jobs/${project}-${job}"]
   }
 
 }
