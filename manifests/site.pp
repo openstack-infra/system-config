e992c82c55f966b998368234a69cb6a640fc23a3
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-04-15 16:06:41 +0100

    Puts meetbot under control of puppet
    Adds meetbot and an nginx setup to puppet.  See manifests/site.pp for usage
    
    Change-Id: I47dcf2884a06441b482585bf5dae9f7d0bd7e543

--- manifests/site.pp
+++ manifests/site.pp
@@ -207,6 +207,22 @@ node "planet.openstack.org" {
   }
 }
 
+node "meetbot.openstack.org" {
+  class { 'openstack_server':
+    iptables_public_tcp_ports => [80]
+  }
+  include meetbot
+
+  meetbot::site { "openstack":
+    nick => "openstack",
+    network => "FreeNode",
+    server => "chat.us.freenode.net:7000",
+    url => "meetbot.openstack.org",
+    channels => "#openstack #openstack-dev #openstack-meeting",
+    use_ssl => "True"
+  }
+}
+
 # A bare machine, but with a jenkins user
 node /^.*\.template\.openstack\.org$/ {
   class { 'openstack_template':
---
+++ modules/meetbot/manifests/site.pp
@@ -0,0 +1,96 @@
+define meetbot::site($nick, $network, $server, $url, $channels, $use_ssl) {
+
+  file { "/etc/nginx/sites-available/${name}-meetbot":
+    ensure => 'present',
+    content => template("meetbot/nginx.erb"),
+    replace => 'true',
+    require => Package[nginx],
+    notify => Service[nginx]
+  }
+
+  file { "/etc/nginx/sites-enabled/${name}-meetbot":
+    ensure => link,
+    target => "/etc/nginx/sites-available/${name}-meetbot",
+    require => Package[nginx]
+  }
+
+  file { "/var/lib/meetbot/${name}":
+    ensure => directory,
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot"]
+  }
+
+  file { "/var/lib/meetbot/${name}/conf":
+    ensure => directory,
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}"]
+  }
+
+  file { "/var/lib/meetbot/${name}/data":
+    ensure => directory,
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}"]
+  }
+
+  file { "/var/lib/meetbot/${name}/data/tmp":
+    ensure => directory,
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}/data"]
+  }
+
+  file { "/var/lib/meetbot/${name}/backup":
+    ensure => directory,
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}"]
+  }
+
+  file { "/var/lib/meetbot/${name}/logs":
+    ensure => directory,
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}"]
+  }
+
+  # set to root/root so meetbot doesn't overwrite
+  file { "/var/lib/meetbot/${name}.conf":
+    ensure => present,
+    content => template("meetbot/supybot.conf.erb"),
+    owner => 'root',
+    group => 'root',
+    require => File["/var/lib/meetbot"],
+    notify => Service["${name}-meetbot"]
+  }
+
+  file { "/var/lib/meetbot/${name}/ircmeeting":
+    ensure => directory,
+    recurse => true,
+    source => "/tmp/meetbot/ircmeeting",
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}"]
+  }
+
+  file { "/var/lib/meetbot/${name}/ircmeeting/meetingLocalConfig.py":
+    ensure => present,
+    content => template("meetbot/meetingLocalConfig.py.erb"),
+    owner => 'meetbot',
+    require => File["/var/lib/meetbot/${name}/ircmeeting"],
+    notify => Service["${name}-meetbot"]
+  }
+
+# we set this file as root ownership because meetbot overwrites it on shutdown
+# this means when puppet changes it and restarts meetbot the file is reset
+
+  file { "/etc/init/${name}-meetbot.conf":
+    ensure => 'present',
+    content => template("meetbot/upstart.erb"),
+    replace => 'true',
+    require => File["/var/lib/meetbot/${name}.conf"],
+    owner => 'root',
+    notify => Service["${name}-meetbot"]
+  }
+
+  service { "${name}-meetbot":
+    provider => upstart,
+    ensure => running,
+    require => File["/etc/init/${name}-meetbot.conf"]
+  }
+}
