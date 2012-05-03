57d5ba1654ac9e1c6339b5f81ae313eb2ebd5ccc
David Shrewsbury <shrewsbury.dave@gmail.com>
2012-05-03 09:27:14 -0400

    Compress database backups.
    Fixes bug 951600.
    
    The update_gerrit_users.py script creates a database backup
    everytime it runs. This change compresses the backups as they
    are being made and adds a cron job that will remove backups that
    are 30 days or older. Current backups will need to be manually
    compressed.
    
    Change-Id: Idb90d4af629892f1bc2e3e98f35f7de2ad58d4be

--- modules/gerrit/manifests/init.pp
+++ modules/gerrit/manifests/init.pp
@@ -140,6 +140,15 @@ class gerrit($virtual_hostname='',
       command => 'find /home/gerrit2/review_site/git/ -type d -name "*.git" -print -exec git --git-dir="{}" repack -afd \;',
       environment => "PATH=/usr/bin:/bin:/usr/sbin:/sbin",
     }
+
+    cron { "removedbdumps":
+      user => gerrit2,
+      hour => 5,
+      minute => 1,
+      command => 'find /home/gerrit2/dbupdates/ -name "*.sql.gz" -mtime +30 -exec rm -f {} \;',
+      environment => "PATH=/usr/bin:/bin:/usr/sbin:/sbin",
+    }
+
   } # testmode==false
 
   file { "/var/log/gerrit":
