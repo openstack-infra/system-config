031cc983dc4957787f6d75de91c850fb28a4473a
Clark Boylan <clark.boylan@gmail.com>
2012-05-07 16:03:24 +0000

    Puppet Module to install etherpad-lite.
    Part of the work necessary for bug #995248
    
    Puppet module to do a basic install of etherpad-lite. Currently
    installs etherpad-lite and node.js. Configures etherpad-lite to use
    mysql for its DB backend and sets up nginx to reverse proxy
    etherpad-lite.
    
    Change-Id: I3ee1ce4594fcf2a27b520a3ad7a024318f69111a

---
+++ modules/etherpad_lite/files/pad.js
@@ -0,0 +1,7 @@
+function customStart()
+{
+  //define your javascript here
+  //jquery is available - except index.js
+  //you can load extra scripts with $.getScript http://api.jquery.com/jQuery.getScript/
+  chat.stickToScreen(true);
+}
---
+++ modules/etherpad_lite/manifests/site.pp
@@ -0,0 +1,41 @@
+class etherpad_lite::site (
+  $dbType = 'mysql'
+) {
+
+  include etherpad_lite
+
+  if $dbType == 'mysql' {
+    service { 'etherpad-lite':
+      enable    => true,
+      ensure    => running,
+      subscribe => File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"],
+      require   => Class['etherpad_lite::mysql'],
+    }
+  }
+  else {
+    service { 'etherpad-lite':
+      enable    => true,
+      ensure    => running,
+      subscribe => File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"],
+    }
+  }
+
+  file { "${etherpad_lite::base_install_dir}/etherpad-lite/settings.json":
+    ensure  => 'present',
+    source  => 'file:///root/secret-files/etherpad-lite_settings.json',
+    replace => true,
+    owner   => $etherpad_lite::ep_user,
+    group   => $etherpad_lite::ep_user,
+    mode    => 0600,
+    require => Class['etherpad_lite']
+  }
+
+  file { "${etherpad_lite::base_install_dir}/etherpad-lite/static/custom/pad.js":
+    ensure  => 'present',
+    source  => 'puppet:///modules/etherpad_lite/pad.js',
+    owner   => $etherpad_lite::ep_user,
+    group   => $etherpad_lite::ep_user,
+    require => Class['etherpad_lite']
+  }
+
+}
