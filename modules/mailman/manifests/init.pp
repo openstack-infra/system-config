d68c6987a345f9de6b383482d28f06c4881cfc2b
James E. Blair <jeblair@hp.com>
2012-06-06 12:08:25 -0700

    Install mailman apache config.
    Also includes default index.html file which needs to be customized.
    
    Change-Id: Idd2d03a95b8bfaba42d94701dc5a5ced204a1c65

--- modules/mailman/manifests/init.pp
+++ modules/mailman/manifests/init.pp
@@ -8,6 +8,16 @@ class mailman($mailman_host='') {
     ensure => installed,
   }
 
+  file { "/var/www/index.html":
+    source => 'puppet:///modules/mailman/index.html',
+    owner => 'root',
+    group => 'root',
+    ensure => 'present',
+    replace => 'true',
+    mode => 444,
+    require => Package["apache2"],
+  }
+
   file { '/etc/mailman/mm_cfg.py':
     owner => 'root',
     group => 'root',
@@ -18,14 +28,34 @@ class mailman($mailman_host='') {
     require => Package["mailman"]
   }
 
-  file { '/etc/mailman/apache.conf':
+  file { "/etc/apache2/sites-available/mailman":
+    content => template('mailman/mailman.vhost.erb'),
     owner => 'root',
     group => 'root',
-    mode => 444,
     ensure => 'present',
-    source => 'puppet:///modules/mailman/apache.conf',
     replace => 'true',
-    require => Package["mailman"]
+    mode => 444,
+    require => Package["apache2"],
+  }
+
+  file { "/etc/apache2/sites-enabled/mailman":
+    ensure => link,
+    target => '/etc/apache2/sites-available/mailman',
+    require => [
+      File['/etc/apache2/sites-available/mailman'],
+    ],
+  }
+
+  file { '/etc/apache2/sites-enabled/000-default':
+    require => File['/etc/apache2/sites-available/mailman'],
+    ensure => absent,
+  }
+
+  exec { "gracefully restart apache":
+    subscribe => [ File["/etc/apache2/sites-available/mailman"]],
+    refreshonly => true,
+    path => "/bin:/usr/bin:/usr/sbin",
+    command => "apache2ctl graceful",
   }
 
   service { 'mailman':
@@ -38,7 +68,7 @@ class mailman($mailman_host='') {
   service { 'apache2':
     ensure          => running,
     hasrestart      => true,
-    subscribe       => File['/etc/mailman/apache.conf'],
+    subscribe       => File["/etc/apache2/sites-available/mailman"],
     require         => Package["apache2"]
   }
 }
