69ad51ef4c0dcf0b16dd6dcba6239bcb3b7b386b
Clark Boylan <clark.boylan@gmail.com>
2012-06-11 12:58:45 -0700

    Update review-dev WAR.
    Update review-dev WAR in order to test two patches. The first patch
    uses the strongest approvals change formatter on the important changes
    lists. This removes redundant bolding in the reviewable list and makes
    the VRA columns useful and consistent. The second patch removes draft
    changes from the "reviewable" list.
    
    Change-Id: I3333b8285a59d91811451c8af553fe85fbcc38ac

--- manifests/site.pp
+++ manifests/site.pp
@@ -225,7 +225,7 @@ node "gerrit-dev.openstack.org", "review-dev.openstack.org" {
                          close_pull => 'true'
                          } ],
     logo => 'openstack.png',
-    war => 'http://ci.openstack.org/tarballs/test/gerrit-2.4-13-g80f364a.war',
+    war => 'http://ci.openstack.org/tarballs/test/gerrit-2.4-14-geb78cd0.war',
     script_user => 'update',
     script_key_file => '/home/gerrit2/.ssh/id_rsa',
     script_site => 'openstack'
