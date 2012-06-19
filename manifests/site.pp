f0662c007d26a211e55402d7639511725374c8e3
David Shrewsbury <shrewsbury.dave@gmail.com>
2012-06-19 11:54:09 -0400

    Update production Gerrit WAR.
    Updates to Gerrit 2.4.1 plus our changes.
    
    Change-Id: I34c8288f4a2aff0ec418ba172de0a811a50f0b1c

--- manifests/site.pp
+++ manifests/site.pp
@@ -200,7 +200,7 @@ node "review.openstack.org" {
                          remote => 'https://gerrit.googlesource.com/gerrit'
                          } ],
     logo => 'openstack.png',
-    war => 'http://ci.openstack.org/tarballs/gerrit-2.4-17-gd201697.war',
+    war => 'http://ci.openstack.org/tarballs/gerrit-2.4.1-8-g5bbcc7f.war',
     script_user => 'launchpadsync',
     script_key_file => '/home/gerrit2/.ssh/launchpadsync_rsa',
     script_site => 'openstack'
