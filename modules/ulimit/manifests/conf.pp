c025f1a050b133039625bf60cc05533640e8d54c
Clark Boylan <clark.boylan@gmail.com>
2012-06-27 10:00:50 -0700

    Add ulimit module.
    A new module that provides a define to add a file to
    /etc/security/limits.d for each limit line defined. This can be used
    to limit the number of processes a user can have.
    
    Change-Id: I7d1074ec85dbf3e212c0eccbe4e6e926b56f561c

---
+++ modules/ulimit/manifests/conf.pp
@@ -0,0 +1,17 @@
+define ulimit::conf (
+  $limit_domain,
+  $limit_item,
+  $limit_value,
+  $limit_type = 'soft',
+) {
+
+  file { "/etc/security/limits.d/99-${limit_domain}-${limit_type}-${limit_item}.conf":
+    ensure  => present,
+    content => template('ulimit/limits.erb'),
+    replace => true,
+    owner   => 'root',
+    mode    => 0644,
+    require => File['/etc/security/limits.d']
+  }
+
+}
