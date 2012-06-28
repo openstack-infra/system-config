609c350454766d185bcb990e3c57f24f7b81e4bd
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-06-28 21:24:50 +0100

    Revert theme patch live
    Javascript bug breaking Chrome again
    
    Change-Id: I4787c360ac58ea0449c828f0698ba9e89cd6bdb9

--- manifests/site.pp
+++ manifests/site.pp
@@ -228,7 +228,7 @@ node "review.openstack.org" {
                          remote => 'https://gerrit.googlesource.com/gerrit'
                          } ],
     logo => 'openstack.png',
-    war => 'http://ci.openstack.org/tarballs/gerrit-2.4.1-9-g8b48c79.war',
+    war => 'http://ci.openstack.org/tarballs/gerrit-2.4.1-8-g5bbcc7f.war',
     script_user => 'launchpadsync',
     script_key_file => '/home/gerrit2/.ssh/launchpadsync_rsa',
     script_site => 'openstack'
