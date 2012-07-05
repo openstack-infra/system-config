bc2448199a8e883725c60d16fa72ccfb92a02543
Clark Boylan <clark.boylan@gmail.com>
2012-07-05 11:54:19 -0700

    Allow GerritBot to talk on multiple channels.
    Fixes bug #1020987
    
    Update GerritBot with the ability to talk on multiple channels. This
    way a single GerritBot instance can operate in multiple channels for
    multiple projects. To make this work this change introduces a new
    channel configuration file (yaml) for GerritBot that specifies each
    channel that GerritBot should join and the changes that channel is
    interested in.
    
    The config should look something like:
    channel-foo:
        events:
          - patchset-created
          - change-merged
        projects:
          - test/bar
          - test/foo
        branches:
          - master
    
    Change-Id: I8e278f9be5182611981a3d912cc323bd3d386fc5

--- modules/gerrit/manifests/init.pp
+++ modules/gerrit/manifests/init.pp
@@ -200,13 +200,24 @@ class gerrit($virtual_hostname='',
       require => File['/usr/local/gerrit/gerritbot'],
     }
 
+    file { "/home/gerrit2/gerritbot_channel_config.yaml":
+      owner   => 'root',
+      group   => 'gerrit2',
+      mode    => 440,
+      ensure  => 'present',
+      source  => 'puppet:///modules/gerrit/gerritbot_channel_config.yaml',
+      replace => true,
+      require => User['gerrit2']
+    }
+
     service { 'gerritbot':
       name       => 'gerritbot',
       ensure     => running,
       enable     => true,
       hasrestart => true,
       require => File['/etc/init.d/gerritbot'],
-      subscribe => [ File["/usr/local/gerrit/gerritbot"] ],
+      subscribe => [File["/usr/local/gerrit/gerritbot"],
+                    File["/home/gerrit2/gerritbot_channel_config.yaml"]],
     }
 
   } # testmode==false
