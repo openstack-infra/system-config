d95024c4977042adaad9f13812af904edfcbbf2a
Monty Taylor <mordred@inaugust.com>
2012-07-19 17:17:29 -0700

    First pass at parameterizing secret infos.
    Change-Id: Iee56a7e65be51ebf19a61eefd60cc93de6a764bf

--- modules/etherpad_lite/manifests/mysql.pp
+++ modules/etherpad_lite/manifests/mysql.pp
@@ -1,4 +1,9 @@
-class etherpad_lite::mysql {
+class etherpad_lite::mysql (
+  $dbType = 'mysql',
+  $database_user = 'eplite',
+  $database_name = 'etherpad-lite',
+  $database_password
+) {
 
   include etherpad_lite
 
@@ -18,20 +23,42 @@ class etherpad_lite::mysql {
                    Package['mysql-client']]
   }
 
+  file { "${etherpad_lite::base_install_dir}/etherpad-lite/create_database.sh":
+    ensure  => 'present',
+    content => template('etherpad_lite/create_database.sh.erb'),
+    replace => true,
+    owner   => $etherpad_lite::ep_user,
+    group   => $etherpad_lite::ep_user,
+    mode    => 0755,
+    require => Class['etherpad_lite']
+  }
+
+  file { "${etherpad_lite::base_install_dir}/etherpad-lite/create_user.sh":
+    ensure  => 'present',
+    content => template('etherpad_lite/create_user.sh.erb'),
+    replace => true,
+    owner   => $etherpad_lite::ep_user,
+    group   => $etherpad_lite::ep_user,
+    mode    => 0755,
+    require => Class['etherpad_lite']
+  }
+
   exec { "create-etherpad-lite-db":
-    unless  => 'mysql --defaults-file=/etc/mysql/debian.cnf etherpad-lite',
+    unless  => "mysql --defaults-file=/etc/mysql/debian.cnf ${database_name}",
     path    => ['/bin', '/usr/bin'],
-    command => "mysql --defaults-file=/etc/mysql/debian.cnf -e \"create database \`etherpad-lite\` CHARACTER SET utf8 COLLATE utf8_bin;\"",
+    command => "${etherpad_lite::base_install_dir}/etherpad-lite/create_database.sh",
     require => [Service['mysql'],
-                File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"]]
+                File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"],
+                File["${etherpad_lite::base_install_dir}/etherpad-lite/create_database.sh"]]
   } ->
 
   exec { "grant-etherpad-lite-db":
-    unless  => "mysql -ueplite -p\"`grep password ${etherpad_lite::base_install_dir}/etherpad-lite/settings.json | cut -d: -f2 | sed -e 's/.*\"\(.*\)\".*/\1/'`\" etherpad-lite",
+    unless  => "mysql -u${database_user} -p${database_password} ${database_name}",
     path    => ['/bin', '/usr/bin'],
-    command => "mysql --defaults-file=/etc/mysql/debian.cnf -e \"grant all on \`etherpad-lite\`.* to 'eplite'@'localhost' identified by '`grep password ${etherpad_lite::base_install_dir}/etherpad-lite/settings.json | cut -d: -f2 | sed -e 's/.*\"\(.*\)\".*/\1/'`';\" mysql",
+    command => "${etherpad_lite::base_install_dir}/etherpad-lite/create_user.sh",
     require => [Service['mysql'],
-                File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"]]
+                File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"],
+                File["${etherpad_lite::base_install_dir}/etherpad-lite/create_user.sh"]]
   }
 
 }
