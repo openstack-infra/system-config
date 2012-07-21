e589481d2711ecda23b79cb860c8f4d78f83e42c
Monty Taylor <mordred@inaugust.com>
2012-07-20 18:56:35 -0700

    Move OpenStack classes to openstack_project module
    Change-Id: Iafcd2e06c5b62e4cde5eccaab3173a20bb08a78d

---
+++ modules/openstack_project/manifests/server.pp
@@ -0,0 +1,9 @@
+# A server that we expect to run for some time
+class openstack_project::server ($iptables_public_tcp_ports = []) {
+  class { 'openstack_project::template':
+    iptables_public_tcp_ports => $iptables_public_tcp_ports
+  }
+  class { 'exim':
+    sysadmin => $openstack_project::sysadmins
+  }
+}
