aafb453cdb4ad59e33143e045e6ea9876f8a4d9a
Monty Taylor <mordred@inaugust.com>
2012-07-20 19:51:44 -0700

    Make an OpenStack Gerrit base class.
    Change-Id: Ib16ff355199d330a74aaef384a11628b649f06ec

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -1,22 +1,12 @@
 class openstack_project::review_dev {
-  class { 'openstack_project::server':
-    iptables_public_tcp_ports => [80, 443, 29418]
-  }
-
-  class { 'gerrit':
-    virtual_hostname => 'review-dev.openstack.org',
-    canonicalweburl => "https://review-dev.openstack.org/",
+  class { 'openstack_project::gerrit':
     ssl_cert_file => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
     ssl_key_file => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_chain_file => '',
     email => "review-dev@openstack.org",
     github_projects => [ 'gtest-org/test' ],
-    logo => 'openstack.png',
     war => 'http://tarballs.openstack.org/ci/gerrit-2.4.2-10-g93ffc27.war',
     script_user => 'update',
     script_key_file => '/home/gerrit2/.ssh/id_rsa',
-    script_site => 'openstack',
-    enable_melody => 'true',
-    melody_session => 'true'
   }
 }
