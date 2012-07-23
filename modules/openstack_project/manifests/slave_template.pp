a841e99c0e7d1e6ac64c0145b20693edca5824fd
Monty Taylor <mordred@inaugust.com>
2012-07-23 18:12:59 -0500

    Removed unnecessary ::
    Change-Id: I1ca84c36e0311850d409a9db4b4a8b40a9126273

--- modules/openstack_project/manifests/slave_template.pp
+++ modules/openstack_project/manifests/slave_template.pp
@@ -2,7 +2,7 @@ class openstack_project::slave_template {
   class { 'openstack_project::template':
     iptables_public_tcp_ports => []
   }
-  class { '::jenkins_slave':
+  class { 'jenkins_slave':
     ssh_key => $openstack_project::jenkins_ssh_key,
     sudo => true,
     bare => true
