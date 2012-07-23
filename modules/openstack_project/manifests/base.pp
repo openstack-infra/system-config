53fa5a8604d3b42f2f1d8c3ccf41c6a866ed2c11
Monty Taylor <mordred@inaugust.com>
2012-07-23 16:56:20 -0500

    Upgrade all of our hosts to latest puppet release.
    Change-Id: I3a226e9d7d0c1e3288590d637532b5e5b897d780

--- modules/openstack_project/manifests/base.pp
+++ modules/openstack_project/manifests/base.pp
@@ -28,4 +28,15 @@ class openstack_project::base {
     User::Virtual::Localuser["devananda"],
     User::Virtual::Localuser["clarkb"],
   )
+
+  # Download and set up puppet apt repo
+  exec { "download:puppetlabs-release-${lsbdistcodename}.deb":
+    command => "/usr/bin/wget http://apt.puppetlabs.com/puppetlabs-release-${lsbdistcodename}.deb -O /root/puppetlabs-release-${lsbdistcodename}.deb",
+    creates => "/root/puppetlabs-release-${lsbdistcodename}.deb",
+  }
+  exec { "dpkg:puppetlabs-release-${lsbdistcodename}.deb":
+    command => "/usr/bin/dpkg -i /root/puppetlabs-release-${lsbdistcodename}.deb",
+    onlyif => "test ! -f /etc/apt/sources.list.d/puppetlabs.list",
+    require => Exec["download:puppetlabs-release-${lsbdistcodename}.deb"],
+  }
 }
