26d074aba3172d7019a47ada003531b7b7f80584
Monty Taylor <mordred@inaugust.com>
2012-07-22 23:27:01 -0500

    Backport the init script from precise for snmpd.
    This can go away after everything is upgraded to precise,
    but it's the cause of the constant puppet attempts to restart snmpd on our
    servers.
    
    Change-Id: I99b1931aefa3444d5ceef8b6dd6014152f3f53a0

--- modules/snmpd/manifests/init.pp
+++ modules/snmpd/manifests/init.pp
@@ -3,7 +3,21 @@ class snmpd {
     service { snmpd: 
       ensure          => running,
       hasrestart      => true,
-      require => File["/etc/snmp/snmpd.conf"],
+      require => [File["/etc/snmp/snmpd.conf"],
+                  File["/etc/init.d/snmpd"]]
+    }
+    # This file is only needed on machines pre-precise. There is a bug in
+    # the previous init script versions which causes them to attempt
+    # snmptrapd even if it's configured not to run, and then to report
+    # failure.
+    file { "/etc/init.d/snmpd":
+      owner => 'root',
+      group => 'root',
+      mode => 755,
+      ensure => 'present',
+      source => 'puppet:///modules/snmpd/snmpd.init',
+      replace => 'true',
+      require => Package[snmpd]
     }
     file { "/etc/snmp/snmpd.conf":
       owner => 'root',
