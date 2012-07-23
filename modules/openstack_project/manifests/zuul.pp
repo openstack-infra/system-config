d0bb131da94d88ed64515566613aff2867189c35
Monty Taylor <mordred@inaugust.com>
2012-07-23 17:31:08 -0500

    Fixed scoping problem for zuul module.
    Change-Id: If414a7355832c2444bed82f228ff373080a1c11c

--- modules/openstack_project/manifests/zuul.pp
+++ modules/openstack_project/manifests/zuul.pp
@@ -1,5 +1,5 @@
 class openstack_project::zuul {
-  class { 'zuul': }
+  class { '::zuul': }
   file { "/etc/zuul/layout.yaml":
     ensure => 'present',
     source => 'puppet:///modules/openstack_ci/zuul/layout.yaml'
