c918fc6c16d3ec62c710173fd9a480c22ded27a1
Monty Taylor <mordred@inaugust.com>
2012-07-24 11:11:31 -0500

    Add root hiera config for the puppetmaster.
    Change-Id: I4e9358f083a9a1938a7080610cc5964834889593

--- modules/openstack_project/manifests/puppetmaster.pp
+++ modules/openstack_project/manifests/puppetmaster.pp
@@ -8,4 +8,14 @@ class openstack_project::puppetmaster {
     command => 'sleep $((RANDOM\%600)) && cd /opt/openstack-ci-puppet/production && /usr/bin/git pull -q && /bin/bash install_modules.sh',
     environment => "PATH=/var/lib/gems/1.8/bin:/usr/bin:/bin:/usr/sbin:/sbin",
   }
+
+  file { '/etc/puppet/hiera.yaml':
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => 'present',
+    source => 'puppet:///modules/openstack_project/puppetmaster/hiera.yaml',
+    replace => 'true',
+    require => Class['openstack_project::server']
+  }
 }
