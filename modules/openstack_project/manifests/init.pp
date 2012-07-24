e88fc588a00d5185024fcd04779d5af59599973b
Monty Taylor <mordred@inaugust.com>
2012-07-23 19:03:09 -0500

    Include openstack_project class to access members.
    Change-Id: I4d04bd6cc248ff6b724a42fe872afadc080bb126

--- modules/openstack_project/manifests/init.pp
+++ modules/openstack_project/manifests/init.pp
@@ -2,7 +2,13 @@ class openstack_project {
 
   $jenkins_ssh_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtioTW2wh3mBRuj+R0Jyb/mLt5sjJ8dEvYyA8zfur1dnqEt5uQNLacW4fHBDFWJoLHfhdfbvray5wWMAcIuGEiAA2WEH23YzgIbyArCSI+z7gB3SET8zgff25ukXlN+1mBSrKWxIza+tB3NU62WbtO6hmelwvSkZ3d7SDfHxrc4zEpmHDuMhxALl8e1idqYzNA+1EhZpbcaf720mX+KD3oszmY2lqD1OkKMquRSD0USXPGlH3HK11MTeCArKRHMgTdIlVeqvYH0v0Wd1w/8mbXgHxfGzMYS1Ej0fzzJ0PC5z5rOqsMqY1X2aC1KlHIFLAeSf4Cx0JNlSpYSrlZ/RoiQ== hudson@hudson'
 
-  $sysadmin = hiera('sysadmins')
+  $sysadmins = [
+    'corvus@inaugust.com',
+    'mordred@inaugust.com',
+    'andrew@linuxjedi.co.uk',
+    'devananda.vdv@gmail.com',
+    'clark.boylan@gmail.com'
+    ]
 
   $project_list = [ {
      name => 'openstack/keystone',
