4e2a59a2d6b19c806180458e95639b96aee9764a
Monty Taylor <mordred@inaugust.com>
2012-07-24 10:03:23 -0500

    Add support for install puppet modules.
    Change-Id: I0fb81d64daea389a564405c76773108eaeec6684

---
+++ install_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MODULES="puppetlabs-mysql"
+MODULE_LIST=`puppet module list`
+
+for MOD in $MODULES ; do
+  if ! echo $MODULE_LIST | grep $MOD >/dev/null 2>&1 ; then
+    # This will get run in cron, so silence non-error output
+    puppet module install $MOD >/dev/null
+  fi
+done
---
+++ run_puppet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASE_DIR=$1
+MODULE_DIR=${BASE_DIR}/modules
+MODULE_PATH=${MODULE_DIR}:/etc/puppet/modules
+MANIFEST_LOG=/var/log/manifest.log
+
+cd $BASE_DIR
+/usr/bin/git pull -q && \
+  /bin/bash install_modules.sh && \
+  /usr/bin/puppet apply -l $MANIFEST_LOG --modulepath=$MODULE_PATH manifests/site.pp
