5fdfa267042b8ed986b06308b6a436cf3c99146e
Monty Taylor <mordred@inaugust.com>
2012-07-28 10:51:10 -0500

    Add zuul.conf.
    Add a templated zuul.conf with apikey filled in by hiera.
    Made an openstack_project::zuul class to encapsulate installation
    and config of zuul with our config files.
    
    Change-Id: Idc09819b27672879b74b90bdbf699b07aa416a2a
    Reviewed-on: https://review.openstack.org/10460
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Approved: Monty Taylor <mordred@inaugust.com>
    Tested-by: Jenkins

--- modules/openstack_project/manifests/zuul.pp
+++ modules/openstack_project/manifests/zuul.pp
@@ -1,5 +1,18 @@
-class openstack_project::zuul_config {
-  include ::zuul
+class openstack_project::zuul(
+  $jenkins_server,
+  $jenkins_user,
+  $jenkins_apikey,
+  $gerrit_server,
+  $gerrit_user
+  ) {
+
+  class { "::zuul":
+    jenkins_server => $jenkins_server,
+    jenkins_user => $jenkins_user,
+    jenkins_apikey => $jenkins_apikey,
+    gerrit_server => $gerrit_server,
+    gerrit_user => $gerrit_user,
+  }
 
   file { "/etc/zuul/layout.yaml":
     ensure => 'present',
