b73a60801500dd94be807828ad4fd3a79c943fbc
Clark Boylan <clark.boylan@gmail.com>
2012-08-01 11:33:14 -0700

    Use upstream gerritbot.
    Stop using the gerritbot script within the puppet tree. Use the
    upstream gerritbot implementation instead.
    
    Remove old gerritbot script, use vcsrepo to pull upstream gerritbot,
    install gerritbot with pip, and update init script to find new location
    of the gerritbot script.
    
    Change-Id: I62afe404b1c4d130901e54ca4e7857bb71b4cdd8

--- modules/gerritbot/manifests/init.pp
+++ modules/gerritbot/manifests/init.pp
@@ -1,57 +1,68 @@
 class gerritbot(
-      $nick,
-      $password,
-      $server,
-      $user,
-      $virtual_hostname
-      ) {
+  $nick,
+  $password,
+  $server,
+  $user,
+  $virtual_hostname,
+  $repo_dir
+) {
 
-    file { "/usr/local/gerrit/gerritbot":
-      owner => 'root',
-      group => 'root',
-      mode => 555,
-      ensure => 'present',
-      source => 'puppet:///modules/gerritbot/gerritbot',
-      require => File['/usr/local/gerrit'],
-    }
+  file { "${repo_dir}":
+    ensure => directory
+  }
 
-    file { "/etc/init.d/gerritbot":
-      owner => 'root',
-      group => 'root',
-      mode => 555,
-      ensure => 'present',
-      source => 'puppet:///modules/gerritbot/gerritbot.init',
-      require => File['/usr/local/gerrit/gerritbot'],
-    }
+  vcsrepo { "${repo_dir}/gerritbot":
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/openstack-ci/gerritbot.git',
+    require  => File["${repo_dir}"]
+  }
 
-    file { "/home/gerrit2/gerritbot_channel_config.yaml":
-      owner   => 'root',
-      group   => 'gerrit2',
-      mode    => 440,
-      ensure  => 'present',
-      source  => 'puppet:///modules/gerritbot/gerritbot_channel_config.yaml',
-      replace => true,
-      require => User['gerrit2'],
-    }
+  exec { 'install_gerritbot':
+    command     => 'pip install --upgrade .'
+    cwd         => "${repo_dir}/gerritbot",
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
+    subscribe   => Vcsrepo["${repo_dir}/gerritbot"]
+  }
 
-    service { 'gerritbot':
-      name       => 'gerritbot',
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      require => File['/etc/init.d/gerritbot'],
-      subscribe => [File["/usr/local/gerrit/gerritbot"],
-                    File["/home/gerrit2/gerritbot_channel_config.yaml"]],
-    }
+  file { '/etc/init.d/gerritbot':
+    owner   => 'root',
+    group   => 'root',
+    mode    => 555,
+    ensure  => 'present',
+    source  => 'puppet:///modules/gerritbot/gerritbot.init',
+    require => Exec['install_gerritbot'],
+  }
 
-    file { '/home/gerrit2/gerritbot.config':
-      owner => 'root',
-      group => 'gerrit2',
-      mode => 440,
-      ensure => 'present',
-      content => template('gerritbot/gerritbot.config.erb'),
-      replace => 'true',
-      require => User['gerrit2']
-    }
+  service { 'gerritbot':
+    name       => 'gerritbot',
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    require    => File['/etc/init.d/gerritbot'],
+    subscribe  => [Exec['install_gerritbot'],
+                   File['/home/gerrit2/gerritbot_channel_config.yaml']],
+  }
+
+  file { '/home/gerrit2/gerritbot_channel_config.yaml':
+    owner   => 'root',
+    group   => 'gerrit2',
+    mode    => 440,
+    ensure  => 'present',
+    source  => 'puppet:///modules/gerritbot/gerritbot_channel_config.yaml',
+    replace => true,
+    require => User['gerrit2'],
+  }
+
+  file { '/home/gerrit2/gerritbot.config':
+    owner   => 'root',
+    group   => 'gerrit2',
+    mode    => 440,
+    ensure  => 'present',
+    content => template('gerritbot/gerritbot.config.erb'),
+    replace => 'true',
+    require => User['gerrit2']
+  }
 
 }
