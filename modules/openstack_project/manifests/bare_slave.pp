ed87d6d0025bdad9071c7aa47c89df677531228a
Monty Taylor <mordred@inaugust.com>
2012-08-01 13:45:10 -0500

    Add script for jenkins slave bootstrap.
    Change-Id: I7e94193bc81c3caabd77ca112952a98f15f3c434

--- modules/openstack_project/manifests/bare_slave.pp
+++ modules/openstack_project/manifests/bare_slave.pp
@@ -1,6 +1,6 @@
 # bare-bones slaves spun up by jclouds. Specifically need to not set ssh
 # login limits, because it screws up jclouds provisioning
-class openstack_project::jclouds_slave {
+class openstack_project::bare_slave {
   include openstack_project::base
 
   class { 'jenkins_slave':
