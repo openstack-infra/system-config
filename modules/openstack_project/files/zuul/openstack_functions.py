856ca2b2e8d6dd129a242a0baaab06119246b313
James E. Blair <jeblair@hp.com>
2012-08-02 13:04:00 -0700

    Use oneiric for stable/diablo.
    For all python27 jobs, use oneiric slaves if the branch is
    stable/diablo.  Python26 jobs are already on oneiric due to
    lack of python 2.6 on anything later.
    
    Change-Id: Id440e14721ccdd4225c5e5b170510c3c000a36b3

--- modules/openstack_project/files/zuul/openstack_functions.py
+++ modules/openstack_project/files/zuul/openstack_functions.py
@@ -1,3 +1,7 @@
 def devstack_params(change, params):
     if change.branch == 'stable/diablo':
         params['NODE_LABEL'] = 'devstack-oneiric'
+
+def python27_params(change, params):
+    if change.branch == 'stable/diablo':
+        params['NODE_LABEL'] = 'oneiric'
