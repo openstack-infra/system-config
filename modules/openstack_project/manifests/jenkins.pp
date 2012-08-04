5e02a969304305b3dd8bad18c5858e6596c9a48b
Monty Taylor <mordred@inaugust.com>
2012-08-04 14:30:15 -0500

    Aggregate jenkins modules into one.
    Put jenkins_master into jenkins::master and jenkins_job_filler into
    jenkins::job_builder. Additionally, put the actual openstack jenkins
    jobs into the openstack_project.
    
    Change-Id: I2a1d242ecab0e765ab1b1556bbebd2db8f632888

--- modules/openstack_project/manifests/jenkins.pp
+++ modules/openstack_project/manifests/jenkins.pp
@@ -3,7 +3,7 @@ class openstack_project::jenkins($jenkins_jobs_password) {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80, 443, 4155]
   }
-  class { 'jenkins_master':
+  class { '::jenkins::master':
     site => 'jenkins.openstack.org',
     serveradmin => 'webmaster@openstack.org',
     logo => 'openstack.png',
@@ -11,7 +11,7 @@ class openstack_project::jenkins($jenkins_jobs_password) {
     ssl_key_file => '/etc/ssl/private/jenkins.openstack.org.key',
     ssl_chain_file => '/etc/ssl/certs/intermediate.pem',
   }
-  class { "jenkins_jobs":
+  class { "::jenkins::job_builder":
     url => "https://jenkins.openstack.org/",
     username => "gerrig",
     password => $jenkins_jobs_password,
