f36e38f697cc747f3eedfd1f10d3acc666729d91
James E. Blair <jeblair@hp.com>
2012-08-10 08:22:11 -0700

    Remove jenkins_jobs, add jenkins_job_builder.
    Change-Id: I774b08719297979c0079c37679c3face3788e111
    Reviewed-on: https://review.openstack.org/11151
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Approved: James E. Blair <corvus@inaugust.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

---
+++ modules/jenkins_job_builder/manifests/init.pp
@@ -0,0 +1,57 @@
+class jenkins_job_builder (
+    $url,
+    $username,
+    $password,
+) {
+
+  package { 'python-yaml':
+    ensure => "present",
+  }
+
+  package { "python-jenkins":
+    ensure => latest,  # okay to use latest for pip
+    provider => pip,
+    require => Class[pip]
+  }
+
+  vcsrepo { "/opt/jenkins_job_builder":
+    ensure => latest,
+    provider => git,
+    revision => "master",
+    source => "https://github.com/openstack-ci/jenkins_job_builder.git",
+  }
+
+  exec { "install_jenkins_job_builder":
+    command => "python setup.py install",
+    cwd => "/opt/jenkins_job_builder",
+    path => "/bin:/usr/bin",
+    refreshonly => true,
+    subscribe => Vcsrepo["/opt/jenkins_job_builder"],
+  }
+
+  file { "/etc/jenkins_jobs":
+    ensure => "directory",
+  }
+
+  exec { "jenkins_jobs_update":
+    command => "jenkins-jobs update /etc/jenkins_jobs/config",
+    path => '/bin:/usr/bin:/usr/local/bin',
+    refreshonly => true,
+    require => [
+      File['/etc/jenkins_jobs/jenkins_jobs.ini'],
+      Package['python-jenkins'],
+      Package['python-yaml']
+      ]
+  }
+
+# TODO: We should put in  notify Exec['jenkins_jobs_update']
+#       at some point, but that still has some problems.
+  file { "/etc/jenkins_jobs/jenkins_jobs.ini":
+    owner => 'jenkins',
+    mode => 400,
+    ensure => 'present',
+    content => template('jenkins_jobs/jenkins_jobs.ini.erb'),
+    require => File["/etc/jenkins_jobs"],
+  }
+
+}
