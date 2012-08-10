571b5980c20998f3c335ee2a345a50df978f0f77
James E. Blair <jeblair@hp.com>
2012-08-10 08:22:11 -0700

    Remove jenkins_jobs, add jenkins_job_builder.
    Change-Id: I774b08719297979c0079c37679c3face3788e111

--- modules/openstack_project/manifests/jenkins.pp
+++ modules/openstack_project/manifests/jenkins.pp
@@ -3,6 +3,7 @@ class openstack_project::jenkins($jenkins_jobs_password) {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80, 443, 4155]
   }
+
   class { 'jenkins_master':
     site => 'jenkins.openstack.org',
     serveradmin => 'webmaster@openstack.org',
@@ -11,12 +12,23 @@ class openstack_project::jenkins($jenkins_jobs_password) {
     ssl_key_file => '/etc/ssl/private/jenkins.openstack.org.key',
     ssl_chain_file => '/etc/ssl/certs/intermediate.pem',
   }
-  class { "jenkins_jobs":
+
+  class { "jenkins_job_builder":
     url => "https://jenkins.openstack.org/",
     username => "gerrig",
     password => $jenkins_jobs_password,
-    site => "openstack",
   }
+
+  file { '/etc/jenkins_jobs/config':
+    owner => 'root',
+    group => 'root',
+    mode => 755,
+    ensure => 'directory',
+    recurse => true,
+    source => ['puppet:///modules/openstack_project/jenkins_job_builder/config'],
+    notify => Exec["jenkins_jobs_update"]
+  }
+
   file { "/etc/default/jenkins":
     ensure => 'present',
     source => 'puppet:///modules/openstack_project/jenkins/jenkins.default'
