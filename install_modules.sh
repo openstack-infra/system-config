a9f62a2c1728cb116ef57506d292bcbed0fd4e9e
Monty Taylor <mordred@inaugust.com>
2012-08-12 08:13:08 -0700

    Start working with module repos and releases.
    Change-Id: I4a596f8744a16e7d5495da1a8824a15c1ef64a7c
    Reviewed-on: https://review.openstack.org/11242
    Approved: Monty Taylor <mordred@inaugust.com>
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Tested-by: Jenkins

--- install_modules.sh
+++ install_modules.sh
@@ -26,36 +26,17 @@ if ! puppet help module >/dev/null 2>&1 ; then
     apt-get install -y -o Dpkg::Options::="--force-confold" puppet facter
 fi
 
-MODULES="puppetlabs-apt puppetlabs-mysql puppetlabs-dashboard"
+MODULES="puppetlabs-apt puppetlabs-mysql openstackci-dashboard openstackci-vcsrepo"
 MODULE_LIST=`puppet module list`
+
+# Transition away from old things
+if [ -d /etc/puppet/modules/vcsrepo/.git ] ; then
+    rm -rf /etc/puppet/modules/vcsrepo
+fi
 
 for MOD in $MODULES ; do
   if ! echo $MODULE_LIST | grep $MOD >/dev/null 2>&1 ; then
     # This will get run in cron, so silence non-error output
-    puppet module install $MOD >/dev/null
+    puppet module install --force $MOD >/dev/null
   fi
 done
-
-# Install vcsrepo from git
-clone_git git://github.com/openstack-ci/puppet-vcsrepo.git vcsrepo 44eb9ad8
-
-# Fix a problem with the released verison of the dashboard module
-if grep scope.lookupvar ${MODULE_PATH}/dashboard/templates/passenger-vhost.erb | grep dashboard_port >/dev/null 2>&1 ; then
-
-  cd ${MODULE_PATH}/dashboard
-  echo | patch -p1 <<'EOD'
-diff --git a/templates/passenger-vhost.erb b/templates/passenger-vhost.erb
-index a2f6d16..de7dd0a 100644
---- a/templates/passenger-vhost.erb
-+++ b/templates/passenger-vhost.erb
-@@ -1,6 +1,6 @@
--Listen <%= scope.lookupvar("dashboard::params::dashboard_port") %>
-+Listen <%= dashboard_port %>
- 
--<VirtualHost *:<%= scope.lookupvar("dashboard::params::dashboard_port") %>>
-+<VirtualHost *:<%= dashboard_port %>>
-   ServerName <%= name %>
-   DocumentRoot <%= docroot %>
-   RailsBaseURI <%= rails_base_uri %>
-EOD
-fi
