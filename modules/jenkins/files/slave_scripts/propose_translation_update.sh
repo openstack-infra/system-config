81dd0ba3d4ddbd641fe2cc4fd9344ced77af45dd
Clark Boylan <clark.boylan@gmail.com>
2012-08-14 12:47:01 -0700

    New job template for translation management.
    Add a new job template for translation management. Add two jobs for
    translation management. The first will will update transifex with the
    new .pot and .po files after every merge to a project. The second will
    commit the new .pot and .po files and submit for review once a day.
    
    Add puppet manifest for the slave that these jobs will be running on.
    
    Apply the template to nova.
    
    Change-Id: I5242f81bd6ff13d1ed8610ef9e3b39fd3dbd7301

---
+++ modules/jenkins/files/slave_scripts/propose_translation_update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -xe
+
+PROJECT=$1
+
+git config user.name "OpenStack Jenkins"
+git config user.email "jenkins@openstack.org"
+
+# See if there is an open change in the transifex/translations topic
+# If so, amend the commit with new changes since then
+previous=`ssh -p 29418 review.openstack.org gerrit query --current-patch-set status:open project:openstack/$PROJECT topic:transifex/translations | grep "^  number:" | awk '{print $2}'`
+if [ "x${previous}" != "x" ] ; then
+    git review -d ${previous}
+    amend="--amend"
+fi
+
+# initialize transifex client
+tx init
+tx set --auto-local -r ${PROJECT}.${PROJECT}-translations "${PROJECT}/locale/<lang>/LC_MESSAGES/${PROJECT}.po" --source-lang en --source-file ${PROJECT}/locale/${PROJECT}.pot --execute
+
+# Pull all upstream translations
+tx pull -a
+# Update the .pot file
+python setup.py extract_messages
+# Use updated .pot file to update translations
+python setup.py update_catalog
+# Add all changed files to git
+git add $PROJECT/locale/*
+
+if [ ! `git diff-index --quiet HEAD --` ]
+then
+    # Commit and review
+    git commit ${amend} -m "Imported Translations from Transifex"
+    git review -t transifex/translations
+
+    # Push changes to transifex
+    tx push -st
+fi
