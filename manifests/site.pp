72385e5f2fbd34243a1959f5bc7aae3acb8dbb05
Monty Taylor <mordred@inaugust.com>
2012-08-19 19:39:49 -0400

    Fix another typo.
    Change-Id: Ie0cad848d32b2ed104305283e6d12d1935f5c209
    Reviewed-on: https://review.openstack.org/11633
    Approved: Monty Taylor <mordred@inaugust.com>
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Tested-by: Jenkins

--- manifests/site.pp
+++ manifests/site.pp
@@ -126,7 +126,7 @@ node /^precise.*\.slave\.openstack\.org$/ {
   class { 'openstack_project::glancetest':
     s3_store_access_key => hiera('s3_store_access_key'),
     s3_store_secret_key => hiera('s3_store_secret_key'),
-    s3_store_secret_key => hiera('s3_store_bucket'),
+    s3_store_bucket => hiera('s3_store_bucket'),
     swift_store_user => hiera('swift_store_user'),
     swift_store_key => hiera('swift_store_key'),
     swift_store_container => hiera('swift_store_container'),
