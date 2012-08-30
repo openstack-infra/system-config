046c49f915d53399f23fbe4d0642850c6dc40d0a
annegentle <anne@openstack.org>
2012-08-30 14:35:23 -0500

    Adds a markdown to docbook script.
    Patch set 3 makes the jobname the filename and adds a second job for
    building the programming api mdown book.
    
    Patch set 4 corrects the job name to match the file name.
    
    Patch set 5 renames the .sh.save file and adds it to the job builders.
    
    Patch set 6 addresses final comments - appending to the gerrit-doc.properties
    file and removes tabs.
    
    Patch set 7 addresses comments. Would like to continue work on markdown to
    docbook and delay work on the properties script if possible.
    
    Patch set 8 removes docbook-properties as a prebuilder and removes DOC_OPERATING_SYSTEM
    as a variable. Rebase against master also.
    
    Updates macros.yaml and manuals.yaml to pass in filename for markdown files.
    
    Adds properties injection to the markdown to docbook jobs.
    
    Removes extraenous tab from modules/openstack_project/files/zuul/layout.yaml
    
    Additional fixes to pass in filename correctly and remove DOC_OPERATING_SYSTEM, fix
    filname typo.
    
    Removes file extension from markdown files, removes echo to gerrit-doc-properties
    from markdown-docbook script.
    
    Change-Id: I46c07cd11e96e8fc463a377519926a43981e4b3d

---
+++ modules/jenkins/files/slave_scripts/markdown-docbook.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# Documentation can be submitted in markdown and then converted to docbook
+# so it can be built with the maven plugin. This is used by Jenkins when
+# invoking certain docs jobs and the resulting output is made available to maven.
+
+# In case we start doing something more sophisticated with other refs
+# later (such as tags).
+BRANCH=$ZUUL_REFNAME
+shopt -s extglob
+
+# Need to get the file name to insert here so it can be reused for multiple projects
+# Filenames for the known repos that could do this are openstackapi-programming.mdown
+# and images-api-v2.0.md and openstackapi-programming and images-api-v2.0 are the names
+# for the ID and xml filename.
+FILENAME=$1
+pandoc -f markdown -t docbook -s ${FILENAME}.@(md|mdown) |  xsltproc -o - /usr/share/xml/docbook/stylesheet/docbook5/db4-upgrade.xsl - |  xmllint  --format - | sed -e 's,<article,<book xml:id="$FILENAME",' | sed -e 's,</article>,</book> > $FILENAME.xml'
+
+pwd
