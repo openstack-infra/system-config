6a809e85095b0eb9a49f51e47e6784c7da0588a6
Clark Boylan <clark.boylan@gmail.com>
2012-09-04 11:17:23 -0700

    Run tmpreaper every six hours.
    Run tmpreaper every six hours and remove files/directories more than 12
    hours old.
    
    Change-Id: Iba105c7defd950590c6ff437035830e741b9e16a

--- modules/tmpreaper/manifests/init.pp
+++ modules/tmpreaper/manifests/init.pp
@@ -1,23 +1,39 @@
 class tmpreaper() {
-   package { 'tmpreaper':
-      ensure => present,
-   }
+  package { 'tmpreaper':
+    ensure => present,
+  }
 
-   file { 'tmpreaper-cron.daily':
-      name => '/etc/cron.daily/tmpreaper',
-      ensure => 'present',
-      owner => 'root',
-      group => 'root',
-      mode => 755,
-      source => 'puppet:///modules/tmpreaper/tmpreaper-cron.daily',
-   }
+  file { '/etc/tmpreaper':
+    name   => '/etc/tmpreaper',
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => 755
+  }
 
-   file { 'tmpreaper.conf':
-      name => '/etc/tmpreaper.conf',
-      ensure => 'present',
-      owner => 'root',
-      group => 'root',
-      mode => 644,
-      source => 'puppet:///modules/tmpreaper/tmpreaper.conf',
-   }
+  file { 'tmpreaper.sh':
+    name   => '/etc/tmpreaper/tmpreaper.sh',
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => 755,
+    source => 'puppet:///modules/tmpreaper/tmpreaper.sh',
+  }
+
+  file { 'tmpreaper.conf':
+    name   => '/etc/tmpreaper.conf',
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => 644,
+    source => 'puppet:///modules/tmpreaper/tmpreaper.conf',
+  }
+
+  cron { "sync_launchpad_users":
+    user    => root,
+    minute  => '42',
+    hour    => '*/6',
+    command => 'sleep $((RANDOM\\%60+60)) && /etc/tmpreaper/tmpreaper.sh',
+    require => File['/etc/tmpreaper/tmpreaper.sh'],
+  }
 }
