a6799eadf58fc33a9112c8018ed7b581c043852a
Jeremy Stanley <fungi@yuggoth.org>
2012-09-04 18:32:54 +0000

    Test Trivial Rebase Detection
    Trying trivial_rebase.py in patchset-created hook.
    
    * modules/gerrit/files/scripts/trivial_rebase.py: A modified version of
    the contrib/trivial_rebase.py included in upstream Gerrit's git tree
    (we'll try to get individual patches submitted upstream soon):
    
        git clone https://gerrit.googlesource.com/gerrit
    
    * modules/gerrit/templates/patchset-created.erb: Slated to eventually
    replace modules/openstack_project/files/gerrit/patchset-created, adding
    an entry calling trivial_rebase.py with variables for its command-line
    options.
    
    * modules/openstack_project/manifests/review_dev.pp: Overrides the file
    definition for /home/gerrit2/review_site/hooks/patchset-created to use a
    template, adding the ssh_host_key and trivial_rebase_role_id variables
    it requires. These changes will eventually be moved into
    modules/openstack_project/manifests/gerrit.pp for production in a
    separate commit, once everthing checks out on review-dev.openstack.org.
    
    Change-Id: I941f15525c72b84708ae1de6832834eb53ab6863

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -1,11 +1,14 @@
 class openstack_project::review_dev(
+  $ssh_host_key,
   $github_oauth_token,
   $mysql_password,
   $mysql_root_password,
+  $trivial_rebase_role_id,
   $email_private_key) {
   class { 'openstack_project::gerrit':
     vhost_name => 'review-dev.openstack.org',
     canonicalweburl => "https://review-dev.openstack.org/",
+    ssh_host_key => '/home/gerrit2/review_site/etc/ssh_host_rsa_key',
     ssl_cert_file => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
     ssl_key_file => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_chain_file => '',
@@ -16,6 +19,16 @@ class openstack_project::review_dev(
     github_oauth_token => $github_oauth_token,
     mysql_password => $mysql_password,
     mysql_root_password => $mysql_root_password,
-    email_private_key => $email_private_key,
+    trivial_rebase_role_id => 'trivial-rebase@review-dev.openstack.org',
+    email_private_key => $email_private_key
+  }
+  file { '/home/gerrit2/review_site/hooks/patchset-created':
+    owner => 'root',
+    group => 'root',
+    mode => 555,
+    ensure => 'present',
+    content => template('gerrit/patchset-created.erb'),
+    replace => 'true',
+    require => Class['::review_dev']
   }
 }
