49a0c949159dfd6f465d056a4cfdc09433db8a64
Clark Boylan <clark.boylan@gmail.com>
2012-09-05 15:06:33 -0700

    Fix launchapd user syncing on review-dev.
    Launchpad user syncing to review-dev's gerrit instance was not working
    as it was using the wrong ssh key to ssh into gerrit's command line
    interface. Correct the ssh key and add proper logging to the sync script
    on this host.
    
    Change-Id: I1cbaafd736dafba6afee35ec04ed0024b0f2565a

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -11,11 +11,30 @@ class openstack_project::review_dev(
     ssl_chain_file => '',
     email => "review-dev@openstack.org",
     war => 'http://tarballs.openstack.org/ci/gerrit-2.4.2-11-gb5a28fb.war',
+    script_user => 'launchpadsync',
+    script_key_file => '/home/gerrit2/.ssh/launchpadsync_rsa',
+    script_logging_conf => '/home/gerrit2/.sync_logging.conf',
     projects_file => 'puppet:///openstack_project/review-dev.projects.yaml',
     github_username => 'openstack-gerrit-dev',
     github_oauth_token => $github_oauth_token,
     mysql_password => $mysql_password,
     mysql_root_password => $mysql_root_password,
     email_private_key => $email_private_key,
+  }
+
+  file { '/var/log/gerrit_user_sync':
+    ensure => directory,
+    owner => root,
+    group => gerrit2,
+    mode => 0775,
+    require => User['gerrit2']
+  }
+  file { '/home/gerrit2/.sync_logging.conf':
+    ensure => present,
+    owner => root,
+    group => gerrit2,
+    mode => 0644,
+    source => 'puppet:///modules/openstack_project/gerrit/launchpad_sync_logging.conf',
+    require => User['gerrit2']
   }
 }
