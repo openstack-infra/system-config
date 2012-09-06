c4c338d9ce5c21e1ed4e62e6025354edea7d984d
Clark Boylan <clark.boylan@gmail.com>
2012-09-06 10:32:48 -0700

    Pass sysadmins list into node defs.
    Pass the sysadmins list into each node definition. This allows us to
    retrieve the data from hiera rather than hard coding it in the puppet
    manifests.
    
    Change-Id: Ide3560f16bce4d66fb95cc5021fc879476e6a712

--- modules/openstack_project/manifests/jenkins_dev.pp
+++ modules/openstack_project/manifests/jenkins_dev.pp
@@ -1,7 +1,10 @@
-class openstack_project::jenkins_dev {
+class openstack_project::jenkins_dev (
+  $sysadmins = []
+) {
   class { 'openstack_project::server':
-    iptables_public_tcp_ports => [80, 443, 4155]
-  } 
+    iptables_public_tcp_ports => [80, 443, 4155],
+    sysadmins => $sysadmins
+  }
   include bup
   bup::site { 'rs-ord':
     backup_user => 'bup-jenkins-dev',
--- modules/openstack_project/manifests/slave.pp
+++ modules/openstack_project/manifests/slave.pp
@@ -1,16 +1,16 @@
-class openstack_project::slave(
-  $certname=$fqdn
-  ) {
+class openstack_project::slave (
+  $certname=$fqdn,
+  $sysadmins=[]
+) {
   include openstack_project
   include tmpreaper
   include unattended_upgrades
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [],
     certname => $certname,
+    sysadmins => $sysadmins
   }
   class { 'jenkins::slave':
     ssh_key => $openstack_project::jenkins_ssh_key
   }
 }
-
-
