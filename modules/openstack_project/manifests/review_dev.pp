3b1b6f2fd6b101fa8e8adeeea1a76966680b258d
Jeremy Stanley <fungi@yuggoth.org>
2012-09-20 16:44:11 +0000

    Fix Gerrit CLA and Contact Store on review-dev.
    * modules/openstack_project/manifests/gerrit.pp
    (openstack_project::gerrit): Set defaults for contactstore,
    contactstore_appsec, contactstore_pubkey and contactstore_url since
    openstack_project::review isn't setting any yet.
    
    * modules/openstack_project/manifests/review_dev.pp
    (openstack_project::review_dev): Retrieve values for cla_description,
    cla_file, cla_id and cla_name from openstack::gerrit where they're
    defined.
    
    Change-Id: I010a8cffd2f5c2bd1c04f7f8d5b9a4089551e69f
    Reviewed-on: https://review.openstack.org/13378
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Approved: Monty Taylor <mordred@inaugust.com>
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Tested-by: Jenkins

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -5,6 +5,10 @@ class openstack_project::review_dev (
   $email_private_key,
   $contactstore_appsec,
   $contactstore_pubkey,
+  $cla_description=$openstack_project::gerrit::cla_description,
+  $cla_file=$openstack_project::gerrit::cla_file,
+  $cla_id=$openstack_project::gerrit::cla_id,
+  $cla_name=$openstack_project::gerrit::cla_name,
   $sysadmins = []
 ) {
   class { 'openstack_project::gerrit':
