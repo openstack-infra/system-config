e1ee247dd3baefb2497a1f71a5354fa779c374b8
Antoine Musso <hashar@free.fr>
2012-09-25 10:31:06 +0200

    fix file paths in openstack_project::zuul
    Apparently the zuul file assets under the openstack_project module have
    been moved from 'zuul' to 'files/zuul'. This patch update the file
    sources. That might trigger unwanted reload/change in OpenStack zuul
    installation.
    
    Change-Id: I60ae0ed6397e16737f60214d3f8b1720e0f06956

--- modules/openstack_project/manifests/zuul.pp
+++ modules/openstack_project/manifests/zuul.pp
@@ -18,17 +18,17 @@ class openstack_project::zuul(
 
   file { "/etc/zuul/layout.yaml":
     ensure => 'present',
-    source => 'puppet:///modules/openstack_project/zuul/layout.yaml',
+    source => 'puppet:///modules/openstack_project/files/zuul/layout.yaml',
     notify => Exec['zuul-reload'],
   }
   file { "/etc/zuul/openstack_functions.py":
     ensure => 'present',
-    source => 'puppet:///modules/openstack_project/zuul/openstack_functions.py',
+    source => 'puppet:///modules/openstack_project/files/zuul/openstack_functions.py',
     notify => Exec['zuul-reload'],
   }
   file { "/etc/zuul/logging.conf":
     ensure => 'present',
-    source => 'puppet:///modules/openstack_project/zuul/logging.conf',
+    source => 'puppet:///modules/openstack_project/files/zuul/logging.conf',
     notify => Exec['zuul-reload'],
   }
 }
