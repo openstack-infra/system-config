5aebb3d48e4629aac011cbd86b30d94160acb097
Paul Belanger <paul.belanger@polybeacon.com>
2012-09-26 13:40:42 -0400

    Style Guide updates for puppet-lint
    Once our gate-ci-puppet-lint job goes live, this will help to get it to
    pass.
    
    Change-Id: I2eb363038b8e63e4b17a3f80cc40dc6c6bf90bee
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -4,16 +4,18 @@ class openstack_project::eavesdrop (
 ) {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80],
-    sysadmins => $sysadmins
+    sysadmins                 => $sysadmins
   }
   include meetbot
 
-  meetbot::site { "openstack":
-    nick => "openstack",
-    nickpass => $nickpass,
-    network => "FreeNode",
-    server => "chat.us.freenode.net:7000",
-    channels => "#openstack #openstack-dev #openstack-meeting",
-    use_ssl => "True"
+  meetbot::site { 'openstack':
+    nick      => 'openstack',
+    nickpass  => $nickpass,
+    network   => 'FreeNode',
+    server    => 'chat.us.freenode.net:7000',
+    channels  => '#openstack #openstack-dev #openstack-meeting',
+    use_ssl   => 'True'
   }
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
