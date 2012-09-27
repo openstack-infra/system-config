af75c22928a3ed977c36d9aecb8803be43192e70
Paul Belanger <paul.belanger@polybeacon.com>
2012-09-27 11:19:13 -0400

    Style Guide updates for puppet-lint
    More fixes in preparation for our gate-ci-puppet-lint job.
    
    Change-Id: I1cd3499e6cd958fbbacd01f3daef581a7b4d3217
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/exim/manifests/init.pp
+++ modules/exim/manifests/init.pp
@@ -1,36 +1,46 @@
-class exim($sysadmin=[], $mailman_domains=[]) {
+class exim(
+  $mailman_domains = [],
+  $sysadmin = []
+) {
   package { 'exim4-base':
-    ensure => present;
-  }
-  package { 'exim4-config':
-    ensure => present;
-  }
-  package { 'exim4-daemon-light':
     ensure => present,
-    require => [Package[exim4-base], Package[exim4-config]],
+  }
+
+  package { 'exim4-config':
+    ensure => present,
+  }
+
+  package { 'exim4-daemon-light':
+    ensure  => present,
+    require => [
+      Package[exim4-base],
+      Package[exim4-config]
+    ],
   }
 
   service { 'exim4':
-    ensure          => running,
-    hasrestart      => true,
-    subscribe       => File['/etc/exim4/exim4.conf'],
+    ensure      => running,
+    hasrestart  => true,
+    subscribe   => File['/etc/exim4/exim4.conf'],
   }
 
   file { '/etc/exim4/exim4.conf':
-    owner => 'root',
-    group => 'root',
-    mode => 444,
-    ensure => 'present',
-    content => template("exim/exim4.conf.erb"),
-    replace => 'true',
+    ensure  => present,
+    content => template('exim/exim4.conf.erb'),
+    group   => 'root',
+    mode    => '0444',
+    owner   => 'root',
+    replace => true,
   }
 
   file { '/etc/aliases':
-    owner => 'root',
-    group => 'root',
-    mode => 444,
-    ensure => 'present',
-    content => template("exim/aliases.erb"),
-    replace => 'true',
+    ensure  => present,
+    content => template('exim/aliases.erb'),
+    group   => 'root',
+    mode    => '0444',
+    owner   => 'root',
+    replace => true,
   }
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
