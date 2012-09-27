bd74f69d00cc033178ba8ad0f8e37d7787598ac2
Paul Belanger <paul.belanger@polybeacon.com>
2012-09-27 11:19:13 -0400

    Style Guide updates for puppet-lint
    More fixes in preparation for our gate-ci-puppet-lint job.
    
    Change-Id: I1cd3499e6cd958fbbacd01f3daef581a7b4d3217
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>
    Reviewed-on: https://review.openstack.org/13764
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Approved: James E. Blair <corvus@inaugust.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

--- modules/launchpad_sync/manifests/init.pp
+++ modules/launchpad_sync/manifests/init.pp
@@ -1,37 +1,37 @@
 class launchpad_sync(
-  $user='gerrit2',
-  $script_user='update',
-  $script_key_file='/home/gerrit2/.ssh/id_rsa',
+  $root_team,
   $script_logging_conf,
   $site,
-  $root_team
+  $script_key_file = '/home/gerrit2/.ssh/id_rsa',
+  $script_user = 'update',
+  $user = 'gerrit2'
 ) {
-
   include mysql
   include mysql::python
 
   $packages = [
-               "python-openid",       # for launchpad sync script
-               "python-launchpadlib", # for launchpad sync script
-               ]
+    'python-openid',       # for launchpad sync script
+    'python-launchpadlib', # for launchpad sync script
+  ]
 
   package { $packages:
     ensure => present,
   }
 
   file { '/usr/local/bin/update_gerrit_users.py':
-    owner => 'root',
-    group => 'root',
-    mode => 755,
-    source => "puppet:///modules/launchpad_sync/update_gerrit_users.py",
-    ensure => present,
+    ensure  => present,
+    group   => 'root',
+    mode    => '0755',
+    owner   => 'root',
+    source  => 'puppet:///modules/launchpad_sync/update_gerrit_users.py',
   }
 
-  cron { "sync_launchpad_users":
-    user => $user,
-    minute => "*/15",
+  cron { 'sync_launchpad_users':
+    user    => $user,
+    minute  => '*/15',
     command => "sleep $((RANDOM\\%60+60)) && timeout -k 5m 8h python /usr/local/bin/update_gerrit_users.py ${script_user} ${script_key_file} ${site} ${root_team} ${script_logging_conf}",
     require => File['/usr/local/bin/update_gerrit_users.py'],
   }
-
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
