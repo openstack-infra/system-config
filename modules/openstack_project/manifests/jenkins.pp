f7f5c2dd74e968ab64365ecdaa0ca3aa4e5980ad
Clark Boylan <clark.boylan@gmail.com>
2012-10-01 18:53:58 -0700

    Pass jenkins.o.o cert contents in from hiera.
    Use hiera to store the jenkins.o.o cert contents and populate the cert
    files from the values in hiera.
    
    Change-Id: Iffd724b7fabf9403506f08f76fa927c3b461ba19

--- modules/openstack_project/manifests/jenkins.pp
+++ modules/openstack_project/manifests/jenkins.pp
@@ -1,40 +1,47 @@
 class openstack_project::jenkins (
   $jenkins_jobs_password,
-  $sysadmins = []
+  $ssl_cert_file_contents = '',
+  $ssl_key_file_contents = '',
+  $ssl_chain_file_contents = '',
+  $sysadmins = [],
 ) {
 
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80, 443, 4155],
-    sysadmins => $sysadmins
+    sysadmins                 => $sysadmins
   }
 
+  $vhost_name = 'jenkins.openstack.org'
   class { '::jenkins::master':
-    vhost_name => 'jenkins.openstack.org',
-    serveradmin => 'webmaster@openstack.org',
-    logo => 'openstack.png',
-    ssl_cert_file => '/etc/ssl/certs/jenkins.openstack.org.pem',
-    ssl_key_file => '/etc/ssl/private/jenkins.openstack.org.key',
-    ssl_chain_file => '/etc/ssl/certs/intermediate.pem',
+    vhost_name              => $vhost_name,
+    serveradmin             => 'webmaster@openstack.org',
+    logo                    => 'openstack.png',
+    ssl_cert_file           => '/etc/ssl/certs/jenkins.openstack.org.pem',
+    ssl_key_file            => '/etc/ssl/private/jenkins.openstack.org.key',
+    ssl_chain_file          => '/etc/ssl/certs/intermediate.pem',
+    ssl_cert_file_contents  => $ssl_cert_file_contents,
+    ssl_key_file_contents   => $ssl_key_file_contents,
+    ssl_chain_file_contents => $ssl_chain_file_contents,
   }
 
-  class { "::jenkins::job_builder":
-    url => "https://jenkins.openstack.org/",
-    username => "gerrig",
+  class { '::jenkins::job_builder':
+    url      => "https://${vhost_name}/",
+    username => 'gerrig',
     password => $jenkins_jobs_password,
   }
 
   file { '/etc/jenkins_jobs/config':
-    owner => 'root',
-    group => 'root',
-    mode => 755,
-    ensure => 'directory',
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
     recurse => true,
-    source => ['puppet:///modules/openstack_project/jenkins_job_builder/config'],
-    notify => Exec["jenkins_jobs_update"]
+    source  => ['puppet:///modules/openstack_project/jenkins_job_builder/config'],
+    notify  => Exec['jenkins_jobs_update']
   }
 
-  file { "/etc/default/jenkins":
-    ensure => 'present',
+  file { '/etc/default/jenkins':
+    ensure => present,
     source => 'puppet:///modules/openstack_project/jenkins/jenkins.default'
   }
 
