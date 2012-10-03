13773cd1054bac4d105360ad4b24f34291d461e3
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-03 11:49:33 -0400

    More puppet-lint formatting fixes
    Yet another bunch of puppet-lint fixes.
    
    Change-Id: I9574cf1c5e35620b12e4d5e2bd6b2eb4d320c935
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>
    Reviewed-on: https://review.openstack.org/13988
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Approved: James E. Blair <corvus@inaugust.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

--- modules/planet/manifests/site.pp
+++ modules/planet/manifests/site.pp
@@ -1,26 +1,29 @@
-define planet::site($git_url, $vhost_name="planet.${name}.org") {
-
+define planet::site(
+  $git_url,
+  $vhost_name = "planet.${name}.org"
+) {
   include apache
   include remove_nginx
 
   apache::vhost { $vhost_name:
-    port => 80,
+    docroot  => "/srv/planet/${name}",
+    port     => 80,
     priority => '50',
-    docroot => "/srv/planet/${name}",
-    require => File["/srv/planet"],
+    require  => File['/srv/planet'],
   }
 
   vcsrepo { "/var/lib/planet/${name}":
-    ensure => present,
+    ensure   => present,
     provider => git,
-    source => $git_url,
-    require => File['/var/lib/planet'],
+    require  => File['/var/lib/planet'],
+    source   => $git_url,
   }
 
   cron { "update_planet_${name}":
-    user => root,
-    minute => "*/5",
-    command => "date >> /var/log/planet/${name}.log && cd /var/lib/planet/${name} && git pull -q --ff-only && planet /var/lib/planet/${name}/planet.ini >> /var/log/planet/${name}.log 2>&1"
+    command => "date >> /var/log/planet/${name}.log && cd /var/lib/planet/${name} && git pull -q --ff-only && planet /var/lib/planet/${name}/planet.ini >> /var/log/planet/${name}.log 2>&1",
+    minute  => '*/5',
+    user    => 'root',
   }
-
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
