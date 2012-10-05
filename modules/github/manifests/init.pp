e61170cff65be870d56e27cdaf897174842e68e9
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-05 14:37:04 -0400

    Additional puppet-lint reformatting
    Change-Id: I145048651c56d4a9648ccde2ea65100d69f43fa4
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/github/manifests/init.pp
+++ modules/github/manifests/init.pp
@@ -1,34 +1,33 @@
-class github (
-              $username,
-              $oauth_token,
-              $projects = []
-             ) {
-
+class github(
+  $oauth_token,
+  $username,
+  $projects = []
+) {
   include pip
 
-  package { "PyGithub":
-    ensure => latest,  # okay to use latest for pip
+  package { 'PyGithub':
+    ensure   => latest,  # okay to use latest for pip
     provider => pip,
-    require => Class[pip]
+    require  => Class['pip'],
   }
 
-  group { "github":
-    ensure => present
-  }
-
-  user { "github":
+  group { 'github':
     ensure => present,
-    comment => "Github API User",
-    shell => "/bin/bash",
-    gid => "github",
-    require => Group["github"]
+  }
+
+  user { 'github':
+    ensure  => present,
+    comment => 'Github API User',
+    shell   => '/bin/bash',
+    gid     => 'github',
+    require => Group['github'],
   }
 
   file { '/etc/github':
-    owner => 'root',
-    group => 'root',
-    mode => 755,
-    ensure => 'directory',
+    ensure => directory,
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
   }
 
   file { '/etc/github/github.config':
@@ -36,39 +35,43 @@ class github (
   }
 
   file { '/etc/github/github.secure.config':
-    owner => 'root',
-    group => 'github',
-    mode => 440,
-    ensure => 'present',
+    ensure  => present,
     content => template('github/github.secure.config.erb'),
-    replace => 'true',
-    require => [Group['github'], File['/etc/github']],
+    group   => 'github',
+    mode    => '0440',
+    owner   => 'root',
+    replace => true,
+    require => [
+      Group['github'],
+      File['/etc/github']
+    ],
   }
 
   file { '/usr/local/github':
-    owner => 'root',
-    group => 'root',
-    mode => 755,
-    ensure => 'directory',
+    ensure => directory,
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
   }
 
   file { '/usr/local/github/scripts':
-    owner => 'root',
-    group => 'root',
-    mode => 755,
-    ensure => 'directory',
+    ensure  => 'directory',
+    group   => 'root',
+    mode    => '0755',
+    owner   => 'root',
     recurse => true,
     require => File['/usr/local/github'],
-    source => [
-                "puppet:///modules/github/scripts",
-              ],
+    source  => [
+      'puppet:///modules/github/scripts',
+    ],
   }
 
-  cron { "githubclosepull":
-    user => github,
-    minute => "*/5",
+  cron { 'githubclosepull':
     command => 'sleep $((RANDOM\%60+90)) && python /usr/local/github/scripts/close_pull_requests.py',
+    minute  => '*/5',
     require => File['/usr/local/github/scripts'],
+    user    => 'github',
   }
-
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
