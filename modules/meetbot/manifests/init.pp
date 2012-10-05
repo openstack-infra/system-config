493ea1256624646395ded9576334bb8308863202
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-05 17:49:57 -0400

    Puppet-lint / Refactor meetbot module
    This started as a puppet-lint clean up and ended up more a refactoring
    of some of the logic.
    
    Change-Id: I5315571fbef8ff4ebe2ae77d2d2b19a6fcd5798d
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/meetbot/manifests/init.pp
+++ modules/meetbot/manifests/init.pp
@@ -1,47 +1,54 @@
 class meetbot {
-
   include apache
 
-  vcsrepo { "/opt/meetbot":
-    ensure => latest,
+  vcsrepo { '/opt/meetbot':
+    ensure   => latest,
     provider => git,
-    source => "https://github.com/openstack-ci/meetbot.git",
+    source   => 'https://github.com/openstack-ci/meetbot.git',
   }
 
-  user { "meetbot":
-    shell => "/sbin/nologin",
-    home => "/var/lib/meetbot",
-    system => true,
-    gid => "meetbot",
-    require => Group["meetbot"]
+  user { 'meetbot':
+    gid     => 'meetbot',
+    home    => '/var/lib/meetbot',
+    shell   => '/sbin/nologin',
+    system  => true,
+    require => Group['meetbot'],
   }
 
-  group { "meetbot":
-    ensure => present
+  group { 'meetbot':
+    ensure => present,
   }
 
-  package { ['supybot', 'python-twisted']:
-    ensure => present
+  $packages = [
+    'supybot',
+    'python-twisted'
+  ]
+
+  package { $packages:
+    ensure => present,
   }
 
-  file { "/var/lib/meetbot":
-    ensure => directory,
-    owner => 'meetbot',
-    require => User['meetbot']
+  file { '/var/lib/meetbot':
+    ensure  => directory,
+    owner   => 'meetbot',
+    require => User['meetbot'],
   }
 
-  file { "/usr/share/pyshared/supybot/plugins/MeetBot":
-    ensure => directory,
+  file { '/usr/share/pyshared/supybot/plugins/MeetBot':
+    ensure  => directory,
     recurse => true,
-    source => "/opt/meetbot/MeetBot",
-    require => [Package["supybot"],
-                Vcsrepo["/opt/meetbot"]]
+    require => [
+      Package['supybot'],
+      Vcsrepo['/opt/meetbot']
+    ],
+    source  => '/opt/meetbot/MeetBot',
   }
 
-  file { "/etc/nginx/sites-enabled/default":
-    ensure => absent,
+  file { '/etc/nginx/sites-enabled/default':
+    ensure  => absent,
+    notify  => Service['nginx'],
     require => Package['nginx'],
-    notify => Service['nginx']
   }
-
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
