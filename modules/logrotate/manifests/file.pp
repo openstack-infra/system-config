1283472ca108c49e744874e63217129633229473
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-08 19:52:17 -0400

    style edits to puppet config files
    Change-Id: I4f7314bcb1cb58f94ff7a78aebe27ec4591fc11c

--- modules/logrotate/manifests/file.pp
+++ modules/logrotate/manifests/file.pp
@@ -1,10 +1,10 @@
-define logrotate::file($log,
-                       $options,
-                       $ensure=present,
-                       $prerotate='undef',
-                       $postrotate='undef',
-                       $firstaction='undef',
-                       $lastaction='undef') {
+define logrotate::file( $log,
+  $options,
+  $ensure=present,
+  $prerotate='undef',
+  $postrotate='undef',
+  $firstaction='undef',
+  $lastaction='undef') {
 
   # $options should be an array containing 1 or more logrotate
   # directives (e.g. missingok, compress).
@@ -12,11 +12,11 @@ define logrotate::file($log,
   include logrotate
 
   file { "/etc/logrotate.d/${name}":
-    owner => root,
-    group => root,
-    mode => 644,
-    ensure => $ensure,
-    content => template("logrotate/config.erb"),
-    require => File["/etc/logrotate.d"],
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('logrotate/config.erb'),
+    require => File['/etc/logrotate.d'],
   }
 }
--- modules/logrotate/manifests/init.pp
+++ modules/logrotate/manifests/init.pp
@@ -1,16 +1,15 @@
 # Adapted from http://projects.puppetlabs.com/projects/1/wiki/Logrotate_Patterns
 
 class logrotate {
-
-  package { "logrotate":
+  package { 'logrotate':
     ensure => present,
   }
 
-  file { "/etc/logrotate.d":
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 755,
-    require => Package["logrotate"],
+  file { '/etc/logrotate.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['logrotate'],
   }
 }
