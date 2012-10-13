ac71aa72a6ff7a8724f122448fc325e47f50e281
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-12 23:37:31 -0400

    Cleanup jenkins manifest lint errors.
    Change-Id: Ie79736ec1121859cee30f79c55e3760e22e090ba

--- modules/jenkins/manifests/cgroups.pp
+++ modules/jenkins/manifests/cgroups.pp
@@ -7,31 +7,30 @@ class jenkins::cgroups {
   file { '/etc/cgconfig.conf':
     ensure  => present,
     replace => true,
-    owner   => root,
-    mode    => 0644,
+    owner   => 'root',
+    mode    => '0644',
     content => template('jenkins/cgconfig.erb')
   }
 
   file { '/etc/cgrules.conf':
     ensure  => present,
     replace => true,
-    owner   => root,
-    mode    => 0644,
-    source  => 'puppet:///modules/jenkins/cgroups/cgrules.conf'
+    owner   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/jenkins/cgroups/cgrules.conf',
   }
 
   service { 'cgconfig':
-    enable    => true,
     ensure    => running,
+    enable    => true,
     require   => Package['cgroup-bin'],
-    subscribe => File['/etc/cgconfig.conf']
+    subscribe => File['/etc/cgconfig.conf'],
   }
 
   service { 'cgred':
-    enable    => true,
     ensure    => running,
+    enable    => true,
     require   => Package['cgroup-bin'],
     subscribe => File['/etc/cgrules.conf']
   }
-
 }
