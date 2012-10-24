234804ed843e2cb61aba66c93361d8a3db493cbb
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-23 22:45:02 -0400

    Cleanup lodgeit manifest lint errors.
    Change-Id: Iec09a46b4c3a540154d962989dcf02b1a4605b52
    Reviewed-on: https://review.openstack.org/14825
    Reviewed-by: Paul Belanger <paul.belanger@polybeacon.com>
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Approved: James E. Blair <corvus@inaugust.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

--- modules/lodgeit/manifests/init.pp
+++ modules/lodgeit/manifests/init.pp
@@ -1,52 +1,56 @@
+# == Class: lodgeit
+#
 class lodgeit {
-  $packages = [ "python-imaging",
-                "python-jinja2",
-                "python-pybabel",
-                "python-werkzeug",
-                "python-simplejson",
-                "python-pygments",
-                "drizzle",
-                "python-mysqldb" ]
+  $packages = [ 'python-imaging',
+                'python-jinja2',
+                'python-pybabel',
+                'python-werkzeug',
+                'python-simplejson',
+                'python-pygments',
+                'drizzle',
+                'python-mysqldb' ]
 
   include apache
 
   include pip
   a2mod { 'proxy':
-    ensure => present
+    ensure => present,
   }
   a2mod { 'proxy_http':
-    ensure => present
+    ensure => present,
   }
 
-  package { $packages: ensure => present }
+  package { $packages:
+    ensure => present,
+  }
 
   package { 'SQLAlchemy':
+    ensure   => present,
     provider => pip,
-    ensure => present,
-    require => Class[pip]
+    require  => Class[pip],
   }
 
   file { '/srv/lodgeit':
-    ensure => directory
+    ensure => directory,
   }
 
   service { 'drizzle':
-    ensure => running,
-    hasrestart => true
+    ensure     => running,
+    hasrestart => true,
   }
 
-  vcsrepo { "/tmp/lodgeit-main":
-    ensure => latest,
+  vcsrepo { '/tmp/lodgeit-main':
+    ensure   => latest,
     provider => git,
-    source => "https://github.com/openstack-ci/lodgeit.git",
+    source   => 'https://github.com/openstack-ci/lodgeit.git',
   }
 
 # create initial git DB backup location
 
-  exec { "create_db_backup":
-    command => "git init /var/backups/lodgeit_db",
-    path => "/bin:/usr/bin",
-    onlyif => "test ! -d /var/backups/lodgeit_db"
+  exec { 'create_db_backup':
+    command => 'git init /var/backups/lodgeit_db',
+    path    => '/bin:/usr/bin',
+    onlyif  => 'test ! -d /var/backups/lodgeit_db',
   }
 
 }
