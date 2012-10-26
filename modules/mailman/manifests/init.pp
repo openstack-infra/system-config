36492cf575aaea2ed3619896f404895a3b691a30
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-26 14:48:53 -0400

    Cleanup mailman manifest init.pp lint errors.
    Change-Id: I387b377d65fbbe49581fa85a371d214ad60e4552

--- modules/mailman/manifests/init.pp
+++ modules/mailman/manifests/init.pp
@@ -1,58 +1,58 @@
-class mailman($vhost_name=$fqdn) {
+# == Class: mailman
+#
+class mailman($vhost_name=$::fqdn) {
 
   include apache
 
-  package { "mailman":
+  package { 'mailman':
     ensure => installed,
   }
 
   apache::vhost { $vhost_name:
-    port => 80,
-    docroot => "/var/www/",
+    port     => 80,
+    docroot  => '/var/www/',
     priority => '50',
     template => 'mailman/mailman.vhost.erb',
   }
   a2mod { 'rewrite':
-    ensure => present
+    ensure => present,
   }
 
-  file { "/var/www/index.html":
-    source => 'puppet:///modules/mailman/index.html',
-    owner => 'root',
-    group => 'root',
-    ensure => 'present',
-    replace => 'true',
-    mode => 444,
-    require => Package["apache2"],
+  file { '/var/www/index.html':
+    ensure  => present,
+    source  => 'puppet:///modules/mailman/index.html',
+    owner   => 'root',
+    group   => 'root',
+    replace => true,
+    mode    => '0444',
+    require => Package['apache2'],
   }
 
   file { '/etc/mailman/mm_cfg.py':
-    owner => 'root',
-    group => 'root',
-    mode => 444,
-    ensure => 'present',
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
     content => template('mailman/mm_cfg.py.erb'),
-    replace => 'true',
-    require => Package["mailman"]
+    replace => true,
+    require => Package['mailman'],
   }
 
   service { 'mailman':
-    ensure          => running,
-    hasrestart      => true,
-    hasstatus       => false,
-    subscribe       => File['/etc/mailman/mm_cfg.py'],
-    require         => Package["mailman"]
+    ensure     => running,
+    hasrestart => true,
+    hasstatus  => false,
+    subscribe  => File['/etc/mailman/mm_cfg.py'],
+    require    => Package['mailman'],
   }
 
   file { '/etc/mailman/en':
-    owner => 'root',
-    group => 'list',
-    mode => 644,
-    ensure => 'directory',
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'list',
+    mode    => '0644',
     recurse => true,
     require => Package['mailman'],
-    source => [
-                "puppet:///modules/mailman/html-templates-en",
-              ],
+    source  => 'puppet:///modules/mailman/html-templates-en',
   }
 }
