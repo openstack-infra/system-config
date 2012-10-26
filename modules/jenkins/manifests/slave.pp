35d342965f68abd2b8e180dac8628125f8893ca1
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-26 12:36:43 -0400

    Cleanup jenkins manifest lint errors.  Take 2.
    Change-Id: Id45aa43e20d281babd89f06f552bb244f2da353a

--- modules/jenkins/manifests/slave.pp
+++ modules/jenkins/manifests/slave.pp
@@ -1,49 +1,57 @@
-class jenkins::slave($ssh_key, $sudo = false, $bare = false, $user = true) {
+# == Class: jenkins::slave
+#
+class jenkins::slave(
+  $ssh_key,
+  $sudo = false,
+  $bare = false,
+  $user = true,
+) {
 
     include pip
 
     if ($user == true) {
       class { 'jenkins::jenkinsuser':
-        ensure => present,
-        sudo => $sudo,
-        ssh_key => "${ssh_key}"
+        ensure  => present,
+        sudo    => $::sudo,
+        ssh_key => $::ssh_key,
       }
     }
 
     # Packages that all jenkins slaves need
     $common_packages = [
-                 "default-jdk", # jdk for building java jobs
-                 "build-essential",
-                 "ccache",
-                 "python-netaddr", # Needed for devstack address_in_net()
-                 ]
+      'default-jdk', # jdk for building java jobs
+      'build-essential',
+      'ccache',
+      'python-netaddr', # Needed for devstack address_in_net()
+    ]
 
     # Packages that most jenkins slaves (eg, unit test runners) need
     $standard_packages = [
-                 "asciidoc", # for building gerrit/building openstack docs
-                 "curl",
-                 "docbook-xml", # for building openstack docs
-                 "docbook5-xml", # for building openstack docs
-                 "docbook-xsl", # for building openstack docs
-                 "firefox", # for selenium tests
-                 "libapache2-mod-wsgi",
-                 "libcurl4-gnutls-dev",
-                 "libldap2-dev",
-                 "libmysqlclient-dev",
-                 "libsqlite3-dev",
-                 "libxml2-dev",
-                 "libxslt1-dev",
-                 "maven2",
-                 "pandoc", #for docs, markdown->docbook, bug 924507
-                 "python-libvirt",
-                 "python-zmq", # zeromq unittests (not pip installable)
-                 "python3-all-dev",
-                 "rubygems",
-                 "sqlite3",
-                 "unzip",
-                 "xsltproc", # for building openstack docs
-                 "xvfb", # for selenium tests
-                 "pyflakes"]
+      'asciidoc', # for building gerrit/building openstack docs
+      'curl',
+      'docbook-xml', # for building openstack docs
+      'docbook5-xml', # for building openstack docs
+      'docbook-xsl', # for building openstack docs
+      'firefox', # for selenium tests
+      'libapache2-mod-wsgi',
+      'libcurl4-gnutls-dev',
+      'libldap2-dev',
+      'libmysqlclient-dev',
+      'libsqlite3-dev',
+      'libxml2-dev',
+      'libxslt1-dev',
+      'maven2',
+      'pandoc', #for docs, markdown->docbook, bug 924507
+      'pyflakes',
+      'python-libvirt',
+      'python-zmq', # zeromq unittests (not pip installable)
+      'python3-all-dev',
+      'rubygems',
+      'sqlite3',
+      'unzip',
+      'xsltproc', # for building openstack docs
+      'xvfb', # for selenium tests
+    ]
 
     if ($bare == false) {
         $packages = [$common_packages, $standard_packages]
@@ -70,68 +78,60 @@ class jenkins::slave($ssh_key, $sudo = false, $bare = false, $user = true) {
 
     # Packages that need to be installed from pip
     $pip_packages = [
-                 "setuptools-git",
-                 "tox"]
+      'setuptools-git',
+      'tox',
+    ]
 
     package { $pip_packages:
-      ensure => latest,  # we want the latest from these
+      ensure   => latest,  # we want the latest from these
       provider => pip,
-      require => Class[pip]
+      require  => Class[pip],
     }
 
     package { 'git-review':
-      ensure => '1.17',
+      ensure   => '1.17',
       provider => pip,
-      require => Class[pip]
+      require  => Class[pip],
     }
 
-    file { 'profilerubygems':
-      name => '/etc/profile.d/rubygems.sh',
-      owner => 'root',
-      group => 'root',
-      mode => 644,
-      ensure => 'present',
-      source => [
-         "puppet:///modules/jenkins/rubygems.sh",
-       ],
+    file { '/etc/profile.d/rubygems.sh':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => 'puppet:///modules/jenkins/rubygems.sh',
     }
 
-    file { 'ccachegcc':
-      name => '/usr/local/bin/gcc',
-      ensure => link,
-      target => '/usr/bin/ccache',
+    file { '/usr/local/bin/gcc':
+      ensure  => link,
+      target  => '/usr/bin/ccache',
       require => Package['ccache'],
     }
 
-    file { 'ccacheg++':
-      name => '/usr/local/bin/g++',
-      ensure => link,
-      target => '/usr/bin/ccache',
+    file { '/usr/local/bin/g++':
+      ensure  => link,
+      target  => '/usr/bin/ccache',
       require => Package['ccache'],
     }
 
-    file { 'ccachecc':
-      name => '/usr/local/bin/cc',
-      ensure => link,
-      target => '/usr/bin/ccache',
+    file { '/usr/local/bin/cc':
+      ensure  => link,
+      target  => '/usr/bin/ccache',
       require => Package['ccache'],
     }
 
-    file { 'ccachec++':
-      name => '/usr/local/bin/c++',
-      ensure => link,
-      target => '/usr/bin/ccache',
+    file { '/usr/local/bin/c++':
+      ensure  => link,
+      target  => '/usr/bin/ccache',
       require => Package['ccache'],
     }
-
 
     if ($bare == false) {
-
         class {'mysql::server':
             config_hash =>  {
-                'root_password' => 'insecure_slave',
-                'default_engine' => 'MyISAM',
-                'bind_address' => '127.0.0.1',
+              'root_password'  => 'insecure_slave',
+              'default_engine' => 'MyISAM',
+              'bind_address'   => '127.0.0.1',
             }
         }
         include mysql::server::account_security
@@ -141,52 +141,51 @@ class jenkins::slave($ssh_key, $sudo = false, $bare = false, $user = true) {
             password => 'openstack_citest',
             host     => 'localhost',
             grant    => ['all'],
-            require  => [Class['mysql::server'],
-                         Class['mysql::server::account_security']]
+            require  => [
+              Class['mysql::server'],
+              Class['mysql::server::account_security'],
+            ]
         }
-
     }
 
     file { '/usr/local/jenkins':
-      owner => 'root',
-      group => 'root',
-      mode => 755,
-      ensure => 'directory',
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
     }
 
     file { '/usr/local/jenkins/slave_scripts':
-      owner => 'root',
-      group => 'root',
-      mode => 755,
-      ensure => 'directory',
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
       recurse => true,
       require => File['/usr/local/jenkins'],
-      source => [
-                  "puppet:///modules/jenkins/slave_scripts",
-                ],
+      source  => 'puppet:///modules/jenkins/slave_scripts',
     }
 
     file { '/etc/sudoers.d/jenkins-sudo-grep':
       ensure => present,
-      source => "puppet:///modules/jenkins/jenkins-sudo-grep.sudo",
-      owner => 'root',
-      group => 'root',
-      mode => 440,
+      source => 'puppet:///modules/jenkins/jenkins-sudo-grep.sudo',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0440',
     }
 
     # Temporary for debugging glance launch problem
     # https://lists.launchpad.net/openstack/msg13381.html
     file { '/etc/sysctl.d/10-ptrace.conf':
       ensure => present,
-      source => "puppet:///modules/jenkins/10-ptrace.conf",
-      owner => 'root',
-      group => 'root',
-      mode => 444,
+      source => 'puppet:///modules/jenkins/10-ptrace.conf',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0444',
     }
 
-    exec { "ptrace sysctl":
-      subscribe => File['/etc/sysctl.d/10-ptrace.conf'],
+    exec { 'ptrace sysctl':
+      subscribe   => File['/etc/sysctl.d/10-ptrace.conf'],
       refreshonly => true,
-      command => "/sbin/sysctl -p /etc/sysctl.d/10-ptrace.conf",
+      command     => '/sbin/sysctl -p /etc/sysctl.d/10-ptrace.conf',
     }
 }
