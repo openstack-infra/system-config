6d44f9680ee9801239bbdf54deab7458f06126d6
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-26 15:13:47 -0400

    Cleanup gerrit manifest lint errors.
    Change-Id: I5a52c0fd0f5a35c32aa71c0f93500aa59e495066

--- modules/gerrit/manifests/remotes.pp
+++ modules/gerrit/manifests/remotes.pp
@@ -1,13 +1,16 @@
+# == Class: gerrit::remotes
+#
 class gerrit::remotes($ensure=present) {
-    cron { "gerritfetchremotes":
-      user => gerrit2,
-      ensure => $ensure,
-      minute => "*/30",
-      command => 'sleep $((RANDOM\%60+90)) && python /usr/local/gerrit/scripts/fetch_remotes.py',
+    cron { 'gerritfetchremotes':
+      ensure  => $ensure,
+      user    => 'gerrit2',
+      minute  => '*/30',
+      command => 'sleep $((RANDOM\%60+90)) && python \
+        /usr/local/gerrit/scripts/fetch_remotes.py',
       require => File['/usr/local/gerrit/scripts'],
     }
 
     file { '/home/gerrit2/remotes.config':
-      ensure => absent
+      ensure => absent,
     }
 }
