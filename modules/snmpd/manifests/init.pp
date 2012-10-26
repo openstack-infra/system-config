be5ac225a00213a4ae838d4ba7ea7aab8da05b50
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-26 14:36:46 -0400

    Cleanup snmpd manifest init.pp lint errors.
    Change-Id: I970c492c11d91dfab3b206859a72598a16602a24

--- modules/snmpd/manifests/init.pp
+++ modules/snmpd/manifests/init.pp
@@ -1,30 +1,36 @@
+# == Class: snmpd
+#
 class snmpd {
-    package { snmpd: ensure => present }
-    service { snmpd:
-      ensure          => running,
-      hasrestart      => true,
-      require => [File["/etc/snmp/snmpd.conf"],
-                  File["/etc/init.d/snmpd"]]
-    }
-    # This file is only needed on machines pre-precise. There is a bug in
-    # the previous init script versions which causes them to attempt
-    # snmptrapd even if it's configured not to run, and then to report
-    # failure.
-    file { "/etc/init.d/snmpd":
-      owner => 'root',
-      group => 'root',
-      mode => 755,
-      ensure => 'present',
-      source => 'puppet:///modules/snmpd/snmpd.init',
-      replace => 'true',
-      require => Package[snmpd]
-    }
-    file { "/etc/snmp/snmpd.conf":
-      owner => 'root',
-      group => 'root',
-      mode => 444,
-      ensure => 'present',
-      source => 'puppet:///modules/snmpd/snmpd.conf',
-      replace => 'true',
-    }
+  package { 'snmpd':
+    ensure => present,
+  }
+  service { 'snmpd':
+    ensure     => running,
+    hasrestart => true,
+    require    => [
+      File['/etc/snmp/snmpd.conf'],
+      File['/etc/init.d/snmpd'],
+    ],
+  }
+  # This file is only needed on machines pre-precise. There is a bug in
+  # the previous init script versions which causes them to attempt
+  # snmptrapd even if it's configured not to run, and then to report
+  # failure.
+  file { '/etc/init.d/snmpd':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/snmpd/snmpd.init',
+    replace => true,
+    require => Package['snmpd'],
+  }
+  file { '/etc/snmp/snmpd.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'puppet:///modules/snmpd/snmpd.conf',
+    replace => true,
+  }
 }
