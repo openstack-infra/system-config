8e3f1581fe59118f60a4a6c8dd71aee0d64c2a26
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-29 11:09:34 -0400

    Additional puppet-lint reformatting
    Change-Id: I49e63755b8b5bf60673cf37b62371b3f299e3bbe
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/devstack_host/manifests/init.pp
+++ modules/devstack_host/manifests/init.pp
@@ -1,41 +1,39 @@
 # A machine ready to run devstack
 class devstack_host {
+  package { 'linux-headers-virtual':
+    ensure => present,
+  }
 
-    package { "linux-headers-virtual":
-      ensure => present,
-    }
+  package { 'mysql-server':
+    ensure => present,
+  }
 
-    package { "mysql-server":
-      ensure => present,
-    }
+  package { 'rabbitmq-server':
+    ensure => present,
+    require => File['/etc/rabbitmq/rabbitmq-env.conf'],
+  }
 
-    package { "rabbitmq-server":
-      ensure => present,
-      require => File['rabbitmq-env.conf'],
-    }
+  file { '/etc/rabbitmq':
+    ensure => directory,
+  }
 
-    file { "/etc/rabbitmq":
-      ensure => "directory",
-    }
+  file { '/etc/rabbitmq/rabbitmq-env.conf':
+    ensure  => present,
+    group   => 'root',
+    mode    => '0444',
+    owner   => 'root',
+    require => File['/etc/rabbitmq'],
+    source  => 'puppet:///modules/devstack_host/rabbitmq-env.conf',
+  }
 
-    file { 'rabbitmq-env.conf':
-      name => '/etc/rabbitmq/rabbitmq-env.conf',
-      owner => 'root',
-      group => 'root',
-      mode => 444,
-      ensure => 'present',
-      source => [
-         "puppet:///modules/devstack_host/rabbitmq-env.conf",
-       ],
-      require => File['/etc/rabbitmq'],
-    }
-
-    exec { "Set MySQL server root password":
-    	 subscribe => [ Package["mysql-server"]],
-	 refreshonly => true,
-	 unless => "mysqladmin -uroot -psecret status",
-	 path => "/bin:/usr/bin",
-	 command => "mysqladmin -uroot password secret",
-    }
-
+  # TODO: We should be using existing mysql functions do this.
+  exec { 'Set MySQL server root password':
+    command     => 'mysqladmin -uroot password secret',
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
+    subscribe   => Package['mysql-server'],
+    unless      => 'mysqladmin -uroot -psecret status',
+  }
 }
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
