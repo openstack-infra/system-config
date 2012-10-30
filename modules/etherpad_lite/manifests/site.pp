c954b1938fd73d445fdf5282e106372608e85eda
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-30 16:22:20 -0400

    Cleanup etherpad_lite manifest lint errors.
    Also, seperate out the buildsource defined resource from init.pp
    
    Change-Id: I9fe46ad31943f667ebe8bb6b01a2007e0b3cf022

--- modules/etherpad_lite/manifests/site.pp
+++ modules/etherpad_lite/manifests/site.pp
@@ -1,48 +1,51 @@
+# == Class: etherpad_lite::site
+#
 class etherpad_lite::site (
+  $database_password,
   $dbType = 'mysql',
   $database_user = 'eplite',
   $database_name = 'etherpad-lite',
-  $database_password,
+  $base = "${etherpad_lite::base_install_dir}/etherpad-lite}"
 ) {
 
   include etherpad_lite
 
   if $dbType == 'mysql' {
     service { 'etherpad-lite':
-      enable    => true,
       ensure    => running,
-      subscribe => File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"],
+      enable    => true,
+      subscribe => File["${base}/settings.json"],
       require   => Class['etherpad_lite::mysql'],
     }
   }
   else {
     service { 'etherpad-lite':
-      enable    => true,
       ensure    => running,
-      subscribe => File["${etherpad_lite::base_install_dir}/etherpad-lite/settings.json"],
+      enable    => true,
+      subscribe => File["${base}/settings.json"],
     }
   }
 
-  file { "${etherpad_lite::base_install_dir}/etherpad-lite/settings.json":
-    ensure  => 'present',
+  file { "${base}/settings.json":
+    ensure  => present,
     content => template('etherpad_lite/etherpad-lite_settings.json.erb'),
     replace => true,
     owner   => $etherpad_lite::ep_user,
     group   => $etherpad_lite::ep_user,
-    mode    => 0600,
-    require => Class['etherpad_lite']
+    mode    => '0600',
+    require => Class['etherpad_lite'],
   }
 
-  file { "${etherpad_lite::base_install_dir}/etherpad-lite/src/static/custom/pad.js":
-    ensure  => 'present',
+  file { "${base}/src/static/custom/pad.js":
+    ensure  => present,
     source  => 'puppet:///modules/etherpad_lite/pad.js',
     owner   => $etherpad_lite::ep_user,
     group   => $etherpad_lite::ep_user,
-    mode    => 0644,
-    require => Class['etherpad_lite']
+    mode    => '0644',
+    require => Class['etherpad_lite'],
   }
 
-  file { "${etherpad_lite::base_install_dir}/etherpad-lite/src/static/robots.txt":
+  file { "${base}/src/static/robots.txt":
     ensure  => present,
     source  => 'puppet:///modules/etherpad_lite/robots.txt',
     owner   => $etherpad_lite::ep_user,
@@ -53,15 +56,30 @@ class etherpad_lite::site (
 
   include logrotate
   logrotate::file { 'epliteerror':
-    log     => "${etherpad_lite::base_log_dir}/${etherpad_lite::ep_user}/error.log",
-    options => ['compress', 'copytruncate', 'missingok', 'rotate 7', 'daily', 'notifempty'],
-    require => Service['etherpad-lite']
+    log     =>
+      "${etherpad_lite::base_log_dir}/${etherpad_lite::ep_user}/error.log",
+    options => [
+      'compress',
+      'copytruncate',
+      'missingok',
+      'rotate 7',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['etherpad-lite'],
   }
 
   logrotate::file { 'epliteaccess':
-    log     => "${etherpad_lite::base_log_dir}/${etherpad_lite::ep_user}/access.log",
-    options => ['compress', 'copytruncate', 'missingok', 'rotate 7', 'daily', 'notifempty'],
-    require => Service['etherpad-lite']
+    log     =>
+      "${etherpad_lite::base_log_dir}/${etherpad_lite::ep_user}/access.log",
+    options => [
+      'compress',
+      'copytruncate',
+      'missingok',
+      'rotate 7',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['etherpad-lite'],
   }
-
 }
