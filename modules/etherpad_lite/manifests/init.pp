6d88be529bfb87b43f9906ee4d5aefbccd8d6931
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-30 16:22:20 -0400

    Cleanup etherpad_lite manifest lint errors.
    Also, seperate out the buildsource defined resource from init.pp
    
    Change-Id: I9fe46ad31943f667ebe8bb6b01a2007e0b3cf022

--- modules/etherpad_lite/manifests/init.pp
+++ modules/etherpad_lite/manifests/init.pp
@@ -1,36 +1,5 @@
-# define to build from source using ./configure && make && make install.
-define buildsource(
-  $dir     = $title,
-  $user    = 'root',
-  $creates = '/nonexistant/file'
-) {
-
-  exec { "./configure in ${dir}":
-    command => './configure',
-    path    => "/usr/bin:/bin:/usr/local/bin:${dir}",
-    user    => $user,
-    cwd     => $dir,
-    creates => $creates
-  } ->
-
-  exec { "make in ${dir}":
-    command => 'make',
-    path    => '/usr/bin:/bin',
-    user    => $user,
-    cwd     => $dir,
-    creates => $creates
-  } ->
-
-  exec { "make install in ${dir}":
-    command => 'make install',
-    path    => '/usr/bin:/bin',
-    user    => $user,
-    cwd     => $dir,
-    creates => $creates
-  }
-
-}
-
+# == Class: etherpad_lite
+#
 # Class to install etherpad lite. Puppet acts a lot like a package manager
 # through this class.
 #
@@ -55,19 +24,19 @@ class etherpad_lite (
     home    => "${base_log_dir}/${ep_user}",
     system  => true,
     gid     => $ep_user,
-    require => Group[$ep_user]
+    require => Group[$ep_user],
   }
 
   group { $ep_user:
-    ensure => present
+    ensure => present,
   }
 
   # Below is what happens when you treat puppet as a package manager.
   # This is probably bad, but it works and you don't need to roll .debs.
-  file { "${base_install_dir}":
+  file { $base_install_dir:
     ensure => directory,
     group  => $ep_user,
-    mode   => 0664,
+    mode   => '0664',
   }
 
   vcsrepo { "${base_install_dir}/nodejs":
@@ -78,29 +47,33 @@ class etherpad_lite (
     require  => Package['git']
   }
 
-  package { ['gzip',
-             'curl',
-             'python',
-             'libssl-dev',
-             'pkg-config',
-             'abiword',
-             'build-essential']:
-    ensure => present
+  package { [
+      'gzip',
+      'curl',
+      'python',
+      'libssl-dev',
+      'pkg-config',
+      'abiword',
+      'build-essential',
+    ]:
+    ensure => present,
   }
 
   package { ['nodejs', 'npm']:
-    ensure => purged
+    ensure => purged,
   }
 
   buildsource { "${base_install_dir}/nodejs":
     creates => '/usr/local/bin/node',
-    require => [Package['gzip'],
-                Package['curl'],
-                Package['python'],
-                Package['libssl-dev'],
-                Package['pkg-config'],
-                Package['build-essential'],
-                Vcsrepo["${base_install_dir}/nodejs"]]
+    require => [
+      Package['gzip'],
+      Package['curl'],
+      Package['python'],
+      Package['libssl-dev'],
+      Package['pkg-config'],
+      Package['build-essential'],
+      Vcsrepo["${base_install_dir}/nodejs"],
+    ],
   }
 
   # Allow existing install to exist without modifying its git repo.
@@ -126,26 +99,29 @@ class etherpad_lite (
 
   exec { 'install_etherpad_dependencies':
     command     => './bin/installDeps.sh',
-    path        => "/usr/bin:/bin:/usr/local/bin:${base_install_dir}/etherpad-lite",
+    path        =>
+      "/usr/bin:/bin:/usr/local/bin:${base_install_dir}/etherpad-lite",
     user        => $ep_user,
     cwd         => "${base_install_dir}/etherpad-lite",
     environment => "HOME=${base_log_dir}/${ep_user}",
-    require     => [Vcsrepo["${base_install_dir}/etherpad-lite"],
-                    Buildsource["${base_install_dir}/nodejs"]],
+    require     => [
+      Vcsrepo["${base_install_dir}/etherpad-lite"],
+      Buildsource["${base_install_dir}/nodejs"],
+    ],
     before      => File["${base_install_dir}/etherpad-lite/settings.json"],
-    creates     => "${base_install_dir}/etherpad-lite/node_modules"
+    creates     => "${base_install_dir}/etherpad-lite/node_modules",
   }
 
   file { '/etc/init/etherpad-lite.conf':
-    ensure  => 'present',
+    ensure  => present,
     content => template('etherpad_lite/upstart.erb'),
-    replace => 'true',
+    replace => true,
     owner   => 'root',
   }
 
   file { '/etc/init.d/etherpad-lite':
     ensure => link,
-    target => '/lib/init/upstart-job'
+    target => '/lib/init/upstart-job',
   }
 
   file { "${base_log_dir}/${ep_user}":
@@ -153,5 +129,4 @@ class etherpad_lite (
     owner  => $ep_user,
   }
   # end package management ugliness
-
 }
