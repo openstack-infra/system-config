af2a8e80cf4bda123c5b8b5b200e3a0dc5412173
Matthew Wagoner <matthew.wagoner@hp.com>
2012-10-30 15:02:37 -0400

    Cleanup openstack_project manifest lint errors.
    Change-Id: I7c622ffa77821f33f911793fc6b6cdaaba37904a

--- modules/openstack_project/manifests/translation_slave.pp
+++ modules/openstack_project/manifests/translation_slave.pp
@@ -1,3 +1,5 @@
+# == Class: openstack_project::translation_slave
+#
 class openstack_project::translation_slave (
   transifex_username = 'openstackci',
   transifex_password
@@ -6,18 +8,17 @@ class openstack_project::translation_slave (
   include openstack_project::slave
 
   package { ['transifex-client', 'Babel']:
-    ensure => latest,
+    ensure   => latest,
     provider => pip,
-    require => Class[pip]
+    require  => Class['pip'],
   }
 
   file { '/home/jenkins/.transifexrc':
-    owner => 'jenkins',
-    group => 'jenkins',
-    mode => 0600,
-    ensure => 'present',
+    ensure  => present,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    mode    => '0600',
     content => template('openstack_project/transifexrc.erb'),
     require => User['jenkins'],
   }
-
 }
