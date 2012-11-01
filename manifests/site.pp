4c1f2dc4bdf3d41719c148d7c0c163326eea1b72
Clark Boylan <clark.boylan@gmail.com>
2012-11-01 10:00:57 -0700

    Sample what puppeted mail lists would look like.
    Example change of what puppeting the lists.openstack.org mail lists
    would look like.
    
    Change-Id: Ib330245512e75e73adbcde3a55b3da9871df51c7

--- manifests/site.pp
+++ manifests/site.pp
@@ -123,7 +123,8 @@ node 'ci-puppetmaster.openstack.org' {
 
 node 'lists.openstack.org' {
   class { 'openstack_project::lists':
-    listadmins => hiera('listadmins'),
+    listadmins   => hiera('listadmins'),
+    listpassword => hiera('listpassword'),
   }
 }
 
--- modules/openstack_project/manifests/lists.pp
+++ modules/openstack_project/manifests/lists.pp
@@ -1,4 +1,7 @@
-class openstack_project::lists($listadmins) {
+class openstack_project::lists(
+  $listadmins,
+  $listpassword = 'insecure_password'
+) {
   # Using openstack_project::template instead of openstack_project::server
   # because the exim config on this machine is almost certainly
   # going to be more complicated than normal.
@@ -6,13 +9,24 @@ class openstack_project::lists($listadmins) {
     iptables_public_tcp_ports => [25, 80, 465]
   }
 
+  $listdomain = 'lists.openstack.org'
+
   class { 'exim':
     sysadmin => $listadmins,
-    mailman_domains => ['lists.openstack.org'],
+    mailman_domains => [$listdomain],
   }
 
   class { 'mailman':
-    vhost_name => 'lists.openstack.org'
+    vhost_name => $listdomain,
+  }
+
+  maillist { 'footest':
+    ensure      => present,
+    admin       => 'foo@openstack.org',
+    password    => $listpassword,
+    description => 'Foo Testing Bar',
+    webserver   => $listdomain,
+    mailserver  => $listdomain,
   }
 
   realize (
