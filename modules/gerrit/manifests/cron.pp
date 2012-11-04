dceaaedb2d98357a26d18e7b4c9d2de83fead137
Monty Taylor <mordred@inaugust.com>
2012-11-04 22:20:36 +0100

    Attempt to more fully manage project creation.
    This should allow for all of the bits of project creation except for
    setting ACLs.
    
    Change-Id: I15a1ec13b381dce3c115c01c21f404ab79e72cc4

--- modules/gerrit/manifests/cron.pp
+++ modules/gerrit/manifests/cron.pp
@@ -19,8 +19,7 @@ class gerrit::cron(
     weekday => '0',
     hour    => '4',
     minute  => '7',
-    command => 'find /home/gerrit2/review_site/git/ -type d -name "*.git" \
-      -print -exec git --git-dir="{}" repack -afd \;',
+    command => 'find /home/gerrit2/review_site/git/ -type d -name "*.git" -print -exec git --git-dir="{}" repack -afd \;',
     environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
   }
 
@@ -28,8 +27,7 @@ class gerrit::cron(
     user    => 'gerrit2',
     hour    => '5',
     minute  => '1',
-    command => 'find /home/gerrit2/dbupdates/ -name "*.sql.gz" -mtime +30 \
-      -exec rm -f {} \;',
+    command => 'find /home/gerrit2/dbupdates/ -name "*.sql.gz" -mtime +30 -exec rm -f {} \;',
     environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
   }
 }
