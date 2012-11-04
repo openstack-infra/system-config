08b0fe0918132805b9fb91bb7f13d482ee5653ec
Monty Taylor <mordred@inaugust.com>
2012-11-04 22:20:36 +0100

    Attempt to more fully manage project creation.
    Change-Id: I15a1ec13b381dce3c115c01c21f404ab79e72cc4

--- modules/gerrit/files/scripts/make_local_repos.py
+++ modules/gerrit/files/scripts/make_local_repos.py
@@ -18,18 +18,23 @@
 
 # - project: PROJECT_NAME
 #   options:
-#   - close-pull
+#    - close-pull
+#   description: This is a great project
 #   remote: https://gerrit.googlesource.com/gerrit
 
 # TODO: add support for
 #         ssh -p 29418 localhost gerrit -name create-project PROJECT
 
+import ConfigParser
 import logging
 import os
+import shlex
 import subprocess
 import sys
-import shlex
 import yaml
+
+import github
+import gerritlib
 
 def run_command(cmd, status=False, env={}):
     cmd_list = shlex.split(str(cmd))
@@ -52,16 +57,63 @@ logging.basicConfig(level=logging.ERROR)
 REPO_ROOT = sys.argv[1]
 PROJECTS_YAML = os.environ.get('PROJECTS_YAML',
                                '/home/gerrit2/projects.yaml')
+GITHUB_SECURE_CONFIG = os.environ.get('GITHUB_SECURE_CONFIG',
+                                      '/etc/github/github.secure.config')
+GERRIT_CONFIG = os.environ.get('GERRIT_CONFIG',
+                               '/etc/gerrit/gerrit.secure.config')
+ 
+secure_config = ConfigParser.ConfigParser()
+secure_config.read(GITHUB_SECURE_CONFIG)
+
+if secure_config.has_option("github", "oauth_token"):
+    ghub = github.Github(secure_config.get("github", "oauth_token"))
+else:
+    ghub = github.Github(secure_config.get("github", "username"),
+                         secure_config.get("github", "password"))
+orgs = ghub.get_user().get_orgs()
+orgs_dict = dict(zip([o.login.lower() for o in orgs], orgs))
+
+gerrit_config = ConfigParser.ConfigParser()
+gerrit_config.read(GERRIT_CONFIG)
+
+gerrit = gerritlib.gerrit.Gerrit(
+                gerrit_config.get('gerrit', 'host'),
+                gerrit_config.get('gerrit', 'user'),
+                gerrit_config.getint('gerrit', 'port'),
+                gerrit_config.get('gerrit', 'key'))
+project_list = gerrit.listProjects()
 
 config = yaml.load(open(PROJECTS_YAML))
 
 for section in config:
     project = section['project']
+    options = section['options']
+    description = section.get('description', None)
 
     project_git = "%s.git" % project
     project_dir = os.path.join(REPO_ROOT, project_git)
 
-    if os.path.exists(project_dir):
-        continue
+    # Find the project's repo
+    project_split = project.split('/', 1)
+    if len(project_split) > 1:
+        repo_name = project_split[1]
+    else:
+        repo_name = project
+    try:
+        repo = orgs_dict[project_split[0].lower()].get_repo(repo_name)
+    except github.GithubException.GithubException:
+        repo = ghub.get_user().create_repo(repo_name,
+                                           homepage='http://openstack.org',
+                                           has_issues=False,
+                                           has_downloads=False,
+                                           team_id=153703,
+                                           has_wiki = False)
+    if description:
+        ghub.get_user().edit_repo(repo_name, description=description)
 
-    run_command("git --bare init --shared=group %s" % project_dir)
+    if not os.path.exists(project_dir):
+        run_command("git --bare init %s" % project_dir)
+        run_command("chown -R gerrit2:gerrit2 %s" % project_dir)
+
+    if project not in project_list:
+        gerrit.createProject(project)
