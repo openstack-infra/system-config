28b02a74edce529fe93d0c7f302d43bfe03cd272
James E. Blair <jeblair@hp.com>
2012-11-08 10:52:37 +0100

    Add groups.openstack.org.
    Change-Id: Ie143fe2477372c0c1a7fe6675b538b2b40146626

---
+++ modules/openstack_project/manifests/groups.pp
@@ -0,0 +1,9 @@
+# User group management server
+class openstack_project::groups (
+  $sysadmins = []
+) {
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [80, 443],
+    sysadmins                 => $sysadmins,
+  }
+}
