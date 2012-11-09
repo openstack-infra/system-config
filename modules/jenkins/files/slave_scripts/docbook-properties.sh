68af10596ddd4e34ca3e6e8ea7a86d32102e2cac
annegentle <anne@openstack.org>
2012-11-09 15:48:38 -0600

    Removes unused gerrit-doc-properties.sh file as it is identical to docbook-properties.sh.
    Minor comment changes - just checking to make sure the folsom
    doc branch will have comments.
    
    Change-Id: I69a415354e60e01ade8e8341a93d68d3c09dee4a
    Reviewed-on: https://review.openstack.org/15793
    Reviewed-by: Paul Belanger <paul.belanger@polybeacon.com>
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Approved: James E. Blair <corvus@inaugust.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

--- modules/jenkins/files/slave_scripts/docbook-properties.sh
+++ modules/jenkins/files/slave_scripts/docbook-properties.sh
@@ -16,7 +16,7 @@ then
     DOC_COMMENTS_ENABLED=0
 fi
 
-# The stable/<releasename> branch should get published to /releasename, such as diablo or essex
+# The stable/<releasename> branch should get published to /releasename, such as folsom or essex
 if [[ $BRANCH =~ ^stable/(.*)$ ]]
 then
     DOC_RELEASE_PATH=${BASH_REMATCH[1]}
