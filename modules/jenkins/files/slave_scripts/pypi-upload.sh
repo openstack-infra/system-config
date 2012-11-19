90cc9688ced5d68f55aeac25d216afd54f1c2c98
Clark Boylan <clark.boylan@gmail.com>
2012-11-19 15:17:14 -0800

    Correct pypi upload form and copy artifact source
    The file type form entry is 'filetype' not 'file_type'. Correct that in
    pypi-upload.sh.
    
    The artifact to copy is 'dist/project*.tar.gz' not 'project*.tar.gz'.
    Correct that in pypi-jobs.yaml. Also cd into the dist dir that will be
    created before attempting to copy the sdist out of that dir.
    
    Change-Id: Ifa27e683c162f5acfc5136d0729be42af9b4ca41
    Reviewed-on: https://review.openstack.org/16493
    Reviewed-by: Jeremy Stanley <fungi@yuggoth.org>
    Reviewed-by: Paul Belanger <paul.belanger@polybeacon.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Approved: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

--- modules/jenkins/files/slave_scripts/pypi-upload.sh
+++ modules/jenkins/files/slave_scripts/pypi-upload.sh
@@ -8,12 +8,12 @@ VERSION=`echo ${FILENAME} | sed -n "s/${PROJECT}-\(.*\).tar.gz/\1/p"`
 MD5_DIGEST=`md5sum ${FILENAME} | cut -d' ' -f1`
 
 curl --config /home/jenkins/.pypicurl \
+     -F "filetype=sdist" \
      -F "content=@${FILENAME};filename=${FILENAME}" \
      -F ":action=file_upload" \
      -F "protocol_version=1" \
      -F "name=${PROJECT}" \
      -F "version=${VERSION}" \
-     -F "file_type=sdist" \
      -F "md5_digest=${MD5_DIGEST}" \
      http://pypi.python.org/pypi > /dev/null 2>&1
 
