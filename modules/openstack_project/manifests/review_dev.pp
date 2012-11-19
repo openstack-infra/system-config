6bbcebae743532dd883e440a01fd9108b4df6e0a
Jeremy Stanley <fungi@yuggoth.org>
2012-11-19 21:28:14 +0000

    Point review-dev at the foundation contact store.
    * modules/openstack_project/manifests/review_dev.pp: Set
    contactstore_url to the URL of the new OpenStack Foundation contact
    store server for testing purposes.
    
    Change-Id: I161fb3cf4f5c414495c79509fb71da633d1cf08f

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -42,7 +42,7 @@ class openstack_project::review_dev (
     contactstore => true,
     contactstore_appsec => $contactstore_appsec,
     contactstore_pubkey => $contactstore_pubkey,
-    contactstore_url => 'https://www.yuggoth.org/gerrit_test',
+    contactstore_url => 'http://www.openstack.org/verify/member/',
     script_user => 'launchpadsync',
     script_key_file => '/home/gerrit2/.ssh/launchpadsync_rsa',
     script_logging_conf => '/home/gerrit2/.sync_logging.conf',
