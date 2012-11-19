63b990b52f08e3302800eff76d7bc1289871ab03
Antoine Musso <hashar@free.fr>
2012-11-19 22:59:25 +0100

    zuul: learn the ability to set push_change_refs
    push_change_refs controls whether zuul should push its reference back in
    Gerrit.  This might not be always wanted, for example on very busy
    Gerrit installation that never use the refs/zuul/* hierarchy for any
    purpose.
    
    Pushing back to Gerrit is now disabled by default and is explicitly
    enabled on OpenStack infrastructure (openstack_project::zuul).
    
    Change-Id: I24111ba4b20ab3d5198fdf7deaf902485260d8a5

--- modules/openstack_project/manifests/zuul.pp
+++ modules/openstack_project/manifests/zuul.pp
@@ -13,7 +13,8 @@ class openstack_project::zuul(
     jenkins_apikey => $jenkins_apikey,
     gerrit_server => $gerrit_server,
     gerrit_user => $gerrit_user,
-    url_pattern => $url_pattern
+    url_pattern => $url_pattern,
+    push_change_refs => true
   }
 
   file { "/etc/zuul/layout.yaml":
