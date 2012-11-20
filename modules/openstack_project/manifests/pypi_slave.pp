51295da046c86ad3f4d3f4369d45f62231c7a6fd
Clark Boylan <clark.boylan@gmail.com>
2012-11-19 17:10:35 -0800

    Push PKG-INFO metadata to pypi.
    Add a mini python script to make sense of the PKG-INFO files in sdists
    and output a curl config file to insert these values into the POSTed
    form.
    
    Requires the pkginfo python package.
    
    Add Apache2 license headers to the new files as well.
    
    Change-Id: I9b4b8dd17a26da181d9f4ee833f1ef1106fbe12d

--- modules/openstack_project/manifests/pypi_slave.pp
+++ modules/openstack_project/manifests/pypi_slave.pp
@@ -1,8 +1,31 @@
+# Copyright 2012  Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to install dependencies for uploading python packages to pypi.
+#
 class openstack_project::pypi_slave (
   pypi_password,
   pypi_username = 'openstackci'
 ) {
   include openstack_project::slave
+  include pip
+
+  package { 'pkginfo':
+    ensure   => present,
+    provider => 'pip',
+    require  => Class['pip'],
+  }
 
   file { '/home/jenkins/.pypicurl':
     ensure  => present,
