7c30e9c39af2dc4eb623600513a68456dbd30c93
annegentle <anne@openstack.org>
2012-11-26 10:43:56 -0600

    Changes top-level element to chapter instead of book for markdown builds.
    Change-Id: Ia9719b96594f5934d3a783bb85078ab474bacb35

--- modules/jenkins/files/slave_scripts/markdown-docbook.sh
+++ modules/jenkins/files/slave_scripts/markdown-docbook.sh
@@ -16,6 +16,6 @@ shopt -s extglob
 FILENAME=$1
 FILEPATH=`find ./ -regextype posix-extended -regex ".*${FILENAME}\.(md|markdown|mdown)"`
 DIRPATH=`dirname $FILEPATH`
-pandoc -f markdown -t docbook -s ${FILEPATH} |  xsltproc -o - /usr/share/xml/docbook/stylesheet/docbook5/db4-upgrade.xsl - |  xmllint  --format - | sed -e "s,<article,<book xml:id=\"$FILENAME\"," | sed -e 's,</article>,</book>,' > ${DIRPATH}/$FILENAME.xml
+pandoc -f markdown -t docbook -s ${FILEPATH} |  xsltproc -o - /usr/share/xml/docbook/stylesheet/docbook5/db4-upgrade.xsl - |  xmllint  --format - | sed -e "s,<article,<chapter xml:id=\"$FILENAME\"," | sed -e 's,</article>,</chapter>,' > ${DIRPATH}/$FILENAME.xml
 
 pwd
