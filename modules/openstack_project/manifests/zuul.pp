e6b8848bcf9eef237dc70e723471b476c4fd3613
James E. Blair <jeblair@hp.com>
2012-11-29 13:43:20 -0800

    Add a dedicated zuul server.
    Just bootstrapping the server; a follow-on change will switch
    hostnames in configurations.
    
    Change-Id: Ie453a3d046014137c66f2bc25e0a5916d09b9267
    Reviewed-on: https://review.openstack.org/17167
    Reviewed-by: Jeremy Stanley <fungi@yuggoth.org>
    Approved: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

--- modules/openstack_project/manifests/zuul.pp
+++ modules/openstack_project/manifests/zuul.pp
@@ -1,16 +1,29 @@
 # == Class: openstack_project::zuul
 #
 class openstack_project::zuul(
-  $jenkins_server = '',
+  $jenkins_host = '',
+  $jenkins_url = '',
   $jenkins_user = '',
   $jenkins_apikey = '',
   $gerrit_server = '',
   $gerrit_user = '',
-  $url_pattern = ''
+  $url_pattern = '',
+  $sysadmins = []
 ) {
 
+  $rules = [ "-m state --state NEW -m tcp -p tcp --dport 8001 -s ${jenkins_host} -j ACCEPT" ]
+
+  # TODO: This is temporary to handle the transition to a standalone server
+  if ($sysadmins != []) {
+    class { 'openstack_project::server':
+      iptables_public_tcp_ports => [80, 443],
+      iptables_rules4           => $rules,
+      sysadmins                 => $sysadmins,
+    }
+  }
+
   class { '::zuul':
-    jenkins_server   => $jenkins_server,
+    jenkins_server   => $jenkins_url,
     jenkins_user     => $jenkins_user,
     jenkins_apikey   => $jenkins_apikey,
     gerrit_server    => $gerrit_server,
