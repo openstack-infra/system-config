3c8ac4c2a218c6fdf03f2636a105a8b33eedc46f
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-29 19:40:14 -0500

    Additional puppet-lint formatting
    Change-Id: I6e5fa77a301eec30cff8e16bad33a91bfd95b13f
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/etherpad_lite/manifests/init.pp
+++ modules/etherpad_lite/manifests/init.pp
@@ -46,7 +46,7 @@ class etherpad_lite (
     revision => $nodejs_version,
     require  => [
         Package['git'],
-        File["${base_install_dir}"],
+        File[$base_install_dir],
     ],
   }
 
