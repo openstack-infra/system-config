6b448852d2282f7098a66d8b005086fd6d69da4f
Clark Boylan <clark.boylan@gmail.com>
2012-11-30 17:55:48 -0800

    Handle jeepyb package dependencies.
    Let jeepyb's setup.py install its dependencies. Also, pypimirror should
    be conservative installing python-yaml.
    
    Change-Id: Icf81ddec518caede9a97c29f8c70956c03d4c992

--- modules/jeepyb/manifests/init.pp
+++ modules/jeepyb/manifests/init.pp
@@ -3,43 +3,6 @@
 class jeepyb (
   $git_source_repo = 'https://github.com/openstack-ci/jeepyb.git',
 ) {
-
-  if ! defined(Package['python-mysqldb']) {
-    package { 'python-mysqldb':
-      ensure   => present,
-    }
-  }
-
-  if ! defined(Package['python-paramiko']) {
-    package { 'python-paramiko':
-      ensure   => present,
-    }
-  }
-
-  if ! defined(Package['PyGithub']) {
-    package { 'PyGithub':
-      ensure   => latest,
-      provider => pip,
-      require  => Class['pip'],
-    }
-  }
-
-  if ! defined(Package['gerritlib']) {
-    package { 'gerritlib':
-      ensure   => latest,
-      provider => pip,
-      require  => Class['pip'],
-    }
-  }
-
-  # A lot of things need yaml, be conservative requiring this package to avoid
-  # conflicts with other modules.
-  if ! defined(Package['python-yaml']) {
-    package { 'python-yaml':
-      ensure => present,
-    }
-  }
-
   vcsrepo { '/opt/jeepyb':
     ensure   => latest,
     provider => git,
@@ -54,5 +17,4 @@ class jeepyb (
     refreshonly => true,
     subscribe   => Vcsrepo['/opt/jeepyb'],
   }
-
 }
