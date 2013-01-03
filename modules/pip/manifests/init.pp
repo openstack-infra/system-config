f983ab9785a46f48337a3ea4b84cee005f29eec3
Dan Prince <dprince@redhat.com>
2013-01-03 13:34:07 -0500

    Update pip module to support Red Hat distros.
    Adds a new pip::params module where we set parameters based on
    the operating system.
    
    Updates the pip::init module so that it makes use of distro specific
    parameters.
    
    Also, includes a change to pip::init so that a soft link is
    created for /usr/bin/pip (pointing to /usr/bin/pip-python). This
    is required in order for the Puppet pip provider to work on Red Hat
    distributions. Seems like we should push a fix into Puppet for this
    as well but having this live here for now seems reasonable.
    
    Change-Id: Ifee6bc42fabcf65ee1241ffac38f3bead7335be1

--- modules/pip/manifests/init.pp
+++ modules/pip/manifests/init.pp
@@ -1,12 +1,24 @@
 # Class: pip
 #
 class pip {
-  package { 'python-all-dev':
+  include pip::params
+
+  package { $::pip::params::python_devel_package:
     ensure => present,
   }
 
-  package { 'python-pip':
+  package { $::pip::params::python_pip_package:
     ensure  => present,
-    require => Package['python-all-dev'],
+    require => Package[$::pip::params::python_devel_package]
   }
+
+  if ($::operatingsystem == 'Redhat' or $::operatingsystem == 'Fedora') {
+
+    file { '/usr/bin/pip':
+      ensure => 'link',
+      target => '/usr/bin/pip-python',
+    }
+
+  }
+
 }
