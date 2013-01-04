d5e6f654146d87e2b4544b662acb68f9a9b1f2e8
James E. Blair <jeblair@openstack.org>
2013-01-04 09:33:56 -0800

    Add a script to install puppet.
    Follow on changes should invoke this script from
    install_jenkins slave, and launch-node.py.
    
    Change-Id: Id48c3967efe8ce4087d18975f3526564e09daecc

---
+++ install_puppet.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Copyright 2012 OpenStack Foundation.
+# Copyright 2012 Hewlett-Packard Development Company, L.P.
+# Copyright 2012 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Install puppet version 2.7.x from puppetlabs.
+# The repo and preferences files are also managed by puppet, so be sure
+# to keep them in sync with this file.
+
+if cat /etc/*release | grep "Red Hat" &> /dev/null; then
+    rpm -qi epel-release &> /dev/null || rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-6.noarch.rpm
+    yum update -y
+    # NOTE: enable the optional-rpms channel (if not already enabled)
+    # yum-config-manager --enable rhel-6-server-optional-rpms
+    yum install -y git puppet-2.7.20-1.el6.noarch
+else
+    #defaults to Ubuntu
+    cat > /etc/apt/preferences.d/00-puppet.pref <<EOF
+Package: puppet puppet-common puppetmaster puppetmaster-common
+Pin: version 2.7*
+Pin-Priority: 501
+EOF
+
+    lsbdistcodename=`lsb_release -c -s`
+    puppet_deb=puppetlabs-release-${lsbdistcodename}.deb
+    wget http://apt.puppetlabs.com/$puppet_deb -O $puppet_deb
+    dpkg -i $puppet_deb
+    rm $puppet_deb
+
+    apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get --option 'Dpkg::Options::=--force-confold' \
+        --assume-yes upgrade
+    DEBIAN_FRONTEND=noninteractive apt-get --option 'Dpkg::Options::=--force-confold' \
+        --assume-yes install -y --force-yes puppet git rubygems
+fi
