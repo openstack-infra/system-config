662c2cf3e817c4c1a56f4f4c3131dc141c64fddf
François Rossigneux <francois.rossigneux@inria.fr>
2013-01-15 19:00:59 +0100

    Add third-party library for py-rrdtool, used by kwapi.
    Change-Id: I57d789add8e0cdb4ae789b803750bc30eb0f480c

--- modules/jenkins/manifests/slave.pp
+++ modules/jenkins/manifests/slave.pp
@@ -37,6 +37,7 @@ class jenkins::slave(
     $::jenkins::params::libcurl_dev_package,
     $::jenkins::params::ldap_dev_package,
     $::jenkins::params::libsasl_dev, # for keystone ldap auth integration
+    $::jenkins::params::librrd_dev_package, # for py-rrdtool, used by kwapi
     $::jenkins::params::mysql_dev_package,
     $::jenkins::params::nspr_dev_package, # for spidermonkey, used by ceilometer
     $::jenkins::params::sqlite_dev_package,
