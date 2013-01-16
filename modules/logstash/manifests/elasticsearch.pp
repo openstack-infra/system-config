a1e501a44e2c279cee1fc2ead1452816cdc9de08
Clark Boylan <clark.boylan@gmail.com>
2013-01-16 15:01:25 -0800

    Add skeleton logstash module.
    This new logstash module adds classes to install logstash agents and
    indexers as well as redis and elasticsearch. The configuration for each
    of these services is rudimentary but it shouldn't be difficult to expand
    the configs and make them useful.
    
    Also, add a logstash.openstack.org node that will have an agent,
    indexer, web frontend, redis, and elasticsearch installed on it.
    
    Change-Id: I25b635f088f99d45cfaa70ed122c6433d3784937

---
+++ modules/logstash/manifests/elasticsearch.pp
@@ -0,0 +1,55 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to install elasticsearch.
+#
+class logstash::elasticsearch {
+  # install java runtime
+  package { 'java7-runtime-headless':
+    ensure => present,
+  }
+
+  exec { 'get_elasticsearch_deb':
+    command => 'wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.2.deb -O /tmp/elasticsearch-0.20.2.deb',
+    path    => '/bin:/usr/bin',
+    creates => '/tmp/elasticsearch-0.20.2.deb',
+  }
+
+  # install elastic search
+  package { 'elasticsearch':
+    ensure   => present,
+    source   => '/tmp/elasticsearch-0.20.2.deb',
+    provider => 'dpkg',
+    require  => [
+      Package['java7-runtime-headless'],
+      Exec['get_elasticsearch_deb'],
+    ]
+  }
+
+  file { '/etc/elasticsearch/elasticsearch.yml':
+    ensure  => present,
+    source  => 'puppet:///modules/logstash/elasticsearch.yml',
+    replace => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['elasticsearch'],
+  }
+
+  service { 'elasticsearch':
+    ensure    => running,
+    require   => Package['elasticsearch'],
+    subscribe => File['/etc/elasticsearch/elasticsearch.yml'],
+  }
+}
---
+++ modules/logstash/manifests/web.pp
@@ -0,0 +1,55 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to run logstash web front end.
+#
+class logstash::web (
+  $vhost_name = $::fqdn,
+  $serveradmin = "webmaster@${::fqdn}"
+) {
+  include apache
+  apache::vhost { $vhost_name:
+    port     => 80,
+    docroot  => 'MEANINGLESS ARGUMENT',
+    priority => '50',
+    template => 'logstash/logstash.vhost.erb',
+  }
+  a2mod { 'rewrite':
+    ensure => present,
+  }
+  a2mod { 'proxy':
+    ensure => present,
+  }
+  a2mod { 'proxy_http':
+    ensure => present,
+  }
+
+  include logstash
+
+  file { '/etc/init/logstash-web.conf':
+    ensure  => present,
+    source  => 'puppet:///modules/logstash/logstash-web.conf',
+    replace => true,
+    owner   => 'root',
+  }
+
+  service { 'logstash-web':
+    ensure    => running,
+    enable    => true,
+    require   => [
+      Class['logstash'],
+      File['/etc/init/logstash-web.conf'],
+    ]
+  }
+}
