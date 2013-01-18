b68bd6a2ac711609251a4018fe6b8c009d15bc5c
Clark Boylan <clark.boylan@gmail.com>
2013-01-18 10:45:36 -0800

    Add postgres server to Jenkins slaves.
    Add postgres servers to each non bare Jenkins slave so that tests can be
    performed against postgres.
    
    Change-Id: I374afdf2443086e2c5ad42d2e163adbf2d3df74b

--- install_modules.sh
+++ install_modules.sh
@@ -22,6 +22,7 @@ MODULES["openstackci-vcsrepo"]="0.0.8"
 MODULES["puppetlabs-apache"]="0.0.4"
 MODULES["puppetlabs-apt"]="0.0.4"
 MODULES["puppetlabs-mysql"]="0.5.0"
+MODULES["puppetlabs-postgresql"]="2.0.1"
 MODULES["saz-memcached"]="2.0.2"
 
 MODULE_LIST=`puppet module list`
