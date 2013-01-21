e08069e4646220029e3c070e97f7ac3e0b53206d
Mark McLoughlin <markmc@redhat.com>
2013-01-21 09:50:39 +0000

    Add oslo-config project
    oslo-config is the first set of APIs to graduate from oslo-incubator:
    
      https://blueprints.launchpad.net/oslo/+spec/oslo-config-package
    
    oslo-core has approval rights
    
    The repo is seeded from github:markmc/oslo-config which is the oslo-incubator
    repo translated using 'git filter-branch'.
    
    Change-Id: I07bbbf2d2c8974d5be736bcdb2511f79cb3bd8a2

--- modules/launchpad_sync/files/update_gerrit_users.py
+++ modules/launchpad_sync/files/update_gerrit_users.py
@@ -261,6 +261,7 @@ class Sync(object):
                 'openstack/openstack-planet',
                 'openstack/openstack-puppet',
                 'openstack/oslo-incubator',
+                'openstack/oslo-config',
                 'openstack/python-ceilometerclient',
                 'openstack/python-cinderclient',
                 'openstack/python-glanceclient',
