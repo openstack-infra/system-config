1b7801d6298fbcc8e365a5736e354bb5ede88f6c
Clark Boylan <clark.boylan@gmail.com>
2013-02-05 10:25:15 -0800

    Increase Jenkins slave syslog max message size.
    To better capture python tracebacks with syslog set the rsyslog
    $MaxMessageSize to 64k. This change is being made to better accomodate
    devstack-gate logging after we have stopped archiving the devstack
    screen logs.
    
    Change-Id: Ibb01f759f111d924a21c20f8521ea5db4a35e676
    Reviewed-on: https://review.openstack.org/21233
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Reviewed-by: Jeremy Stanley <fungi@yuggoth.org>
    Approved: Clark Boylan <clark.boylan@gmail.com>
    Tested-by: Jenkins

--- modules/jenkins/manifests/slave.pp
+++ modules/jenkins/manifests/slave.pp
@@ -275,4 +275,20 @@ class jenkins::slave(
 
   }
 
+  # Increase syslog message size in order to capture
+  # python tracebacks with syslog.
+  file { '/etc/rsyslog.d/99-maxsize.conf':
+    ensure  => present,
+    # Note MaxMessageSize is not a puppet variable.
+    content => '$MaxMessageSize 64k',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  service { 'rsyslog':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/rsyslog.d/99-maxsize.conf'],
+  }
 }
