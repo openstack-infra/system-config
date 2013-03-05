e3154e1400391975f8c5d016463426c6b599d7a1
Anita Kuno <akuno@lavabit.com>
2013-03-05 13:12:02 -0500

    Get openstackwatch working using puppet.
    Created an openstackwatch puppet manifest in modules/jeepyb.
    Created an openstackwatch puppet template in modules/jeepyb.
    Added a puppet class in modules/openstack_project/manifests/gerrit.pp.
    
    Change-Id: Id38cda4fdf9feee4f018bc5ccb0ebbf462311061

---
+++ modules/jeepyb/manifests/openstackwatch.pp
@@ -0,0 +1,41 @@
+# == Class: jeepyb::openstackwatch
+
+class jeepyb::openstackwatch(
+  $projects = [],
+  $mode = 'multiple',
+  $container = 'rss',
+  $feed = '',
+  $json_url = '',
+  $minute = '18',
+  $hour = '*',
+) {
+  include jeepyb
+
+  user { 'openstackwatch':
+    ensure  => present,
+    comment => 'OpenStackWatch User',
+    shell   => '/bin/bash',
+    gid     => 'openstackwatch',
+  }
+
+  cron { 'openstackwatch':
+    ensure  => present,
+    command => '/usr/local/bin/openstackwatch /home/openstackwatch/openstackwatch.ini',
+    minute  => $minute,
+    hour    => $hour,
+    user    => 'openstackwatch',
+    require => [
+      File['/home/openstackwatch/openstackwatch.ini'],
+      User['openstackwatch'],
+      Class['jeepyb'],
+    ],
+  }
+
+  file { '/home/openstackwatch/openstackwatch.ini':
+    ensure  => present,
+    content => template('openstackwatch.ini.erb'),
+    owner   => 'root',
+    group   => 'openstackwatch',
+    mode    => '0640',
+  }
+}
