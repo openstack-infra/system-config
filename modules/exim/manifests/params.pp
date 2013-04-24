2232afff79a37e7bd0f72a8beb0ea6e2773426ee
Jeremy Stanley <fungi@yuggoth.org>
2013-04-24 02:42:55 +0000

    The facter osfamily of Ubuntu is Debian.
    Clean up facter osfamily matches to just use Debian, not Ubuntu.
    This is manually tested and confirmed to at least be the case on
    Oneiric, Precise and Quantal.
    
    Change-Id: I27b184ac419910f9c3271c3b4e57886333282a5f

--- modules/exim/manifests/params.pp
+++ modules/exim/manifests/params.pp
@@ -10,14 +10,14 @@ class exim::params {
       $config_file = '/etc/exim/exim.conf'
       $conf_dir = '/etc/exim/'
     }
-    'Debian', 'Ubuntu': {
+    'Debian': {
       $package = 'exim4-daemon-light'
       $service_name = 'exim4'
       $config_file = '/etc/exim4/exim4.conf'
       $conf_dir = '/etc/exim4'
     }
     default: {
-      fail("Unsupported osfamily: ${::osfamily} The 'exim' module only supports osfamily Ubuntu or Redhat(slaves only).")
+      fail("Unsupported osfamily: ${::osfamily} The 'exim' module only supports osfamily Debian or RedHat (slaves only).")
     }
   }
 }
