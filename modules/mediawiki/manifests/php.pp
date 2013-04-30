aae7bcca225d394d2b67d79ad02463d03baef55c
Ryan Lane <rlane@wikimedia.org>
2013-04-30 18:17:15 -0500

    Add dependencies for lua and openid for mediawiki
    We have OpenID and Scribunto installed, but their deps aren't
    puppetized.
    
    Change-Id: Iff3209ce0fd5bf811bd3b931e4a83518448e43c0

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,7 +1,12 @@
 # Class: mediawiki::php
 #
 class mediawiki::php {
-  package { ['php5', 'php5-cli', 'php5-mysql', 'php-apc', 'php5-intl']:
+  package { ['php5',
+    'php5-cli',
+    'php5-mysql',
+    'php-apc',
+    'php5-intl',
+    'php-openid']:
     ensure => present,
   }
   # TODO: apc configuration
