c51b209c68d13eb4af47839adc879e4cf8a7aeac
Clark Boylan <clark.boylan@gmail.com>
2013-05-03 14:13:20 -0700

    Set number of faceting indexes to 1 in Kibana.
    * modules/kibana/files/config.rb: Kibana uses ElasticSearch's faceting
    feature to collect statistics. It defaults to performing this over all
    indexes but this is expensive so limit it to 1 index.
    
    Change-Id: I324099f88a9855062aba4905380c9dc8540555a5

--- modules/kibana/files/config.rb
+++ modules/kibana/files/config.rb
@@ -113,7 +113,7 @@ module KibanaConfig
   # Kibana will use for the "Stats" and "Terms" to prevent ES crashes. For very
   # large data sets and undersized ES clusers, a limit of 1 is not unreasonable.
   # Default is 0 (unlimited)
-  Facet_index_limit = 0
+  Facet_index_limit = 1
 
   # You probably don't want to touch anything below this line
   # unless you really know what you're doing
