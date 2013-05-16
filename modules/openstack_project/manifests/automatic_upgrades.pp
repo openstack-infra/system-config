705eb50a51940f5274e22e4edd4e2a96f6374294
Dan Prince <dprince@redhat.com>
2013-05-16 15:53:37 -0400

    Update automatic_upgrades to support RHEL/Centos.
    Updated to use the new packagekit::cron module for RHEL.
    
    This should provide us automatic package upgrades on our Centos
    jenkins slaves.
    
    Fixes LP Bug #1180948.
    
    Change-Id: Iaa67a3573e89339a81761314b662041836f2d3f2
    Reviewed-on: https://review.openstack.org/29423
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Approved: Jeremy Stanley <fungi@yuggoth.org>
    Reviewed-by: Jeremy Stanley <fungi@yuggoth.org>
    Tested-by: Jenkins

--- modules/openstack_project/manifests/automatic_upgrades.pp
+++ modules/openstack_project/manifests/automatic_upgrades.pp
@@ -6,7 +6,8 @@ class openstack_project::automatic_upgrades (
   if $::osfamily == 'Debian' {
     include unattended_upgrades
   }
-
-  #FIXME need to implement an automatic upgrades module for RHEL
+  if $::osfamily == 'RedHat' {
+    include packagekit::cron
+  }
 
 }
