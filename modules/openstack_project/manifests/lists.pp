d542d683c7732917d593501a4b79fd8d62ba231a
Stefano Maffulli <smaffulli@gmail.com>
2013-05-20 13:47:24 -0700

    Puppetizing mailing lists
    Allow for mailing lists to be created with a request for review.
    
    Change-Id: I8b3b0965119418cfc9286cd20221fe38ffd06b24

--- modules/openstack_project/manifests/lists.pp
+++ modules/openstack_project/manifests/lists.pp
@@ -1,6 +1,9 @@
 # == Class: openstack_project::lists
 #
-class openstack_project::lists($listadmins = '') {
+class openstack_project::lists(
+  $listadmins,
+  $listpassword = ''
+) {
   # Using openstack_project::template instead of openstack_project::server
   # because the exim config on this machine is almost certainly
   # going to be more complicated than normal.
@@ -8,17 +11,29 @@ class openstack_project::lists($listadmins = '') {
     iptables_public_tcp_ports => [25, 80, 465],
   }
 
+  $listdomain = 'lists.openstack.org'
+
   class { 'exim':
     sysadmin        => $listadmins,
-    mailman_domains => ['lists.openstack.org'],
+    mailman_domains => [$listdomain],
   }
 
   class { 'mailman':
-    vhost_name => 'lists.openstack.org',
+    vhost_name => $listdomain,
   }
 
   realize (
     User::Virtual::Localuser['oubiwann'],
     User::Virtual::Localuser['smaffulli'],
   )
+
+  maillist { 'openstack-it':
+    ensure      => present,
+    admin       => 'stefano@openstack.org',
+    password    => $listpassword,
+    description => 'Discussioni su OpenStack in italiano',
+    webserver   => $listdomain,
+    mailserver  => $listdomain,
+  }
+
 }
