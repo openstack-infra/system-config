8e35c3117a2813ee19b3c8053395d02bc6576e9e
daisy-ycguo <guoyingc@cn.ibm.com>
2013-06-03 16:42:53 +0800

    Disable fuzzy matching in transifex automation scripts
    Fuzzy matching can cause unexpected problems.
    So add option "--no-fuzzy-matching" to command "python setup.py update_catalog"
    in propose_translation_update and upstream_translation_update scripts.
    
    Bug 1176164
    
    Change-Id: I632d6eb9280ca11883760f17fc4363275692b38b
    Reviewed-on: https://review.openstack.org/31472
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Approved: Clark Boylan <clark.boylan@gmail.com>
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Tested-by: Jenkins

--- modules/jenkins/files/slave_scripts/propose_translation_update.sh
+++ modules/jenkins/files/slave_scripts/propose_translation_update.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -xe
 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 PROJECT=$1
 COMMIT_MSG="Imported Translations from Transifex"
 
@@ -39,7 +51,7 @@ PO_FILES=`find ${PROJECT}/locale -name '*.po'`
 if [ -n "$PO_FILES" ]
 then
     # Use updated .pot file to update translations
-    python setup.py update_catalog
+    python setup.py update_catalog --no-fuzzy-matching
 fi
 # Add all changed files to git
 git add $PROJECT/locale/*
