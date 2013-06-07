8cffd97988cf52423c0d6d17ab461d6baf3ec43b
James E. Blair <jeblair@openstack.org>
2013-06-07 08:53:27 -0700

    Reconfigure jobs for gearman plugin.
    And reconfigure zuul.openstack.org for gearman.
    
    Change-Id: I251135e3b0f2638bcc0d817dadb549c0fc2e0d2f

--- modules/openstack_project/manifests/zuul_prod.pp
+++ modules/openstack_project/manifests/zuul_prod.pp
@@ -15,27 +15,17 @@ class openstack_project::zuul_prod(
   $gearman_workers = []
 ) {
   # Turn a list of hostnames into a list of iptables rules
-  $iptables_rules6 = regsubst ($gearman_workers, '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 4730 -s \1 -j ACCEPT')
-  $iptables_rules4 = flatten ([$iptables_rules6,
-    [ "-m state --state NEW -m tcp -p tcp --dport 8001 -s ${jenkins_host} -j ACCEPT" ]])
+  $iptables_rules = regsubst ($gearman_workers, '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 4730 -s \1 -j ACCEPT')
 
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80],
-    iptables_rules6           => $iptables_rules6,
-    iptables_rules4           => $iptables_rules4,
+    iptables_rules6           => $iptables_rules,
+    iptables_rules4           => $iptables_rules,
     sysadmins                 => $sysadmins,
-  }
-
-  # co-host gearman-job-server
-  class { 'gearman':
-    listen => '::',
   }
 
   class { '::zuul':
     vhost_name           => $vhost_name,
-    jenkins_server       => $jenkins_url,
-    jenkins_user         => $jenkins_user,
-    jenkins_apikey       => $jenkins_apikey,
     gerrit_server        => $gerrit_server,
     gerrit_user          => $gerrit_user,
     zuul_ssh_private_key => $zuul_ssh_private_key,
@@ -43,6 +33,7 @@ class openstack_project::zuul_prod(
     push_change_refs     => false,
     status_url           => 'http://status.openstack.org/zuul/',
     statsd_host          => $statsd_host,
+    revision             => 'feature/gearman',
   }
 
   file { '/etc/zuul/layout.yaml':
@@ -50,14 +41,22 @@ class openstack_project::zuul_prod(
     source => 'puppet:///modules/openstack_project/zuul/layout.yaml',
     notify => Exec['zuul-reload'],
   }
+
   file { '/etc/zuul/openstack_functions.py':
     ensure => present,
     source => 'puppet:///modules/openstack_project/zuul/openstack_functions.py',
     notify => Exec['zuul-reload'],
   }
+
   file { '/etc/zuul/logging.conf':
     ensure => present,
     source => 'puppet:///modules/openstack_project/zuul/logging.conf',
+    notify => Exec['zuul-reload'],
+  }
+
+  file { '/etc/zuul/gearman-logging.conf':
+    ensure => present,
+    source => 'puppet:///modules/openstack_project/zuul/gearman-logging.conf',
     notify => Exec['zuul-reload'],
   }
 
