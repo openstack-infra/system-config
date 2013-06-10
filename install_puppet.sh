42078ca25abb30a5dd1181b9ab83ac6c302266e0
Dan Prince <dprince@redhat.com>
2013-06-10 14:50:50 -0400

    Add support for Fedora 18 to install_puppet.sh.
    Fedora 18 uses puppet2.7 natively so we use that and gem install hiera.
    
    Also updates our openstack_project:base manifest so that we upgrade
    hiera via the gem provider.
    
    Change-Id: I2ad8c1a81f9955f6de7812f72075fae84f7f7d7f
    NOTE: puppetlabs only provides puppet > 3 for Fedora 18 anyway.
    Reviewed-on: https://review.openstack.org/32449
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Approved: Clark Boylan <clark.boylan@gmail.com>
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Tested-by: Jenkins

--- install_puppet.sh
+++ install_puppet.sh
@@ -20,7 +20,19 @@
 # The repo and preferences files are also managed by puppet, so be sure
 # to keep them in sync with this file.
 
-if cat /etc/*release | grep -e "CentOS" -e "Red Hat" &> /dev/null; then
+if cat /etc/*release | grep -e "Fedora" &> /dev/null; then
+
+    yum update -y
+
+    # NOTE: we preinstall lsb_release to ensure facter sets lsbdistcodename
+    yum install -y redhat-lsb-core git puppet
+
+    gem install hiera hiera-puppet
+
+    mkdir -p /etc/puppet/modules/
+    ln -s /usr/local/share/gems/gems/hiera-puppet-* /etc/puppet/modules/
+
+elif cat /etc/*release | grep -e "CentOS" -e "Red Hat" &> /dev/null; then
     rpm -qi epel-release &> /dev/null || rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
     rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-6.noarch.rpm
 
