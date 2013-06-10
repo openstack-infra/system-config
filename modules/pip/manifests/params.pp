89c9324edb94b93337797f6d956fd041580947c1
Dan Prince <dprince@redhat.com>
2013-06-10 14:55:22 -0400

    Add pip::python3 module and pip3 provider.
    Adds python3 support to our pip package. Also adds a custom version
    of the pip package provider which can be used when installing
    things w/ python3-pip is explicitly required. For example:
    
      package { 'tox',
        provider => pip3,
        require  => Class[pip::python3],
      }
    
    Change-Id: I1b563c8f7e647dad3a134ef1f0ec5b45b63f8b71

--- modules/pip/manifests/params.pp
+++ modules/pip/manifests/params.pp
@@ -7,10 +7,14 @@ class pip::params {
     'RedHat': {
       $python_devel_package = 'python-devel'
       $python_pip_package   = 'python-pip'
+      $python3_devel_package = 'python3-devel'
+      $python3_pip_package   = 'python3-pip'
     }
     'Debian': {
-      $python_devel_package = 'python-all-dev'
-      $python_pip_package   = 'python-pip'
+      $python_devel_package  = 'python-all-dev'
+      $python_pip_package    = 'python-pip'
+      $python3_devel_package = 'python3-all-dev'
+      $python3_pip_package   = 'python3-pip'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} The 'pip' module only supports osfamily Debian or RedHat.")
