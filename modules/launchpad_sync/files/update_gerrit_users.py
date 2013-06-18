18f535bf1e35fea4d457ec68f62cd1602c2fcc4b
Mark McLoughlin <markmc@redhat.com>
2013-06-18 10:37:37 +0100

    Add oslo.messaging
    oslo.messaging is a new project to move the RPC code out of
    oslo-incubator with a re-designed API. We hope to have it ready for the
    Havana release.
    
    See:
    
      https://blueprints.launchpad.net/oslo/+spec/messaging-api
      https://wiki.openstack.org/wiki/Oslo/Messaging
    
    Include the project with an identical configuration to oslo.config.
    
    Change-Id: Iec6590249e6eb87c974de4c7f7f768ead672efa4

--- modules/launchpad_sync/files/update_gerrit_users.py
+++ modules/launchpad_sync/files/update_gerrit_users.py
@@ -262,6 +262,7 @@ class Sync(object):
                 'openstack/openstack-puppet',
                 'openstack/oslo-incubator',
                 'openstack/oslo.config',
+                'openstack/oslo.messaging',
                 'openstack/python-ceilometerclient',
                 'openstack/python-cinderclient',
                 'openstack/python-glanceclient',
