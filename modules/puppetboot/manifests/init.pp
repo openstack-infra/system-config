bf4c5f7d884d604da74f150083d39a159aa878e9
Jeremy Stanley <fungi@yuggoth.org>
2013-06-22 01:40:32 +0000

    Fedora and Debian don't use Upstart files.
    * modules/puppetboot/manifests/init.pp: The Upstart conf file will
    fail to apply on systems which lack an /etc/init directory, such as
    Fedora or Debian.
    
    Change-Id: Ibefe69fd30c9c7e0320705276e151472e581d878

--- modules/puppetboot/manifests/init.pp
+++ modules/puppetboot/manifests/init.pp
@@ -1,11 +1,13 @@
 # == Class: puppetboot
 #
 class puppetboot($ensure=present) {
-  file {'/etc/init/puppetboot.conf':
-    ensure => $ensure,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/puppetboot/puppetboot.conf',
+  if ($::operatingsystem in ['CentOS', 'RedHat', 'Ubuntu']) {
+    file {'/etc/init/puppetboot.conf':
+      ensure => $ensure,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => 'puppet:///modules/puppetboot/puppetboot.conf',
+    }
   }
 }
