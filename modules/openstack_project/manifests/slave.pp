6f7d12e248969523adae861f731e9a1b41b305f7
Jeremy Stanley <fungi@yuggoth.org>
2013-06-22 01:08:41 +0000

    Add a source for Python 3.3 on Precise if needed.
    * modules/openstack_project/manifests/slave.pp: If $python3 is set
    add a Python 3.3 PPA for Ubuntu 12.04 LTS, maintained by Chuck Short
    with Canonical.
    
    Change-Id: I6c6e2682e9c1d56698cfbce4f01c81b60eac674e

--- modules/openstack_project/manifests/slave.pp
+++ modules/openstack_project/manifests/slave.pp
@@ -14,6 +14,9 @@ class openstack_project::slave (
     certname                  => $certname,
     sysadmins                 => $sysadmins,
   }
+  if ($python3 == true and $::lsbdistcodename == 'precise') {
+    apt::ppa { 'ppa:zulcss/py3k': }
+  }
   class { 'jenkins::slave':
     bare    => $bare,
     ssh_key => $ssh_key,
