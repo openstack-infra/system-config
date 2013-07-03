681a79209c6b303778e87f7016606ba9cba8e42b
Clark Boylan <clark.boylan@gmail.com>
2013-07-03 10:47:21 -0700

    Update review-dev to 2.4.4.
    * modules/openstack_project/manifests/review_dev.pp: Install
    http://tarballs.openstack.org/ci/test/gerrit-2.4.4-14-gab7f4c1.war on
    review-dev to test the new 2.4.4 patches.
    
    Change-Id: Ie17b9a2c96720892e4140b36c2ebf81fb2f3e058
    Reviewed-on: https://review.openstack.org/35520
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Approved: Jeremy Stanley <fungi@yuggoth.org>
    Reviewed-by: Jeremy Stanley <fungi@yuggoth.org>
    Tested-by: Jenkins

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -39,7 +39,7 @@ class openstack_project::review_dev (
     ssh_project_rsa_pubkey_contents => $ssh_project_rsa_pubkey_contents,
     email                           => 'review-dev@openstack.org',
     war                             =>
-      'http://tarballs.openstack.org/ci/gerrit-2.4.2-17-g19507d3.war',
+      'http://tarballs.openstack.org/ci/test/gerrit-2.4.4-14-gab7f4c1.war',
     contactstore                    => true,
     contactstore_appsec             => $contactstore_appsec,
     contactstore_pubkey             => $contactstore_pubkey,
