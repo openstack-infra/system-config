71726d834ad363f85ae97a553663635e06bff18b
Mark McLoughlin <markmc@redhat.com>
2013-07-08 13:38:14 +0100

    Only skip openstack tarballs in requirements check
    We really only want to allow openstack tarball requirements to be added
    without checking, not just any tarball.
    
    Change-Id: I0ce458c95dc2fba07100b921d094ff174ed7da57
    Reviewed-on: https://review.openstack.org/36070
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Reviewed-by: Khai Do <zaro0508@gmail.com>
    Approved: Clark Boylan <clark.boylan@gmail.com>
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Tested-by: Jenkins

--- modules/jenkins/files/slave_scripts/project-requirements-change.py
+++ modules/jenkins/files/slave_scripts/project-requirements-change.py
@@ -44,7 +44,7 @@ class RequirementsList(object):
             line = line[:line.find('#')]
             line = line.strip()
             if (not line or
-                line.startswith('http') or
+                line.startswith('http://tarballs.openstack.org/') or
                 line.startswith('-e') or
                 line.startswith('-f')):
                 continue
