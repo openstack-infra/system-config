f7f2760fe005926cc676d50f75ba8f068325b51d
James E. Blair <jeblair@openstack.org>
2013-07-09 10:53:27 -0700

    Set the Zuul/Jenkins base log URL in a function
    And use that when constructing log paths and URLs.  Use a substr
    of the change id or commit sha when constructing URLs so the log
    directories are deeper.
    
    Make copying the test results a macro (it's used in several places).
    
    Update the gearman log client to take advantage of the new parameter.
    
    Requires https://review.openstack.org/#/c/36304/
    
    Change-Id: I64faa35eddc4105271efa3de4f83b608b77655c2

--- modules/openstack_project/files/zuul/openstack_functions.py
+++ modules/openstack_project/files/zuul/openstack_functions.py
@@ -1,1 +1,10 @@
-# No content currently.  In repo for ease of future use.
+def set_log_url(item, params):
+    if hasattr(item.change, 'refspec'):
+        path = "%s/%s/%s/%s" % (
+            params['ZUUL_CHANGE'][-2:], params['ZUUL_CHANGE'],
+            params['ZUUL_PATCHSET'], params['ZUUL_PIPELINE'])
+    else:
+        path = "%s/%s/%s" % (
+            params['ZUUL_NEWREV'][:2], params['ZUUL_NEWREV'],
+            params['ZUUL_PIPELINE'])
+    params['BASE_LOG_PATH'] = path
