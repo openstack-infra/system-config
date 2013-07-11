52732dd55072159a3d7c3851e695a0242ea4f8f0
Elizabeth Krumbach <lyz@princessleia.com>
2013-07-11 11:21:02 -0700

    Add cgit web service and git server
    Define git.openstack.org server and deploy cgit web service with
    Apache on CentOS.
    
    Change-Id: Id3c7c870e25e4202915bc081454896895084f9af

---
+++ doc/source/git.rst
@@ -0,0 +1,28 @@
+:title: Git
+
+.. _git:
+
+Git
+########
+
+The web frontend cgit is running on git.openstack.org.
+
+At a Glance
+===========
+
+:Hosts:
+  * http://git.openstack.org
+:Puppet:
+  * :file:`modules/cgit`
+  * :file:`modules/openstack_project/manifests/git.pp`
+:Projects:
+  * http://git.zx2c4.com/cgit/
+:Bugs:
+  * http://bugs.launchpad.net/openstack-ci
+  * http://lists.zx2c4.com/mailman/listinfo/cgit
+
+Overview
+========
+
+Apache is running on a CentOS 6 system with the EPEL repository that includes
+the cgit packages.
---
+++ modules/cgit/manifests/init.pp
@@ -0,0 +1,76 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class: cgit
+#
+class cgit {
+
+  include apache
+
+  package { [
+      'cgit',
+      'git-daemon',
+    ]:
+    ensure => present,
+  }
+
+  service { 'httpd':
+    ensure     => running,
+    require    => Package['httpd'],
+  }
+
+  user { 'cgit':
+    ensure     => present,
+    home       => '/home/cgit',
+    shell      => '/bin/bash',
+    gid        => 'cgit',
+    managehome => true,
+    require    => Group['cgit'],
+  }
+
+  group { 'cgit':
+    ensure => present,
+  }
+
+  file {'/home/cgit':
+    ensure  => directory,
+    owner   => 'cgit',
+    group   => 'cgit',
+    mode    => '0755',
+    require => User['cgit'],
+  }
+
+  file { '/var/lib/git':
+    ensure  => directory,
+    owner   => 'cgit',
+    group   => 'cgit',
+    mode    => '0644',
+  }
+
+  exec { 'restorecon -R -v /var/lib/git':
+    path      => '/sbin',
+    subscribe => Folder['/var/lib/git']
+  }
+
+  selboolean { 'httpd_enable_cgi':
+    persistent => true,
+    value      => on
+  }
+
+  file { '/etc/httpd/conf.d/cgit.conf':
+    ensure  => present,
+    source  => 'puppet:///modules/cgit/cgit.conf',
+    mode    => '0644'
+  }
+}
