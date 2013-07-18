7adb740d8ff42a16886320e00526714ea0151b5e
Elizabeth Krumbach <lyz@princessleia.com>
2013-07-18 15:19:07 -0700

    Add replication of git from gerrit to git.o.o
    Modify gerrit's git replication configuration so that it
    pulls in from a list of replication targets defined in
    puppet rather than individually added stanzas.
    
    Pull the replicate_github variable from files, since it
    is no longer required.
    
    The replicate_local variable remains because it's used
    in the apache configuration and for setup of the local
    replication space for git.
    
    Also add the cgit server to the list of servers.
    
    Change-Id: I68de89bb216565f1754eb9b192bd437adcbf768b

--- modules/openstack_project/manifests/review.pp
+++ modules/openstack_project/manifests/review.pp
@@ -53,7 +53,6 @@ class openstack_project::review (
   $lp_sync_secret='',
   $contactstore_appsec='',
   $contactstore_pubkey='',
-  $replicate_github=true,
   $sysadmins = [],
   $swift_username = '',
   $swift_password = ''
@@ -102,7 +101,6 @@ class openstack_project::review (
     mysql_root_password             => $mysql_root_password,
     trivial_rebase_role_id          => 'trivial-rebase@review.openstack.org',
     email_private_key               => $email_private_key,
-    replicate_github                => $replicate_github,
     sysadmins                       => $sysadmins,
     swift_username                  => $swift_username,
     swift_password                  => $swift_password,
