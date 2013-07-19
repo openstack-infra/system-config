9bcb8c2695941f1b172892ed58428ba57cdf6f15
Elizabeth Krumbach <lyz@princessleia.com>
2013-07-19 11:52:31 -0700

    Fix jeepyb class issue on git.o.o
    Of course jeepyb::git doesn't really exist, so class failed in
    initial attmept to fix missing variables. Moved variables and
    comment to an existing class.
    
    Change-Id: I172588eae4b03427f1ee7862073cd50ca4e5a14e

--- modules/openstack_project/manifests/git.pp
+++ modules/openstack_project/manifests/git.pp
@@ -18,16 +18,12 @@
 class openstack_project::git (
   $sysadmins = [],
   $git_gerrit_ssh_key = '',
+  $local_git_dir = '/var/lib/git',
+  $ssh_project_key = '',
 ) {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80, 9418],
     sysadmins                 => $sysadmins,
-  }
-
-  # We don't actually use these, but jeepyb requires them.
-  class {'jeepyb::git':
-    local_git_dir   => '/var/lib/git',
-    ssh_project_key => '',
   }
 
   include cgit
