ab67af9569f083349651b3d2e1611217bc71bf4b
Jeremy Stanley <fungi@yuggoth.org>
2013-07-22 06:02:25 +0000

    Conditionally install Python 3 develoment libs.
    * modules/pip/manifests/python3.pp: The conditional check introduced
    in the Jenkins slave manifest in commit 3c7af7d was only a half-fix,
    so depending on the order in which it's puppeted the conflict could
    still arise. This similarly checks within the pip provider to make
    sure Python 3 development libs are only installed via distribution
    packaging if they're not already defined elsewhere.
    
    Change-Id: If22aaa1319b61aa58306036a03843fa0cc79b34c

--- modules/pip/manifests/python3.pp
+++ modules/pip/manifests/python3.pp
@@ -3,8 +3,10 @@
 class pip::python3 {
   include pip::params
 
-  package { $::pip::params::python3_devel_package:
-    ensure => present,
+  if ! defined(Package[$::pip::params::python3_devel_package]) {
+    package { $::pip::params::python3_devel_package:
+      ensure => present,
+    }
   }
 
   package { $::pip::params::python3_pip_package:
