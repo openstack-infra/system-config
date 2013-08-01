12ced38ca77715fb5974cb4c3f3fd28650b951ed
Dan Bode <bodepd@gmail.com>
2013-07-31 17:22:56 -0700

    Ensure python pip is installed before zuul
    Previously, installing zuul resulted in the erros:
    
      /usr/bin/python: No module named pip
    
    This commit ensures that python-pip is always installed
    before zuul is installed so that it can install
    correctly.
    
    Change-Id: I6d73a67e1fbb9eb077f39f99c45ceb4a00bc652b

--- modules/zuul/manifests/init.pp
+++ modules/zuul/manifests/init.pp
@@ -36,6 +36,7 @@ class zuul (
   $statsd_host = ''
 ) {
   include apache
+  include pip
 
   $packages = [
     'python-webob',
@@ -93,6 +94,7 @@ class zuul (
     path        => '/bin:/usr/bin',
     refreshonly => true,
     subscribe   => Vcsrepo['/opt/zuul'],
+    require     => Class['pip'],
   }
 
   file { '/etc/zuul':
