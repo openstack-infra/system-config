ceb405b4bbff0d94102b7d2ee966bb0c93a3e190
Elizabeth Krumbach <lyz@princessleia.com>
2013-08-07 21:22:40 -0700

    Fix ssl location for git.o.o on CentOS
    Attempted to put Apache ssl certificates for git.o.o into the
    /etc/ssl/ and /etc/ssl/ directories, but this is a bit of a
    Debianism. Updating to RedHat style /etc/pki/tls directories.
    
    Change-Id: Icb83ee2601d29ad679e7c8503a406faeb5153727

--- modules/openstack_project/manifests/git.pp
+++ modules/openstack_project/manifests/git.pp
@@ -31,9 +31,9 @@ class openstack_project::git (
   include pip
 
   class { '::cgit':
-    ssl_cert_file           => '/etc/ssl/certs/git.openstack.org.pem',
-    ssl_key_file            => '/etc/ssl/private/git.openstack.org.key',
-    ssl_chain_file          => '/etc/ssl/certs/intermediate.pem',
+    ssl_cert_file           => '/etc/pki/tls/certs/git.openstack.org.pem',
+    ssl_key_file            => '/etc/pki/tls/private/git.openstack.org.key',
+    ssl_chain_file          => '/etc/pki/tls/certs/intermediate.pem',
     ssl_cert_file_contents  => $ssl_cert_file_contents,
     ssl_key_file_contents   => $ssl_key_file_contents,
     ssl_chain_file_contents => $ssl_chain_file_contents,
