68fc371ced7a3e80d32777ea745b3d6c9c89b5b2
Clark Boylan <clark.boylan@gmail.com>
2013-08-13 14:11:23 -0700

    Switch etherpad_lite backups to mysql_backup.
    * .../openstack_project/manifests/etherpad.pp:
    Use the new mysql_backup module to backup the etherpad DB. Note that
    this change is not sufficient to completely clean out the old logrotate
    config and cron for the old etherpad DB backups. This should be merged
    in a coordinated manner where the other logrotate configs and cron are
    removed by hand.
    
    Change-Id: Ide60ce13a454f72865d697ee736beada76850f32

--- modules/openstack_project/manifests/etherpad_dev.pp
+++ modules/openstack_project/manifests/etherpad_dev.pp
@@ -19,7 +19,11 @@ class openstack_project::etherpad_dev (
     ep_headings    => true
   }
 
-  include etherpad_lite::backup
+  include mysql_backup
+  mysql_backup::backup { 'etherpad-lite':
+    $dest   => "${::etherpad_lite::base_log_dir}/${::etherpad_lite::ep_user}/db.sql.gz",
+    require => Class['etherpad_lite'],
+  }
 
   class { 'etherpad_lite::apache':
     ssl_cert_file  => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
