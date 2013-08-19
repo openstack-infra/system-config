9930d690c79f969b6c51ba87a314d9d2ee6c6433
Marton Kiss <marton.kiss@gmail.com>
2013-08-19 12:37:17 +0200

    Add groups-staging.openstack.org node
    Add a groups-staging node to openstack infra, based on drupal environment. This node
    checks out the https://github.com/openstack-infra/groups repository and builds a
    new deployment from scratch, including drush make distbuild and drush si auto-installation
    features.
    
    Change-Id: I71eef1c14724ceb13a896ff768923148dedcc2ef

---
+++ modules/drupal/manifests/sitedeploy.pp
@@ -0,0 +1,89 @@
+# == Define: sitedeploy
+#
+# define to deploy drupal site from distribution tarball
+#
+define drupal::sitedeploy (
+  $site_docroot = undef,
+  $site_staging_root = undef,
+  $site_staging_tarball = undef,
+  $site_deploy_flagfile = undef,
+  $site_name = undef,
+  $site_profile = undef,
+  $site_mysql_user = undef,
+  $site_mysql_password = undef,
+  $site_mysql_host = undef,
+  $site_mysql_database = undef,
+) {
+  exec { drupal-extract-tarball:
+    path        => '/usr/bin:/bin',
+    cwd         => $site_docroot,
+    command     => "tar xfz ${site_staging_root}/${$site_staging_tarball} --strip-components=1",
+    creates     => "${site_deploy_flagfile}",
+  }
+
+  exec { drupal-create-config:
+    path        => '/usr/bin:/bin',
+    cwd         => $site_docroot,
+    command     => "cp sites/default/default.settings.php sites/default/settings.php",
+    creates     => "${site_deploy_flagfile}",
+    require     => Exec['drupal-extract-tarball'],
+  }
+
+  exec { drupal-bulk-set-permissions:
+    path        => '/usr/bin:/bin',
+    cwd         => $site_docroot,
+    command     => "chown -R nobody:www-data * &&
+      find * -type d -exec chmod 0755 {} \\;&& \
+      find * -type f -exec chmod 0644 {} \\;",
+    creates     => "${site_deploy_flagfile}",
+    require     => Exec['drupal-create-config'],
+  }
+
+  file { "${site_docroot}/sites/default/settings.php":
+    ensure  => file,
+    mode    => 664,
+    require => Exec['drupal-bulk-set-permissions'],
+  }
+
+  file { "${site_docroot}/sites/default/files":
+    owner   => 'nobody',
+    group   => 'www-data',
+    mode    => 775,
+    ensure  => directory,
+    require => Exec['drupal-bulk-set-permissions'],
+  }
+
+  exec { drupal-site-install:
+    path        => '/usr/bin:/bin',
+    cwd         => $site_docroot,
+    command     => "drush si -y ${site_profile} --db-url=mysql://${$site_mysql_user}:${site_mysql_password}@${site_mysql_host}/${$site_mysql_database}",
+    creates     => "${site_deploy_flagfile}",
+    require     => [ File["${site_docroot}/sites/default/settings.php"], 
+      File["${site_docroot}/sites/default/files"], 
+      Exec['drupal-bulk-set-permissions'] 
+    ],
+  }
+
+  exec { drupal-site-post-install-sitename:
+    path        => '/usr/bin:/bin',
+    cwd         => $site_docroot,
+    command     => "drush vset site_name ${site_name}",
+    creates     => "${site_deploy_flagfile}",
+    require     => Exec['drupal-site-install'],
+  }
+
+  exec { drupal-site-post-install-admin-password:
+    path        => '/usr/bin:/bin',
+    cwd         => $site_docroot,
+    command     => "drush upwd admin --password=\"${site_admin_password}\"",
+    creates     => "${site_deploy_flagfile}",
+    require     => Exec['drupal-site-install'],
+  }
+
+  exec { drupal-site-post-install-done:
+    path        => '/usr/bin:/bin',
+    command     => "touch ${site_deploy_flagfile}",
+    creates     => "${site_deploy_flagfile}",
+    require     => Exec['drupal-site-post-install-admin-password'],
+  }
+}
