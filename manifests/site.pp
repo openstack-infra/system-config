7e66552ca15936a7938cd57f34c49f696b0d9997
Alex Gaynor <alex.gaynor@gmail.com>
2013-08-19 10:45:47 -0700

    Add support for running tox with PyPy and use it for Marconi
    This works by installing PyPy from the Ubuntu PPA, and then invoking
    `tox -e pypy` (similar to all the other Python buidlers). PyPy shares
    nodes with the Python3 builders.
    
    Change-Id: Ibf683c1995b0f5c04c93c56010befb222b386d7a

--- manifests/site.pp
+++ manifests/site.pp
@@ -507,9 +507,10 @@ node /^precisepy3k-?\d+.*\.slave\.openstack\.org$/ {
   include openstack_project
   include openstack_project::puppet_cron
   class { 'openstack_project::slave':
-    ssh_key   => $openstack_project::jenkins_ssh_key,
-    sysadmins => hiera('sysadmins'),
-    python3   => true,
+    ssh_key      => $openstack_project::jenkins_ssh_key,
+    sysadmins    => hiera('sysadmins'),
+    python3      => true,
+    include_pypy => true,
   }
 }
 
@@ -517,9 +518,10 @@ node /^precisepy3k-dev\d+.*\.slave\.openstack\.org$/ {
   include openstack_project
   include openstack_project::puppet_cron
   class { 'openstack_project::slave':
-    ssh_key   => $openstack_project::jenkins_dev_ssh_key,
-    sysadmins => hiera('sysadmins'),
-    python3   => true,
+    ssh_key      => $openstack_project::jenkins_dev_ssh_key,
+    sysadmins    => hiera('sysadmins'),
+    python3      => true,
+    include_pypy => true,
   }
 }
 
