f9f81b3159d07c97404c498422b7260cd4c8a67c
Robert Collins <rbtcollins@hp.com>
2013-08-29 21:38:34 +1200

    Initial bootstrap configuration.
    Just enough to get a puppetmaster up, I hope.
    
    Change-Id: Ibcc3b6863057eb526fd2c65db30df21c832f7674

---
+++ modules/testcabal_project/manifests/template.pp
@@ -0,0 +1,38 @@
+# == Class: testcabal_project::template
+#
+# A template host with no running services
+#
+class testcabal_project::template (
+  $iptables_public_tcp_ports = [],
+  $iptables_public_udp_ports = [],
+  $iptables_rules4           = [],
+  $iptables_rules6           = [],
+  $install_users = true,
+  $certname = $::fqdn
+) {
+  include ssh
+  include snmpd
+  include testcabal_project::automatic_upgrades
+
+  class { 'iptables':
+    public_tcp_ports => $iptables_public_tcp_ports,
+    public_udp_ports => $iptables_public_udp_ports,
+    rules4           => $iptables_rules4,
+    rules6           => $iptables_rules6,
+  }
+
+  class { 'ntp': }
+
+  class { 'testcabal_project::base':
+    install_users => $install_users,
+    certname      => $certname,
+  }
+
+  package { 'strace':
+    ensure => present,
+  }
+
+  package { 'tcpdump':
+    ensure => present,
+  }
+}
