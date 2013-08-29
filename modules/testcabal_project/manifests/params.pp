ff2d45f7cffc01a42fee6763d96fd5a9018ac118
Robert Collins <rbtcollins@hp.com>
2013-08-29 20:59:18 +1200

    Port across params and puppet_cron.
    The bottom of the dependency stack.
    
    Change-Id: I48bdcb02a833a866f414c0bdc0e4eaef7fcd55a8

---
+++ modules/testcabal_project/manifests/params.pp
@@ -0,0 +1,21 @@
+# Class: testcabal_project::params
+#
+# This class holds parameters that need to be
+# accessed by other classes.
+class testcabal_project::params {
+  case $::osfamily {
+    'RedHat': {
+      $packages = ['puppet', 'python-setuptools', 'wget']
+      $user_packages = ['byobu', 'emacs-nox']
+      $update_pkg_list_cmd = ''
+    }
+    'Debian': {
+      $packages = ['puppet', 'python-setuptools', 'wget']
+      $user_packages = ['byobu', 'emacs23-nox']
+      $update_pkg_list_cmd = 'apt-get update >/dev/null 2>&1;'
+    }
+    default: {
+      fail("Unsupported osfamily: ${::osfamily} The 'testcabal_project' module only supports osfamily Debian or RedHat (slaves only).")
+    }
+  }
+}
