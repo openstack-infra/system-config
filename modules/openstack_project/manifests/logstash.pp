2bc15db570e5ee7a85d4221b5fcf02135082546d
Clark Boylan <clark.boylan@gmail.com>
2013-09-19 16:11:28 -0700

    Deploy elastic-recheck on logstash.openstack.org.
    This elastic recheck thing that classifies failures based on logstash
    queries is awesome. We should run it on the OpenStack developer
    infrastructure. Deploy it on logstash.o.o to keep related services
    together.
    
    Change-Id: I16ae7a32c2c48474454df17931779d0f60c3ed85

--- modules/openstack_project/manifests/logstash.pp
+++ modules/openstack_project/manifests/logstash.pp
@@ -15,6 +15,12 @@
 # Logstash web frontend glue class.
 #
 class openstack_project::logstash (
+  $gerrit_host,
+  $gerrit_ssh_private_key,
+  $gerrit_ssh_private_key_contents,
+  #not used today, will be used when elastic-recheck supports it.
+  $elasticsearch_url,
+  $recheck_bot_passwd,
   $elasticsearch_nodes = [],
   $gearman_workers = [],
   $discover_nodes = ['elasticsearch.openstack.org:9200'],
@@ -108,5 +114,13 @@ class openstack_project::logstash (
       'notifempty',
     ],
     require => Service['jenkins-log-client'],
+  }
+
+  class { 'elastic_recheck':
+    gerrit_host                     => $gerrit_host,
+    gerrit_ssh_private_key          => $gerrit_ssh_private_key,
+    gerrit_ssh_private_key_contents => $gerrit_ssh_private_key_contents,
+    elasticsearch_url               => $elasticsearch_url,
+    recheck_bot_passwd              => $recheck_bot_passwd,
   }
 }
