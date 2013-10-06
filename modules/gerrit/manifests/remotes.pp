a4f747561e0d469c509a13b75bc6f2f2b7905003
Monty Taylor <mordred@inaugust.com>
2013-10-06 13:41:53 -0400

    Update projects.yaml for new upstream tracking
    jeepyb gets a new upstream tracking syntax in
    https://review.openstack.org/#/c/35535/
    
    Switch to use it.
    
    The new syntax will operate via git push, so that changes to upstream
    can trigger zuul events.
    
    Change-Id: Ideca999aca0e8583cce9a1227089243216175158

--- modules/gerrit/manifests/remotes.pp
+++ modules/gerrit/manifests/remotes.pp
@@ -5,8 +5,14 @@ class gerrit::remotes($ensure=present) {
       ensure  => $ensure,
       user    => 'gerrit2',
       minute  => '*/30',
-      command => 'sleep $((RANDOM\%60+90)) && /usr/local/bin/fetch-remotes',
-      require => Class['jeepyb'],
+      command => 'sleep $((RANDOM\%60+90)) && /usr/local/bin/manage-projects',
+      require => [Class['jeepyb'], File['/var/lib/jeepyb']],
+    }
+
+    file { '/var/lib/jeepyb':
+      ensure  => directory,
+      owner   => 'gerrit2',
+      require => User['gerrit2'],
     }
 
     file { '/home/gerrit2/remotes.config':
