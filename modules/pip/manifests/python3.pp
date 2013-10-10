e6ed97f0439f067229e7ebf90c712d3f7199528e
Monty Taylor <mordred@inaugust.com>
2013-10-10 14:17:16 -0400

    Make pip install from upstream better
    Combining the installation of things from pip and from distro packages
    leads to death and destruction as per upstream. Eventually, we'll have
    distro pacakges for everything. Until then, there is no need to install
    pip and setuptools themselves from distro packages, since we are going
    to turn right around and use them to install things from not-distro
    pacakges.
    
    To avoid the pain, follow the advice of pip/setuptools upstream, and
    install setuptools and pip from ez_setup.py and get-pip.py.
    
    patch includes syntax fixes , dealing with wget
    updated python3 refrences where it looks to be python2, fix to work behind proxy
    
    Change-Id: I0f96fa59e59cf507b83c5e52926a90eabe0dd1c3

--- modules/pip/manifests/python3.pp
+++ modules/pip/manifests/python3.pp
@@ -1,15 +1,37 @@
-# Class: pip
+# Class: pip::python3
 #
 class pip::python3 {
   include pip::params
+  pip::bootstrap{'python3':}
 
   package { $::pip::params::python3_devel_package:
     ensure => present,
   }
 
   package { $::pip::params::python3_pip_package:
-    ensure  => present,
-    require => Package[$::pip::params::python3_devel_package]
+    ensure  => absent,
   }
 
+  package { $::pip::params::python3_setuptools_package:
+    ensure => absent,
+  }
+
+  exec { 'install_setuptools':
+    command   => 'python /var/lib/ez_setup.py',
+    path      => '/bin:/usr/bin',
+    subscribe => Exec['get_ez_setup for python3'],
+    creates   => $::pip::params::setuptools_pth,
+    require   => [
+      Package[$::pip::params::python3_devel_package],
+      Pip::Bootstrap['python3'],
+    ],
+  }
+
+  exec { 'install_pip':
+    command   => 'python /var/lib/python-install/get-pip.py',
+    path      => '/bin:/usr/bin',
+    subscribe => Exec['get_get_pip for python3'],
+    creates   => $::pip::params::pip_executable,
+    require   => Exec['install_setuptools'],
+  }
 }
