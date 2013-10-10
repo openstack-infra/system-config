9119772516c0854ba6f51d23cc62e4ab4ab5e5d4
Monty Taylor <mordred@inaugust.com>
2013-10-10 14:17:16 -0400

    Make pip install from upstream better
    Combining the installation of things from pip and from distro packages
    leads to death and destruction as per upstream. Eventually, we'll have
    distro pacakges for everything. Until then, there is no need to install
    pip and setuptools themselves from distro packages, since we are going
    to turn right around and use them to install things from not-distro
    pacakges.
    
    To avoid the pain, follow the advice of pip/setuptools upstream, and
    install setuptools and pip from ez_setup.py and get-pip.py.
    
    patch includes syntax fixes , dealing with wget
    updated python3 refrences where it looks to be python2, fix to work behind proxy
    
    Change-Id: I0f96fa59e59cf507b83c5e52926a90eabe0dd1c3

---
+++ modules/openstack_project/manifests/base_install_package.pp
@@ -0,0 +1,13 @@
+# == define: openstack_project::base_install_package
+#
+define openstack_project::base_install_package(
+  $package_name = $title,
+) 
+{
+  if ! defined(Package[$package_name])
+  {
+    package { $package_name:
+      ensure => present
+    }
+  }
+}
