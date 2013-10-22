237dbd80be000f3ec3daa61c6a912d590481a89d
Chris Krelle <nobodycam@gmail.com>
2013-10-22 15:37:21 -0700

    Add Ironic to eavesdrop.
    Add Ironic to the eavesdrop logging list so that Ironic logs are
    saved.
    
    Change-Id: I0d624c49f84a02cb593555b4374d11a7f849f1b4

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -26,6 +26,7 @@ class openstack_project::eavesdrop (
         '#openstack',
         '#openstack-dev',
         '#openstack-infra',
+        '#openstack-ironic',
         '#openstack-marconi',
         '#openstack-meeting',
         '#openstack-meeting-alt',
