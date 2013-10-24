f2d688bd1be3b368cff1e4c1d25b9b958a5f6387
smarcet <smarcet@gmail.com>
2013-10-24 11:18:24 -0300

    Review and create Puppet configuration for dev server
    blueprint sso-openid-provider
    
    Change-Id: I7966f59df6bd6ba851b507c7a408f076d7031abe

---
+++ modules/openstack_project/manifests/openid_dev.pp
@@ -0,0 +1,59 @@
+# Copyright 2013  OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# openstackid idp(sso-openid) dev server
+#
+class openstack_project::openid_dev (
+  $mysql_root_password = '',
+  $site_admin_password = '',
+  $site_mysql_password = '',
+  $sysadmins = [],
+  $db_name  = 'openstackid_openid',
+  $certname = '',
+  $redis_port= '6378',
+  $redis_max_memory = '1gb',
+  $redis_bind = '127.0.0.1',
+) {
+  realize (
+    User::Virtual::Localuser['smarcet'],
+  )
+
+  class { 'mysql::server':
+    root_password => $mysql_root_password,
+  }
+
+  mysql_database {
+    $db_name:
+    ensure  => present,
+    charset => 'utf8',
+    require => Class['mysql::server'],
+  }
+
+  #php
+  package { ['php5-common','php5-curl','php5-cli','php5-json','php5-mcrypt','php5-mysql']:
+    ensure => installed,
+  }
+  # redis (custom module written by tipit)
+
+  class { 'redis':
+    redis_port       => $redis_port,
+    redis_max_memory => $redis_max_memory,
+    redis_bind       => $redis_bind,
+  }
+
+  include apache
+  include apache::ssl
+  include apache::php
+
+}
