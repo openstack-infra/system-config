29f379ea5b06fde51c6bbd0d4e19da6c795295ef
smarcet <smarcet@gmail.com>
2013-10-24 11:18:24 -0300

    Review and create Puppet configuration for dev server
    blueprint sso-openid-provider
    
    Change-Id: I7966f59df6bd6ba851b507c7a408f076d7031abe

---
+++ modules/redis/manifests/init.pp
@@ -0,0 +1,57 @@
+# == Class: redis
+# http://packages.ubuntu.com/quantal/amd64/redis-server/filelist
+class redis(
+  $redis_port = '6379',
+  $redis_max_memory = '1gb',
+  $redis_bind = '127.0.0.1',
+  $redis_bin_dir = '/usr/bin',
+) {
+
+  package {'redis-server':
+    ensure  => installed,
+  }
+
+  case $::redis_version {
+    /2\.2\.\d+/: {
+      $redis_conf_file = 'redis.2.2.conf.erb'
+    }
+    /2\.4\.\d+/: {
+      $redis_conf_file = 'redis.2.4.conf.erb'
+    }
+    /2\.6\.\d+/: {
+      $redis_conf_file = 'redis.2.6.conf.erb'
+    }
+    default: {
+      fail("Invalid redis version, ${::redis_version}")
+    }
+  }
+
+  file { 'redis-init':
+    ensure  => present,
+    path    => '/etc/init.d/redis-server',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['redis-server'],
+    content => template('redis/init_script.erb'),
+  }
+
+  file { 'redis_port.conf':
+    ensure  => present,
+    path    => '/etc/redis/redis.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("redis/${redis_conf_file}"),
+    require => Package['redis-server'],
+    notify  => Service['redis'],
+  }
+
+  service { 'redis':
+    ensure    => running,
+    name      => 'redis-server',
+    enable    => true,
+    require   => [ File['redis_port.conf'],  File['redis-init'], Package['redis-server'] ],
+  }
+
+}
