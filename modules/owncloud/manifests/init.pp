6f45aec6140e99a8bc7fa3ab0a8c6465d896cb37
Anita Kuno <anteaya@anteaya.info>
2013-10-25 10:06:14 -0400

    Create an owncloud instance.
    Initially this is motivated by the file sharing needs of
    the OpenStack Foundation Board and decisions are made with
    their needs in mind. Should use expand, this code may need to be
    reviewed to ensure it is accurate should needs change or expand.
    
    Change-Id: I5b39dc50580979ecfbcb23c4d54b9b38e875591b

---
+++ modules/owncloud/manifests/init.pp
@@ -0,0 +1,19 @@
+# Class owncloud
+#
+class owncloud (
+) {
+  include apache
+
+  # has to be version 5.0.4debian-0ubuntu1~ubuntu12.04
+  package { 'owncloud' :
+    ensure => present,
+  }
+
+  file { '/etc/owncloud/config.php' :
+    ensure  => present,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0640',
+    content => template('owncloud/config.php'),
+  }
+}
