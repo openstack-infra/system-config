9b2872e6520e21bab8f690831eb881b95b8e758c
Anita Kuno <anteaya@anteaya.info>
2013-10-25 10:06:14 -0400

    Create an owncloud instance.
    Initially this is motivated by the file sharing needs of
    the OpenStack Foundation Board and decisions are made with
    their needs in mind. Should use expand, this code may need to be
    reviewed to ensure it is accurate should needs change or expand.
    
    Change-Id: I5b39dc50580979ecfbcb23c4d54b9b38e875591b

---
+++ modules/owncloud/templates/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "trove",
+  "dbname"        => $db_name,
+  "dbuser"        => $db_user,
+  "dbpass"        => $db_password,
+  "dbhost"        => $db_host,
+  "dbtableprefix" => "",
+  "adminlogin"    => $admin_username,
+  "adminpass"     => $admin_password",
+  "directory"     => "/usr/share/owncloud/data",
+);
