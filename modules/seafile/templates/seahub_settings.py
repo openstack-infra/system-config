91cd092d8b4240556e0f640ecfa2129850a1826b
Anita Kuno <anteaya@anteaya.info>
2013-10-27 11:40:08 -0400

    Create a seafile instance.
    This is motivated by the file sharing needs of the OpenStack
    Foundation Board and decisions are made with their needs in mind.
    Should use expand, this code may need to be reviewed to ensure
    it is accurate should needs change or expand.
    
    Change-Id: I70ec66c9be2ce45ee84b7befae521c8550cff277

---
+++ modules/seafile/templates/seahub_settings.py
@@ -0,0 +1,20 @@
+# this is generated here
+# github.com/haiwen/seafile/blob/master/scripts/setup-seafile-mysql.py#L968
+# note the spelling _SECREC_ in SECREC_KEY
+SECREC_KEY = $seafile_secrec_key
+
+DATABASES = {
+    'default': {
+        # this defaults to django.db.backends.mysql, not sure what do to here
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': $seafile_seahub_db,
+        'USER': $seafile_db_user,
+        'PASSWORD': $seafile_db_password,
+        'HOST': $seafile_db_host,
+        # default is 3306
+        'PORT': $seafile_db_port,
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=INNODB',
+        }
+    }
+}
