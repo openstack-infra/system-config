87133fdabe3b5997aa8694c586c9ebd8280a023a
Monty Taylor <mordred@inaugust.com>
2013-11-16 06:16:23 -0500

    Create and upload wheels
    We want to make wheels for pre-release and release events. We will only
    build wheels on python 2.7. If the project supports python 3 as well, the
    project will have marked itself as supporting universal wheels. If it does
    that, python 2.7 will build a wheel that can be used in both places. If it
    does not support python 3, then we don't have to build on on python 3.
    
    Change-Id: I060b59d8adc586ff37065fa4f6172a592ee3f903

---
+++ modules/jenkins/files/slave_scripts/pypi-extract-universal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Extract Python package name from setup.cfg
+
+import ConfigParser
+
+universal = False
+
+setup_cfg = ConfigParser.SafeConfigParser()
+setup_cfg.read("setup.cfg")
+if setup_cfg.has_section("wheel"):
+    universal = setup_cfg.getboolean("wheel", "universal")
+if universal:
+    print("py2.py3")
+else:
+    print("py27")
