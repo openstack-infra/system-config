b5f1d971ba5f380229592ca6b7fcdde97328b22e
Khai Do <zaro0508@gmail.com>
2013-11-19 21:46:13 +0000

    add nodepool to jenkins-dev server
    We are co-locating nodepool on jenkins-dev to allow us to test
    jenkins upgrades.  This dev nodepool can only spin up machines
    in one AZ on hpcloud.
    
    Change-Id: I73f6d4373fa79fec8845d7d81fa1235f7fe15482

--- modules/openstack_project/manifests/jenkins_dev.pp
+++ modules/openstack_project/manifests/jenkins_dev.pp
@@ -2,7 +2,15 @@
 #
 class openstack_project::jenkins_dev (
   $jenkins_ssh_private_key = '',
-  $sysadmins = []
+  $sysadmins = [],
+  $mysql_root_password,
+  $mysql_password,
+  $nodepool_ssh_private_key = '',
+  $jenkins_api_user ='',
+  $jenkins_api_key ='',
+  $hpcloud_username ='',
+  $hpcloud_password ='',
+  $hpcloud_project ='',
 ) {
   include openstack_project
 
@@ -33,4 +41,18 @@ class openstack_project::jenkins_dev (
     mode   => '0644',
     source => 'puppet:///modules/openstack_project/jenkins/jenkins.default',
   }
+
+  class { '::nodepool':
+    mysql_password           => 'mysql_password',
+    mysql_root_password      => 'mysql_root_password',
+    nodepool_ssh_private_key => 'nodepool_ssh_private_key',
+    nodepool_template        => 'nodepool-dev.yaml.erb',
+    sysadmins                => 'sysadmins',
+    jenkins_api_user         => 'jenkins_api_user',
+    jenkins_api_key          => 'jenkins_api_key',
+    hpcloud_username         => 'hpcloud_username',
+    hpcloud_password         => 'hpcloud_password',
+    hpcloud_project          => 'hpcloud_project',
+  }
+
 }
