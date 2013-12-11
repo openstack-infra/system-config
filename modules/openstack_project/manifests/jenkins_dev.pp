702674684de0736ee7bb6a7890dd0a5975dfe58f
Khai Do <zaro0508@gmail.com>
2013-12-11 05:36:16 +0000

    fix installation of nodepool on jenkins-dev
    The patch to add nodepool to jenkins-dev (https://review.openstack.org/#/c/57333)
    did not work.
    
    There were a few issues with it:
    1. jenkins-dev.pp was passing literal strings to the nodepool module, instead it
       should be passing in the variables.
    2. jenkins-dev.pp was calling ::nodepool but puppet seems to think that it wants
       ::openstack_project::nodepool due to puppet's scoping weirdness :(
    3. the script to build nodepool machines needed the jenkins_dev_ssh_key.
    
    Fixes to above issues:
    1. This is trivial, just passed the variables thru instead of literal strings.
    2. The nodepool.pp module is renamed to nodepool_prod.pp to prevent the scoping problem.
    3. Make prepare node script a template so we can pass in the jenkins_dev_ssh_key
    
    There wasn't a good way to pass the jenkins_dev_ssh_key into prepare_node_devstack.sh
    so we just get it from git.o.o
    
    Change-Id: If34735cf61a0efe58e097c42dc48e106e3e4f6f4

--- modules/openstack_project/manifests/jenkins_dev.pp
+++ modules/openstack_project/manifests/jenkins_dev.pp
@@ -3,6 +3,7 @@
 class openstack_project::jenkins_dev (
   $jenkins_ssh_private_key = '',
   $sysadmins = [],
+  $statsd_host = '',
   $mysql_root_password,
   $mysql_password,
   $nodepool_ssh_private_key = '',
@@ -12,6 +13,10 @@ class openstack_project::jenkins_dev (
   $hpcloud_username ='',
   $hpcloud_password ='',
   $hpcloud_project ='',
+  $image_log_document_root = '/var/log/nodepool/image',
+  $enable_image_log_via_http = true,
+  $nodepool_template ='nodepool-dev.yaml.erb',
+  $jenkins_dev_ssh_key = $openstack_project::jenkins_dev_ssh_key,
 ) {
   include openstack_project
 
@@ -32,7 +37,7 @@ class openstack_project::jenkins_dev (
     ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_chain_file          => '',
     jenkins_ssh_private_key => $jenkins_ssh_private_key,
-    jenkins_ssh_public_key  => $openstack_project::jenkins_dev_ssh_key,
+    jenkins_ssh_public_key  => $jenkins_dev_ssh_key,
   }
 
   file { '/etc/default/jenkins':
@@ -44,17 +49,45 @@ class openstack_project::jenkins_dev (
   }
 
   class { '::nodepool':
-    mysql_password           => 'mysql_password',
-    mysql_root_password      => 'mysql_root_password',
-    nodepool_ssh_private_key => 'nodepool_ssh_private_key',
-    nodepool_template        => 'nodepool-dev.yaml.erb',
-    sysadmins                => 'sysadmins',
-    jenkins_api_user         => 'jenkins_api_user',
-    jenkins_api_key          => 'jenkins_api_key',
-    jenkins_credentials_id   => 'jenkins_credentials_id',
-    hpcloud_username         => 'hpcloud_username',
-    hpcloud_password         => 'hpcloud_password',
-    hpcloud_project          => 'hpcloud_project',
+    mysql_root_password       => $mysql_root_password,
+    mysql_password            => $mysql_password,
+    nodepool_ssh_private_key  => $nodepool_ssh_private_key,
+    statsd_host               => $statsd_host,
+    image_log_document_root   => $image_log_document_root,
+    enable_image_log_via_http => $enable_image_log_via_http,
+  }
+
+  file { '/etc/nodepool/nodepool.yaml':
+    ensure  => present,
+    owner   => 'nodepool',
+    group   => 'root',
+    mode    => '0400',
+    content => template("openstack_project/nodepool/${nodepool_template}"),
+    require => [
+      File['/etc/nodepool'],
+      User['nodepool'],
+    ],
+  }
+
+  file { '/etc/nodepool/scripts':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    recurse => true,
+    purge   => true,
+    force   => true,
+    require => File['/etc/nodepool'],
+    source  => 'puppet:///modules/openstack_project/nodepool/scripts',
+  }
+
+  file { '/etc/nodepool/scripts/prepare_node_dev_devstack.sh':
+    ensure  => present,
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    content => template('openstack_project/nodepool/prepare_node_dev_devstack.sh.erb'),
+    require => File['/etc/nodepool/scripts'],
   }
 
 }
