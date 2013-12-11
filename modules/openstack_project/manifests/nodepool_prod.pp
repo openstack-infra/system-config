b80dedc3620a9275e0751f2243e7d6fd8f79710e
Khai Do <zaro0508@gmail.com>
2013-12-11 05:36:16 +0000

    fix installation of nodepool on jenkins-dev
    The patch to add nodepool to jenkins-dev (https://review.openstack.org/#/c/57333)
    did not work.
    
    There were a few issues with it:
    1. jenkins-dev.pp was passing literal strings to the nodepool module, instead it
       should be passing in the variables.
    2. jenkins-dev.pp was calling ::nodepool but puppet seems to think that it wants
       ::openstack_project::nodepool due to puppet's scoping weirdness :(
    3. the script to build nodepool machines needed the jenkins_dev_ssh_key.
    
    Fixes to above issues:
    1. This is trivial, just passed the variables thru instead of literal strings.
    2. The nodepool.pp module is renamed to nodepool_prod.pp to prevent the scoping problem.
    3. Make prepare node script a template so we can pass in the jenkins_dev_ssh_key
    
    There wasn't a good way to pass the jenkins_dev_ssh_key into prepare_node_devstack.sh
    so we just get it from git.o.o
    
    Change-Id: If34735cf61a0efe58e097c42dc48e106e3e4f6f4

--- modules/openstack_project/manifests/nodepool_prod.pp
+++ modules/openstack_project/manifests/nodepool_prod.pp
@@ -4,7 +4,6 @@ class openstack_project::nodepool_prod(
   $mysql_root_password,
   $mysql_password,
   $nodepool_ssh_private_key = '',
-  $nodepool_template = 'nodepool.yaml.erb',
   $sysadmins = [],
   $statsd_host = '',
   $jenkins_api_user ='',
@@ -21,6 +20,7 @@ class openstack_project::nodepool_prod(
   $tripleo_project ='',
   $image_log_document_root = '/var/log/nodepool/image',
   $enable_image_log_via_http = true,
+  $nodepool_template = 'nodepool.yaml.erb',
 ) {
   class { 'openstack_project::server':
     sysadmins                 => $sysadmins,
@@ -59,5 +59,4 @@ class openstack_project::nodepool_prod(
     require => File['/etc/nodepool'],
     source  => 'puppet:///modules/openstack_project/nodepool/scripts',
   }
-
 }
