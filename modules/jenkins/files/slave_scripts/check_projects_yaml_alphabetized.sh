c85153bc2e74056fabe4f9755edc38adc45a1f43
Sergey Lukjanov <slukjanov@mirantis.com>
2013-12-12 19:27:42 +0400

    Add job to check projects.yaml alphabetized
    It checks that projects.yaml alphabetized and prints list of projects
    that should be sorted. Additionally, it adds new job as non-voting to
    the openstack-infra/config project.
    
    Change-Id: I9c0eb5072a5b1446ee654296e60c1f8587a3a7ad

---
+++ modules/jenkins/files/slave_scripts/check_projects_yaml_alphabetized.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+# It checks that projects.yaml alphabetized and prints list of projects that
+# should be sorted.
+
+export TMPDIR=`/bin/mktemp -d`
+trap "rm -rf $TMPDIR" EXIT
+
+pushd $TMPDIR
+
+grep -e '- project' modules/openstack_project/templates/review.projects.yaml.erb \
+    | cut -d: -f2-   `: # take everything after - project:` \
+    | sed 's/^ *//g' `: # trim leading spaces` \
+    | sed 's/ *$//g' `: # trim trailing spaces` \
+    > projects_list
+
+sort projects_list -o projects_list.sorted
+
+diff projects_list projects_list.sorted > projects_list.diff
+
+if [[ -n `cat projects_list.diff` ]]; then
+    echo "The following projects should be alphabetized: "
+    cat projects_list.diff | grep -e '> '
+    exit 1
+else
+    echo "Projects alphabetized."
+fi
+
+popd
