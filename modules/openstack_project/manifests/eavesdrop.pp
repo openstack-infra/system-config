0b760a59e887e345066910ff4a279e348658353d
Serg Melikyan <smelikyan@mirantis.com>
2013-12-12 15:03:41 +0400

    Add eavesdrop to monitor Murano channel
    Change-Id: I71ec161d3bc2d5a169acb1c398e8d7e44d28f24c

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -35,6 +35,7 @@ class openstack_project::eavesdrop (
     vhost_extra => $vhost_extra,
     channels    => [
         '#heat',
+        '#murano',
         '#openstack',
         '#openstack-ceilometer',
         '#openstack-climate',
