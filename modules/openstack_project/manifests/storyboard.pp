0c439456059d8142aeb1386f25ba12cccb91cabc
Ruslan Kamaldinov <rkamaldinov@mirantis.com>
2014-01-06 06:33:45 +0400

    Add Storyboard puppet module
    There are two major parts being installed with this module:
    1. storyboard-api - REST API service served  with
       apache mod_wsgi module
    2. storyboard-webclient - static html/css/js files.
       This project is built and published to tarballs.o.o,
       from where it'll be installed with this puppet module
    
    This module requires three configs from Hiera:
    * storyboard_db_host
    * storyboard_db_password
    * storyboard_db_user
    
    Installed projects:
    * http://git.openstack.org/cgit/openstack-infra/storyboard/
    * http://git.openstack.org/cgit/openstack-infra/storyboard-webclient/
    
    Things to be added in later commits:
    * Documentation for ci.openstack.org
    * Configure logging (once supported by storyboard)
    * SSL
    
    Change-Id: If3da06f8d20a6282036f1f9f063c25a6d0db60c6

---
+++ modules/openstack_project/manifests/storyboard.pp
@@ -0,0 +1,20 @@
+# == Class: openstack_project::storyboard
+#
+class openstack_project::storyboard(
+  $mysql_host = '',
+  $mysql_password = '',
+  $mysql_user = '',
+  $sysadmins = [],
+) {
+  class { 'openstack_project::server':
+    sysadmins                 => $sysadmins,
+    iptables_public_tcp_ports => [80],
+  }
+
+  class { '::storyboard':
+    mysql_db       => $mysql_host,
+    mysql_password => $mysql_password,
+    mysql_user     => $mysql_user,
+  }
+
+}
