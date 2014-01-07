d9cd59d5c55d6d8179390afca7797b84ee6b37c3
Clark Boylan <clark.boylan@gmail.com>
2014-01-06 22:59:08 -0800

    Bump mysql connection limit for nodepool.
    Nodepool is having its file descriptor limit bumped to 8192. Bump the
    mysql connection limit to match to avoid having trouble on the other
    side.
    
    Change-Id: Ic164bddc860f8f569a2a0acb5290aaeb775bdd9a

--- modules/nodepool/manifests/init.pp
+++ modules/nodepool/manifests/init.pp
@@ -51,7 +51,7 @@ class nodepool (
 
   file { '/etc/mysql/conf.d/max_connections.cnf':
     ensure  => present,
-    content => "[server]\nmax_connections = 1024\n",
+    content => "[server]\nmax_connections = 8192\n",
     mode    => '0444',
     owner   => 'root',
     group   => 'root',
