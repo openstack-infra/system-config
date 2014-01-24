8e957b409379c66d7acaddcc4a46fc0bd323ac48
James E. Blair <jeblair@openstack.org>
2014-01-24 11:46:50 -0800

    Cleanup unused param in ggp
    Safe to merge after the change to remove the param from jjb has
    propogated.
    
    Also add a license header.
    
    Change-Id: Ie214be406f936b84f3c1d5eeb0e183fbdc6d483c

--- modules/jenkins/files/slave_scripts/gerrit-git-prep.sh
+++ modules/jenkins/files/slave_scripts/gerrit-git-prep.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -e
 
+# Copyright 2011-2014 OpenStack Foundation
+# Copyright 2012-2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 GERRIT_SITE=$1
 GIT_ORIGIN=$2
-
-# TODO(jeblair): Remove once the arg list is changed in jjb macros
-if [ ! -z $3 ]
-then
-    GIT_ORIGIN=$3
-fi
 
 if [ -z "$GERRIT_SITE" ]
 then
