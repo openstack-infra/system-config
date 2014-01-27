454313f6ca3d82507a8235d7487cead6082449b6
Steve Baker <sbaker@redhat.com>
2014-01-27 17:26:45 +1300

    Open port 8000 for heat-api-cfn
    The heat-slow tests are currently failing because nova servers cannot
    reach heat-api-cfn to signal waitconditions. This change is an attempt
    to fix that.
    
    Change-Id: I08c27dc569b86a73f5aa3ead48569e6a8161e980

--- modules/openstack_project/manifests/slave_template.pp
+++ modules/openstack_project/manifests/slave_template.pp
@@ -5,7 +5,8 @@ class openstack_project::slave_template (
   $ssh_key = $openstack_project::jenkins_ssh_key
 ) inherits openstack_project {
   class { 'openstack_project::template':
-    iptables_public_tcp_ports => [],
+    # Port 8000 to allow nova servers to reach heat-api-cfn
+    iptables_public_tcp_ports => [8000],
     install_users             => $install_users,
   }
   class { 'jenkins::slave':
