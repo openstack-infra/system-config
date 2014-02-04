fd3b131e1df7cc17b594ddd2307bbc45ac17e991
Bhuvan Arumugam <bhuvan@apple.com>
2014-02-04 18:11:59 +0000

    log analyzer for openstack IRC logs
    Log analyzer is setup using super serious stats, http://sss.dutnie.nl/. The
    script to install, configure and generate stats for each channel is added in
    this commit. The config template and dependent packages are managed using
    puppet.
    
    The cron job is added to generate the stats every hour.
    
    Change-Id: I7f3ca633b3d7813c62d46e84f7f2323011b86d0e
    Closes-Bug: 1203137

---
+++ modules/ircstats/manifests/init.pp
@@ -0,0 +1,63 @@
+# Install and manage log analyzer for irc logs.
+class ircstats {
+
+  #include apache
+  package { ['php',
+    'php-mbstring',
+    'php-pdo']:
+    ensure  => present,
+  }
+
+  user { 'ircstats':
+    ensure     => present,
+    home       => '/home/ircstats',
+    shell      => '/bin/bash',
+    gid        => 'ircstats',
+    managehome => true,
+    require    => Group['ircstats'],
+  }
+
+  group { 'ircstats':
+    ensure => present,
+  }
+
+  file { '/etc/ircstats':
+    ensure  => directory,
+    owner   => 'ircstats',
+    group   => 'ircstats',
+    require => User['ircstats'],
+  }
+
+  # template to generate configuration for each channel
+  file { '/etc/ircstats/ircstats.conf.template':
+    ensure  => present,
+    owner   => 'ircstats',
+    group   => 'ircstats',
+    mode    => '0644',
+    require => File['/etc/ircstats'],
+    source  => 'puppet:///modules/ircstats/ircstats.conf.template',
+  }
+
+  # stats install directory
+  file { '/opt/ircstats':
+    ensure  => directory,
+    owner   => 'ircstats',
+    group   => 'ircstats',
+  }
+
+  # stat generation script
+  file { '/usr/local/bin/ircstats.py':
+    ensure  => present,
+    owner   => 'ircstats',
+    group   => 'ircstats',
+    mode    => '0755',
+    source  => 'puppet:///modules/ircstats/ircstats.py',
+  }
+
+  cron { 'ircstats':
+    user    => 'ircstats',
+    hour    => '*',
+    minute  => '0',
+    command => "python /usr/local/bin/ircstats.py -l /var/lib/statusbot/www -t /etc/ircstats/ircstats.conf.template -i /var/lib/statusbot/www/stats",
+  }
+}
