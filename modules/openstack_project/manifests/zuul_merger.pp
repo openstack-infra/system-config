1d13e189f9a5da6bcafb14ea45f4a1a0d6922803
James E. Blair <jeblair@openstack.org>
2014-02-17 17:20:35 -0800

    Fix zuul installation
    The following items were missing from an automated zuul install:
    
     * A pre-populated known hosts file
     * The git user/email for creating commits (.gitconfig existed
       on zuul.o.o, but switch to using zuul's config file instead).
    
    Also, make sure that the mergers specify the zuul server as the
    gearman server address rather than localhost.
    
    Change-Id: I47a473f60c4b5b2daaa910aa61cdf6e0c6fe1528

--- modules/openstack_project/manifests/zuul_merger.pp
+++ modules/openstack_project/manifests/zuul_merger.pp
@@ -2,8 +2,10 @@
 #
 class openstack_project::zuul_merger(
   $vhost_name = $::fqdn,
+  $gearman_server = '127.0.0.1',
   $gerrit_server = '',
   $gerrit_user = '',
+  $gerrit_ssh_host_key = '',
   $zuul_ssh_private_key = '',
   $zuul_url = "http://${::fqdn}/p",
   $sysadmins = [],
@@ -16,13 +18,27 @@ class openstack_project::zuul_merger(
 
   class { '::zuul':
     vhost_name           => $vhost_name,
+    gearman_server	 => $gearman_server,
     gerrit_server        => $gerrit_server,
     gerrit_user          => $gerrit_user,
     zuul_ssh_private_key => $zuul_ssh_private_key,
     zuul_url             => $zuul_url,
+    git_email		 => 'jenkins@openstack.org',
+    git_name		 => 'OpenStack Jenkins',
   }
 
   class { '::zuul::merger': }
+
+  if $gerrit_ssh_host_key != '' {
+    file { '/home/zuul/.ssh/known_hosts':
+      owner   => 'zuul',
+      group   => 'zuul',
+      mode    => '0600',
+      content => "review.openstack.org ${gerrit_ssh_host_key}",
+      replace => true,
+      require => User['zuul'],
+    }
+  }
 
   file { '/etc/zuul/merger-logging.conf':
     ensure => present,
