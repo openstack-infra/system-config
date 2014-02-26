cec801f8723487b2f182097447824228f78031e1
James E. Blair <jeblair@openstack.org>
2014-02-25 17:16:42 -0800

    Add storyboard SSL cert/key/chain
    Commented out because they aren't used, but the keys are in
    hiera with these values.
    
    Change-Id: Ie1d0bc5a9a996d55995c0d1c6d1ae961353f0c65

--- manifests/site.pp
+++ manifests/site.pp
@@ -496,6 +496,9 @@ node 'storyboard.openstack.org' {
     mysql_host          => hiera('storyboard_db_host'),
     mysql_user          => hiera('storyboard_db_user'),
     mysql_password      => hiera('storyboard_db_password'),
+#    ssl_cert_file_contents  => hiera('storyboard_ssl_cert_file_contents'),
+#    ssl_key_file_contents   => hiera('storyboard_ssl_key_file_contents'),
+#    ssl_chain_file_contents => hiera('storyboard_ssl_chain_file_contents'),
   }
 }
 
