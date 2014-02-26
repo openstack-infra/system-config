040e4e1aeee10fbf4010344718d383332eed987a
Elizabeth Krumbach Joseph <lyz@princessleia.com>
2014-02-26 13:33:21 -0800

    Add support for Fedora 20 to nodepool
    For TripleO, one of our test cases is a fully Fedora-driven test,
    this adds support for Fedora as a nodepool VM by adding support
    to prepare node scripts.
    
    Change-Id: I7399cc4d4b6ec6f8598afa68bc99ebb24cb01f64

---
+++ modules/openstack_project/files/nodepool/prepare_node.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -xe
+
+# Copyright (C) 2011-2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+HOSTNAME=$1
+SUDO=$2
+BARE=$3
+PYTHON3=${4:-false}
+PYPY=${5:-false}
+
+sudo hostname $HOSTNAME
+# Fedora 20 image doesn't come with wget
+if [ -f /usr/bin/yum ]; then
+    sudo yum -y install wget
+fi
+wget https://git.openstack.org/cgit/openstack-infra/config/plain/install_puppet.sh
+sudo bash -xe install_puppet.sh
+sudo git clone --depth=1 git://git.openstack.org/openstack-infra/config.git \
+    /root/config
+sudo /bin/bash /root/config/install_modules.sh
+if [ -z "$NODEPOOL_SSH_KEY" ] ; then
+    sudo puppet apply --modulepath=/root/config/modules:/etc/puppet/modules \
+	-e "class {'openstack_project::single_use_slave': sudo => $SUDO, bare => $BARE, python3 => $PYTHON3, include_pypy => $PYPY, }"
+else
+    sudo puppet apply --modulepath=/root/config/modules:/etc/puppet/modules \
+	-e "class {'openstack_project::single_use_slave': install_users => false, sudo => $SUDO, bare => $BARE, python3 => $PYTHON3, include_pypy => $PYPY, ssh_key => '$NODEPOOL_SSH_KEY', }"
+fi
+
+sudo mkdir -p /opt/git
+sudo -i python /opt/nodepool-scripts/cache_git_repos.py
+
+# We don't always get ext4 from our clouds, mount ext3 as ext4 on the next
+# boot (eg when this image is used for testing).
+sudo sed -i 's/ext3/ext4/g' /etc/fstab
+
+sync
+sleep 5
