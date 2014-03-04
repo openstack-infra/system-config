2e3adf1b269ccdc56bdf25cc64c85f6fa676b3e2
Spencer Krum <krum.spencer@gmail.com>
2014-03-04 08:14:35 -0800

    Add PuppetBoard to openstack infra
    This places the puppet web dashboard puppetboard from
    https://github.com/nedap/puppetboard onto the puppetdb server.
    
    This enables anyone to see the output of puppet runs, which nodes
    are failing and which nodes have not checked in over time. We can
    also quickly and intreactivly query the facts database.
    
    Change-Id: I09d09410dd8fa71759ce6b2f9c7f34f78eefe204

---
+++ modules/openstack_project/manifests/puppetboard.pp
@@ -0,0 +1,16 @@
+# Class to configure puppetboard on a node.
+# This will only work on the puppetdb server for now
+class openstack_project::puppetboard(
+) {
+
+  include apache
+
+  class { 'apache::mod::wsgi': }
+
+  class { '::puppetboard':
+    enable_query => 'False', # This being a python false
+  }
+
+  class { 'puppetboard::apache::conf': }
+
+}
