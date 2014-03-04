fe9d763e67b2aaae1e474bc0e002bc6307f3eb2a
Morgan Fainberg <m@metacloud.com>
2014-03-04 15:29:46 -0800

    Add in non-voting sample-config check
    Until the sample-config is generated with documenation and during
    packaging / setup time, there is a non-voting check job to indicate
    the sample config in the git repository is out of date.
    
    This has been enabled for Keystone, Nova, Heat, and Ceilometer (the
    projects with the supported scripts from oslo to perform the check
    and generate a sample config).
    
    Change-Id: Ied16c842dbdd61fa8bc474c03e15370a52197f1a

---
+++ modules/jenkins/files/slave_scripts/check-config-sample-up-to-date.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -xe
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+org=$1
+project=$2
+
+if [[ -z "$org" || -z "$project" ]]
+then
+  echo "Usage: $? ORG PROJECT"
+  echo
+  echo "ORG: The project organization (eg 'stackforge')"
+  echo "PROJECT: The project name (eg 'nova')"
+  exit 1
+fi
+
+/usr/local/jenkins/slave_scripts/jenkins-oom-grep.sh pre
+
+sudo /usr/local/jenkins/slave_scripts/jenkins-sudo-grep.sh pre
+
+source /usr/local/jenkins/slave_scripts/select-mirror.sh $org $project
+
+tox -v -evenv -- /bin/bash tools/config/check_uptodate.sh
+result=$?
+
+/usr/local/jenkins/slave_scripts/jenkins-oom-grep.sh post
+oomresult=$?
+
+if [ $oomresult -ne "0" ]
+then
+    echo
+    echo "This test has failed because it attempted to exceed configured"
+    echo "memory limits and was killed prior to completion.  See above"
+    echo "for related kernel messages."
+    echo
+    exit 1
+fi
+
+exit $result
