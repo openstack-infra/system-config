548413f8aed55f4ed035b7ca7ecdc1e9709c32b9
Monty Taylor <mordred@inaugust.com>
2014-03-06 12:27:42 -0800

    Make storyboard run over ssl
    We're doing auth now, so we should really do it over SSL.
    
    The cert contents are already in hiera.
    
    Change-Id: Ia939e228785168705840acd6d377e6c25ba3370d

--- modules/openstack_project/manifests/storyboard.pp
+++ modules/openstack_project/manifests/storyboard.pp
@@ -5,6 +5,9 @@ class openstack_project::storyboard(
   $mysql_password = '',
   $mysql_user = '',
   $sysadmins = [],
+  $ssl_cert_file_contents = '',
+  $ssl_key_file_contents = '',
+  $ssl_chain_file_contents = '',
 ) {
   class { 'openstack_project::server':
     sysadmins                 => $sysadmins,
@@ -12,11 +15,19 @@ class openstack_project::storyboard(
   }
 
   class { '::storyboard':
-    mysql_host     => $mysql_host,
-    mysql_password => $mysql_password,
-    mysql_user     => $mysql_user,
-    projects_file  =>
+    mysql_host              => $mysql_host,
+    mysql_password          => $mysql_password,
+    mysql_user              => $mysql_user,
+    projects_file           =>
       'puppet:///modules/openstack_project/review.projects.yaml',
+    ssl_cert_file           =>
+      '/etc/ssl/certs/storyboard.openstack.org.pem',
+    ssl_key_file            =>
+      '/etc/ssl/private/storyboard.openstack.org.key',
+    ssl_chain_file          => '/etc/ssl/certs/intermediate.pem',
+    ssl_cert_file_contents  => $ssl_cert_file_contents,
+    ssl_key_file_contents   => $ssl_key_file_contents,
+    ssl_chain_file_contents => $ssl_chain_file_contents,
   }
 
 }
