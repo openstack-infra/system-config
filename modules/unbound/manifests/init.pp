ae4b3e32920d8d37fd73a100430b5792b2fab76b
James E. Blair <jeblair@openstack.org>
2014-03-12 14:24:20 -0700

    Use unbound
    On all machines, set up unbound as a caching recursive resolver.
    On single-use slaves, set it up to forward cache misses to the
    DNS servers obtained by the template host on boot.
    
    Change-Id: I8505f5a277f20b1328900a9a515cd84db77b2b3b

---
+++ modules/unbound/manifests/init.pp
@@ -0,0 +1,65 @@
+# Copyright (C) 2014 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: unbound
+
+# This installs unbound in its default configuration as a caching
+# recursive resolver and configures /etc/unbound/conf.d if needed.
+
+class unbound (
+) {
+  package { 'unbound':
+    ensure => present,
+  }
+
+  # RedHat has a conf.d/* include in its config already; make Debian
+  # match
+  if ($::osfamily == 'Debian') {
+    file { '/etc/unbound/conf.d':
+      ensure => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0775',
+      require => Package['unbound'],
+    }
+
+    file { '/etc/unbound/unbound.conf':
+      ensure  => present,
+      source  => 'puppet:///modules/unbound/unbound.conf.debian',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      require => File['/etc/unbound/conf.d'],
+      notify  => Service['unbound']
+    }
+  }
+
+  # Ubuntu uses resolvconf which sets this.  NOTE: Debian unknown.
+  if ($::osfamily == 'RedHat') {
+    file { '/etc/resolv.conf':
+      content => "nameserver 127.0.0.1",
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      require => Service['unbound']
+    }
+  }
+
+  service { 'unbound':
+    name       => 'unbound',
+    enable     => true,
+    hasrestart => true,
+    require    => Package['unbound'],
+  }
+}
