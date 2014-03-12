c380587c7697eea9cb81e1239dc84cb969e242aa
Ananth Suryanarayana <Ananth@contrailsystems.com>
2014-03-11 19:03:06 -0700

    UNDO comment out mit ket server

--- modules/opencontrail_project/manifests/base.pp
+++ modules/opencontrail_project/manifests/base.pp
@@ -74,8 +74,7 @@ class opencontrail_project::base(
       location   => 'http://apt.puppetlabs.com',
       repos      => 'main',
       key        => '4BD6EC30',
-      key_server => '',
-#     key_server => 'pgp.mit.edu',
+      key_server => 'pgp.mit.edu',
     }
 
     file { '/etc/apt/preferences.d/00-puppet.pref':
