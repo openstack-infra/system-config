3560f35e9692c4e2826944b955b7a59f6aefbf4e
Andreas Jaeger <aj@suse.de>
2014-03-21 10:11:28 +0100

    Special case django_openstack_auth in translation jobs
    django_openstack_auth is translated as part of the horizon transifex
    project, adjust script to handle it.
    
    Change-Id: Ie1e1da7cad6f78d20f39af797029eed57cd057b4

--- modules/jenkins/files/slave_scripts/propose_translation_update.sh
+++ modules/jenkins/files/slave_scripts/propose_translation_update.sh
@@ -14,6 +14,16 @@
 
 ORG=$1
 PROJECT=$2
+
+GIT_REPO={PROJECT}
+RESOURCE=${PROJECT}-translations
+PROJECT_DIR=${PROJECT}
+if [ $PROJECT = "django_openstack_auth" ] ; then
+    PROJECT=horizon
+    PROJECT_DIR=openstack_auth
+    RESOURCE=djangopo
+fi
+
 COMMIT_MSG="Imported Translations from Transifex"
 
 git config user.name "OpenStack Jenkins"
@@ -24,7 +34,7 @@ git review -s
 
 # See if there is an open change in the transifex/translations topic
 # If so, get the change id for the existing change for use in the commit msg.
-change_info=`ssh -p 29418 review.openstack.org gerrit query --current-patch-set status:open project:$ORG/$PROJECT topic:transifex/translations owner:jenkins`
+change_info=`ssh -p 29418 review.openstack.org gerrit query --current-patch-set status:open project:$ORG/$GIT_REPO topic:transifex/translations owner:jenkins`
 previous=`echo "$change_info" | grep "^  number:" | awk '{print $2}'`
 if [ "x${previous}" != "x" ] ; then
     change_id=`echo "$change_info" | grep "^change" | awk '{print $2}'`
@@ -44,20 +54,20 @@ fi
 if [ ! -d .tx ] ; then
     tx init --host=https://www.transifex.com
 fi
-tx set --auto-local -r ${PROJECT}.${PROJECT}-translations "${PROJECT}/locale/<lang>/LC_MESSAGES/${PROJECT}.po" --source-lang en --source-file ${PROJECT}/locale/${PROJECT}.pot -t PO --execute
+tx set --auto-local -r ${PROJECT}.${RESOURCE} "${PROJECT_DIR}/locale/<lang>/LC_MESSAGES/${PROJECT_DIR}.po" --source-lang en --source-file ${PROJECT_DIR}/locale/${PROJECT_DIR}.pot -t PO --execute
 
 # Pull all upstream translations
 tx pull -a
 # Update the .pot file
 python setup.py extract_messages
-PO_FILES=`find ${PROJECT}/locale -name '*.po'`
+PO_FILES=`find ${PROJECT_DIR}/locale -name '*.po'`
 if [ -n "$PO_FILES" ]
 then
     # Use updated .pot file to update translations
     python setup.py update_catalog --no-fuzzy-matching
 fi
 # Add all changed files to git
-git add $PROJECT/locale/*
+git add $PROJECT_DIR/locale/*
 
 # Don't send a review if the only things which have changed are the creation
 # date or comments.
