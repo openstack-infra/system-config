57dce2ecd3e1f43c870da1fad244fd1b949af9e4
Spencer Krum <krum.spencer@gmail.com>
2014-03-28 00:11:10 -0700

    Add node def for puppet3 master
    This change modifies install_puppet.sh to accept a --three option
    setting it to install the latest puppet available. It also creates
    a node definition for the puppetmaster.o.o node, the new 3 master,
    and the master of the future. Changes were made to various classes
    to allow the pinning to version 2.x to be turned off.
    
    Change-Id: I805d6dc50b9de0d8a99cf818d22d06c2dea6090a

--- install_puppet.sh
+++ install_puppet.sh
@@ -16,13 +16,30 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+
+# Test condition to install puppet 3
+if [ "$1" = '--three' ]; then
+    THREE=yes
+    echo "Running in 3 mode"
+fi
+
+if cat /etc/*release | grep -e "Fedora" &> /dev/null; then
+  # Fedora pre-steps
+  :
+else
+  # Ubuntu pre-steps
+  # wget isn't included on the precise lxc template
+  DEBIAN_FRONTEND=noninteractive apt-get --option 'Dpkg::Options::=--force-confold' \
+      --assume-yes install -y --force-yes wget
+fi
+
 # Install pip using get-pip
 PIP_GET_PIP_URL=https://bootstrap.pypa.io/get-pip.py
 
 curl -O $PIP_GET_PIP_URL || wget $PIP_GET_PIP_URL
 python get-pip.py
 
-# Install puppet version 2.7.x from puppetlabs.
+# Install puppet version 2.7.x or 3.x from puppetlabs.
 # The repo and preferences files are also managed by puppet, so be sure
 # to keep them in sync with this file.
 
@@ -33,10 +50,12 @@ if cat /etc/*release | grep -e "Fedora" &> /dev/null; then
     # NOTE: we preinstall lsb_release to ensure facter sets lsbdistcodename
     yum install -y redhat-lsb-core git puppet
 
-    gem install hiera hiera-puppet
 
     mkdir -p /etc/puppet/modules/
-    ln -s /usr/local/share/gems/gems/hiera-puppet-* /etc/puppet/modules/
+    if [ "$THREE" != 'yes' ]; then
+        gem install hiera hiera-puppet
+        ln -s /usr/local/share/gems/gems/hiera-puppet-* /etc/puppet/modules/
+    fi
 
     # Puppet is expecting the command to be pip-python on Fedora
     ln -s /usr/bin/pip /usr/bin/pip-python
@@ -52,8 +71,11 @@ baseurl=http://yum.puppetlabs.com/el/6/products/$basearch
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs
 enabled=1
 gpgcheck=1
-exclude=puppet-2.8* puppet-2.9* puppet-3* facter-2*
 EOF
+
+    if [ "$THREE" != 'yes' ]; then
+        echo "exclude=puppet-2.8* puppet-2.9* puppet-3*" >> /etc/yum.repos.d/puppetlabs.repo
+    fi
 
     yum update -y
     # NOTE: enable the optional-rpms channel (if not already enabled)
@@ -66,20 +88,31 @@ else
 
     lsbdistcodename=`lsb_release -c -s`
     if [ $lsbdistcodename != 'trusty' ] ; then
-        # NB: keep in sync with openstack_project/files/00-puppet.pref
-        cat > /etc/apt/preferences.d/00-puppet.pref <<EOF
-Package: puppet puppet-common puppetmaster puppetmaster-common puppetmaster-passenger
-Pin: version 2.7*
-Pin-Priority: 501
-
-Package: facter
-Pin: version 1.*
-Pin-Priority: 501
-EOF
         rubypkg=rubygems
     else
         rubypkg=ruby
+        THREE=yes
     fi
+
+    # NB: keep in sync with openstack_project/files/00-puppet.pref
+    if [ "$THREE" == 'yes' ]; then
+        PUPPET_VERSION=3.*
+        FACTER_VERSION=2.*
+    else
+        PUPPET_VERSION=2.7*
+        FACTER_VERSION=1.*
+    fi
+
+    cat > /etc/apt/preferences.d/00-puppet.pref <<EOF
+Package: puppet puppet-common puppetmaster puppetmaster-common puppetmaster-passenger
+Pin: version $PUPPET_VERSION
+Pin-Priority: 501
+
+Package: facter
+Pin: version $FACTER_VERSION
+Pin-Priority: 501
+EOF
+
     puppet_deb=puppetlabs-release-${lsbdistcodename}.deb
     wget http://apt.puppetlabs.com/$puppet_deb -O $puppet_deb
     dpkg -i $puppet_deb
