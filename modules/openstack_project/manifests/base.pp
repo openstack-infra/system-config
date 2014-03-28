34d490988a06d5dea7eaa84f1a9e3036f8c539ba
Spencer Krum <krum.spencer@gmail.com>
2014-03-28 00:11:10 -0700

    Add node def for puppet3 master
    This change modifies install_puppet.sh to accept a --three option
    setting it to install the latest puppet available. It also creates
    a node definition for the puppetmaster.o.o node, the new 3 master,
    and the master of the future. Changes were made to various classes
    to allow the pinning to version 2.x to be turned off.
    
    Change-Id: I805d6dc50b9de0d8a99cf818d22d06c2dea6090a

--- modules/openstack_project/manifests/base.pp
+++ modules/openstack_project/manifests/base.pp
@@ -1,8 +1,10 @@
 # == Class: openstack_project::base
 #
 class openstack_project::base(
-  $certname = $::fqdn,
-  $install_users = true
+  $certname      = $::fqdn,
+  $install_users = true,
+  $pin_puppet    = '2.7',
+  $pin_facter    = '1',
 ) {
   if ($::osfamily == 'Debian') {
     include apt
@@ -124,7 +126,7 @@ class openstack_project::base(
           owner   => 'root',
           group   => 'root',
           mode    => '0444',
-          source  => 'puppet:///modules/openstack_project/00-puppet.pref',
+          content => template('openstack_project/00-puppet.pref.erb'),
           replace => true,
         }
       }
