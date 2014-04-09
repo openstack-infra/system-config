a0b87c556935fafd4d400369db823528ab96822a
Jeremy Stanley <fungi@yuggoth.org>
2014-04-09 20:56:36 +0000

    Finish puppeting accessbot
    Change-Id: I329545d815cbacf717aa86a4d140290419be9810

--- modules/accessbot/manifests/init.pp
+++ modules/accessbot/manifests/init.pp
@@ -20,9 +20,18 @@ class accessbot(
     ensure => present,
   }
 
+  # A lot of things need yaml, be conservative requiring this package to avoid
+  # conflicts with other modules.
+  if ! defined(Package['python-yaml']) {
+    package { 'python-yaml':
+      ensure => present,
+    }
+  }
+
   exec { 'run_accessbot' :
     command     => '/usr/local/bin/accessbot -c /etc/accessbot/accessbot.config -l /etc/accessbot/channels.yaml >> /var/log/accessbot/accessbot.log 2>&1',
     path        => '/usr/local/bin:/usr/bin:/bin/',
+    user        => 'accessbot',
     refreshonly => true,
     subscribe   => File['/etc/accessbot/channels.yaml'],
     require     => [File['/etc/accessbot/channels.yaml'],
@@ -65,10 +74,9 @@ class accessbot(
   file { '/usr/local/bin/accessbot':
     ensure  => present,
     source  => 'puppet:///modules/accessbot/accessbot.py',
-    group   => 'accessbot',
-    mode    => '0440',
+    mode    => '0555',
     owner   => 'root',
+    group   => 'root',
     replace => true,
-    require => User['accessbot'],
   }
 }
