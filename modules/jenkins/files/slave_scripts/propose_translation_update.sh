8c76adbad32a1b6bfaf757a23fde938fc60ebbc7
Clark Boylan <clark.boylan@gmail.com>
2014-04-15 11:11:51 -0700

    Use proposal bot username in proposal jobs.
    After the switch to using the proposal-bot gerrit user in proposal jobs
    some jobs continued to attempted to use the jenkins user. Fix that and
    use the proposal-bot user.
    
    Change-Id: I146b6ea29d66bd171072e6529413efcc5cae3d8d

--- modules/jenkins/files/slave_scripts/propose_translation_update.sh
+++ modules/jenkins/files/slave_scripts/propose_translation_update.sh
@@ -24,7 +24,7 @@ git review -s
 
 # See if there is an open change in the transifex/translations topic
 # If so, get the change id for the existing change for use in the commit msg.
-change_info=`ssh -p 29418 review.openstack.org gerrit query --current-patch-set status:open project:$ORG/$PROJECT topic:transifex/translations owner:jenkins`
+change_info=`ssh -p 29418 proposal-bot@review.openstack.org gerrit query --current-patch-set status:open project:$ORG/$PROJECT topic:transifex/translations owner:proposal-bot`
 previous=`echo "$change_info" | grep "^  number:" | awk '{print $2}'`
 if [ "x${previous}" != "x" ] ; then
     change_id=`echo "$change_info" | grep "^change" | awk '{print $2}'`
