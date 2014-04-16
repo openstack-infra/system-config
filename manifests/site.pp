e3736a2926b1885281528d2f782e0f3eb2d52968
Monty Taylor <mordred@inaugust.com>
2014-04-15 17:41:45 -0700

    WIP: Use ansible instead of direct ssh calls
    For sake of argument, this is what a python program version can
    look like using ansible - and also sets the root account on puppetmaster
    up for doing more interesting things.
    
    Please note - I do not think this will work as is - as I have not tested
    the intersection of how ansible may or may not choose to run the listed
    command with how we're limiting command lists in the key.
    
    Change-Id: I53112bd1f61d94c0521a32016c8a47c8cf9e50f7

--- manifests/site.pp
+++ manifests/site.pp
@@ -127,12 +127,8 @@ node 'community.openstack.org' {
 node 'ci-puppetmaster.openstack.org' {
   class { 'openstack_project::puppetmaster':
     root_rsa_key    => hiera('puppetmaster_root_rsa_key'),
-    override_list   => [
-      'git01.openstack.org',
-      'git02.openstack.org',
-      'git03.openstack.org',
-      'git04.openstack.org',
-      'git05.openstack.org',
+    puppet_runlist  => [
+      'git*openstack.org',
       'review.openstack.org',
     ],
     sysadmins       => hiera('sysadmins'),
