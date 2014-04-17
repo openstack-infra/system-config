d5b4444314a33e6febd6e16d6bae5858ae01b3ad
Ananth Suryanarayana <Ananth@contrailsystems.com>
2014-04-17 12:33:54 -0700

    Add contrail-git-prep.rb

--- modules/jenkins/files/slave_scripts/contrail-git-prep.rb
+++ modules/jenkins/files/slave_scripts/contrail-git-prep.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-# Find the project e.g. ZUUL_@project : stackforge/contrail-controller
+# Find the project e.g. ZUUL_PROJECT : stackforge/contrail-controller
 if ENV['ZUUL_PROJECT'] !~ /stackforge\/(.*)/ then
-    puts "Error: Cannot find project information off ZUUL_@project : " +
-         "#{ENV['ZUUL_@project']}"
+    puts "Error: Cannot find project information off ZUUL_PROJECT : " +
+         "#{ENV['ZUUL_PROJECT']}"
     exit -1
 end
 
@@ -13,12 +13,15 @@ puts "Working with project #{@project}"
 
 GERRIT_SITE="https://review.opencontrail.org" # ARGV[0]
 GIT_ORIGIN="ssh://zuul@review.opencontrail.org:29418" # ARGV[1]
-DRY_RUN=false
 TOP=ENV['PWD']
+
+def dry_run?
+    return !ENV['DRY_RUN'].nil? && ENV['DRY_RUN'].casecmp("true")
+end
 
 def sh (cmd, ignore = false)
     puts "#{cmd}"
-    if not DRY_RUN then
+    if not dry_run? then
         if cmd =~ /^cd\s+(.*)/ then
             Dir.chdir($1)
         else
@@ -27,15 +30,15 @@ def sh (cmd, ignore = false)
     end
     puts output if !output.nil? and !output.empty?
 
-    if !DRY_RUN and !ignore and $? != 0 then
+    if !dry_run? and !ignore and $? != 0 then
         puts "Comamnd #{cmd} failed with exit code #{$?}"
     end
     return output
 end
 
 def setup_gerrit_repo
-    sh "mkdir #{TOP}/sb_test"
-    sh "cd #{TOP}/sb_test"
+    sh "mkdir #{TOP}/#{@project}"
+    sh "cd #{TOP}/#{@project}"
 
     # This clones a git repo with appropriate review patch
     sh "/usr/local/jenkins/slave_scripts/gerrit-git-prep.sh " +
@@ -48,7 +51,7 @@ def setup_contrail_repo
     sh "mkdir #{TOP}/repo"
     sh "cd #{TOP}/repo"
 
-    # Initialize a repo - Need to retrieve 
+    # Initialize a repo - Need to retrieve
     sh "/usr/local/jenkins/slave_scripts/repo init "
        "-u git@github.com:Juniper/contrail-vnc-private " +
        "-m mainline/ubuntu-12-04/manifest-havana.xml"
@@ -60,14 +63,20 @@ end
 def switch_gerrit_repo
 
     # Now, switch to project's git repo.
-    sh "mv #{TOP}/#{@project} #{TOP}/.#{@project}.orig"
-    sh "mv #{TOP}/sb_test #{TOP}/repo/#{@project}"
+    sh "mv #{TOP}/repo/#{@project} #{TOP}/repo/.#{@project}.orig"
+    sh "mv #{TOP}/#{@project} #{TOP}/repo/#{@project}"
+end
+
+def make_ready_for_build
+    sh "python #{TOP}/repo/third_party/fetch_packages.py"
+    sh "python #{TOP}/repo/distro/third_party/fetch_packages.py"
 end
 
 def main
     setup_gerrit_repo
     setup_contrail_repo
     switch_gerrit_repo
+    make_ready_for_build
 end
 
 main
