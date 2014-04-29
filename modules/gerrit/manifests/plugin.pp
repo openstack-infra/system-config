cbf66d5ea3f0febd38fd17d614eb238a572b2fdc
Khai Do <zaro0508@gmail.com>
2014-04-29 16:17:23 -0700

    puppetize installation of gerrit third party plugins
    This change automates the installation of gerrit third party plugins.  This
    does not include core plugins, which have already been automated.  The first
    third party plugin that we automate is the javamelody plugin.
    
    Change-Id: I163353c2e9d59e4815b4544f983b54d68ede70a8

---
+++ modules/gerrit/manifests/plugin.pp
@@ -0,0 +1,40 @@
+#
+# Defined resource type to install gerrit plugins.
+#
+# Borrowed from: https://github.com/jenkinsci/puppet-jenkins
+#
+
+define gerrit::plugin(
+  $version=0,
+) {
+  $plugin            = "${name}-${version}.jar"
+  $plugin_dir        = '/home/gerrit2/review_site/plugins'
+  $plugin_parent_dir = '/home/gerrit2/review_site'
+  $base_url          = "http://tarballs.openstack.org/ci/gerrit/plugins/${name}"
+
+  if (!defined(File[$plugin_dir])) {
+    file {
+      [
+        $plugin_parent_dir,
+        $plugin_dir,
+      ]:
+        ensure  => directory,
+        owner   => 'gerrit2',
+        group   => 'gerrit2',
+        require => File[$plugin_parent_dir],
+    }
+  }
+
+  class { 'gerrit::user':
+    ensure  => present,
+  }
+
+  exec { "download-${name}" :
+    command  => "wget --no-check-certificate ${base_url}/${plugin}",
+    cwd      => $plugin_dir,
+    require  => File[$plugin_dir],
+    path     => ['/usr/bin', '/usr/sbin',],
+    user     => 'gerrit2',
+    unless   => "test -f ${plugin_dir}/${plugin}",
+  }
+}
