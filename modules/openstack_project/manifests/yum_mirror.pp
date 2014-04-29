ebdf4c4e0e0294d9a9c7041afd51177173441ef0
Dan Prince <dprince@redhat.com>
2014-04-28 22:51:53 -0400

    Add yum.openstack.org lightweight Fedora 20 mirror
    Adds a new openstack_project manifest to create a lightweight
    F20 mirror containing x86_64 testing/updates/release RPMs.
    
    This will be useful for TripleO where we do lots of disk
    image building and having a reliable mirror is critical.
    
    The plan is to run this mirror in the public cloud and use
    local Squid caches to avoid repeated downloads. We may also
    choose to host individual mirrors in each TripleO CI cloud.
    
    Tested on a Centos 6.5 host machine.
    
    Change-Id: If47acc1e268a38b3bf1c06b2520472b9b089bff0

---
+++ modules/openstack_project/manifests/yum_mirror.pp
@@ -0,0 +1,42 @@
+# == Class: openstack_project::yum_mirror
+#
+class openstack_project::yum_mirror (
+  $sysadmins = [],
+) {
+
+  include yum::mirror
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [22, 80, 443],
+    sysadmins                 => $sysadmins,
+  }
+
+  # A lightweight Fedora 20 x86_64 binary only mirror for
+  # testing/updates/release only
+  # Mirror URL paths are provided to follow the normal Fedora mirroring
+  # conventions to allow for easy baseurl configuration
+  yum::repo { 'f20-x86_64-updates-testing':
+    description => 'Fedora 20 - x86_64 - Updates Testing',
+    mirrorlist  => 'https://mirrors.fedoraproject.org/metalink?repo=updates-testing-f20&arch=x86_64',
+    cron_hour   => 0,
+    cron_minute => 15,
+    url_path    => 'fedora/updates/testing/20/x86_64/',
+  }
+
+  yum::repo { 'f20-x86_64-updates':
+    description  => 'Fedora 20 - x86_64 - Updates',
+    mirrorlist   => 'https://mirrors.fedoraproject.org/metalink?repo=updates-released-f20&arch=x86_64',
+    cron_hour    => 1,
+    cron_minute  => 15,
+    url_path     => 'fedora/updates/20/x86_64/',
+  }
+
+  yum::repo { 'f20-x86_64-release':
+    description  => 'Fedora 20 - x86_64 - Release',
+    mirrorlist   => 'https://mirrors.fedoraproject.org/metalink?repo=fedora-20&arch=x86_64',
+    cron_hour    => 2,
+    cron_minute  => 15,
+    url_path     => 'fedora/releases/20/Everything/x86_64/os/',
+  }
+
+}
