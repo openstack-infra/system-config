eb4e5b20210a375e8895cf789baac767b809a71b
Jeremy Stanley <fungi@yuggoth.org>
2014-04-30 20:02:33 +0000

    Stop over-managing user SSH keyfiles
    * modules/user/manifests/virtual/localuser.pp: This reverts part of
    0d82c62 which was providing a temporary transition correcting
    permissions and ownership as well as removing the results of a
    previous failed implementation of keyfile management. Unfortunately
    that also caused nodepool images to no longer set up accounts
    correctly, but reverting the transitional bits fixes that issue.
    
    Change-Id: Ic3e7f0a3b82bbc8a23707b88f9fe381e9d70e356

--- modules/user/manifests/virtual/localuser.pp
+++ modules/user/manifests/virtual/localuser.pp
@@ -7,7 +7,6 @@ define user::virtual::localuser(
   $groups     = [ 'sudo', 'admin', ],
   $sshkeys    = '',
   $key_id     = '',
-  $old_keys   = [],
   $shell      = '/bin/bash',
   $home       = "/home/${title}",
   $managehome = true
@@ -29,41 +28,13 @@ define user::virtual::localuser(
     require    => Group[$title],
   }
 
-  file { "${title}_sshdir":
-    ensure  => directory,
-    name    => "${home}/.ssh",
-    owner   => $title,
-    group   => $title,
-    mode    => '0700',
-    require => User[$title],
-  }
-
-  file { "${title}_keyfile":
-    ensure  => present,
-    mode    => '0600',
-    name    => "${home}/.ssh/authorized_keys",
-    require => File["${title}_sshdir"],
-  }
-
   ssh_authorized_key { $key_id:
     ensure  => present,
     key     => $sshkeys,
     user    => $title,
     type    => 'ssh-rsa',
-    require => File["${title}_keyfile"],
   }
 
-  ssh_authorized_key { "${title}_keys":
-    ensure => absent,
-    user   => $title,
-  }
-
-  if ( $old_keys != [] ) {
-    ssh_authorized_key { $old_keys:
-      ensure => absent,
-      user   => $title,
-    }
-  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
