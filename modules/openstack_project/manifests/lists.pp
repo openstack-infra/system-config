4dbd9877f3478f023006d4f4aa17a83926520344
Stefano Maffulli <stefano@maffulli.net>
2014-05-01 10:39:09 -0700

    Adding list for OpenStack Superuser magazine's team
    Change-Id: I04a5cac71ce81826b543bd7d155ee718674598f0

--- modules/openstack_project/manifests/lists.pp
+++ modules/openstack_project/manifests/lists.pp
@@ -166,4 +166,13 @@ class openstack_project::lists(
     mailserver  => $listdomain,
   }
 
+  maillist { 'openstack-superuser':
+    ensure      => present,
+    admin       => 'lauren@openstack.org',
+    password    => $listpassword,
+    description => 'Discussions for Superuser editorial advisors to collaborate, and for readers to be able to contact the editorial team to make suggestions, provide feedback',
+    webserver   => $listdomain,
+    mailserver  => $listdomain,
+  }
+
 }
