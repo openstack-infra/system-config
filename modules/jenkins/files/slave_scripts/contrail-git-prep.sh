ffd769c14225103f9e2e3d031ad57f34529a51b5
Ananth Suryanarayana <anantha@juniper.net>
2014-05-02 11:01:37 -0700

    get rid of old scripts first

--- modules/jenkins/files/slave_scripts/contrail-git-prep.sh
+++ modules/jenkins/files/slave_scripts/contrail-git-prep.sh
@@ -3,6 +3,7 @@
 set -ex
 
 # Copy slave scripts from the jenkins.opencontrail.org master
+rm -rf /usr/local/jenkins/slave_scripts
 ssh root@148.251.110.18 tar zcf - /usr/local/jenkins/slave_scripts | tar -zx -C /
 
 # Run the real script
