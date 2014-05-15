f575374d20dac02aa24e4e3e0520e6c2692841a9
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2014-05-15 10:01:53 +0000

    Parameterize SSL certificate attributes on review.pp manifest
    review.pp manifest contains site specific values for
    SSL certificate attributes.
    The hostname may change overtime and forces other developers
    willing to use the manifest to give SSL certificate names
    that are not descriptive of their environment.
    This change parameterize those values by adding a hiera lookup
    at the node definition level and putting some sane defaults
    in the gerrit.pp manifest.
    
    Closes-Bug: 1319746
    Change-Id: I245c3caaf267f3d354daa5c7d5897d62239dcf9e

--- modules/openstack_project/manifests/review.pp
+++ modules/openstack_project/manifests/review.pp
@@ -46,8 +46,11 @@ class openstack_project::review (
   $gerritbot_ssh_rsa_key_contents = '',
   $gerritbot_ssh_rsa_pubkey_contents = '',
   # Register SSL keys and pass their contents in.
+  $ssl_cert_file = '',
   $ssl_cert_file_contents = '',
+  $ssl_key_file = '',
   $ssl_key_file_contents = '',
+  $ssl_chain_file = '',
   $ssl_chain_file_contents = '',
   # Create SSH server key by hand and supply here.
   $ssh_dsa_key_contents = '',
@@ -79,11 +82,9 @@ class openstack_project::review (
 ) {
 
   class { 'openstack_project::gerrit':
-    ssl_cert_file                       =>
-      '/etc/ssl/certs/review.openstack.org.pem',
-    ssl_key_file                        =>
-      '/etc/ssl/private/review.openstack.org.key',
-    ssl_chain_file                      => '/etc/ssl/certs/intermediate.pem',
+    ssl_cert_file                       => $ssl_cert_file,
+    ssl_key_file                        => $ssl_key_file,
+    ssl_chain_file                      => $ssl_chain_file,
     ssl_cert_file_contents              => $ssl_cert_file_contents,
     ssl_key_file_contents               => $ssl_key_file_contents,
     ssl_chain_file_contents             => $ssl_chain_file_contents,
