02669c1e33e94909b39d755c0287955bc9b97b2a
Ananth Suryanarayana <anantha@juniper.net>
2014-05-16 15:10:08 -0700

    Remove unsed openstack jenkins job scripts

--- modules/jenkins/files/slave_scripts/lvm-kexec-reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-lvremove -f /dev/main/last_root
-lvrename /dev/main/root last_root
-lvcreate -L20G -s -n root /dev/main/orig_root
-APPEND="`cat /proc/cmdline`"
-kexec -l /vmlinuz --initrd=/initrd.img --append="$APPEND"
-nohup bash -c "sleep 2; kexec -e" </dev/null >/dev/null 2>&1 &
