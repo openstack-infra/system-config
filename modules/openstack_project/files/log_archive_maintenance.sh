7e480a1ed11ff5813b242d96971b32c2672a73fd
Jeremy Stanley <fungi@yuggoth.org>
2014-05-20 20:54:13 +0000

    Remove old non-gz files in log archive maintenance
    * modules/openstack_project/files/log_archive_maintenance.sh: Start
    matching for removal on any files in the logs tree besides those we
    explicitly don't compress. Also do it via a refactor to allow for
    improved optimization and avoid unnecessary redundancy.
    
    Change-Id: I5ff6defb4d403f3017ab098c2ac1df3777662933

--- modules/openstack_project/files/log_archive_maintenance.sh
+++ modules/openstack_project/files/log_archive_maintenance.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 sleep $((RANDOM%600)) && \
 flock -n /var/run/gziplogs.lock \
-find -O3 /srv/static/logs/ -depth \
-    \( \
-      \( -type f -mmin +10 -not -name robots.txt \
-          -not -wholename /srv/static/logs/help/\* \
-          -not -name \*\[.-\]gz -not -name \*\[._-\]\[zZ\] \
-          \( -name \*.txt -or -name \*.html -or -name tmp\* \) \
-          -exec gzip \{\} \; \) \
-      -o \( -type f -mtime +120 -name \*.gz -execdir rm \{\} \; \) \
-      -o \( -type d -not -name lost+found -empty -mtime +1 \
-          -execdir rmdir {} \; \) \
-    \)
-find -O3 /srv/static/docs-draft/ -depth \
-    \( \
-      \( -type f -mtime +21 -name \*.gz -execdir rm \{\} \; \) \
-      -o \( -type d -not -name lost+found -empty -mtime +1 \
-          -execdir rmdir {} \; \) \
-    \)
+find -O3 /srv/static/logs/ -depth -not -name robots.txt -not -name lost+found \
+        -not -wholename /srv/static/logs/help/\* \( \
+    \( -type f -mmin +10 -not -name \*\[.-\]gz -not -name \*\[._-\]\[zZ\] \
+        \( -name \*.txt -or -name \*.html -or -name tmp\* \) \
+        -exec gzip \{\} \; \) \
+    -o \( -type f -mtime +120 -execdir rm \{\} \; \) \
+    -o \( -type d -not -empty -mtime +1 -execdir rmdir {} \; \) \)
+find -O3 /srv/static/docs-draft/ -depth -not -name lost+found \( \
+    \( -type f -mtime +21 -name \*.gz -execdir rm \{\} \; \) \
+    -o \( -type d -empty -mtime +1 -execdir rmdir {} \; \) \)
