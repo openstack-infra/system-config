3c4bc4ffe434015586ad194091f970da9c63cc5d
Ananth Suryanarayana <anantha@juniper.net>
2014-05-20 17:03:15 -0700

    add dual topo file

--- modules/jenkins/files/slave_scripts/contrail-systest-job.rb
+++ modules/jenkins/files/slave_scripts/contrail-systest-job.rb
@@ -11,6 +11,53 @@ def create
     Vm.create_subslaves(2)
 end
 
+def get_dual_topo (vm1, vm2)
+    topo =<<EOF
+from fabric.api import env
+
+host1 = 'root@#{vm1.hostip}'
+host2 = 'root@#{vm2.hostip}
+host_build = 'root@#{vm1.hostip}'
+ext_routers = []
+router_asn = 64512
+
+env.roledefs = {
+    'all': [host1],
+    'cfgm': [host1],
+    'openstack': [host1],
+    'control': [host1],
+    'compute': [host2],
+    'collector': [host1],
+    'webui': [host1],
+    'database': [host1],
+    'build': [host_build],
+}
+
+env.openstack_admin_password = 'c0ntrail123'
+env.hostnames = {
+    'all': ['#{vm1.vmname}', '#{vm2.vmname}']
+}
+
+env.password = 'c0ntrail123'
+env.passwords = {
+    host1: 'c0ntrail123',
+    host2: 'c0ntrail123',
+    host_build: 'c0ntrail123',
+}
+
+env.ostypes = {
+    host1:'ubuntu',
+    host1:'ubuntu',
+}
+
+env.test_repo_dir='#{ENV['HOME']}/test'
+env.mail_from='ci-admin@opencontrail.org'
+env.mail_to='ci-admin@opencontrail.org'
+# env.interface_rename = True
+EOF
+    return topo
+end
+
 def setup
     image = "/root/contrail-install-packages_1.06-12~havana_all.deb"
 
@@ -18,12 +65,16 @@ def setup
     vms = Vm.init_all if vms.empty?
 
     vms.each { |vm|
-        Sh.run "scp #{image} root@#{vm.vmname}:."
-        Sh.run "ssh #{vm.vmname} dpkg -i #{image}"
+        # Sh.run "scp #{image} root@#{vm.vmname}:."
+        # Sh.run "ssh #{vm.vmname} dpkg -i #{image}"
     }
 
     vm = vms.first
-    puts "ssh #{vm.vmname} /opt/contrail/util/setup.sh"
+    # Sh.run "ssh #{vm.vmname} /opt/contrail/contrail_packages/setup.sh"
+
+    topo_file = "/root/testbed_dual.py"
+    File.open(topo_file, "w") { |fp| fp.write get_dual_topo }
+    Sh.run "scp #{topo_file} #{vm.vmname}:/opt/contrail/utils/fabfile/testbeds/testbed.py"
     puts "ssh #{vm.vmname} fab install_images"
     puts "ssh #{vm.vmname} fab setup_all"
     puts "ssh #{vm.vmname} fab add_images"
