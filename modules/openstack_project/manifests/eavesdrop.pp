eae45cc923f2e74a3f322e4cdbeba2e819e913e1
Sergey Lukjanov <slukjanov@mirantis.com>
2014-05-22 19:40:44 +0400

    Remove obsolete #openstack-climate channel
    Transition period for climate-blazar renaming ended.
    
    Change-Id: Ie9fcca1666f4a6f96e13d420044abee07826fada

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -42,7 +42,6 @@ class openstack_project::eavesdrop (
         '#openstack',
         '#openstack-blazar',
         '#openstack-ceilometer',
-        '#openstack-climate',
         '#openstack-dev',
         '#openstack-dns',
         '#openstack-infra',
