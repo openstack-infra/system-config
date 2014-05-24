ef0ef57a2748962bcd37da97704d17fc66781e31
Andreas Jaeger <aj@suse.de>
2014-05-24 20:12:35 +0200

    Extract translations for log messages
    Create for each log level its own translation file and update it
    like the user visible translation file. Note that the debug log level
    does not get translated and thus is excluded.
    
    implements bp log-messages-translation-domain-rollout
    
    Change-Id: Ib2f6f2450526655c41c27f411fb2868076049068

--- modules/openstack_project/files/slave_scripts/upstream_translation_update.sh
+++ modules/openstack_project/files/slave_scripts/upstream_translation_update.sh
@@ -26,16 +26,48 @@ git config user.email "jenkins@openstack.org"
 if [ ! -d .tx ] ; then
     tx init --host=https://www.transifex.com
 fi
+# User visible strings
 tx set --auto-local -r ${PROJECT}.${PROJECT}-translations "${PROJECT}/locale/<lang>/LC_MESSAGES/${PROJECT}.po" --source-lang en --source-file ${PROJECT}/locale/${PROJECT}.pot -t PO --execute
 
-# Update the .pot file
+# Strings for various log levels
+LEVELS="info warning error critical"
+# Keywords for each log level:
+declare -A LKEYWORD
+LKEYWORD['info']='_LI'
+LKEYWORD['warning']='_LW'
+LKEYWORD['error']='_LE'
+LKEYWORD['critical']='_LC'
+for level in $LEVELS ; do
+  tx set --auto-local -r ${PROJECT}.${PROJECT}-log-${level}-translations \
+    "${PROJECT}/locale/<lang>/LC_MESSAGES/${PROJECT}.po-log-${level}" \
+    --source-lang en \
+    --source-file ${PROJECT}/locale/${PROJECT}-log-${level}.pot -t PO \
+    --execute
+done
+
+# Update the .pot files
 python setup.py extract_messages
+for level in $LEVELS ; do
+  python setup.py extract_messages --no-default-keywords \
+    --keyword ${LKEYWORD[$level]} \
+    --output-file ${PROJECT}/locale/${PROJECT}-log-${level}.pot
+done
+
 PO_FILES=`find ${PROJECT}/locale -name '*.po'`
 if [ -n "$PO_FILES" ]
 then
     # Use updated .pot file to update translations
     python setup.py update_catalog --no-fuzzy-matching --ignore-obsolete=true
 fi
+for level in $LEVELS ; do
+  PO_FILES=`find ${PROJECT}/locale -name '${PROJECT}-log-${level}.po'`
+  if [ -n "$PO_FILES" ]
+  then
+    python setup.py update_catalog --no-fuzzy-matching \
+        --ignore-obsolete=true --domain=${PROJECT}-log-${level}
+  fi
+done
+
 # Add all changed files to git
 git add $PROJECT/locale/*
 
