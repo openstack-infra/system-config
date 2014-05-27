e376d82672ba7df4429de58667875a7fcfc31ecb
Clark Boylan <clark.boylan@gmail.com>
2014-05-27 14:20:40 -0700

    Rotate zuul logs with logrotate
    Python logrotate does not compress. Use logrotate utility to do log
    rotation so that older logs can be compressed saving disk space.
    
    Change python logging to use normal file handlers as it will no longer
    be doing the rotation for us. Keep all the file names the same.
    
    Change-Id: I7302e4b197e99fd49fd9d5678702333b17a46efa

--- modules/zuul/manifests/server.pp
+++ modules/zuul/manifests/server.pp
@@ -29,4 +29,42 @@ class zuul::server (
     require     => File['/etc/init.d/zuul'],
     refreshonly => true,
   }
+
+  include logrotate
+  logrotate::file { 'zuul.log':
+    log     => '/var/log/zuul/zuul.log',
+    options => [
+      'compress',
+      'copytruncate',
+      'missingok',
+      'rotate 30',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['zuul'],
+  }
+  logrotate::file { 'zuul-debug.log':
+    log     => '/var/log/zuul/debug.log',
+    options => [
+      'compress',
+      'copytruncate',
+      'missingok',
+      'rotate 30',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['zuul'],
+  }
+  logrotate::file { 'gearman-server.log':
+    log     => '/var/log/zuul/gearman-server.log',
+    options => [
+      'compress',
+      'copytruncate',
+      'missingok',
+      'rotate 7',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['zuul'],
+  }
 }
