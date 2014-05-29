428aaed3c85baf6d88bd7d479a69e06bead82d3f
Ananth Suryanarayana <anantha@juniper.net>
2014-05-29 13:50:28 -0700

    archive crontab command

--- setup/jenkins/setup.sh
+++ setup/jenkins/setup.sh
@@ -18,8 +18,8 @@ function setup_ntp {
 # Setup jenkins ssh server port to 6000 and jnlp slave port to 6001
 
 function install_crontab_hack() {
-# Run this once every 10 minues
-# */30 * * * * /path/to/command java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 disable-job ci-contrail-controller-build && java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 enable-job ci-contrail-controller-build
+# Run this once every 30 minues
+*/30 * * * * java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 list-jobs | xargs -n 1 java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 disable-job && java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 list-jobs | xargs -n 1 java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 enable-job
 }
 
 function iptables_restart {
