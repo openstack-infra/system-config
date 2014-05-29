1a048ef11bf45aacad47f5e98dbd3e4b9e306c9a
Ananth Suryanarayana <anantha@juniper.net>
2014-05-29 11:19:13 -0700

    cleanup systest job

--- setup/contrail-build/setup.sh
+++ setup/contrail-build/setup.sh
@@ -10,8 +10,9 @@ ANSIBLE=$SB/tools/scripts/config-ansible
 # apt-get update
 # apt-get -y install sshpass
 
+# e.g. ci-slave-10.0.1.2
 HOSTNAME=`curl -s http://169.254.169.254/openstack/2012-08-10/meta_data.json | python -m json.tool | \grep \"name\": | awk -F '\"' '{print $4}'`
-HOSTIP=`echo $HOSTNAME  | cut -d '-' -f 2`
+HOSTIP=`echo $HOSTNAME  | cut -d '-' -f 3`
 
 if [ ! -f $SB/tools/scripts/config-ansible ]; then
     rm -rf $SB
@@ -30,14 +31,49 @@ python $ANSIBLE -n $HOSTNAME -i $HOSTIP -r contrail-ec-build03.juniper.net -d ub
 # pip install --upgrade pip
 
 function build_contrail() {
+rm -rf $REPO
+mkdir -p $REPO
+cd $REPO
 repo init -u git@github.com:Juniper/contrail-vnc-private -m mainline/ubuntu-12-04/manifest-havana.xml
+repo sync
+rm -rf /tmp/cache
+ln -sf /home/jenkins/tmp/cache /tmp/cache
 python $REPO/third_party/fetch_packages.py 
 python $REPO/distro/third_party/fetch_packages.py
-scons -c $REPO/build/third_party/log4cplus/
-scons $REPO/build/third_party/log4cplus/
+scons
 cd $REPO/tools/packaging/build/
 ./packager.py 
+# mkdir -p /root/foo/build/packages/ifmap-server/lib
 # build/debian/contrail-analytics-venv/opt/contrail/analytics-venv/bin/pip install --upgrade setuptools
+}
+
+function misc() {
+apt-get -y install virt-manager vnc4server ruby-ronn qemu-kvm qemu-system bridge-utils uml-utilities  module-assistant default-jdk javahelper lvm2
+rm -rf /tmp/cache
+ln -sf /home/jenkins/tmp/cache /tmp/cache
+rm -rf /cs-shared/builder/cache
+mkdir -p /cs-shared/builder/cache
+ln -sf /cs-shared/builder/cache/ubuntu1204 /cs-shared/builder/cache/ubuntu-12-04
+scp -r anantha@ubuntu-build02:/github-build/distro-packages/build/ubuntu1204 /cs-shared/builder/cache/.
+}
+
+function bridge_eth0() {
+CFG=<<EOF
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+
+auto br0
+iface br0 inet dhcp
+    bridge_ports eth0
+    bridge_stp off
+    bridge_fd 0
+    bridge_maxwait 0
+EOF
+echo $CFG > /etc/network/interfaces
+service networking restart
 }
 
 build_vm
