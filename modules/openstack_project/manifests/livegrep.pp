c27b18194b21f558ac54cd95d7ac8b264d04bb42
Alex Gaynor <alex.gaynor@gmail.com>
2014-05-30 10:21:03 -0700

    Added a puppet module for livegrep.
    Change-Id: Ia3b5b628cc7d8b98cbd3483a0eb6f240eebc9b3f

---
+++ modules/openstack_project/manifests/livegrep.pp
@@ -0,0 +1,63 @@
+# == Class: openstack_project::livegrep
+#
+class openstack_project::livegrep (
+  $sysadmins = []
+) {
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [80],
+    sysadmins                 => $sysadmins,
+  }
+  include livegrep
+  livegrep::site { 'openstack':
+    repos => {
+      'openstack/barbican' => {
+        'github' => 'https://github.com/openstack/barbican'
+      },
+      'openstack/python-barbicanclient' => {
+        'github' => 'https://github.com/openstack/python-barbicanclient'
+      },
+
+      'openstack/cinder' => {
+        'github' => 'https://github.com/openstack/cinder'
+      },
+      'openstack/python-cinderclient' => {
+        'github' => 'https://github.com/openstack/python-cinderclient'
+      },
+
+      'openstack/glance' => {
+        'github' => 'https://github.com/openstack/glance'
+      },
+      'openstack/python-glanceclient' => {
+        'github' => 'https://github.com/openstack/python-glanceclient'
+      },
+
+      'openstack/keystone' => {
+        'github' => 'https://github.com/openstack/keystone'
+      },
+      'openstack/python-keystoneclient' => {
+        'github' => 'https://github.com/openstack/python-keystoneclient'
+      },
+
+      'openstack/marconi' => {
+        'github' => 'https://github.com/openstack/marconi'
+      },
+      'openstack/python-marconiclient' => {
+        'github' => 'https://github.com/openstack/python-marconiclient'
+      },
+
+      'openstack/nova' => {
+        'github' => 'https://github.com/openstack/nova'
+      },
+      'openstack/python-novaclient' => {
+        'github' => 'https://github.com/openstack/python-novaclient'
+      },
+
+      'openstack/swift' => {
+        'github' => 'https://github.com/openstack/swift'
+      },
+      'openstack/python-swiftclient' => {
+        'github' => 'https://github.com/openstack/python-swiftclient'
+      },
+    },
+  }
+}
