43de2620d61636247e7ed7948d826af92de2519d
Ananth Suryanarayana <anantha@juniper.net>
2014-06-01 11:53:03 -0700

    update ci_setup.sh

--- modules/jenkins/files/slave_scripts/ci-infra/ci_setup.sh
+++ modules/jenkins/files/slave_scripts/ci-infra/ci_setup.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-export WORKSPACE=$PWD
-SKIP_JOBS=$WORKSPACE/skip_jobs
-
-# Check if this run needs to be skipped.
-ssh root@jenkins.opencontrail.org ls /root/ci-test/skip_jobs
-if [ "$?" == "0" ]; then
-    echo Jobs skipped due to jenkins.opencontrail.org:/root/ci-test/skip_jobs
-    touch $SKIP_JOBS
-    exit 0
-fi
-
-rm -rf $SKIP_JOBS
-
 BRANCH=master
 HOSTNAME=`cat /etc/hostname`
 ssh root@jenkins.opencontrail.org ls /root/ci-test/\*$HOSTNAME\*-test
@@ -26,3 +13,15 @@ ln -sf /root/contrail-infra-config/modules/jenkins/files/slave_scripts /usr/loca
 git clone -b $BRANCH git@github.com:juniper/contrail-infra-config.git /root/contrail-infra-config
 # ssh root@$SLAVE_MASTER -p $PORT tar zcf - /usr/local/jenkins/slave_scripts | tar -zx -C /
 
+export WORKSPACE=$PWD
+SKIP_JOBS=$WORKSPACE/skip_jobs
+
+# Check if this run needs to be skipped.
+ssh root@jenkins.opencontrail.org ls /root/ci-test/skip_jobs
+if [ "$?" == "0" ]; then
+    echo Jobs skipped due to jenkins.opencontrail.org:/root/ci-test/skip_jobs
+    touch $SKIP_JOBS
+    exit 0
+fi
+
+rm -rf $SKIP_JOBS
