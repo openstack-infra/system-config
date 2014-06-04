c7ad6909e5482eef82154a04d563e7deac3f8ef3
Clark Boylan <clark.boylan@gmail.com>
2014-06-03 17:50:39 -0700

    Install yappi on zuul nodes.
    Zuul is growing the ability to toggle yappi profiling on and off via
    SIGUSR2. This will be an optional requirement so we need to install it
    ourselves if we want it. Install it here.
    
    Change-Id: I97086b9301e1801d4af2156e81dff3c385472333

--- modules/zuul/manifests/init.pp
+++ modules/zuul/manifests/init.pp
@@ -59,6 +59,12 @@ class zuul (
     ensure => present,
   }
 
+  package { 'yappi':
+    ensure   => present,
+    provider => pip,
+    require  => Class['pip'],
+  }
+
   # A lot of things need yaml, be conservative requiring this package to avoid
   # conflicts with other modules.
   if ! defined(Package['python-yaml']) {
