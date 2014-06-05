6fe16ed6e7d43250e462b60d028f1285e781ccf2
Michael Krotscheck <krotscheck@gmail.com>
2014-06-04 18:14:27 -0700

    Modularized StoryBoard Module
    In order to get the puppet module for storyboard up to a level where we can
    publish it to puppetforge, I did some work on it to create separate modules
    which can be used by anyone to install storyboard.
    
    - API and Webclient are now installed via storyboard::application, which
    assumes that you can provide the DB connection criteria.
    - storyboard::cert is now a separate class, which accepts either files or strings,
    which generates the SSL certificate and chain files for storyboard.
    - storyboard::params is our dependency checker.
    - storyboard::init will install a standalone, entirely self-contained instance
    of storyboard.
    - Added various puppet module files necessary for eventual deployment
    to puppetforge.
    - Added README.md documentation for later puppetforge addition.
    
    This patch also includes a new module: example42-puppi, which is a series
    of convenience utilities useful for deployment. For example, puppi::netinstall
    (used here) will fetch tarballs and zip files and extract them into a provided
    directory. It also contains changes to the storyboard configuration for the
    new refresh token support patch in #94363
    
    Change-Id: I6ab8c24b308df38774fc0694d218dcb5022cd899

---
+++ modules/storyboard/manifests/mysql.pp
@@ -0,0 +1,36 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: storyboard::mysql
+#
+# The StoryBoard MySQL manifest will install a standalone, localhost instance
+# of mysql for storyboard to connect to.
+#
+class storyboard::mysql (
+  $mysql_database      = 'storyboard',
+  $mysql_user          = 'storyboard',
+  $mysql_user_password = 'changeme',
+) {
+
+  # Install MySQL
+  include mysql::server
+
+  # Add the storyboard database.
+  mysql::db { $mysql_database:
+    user     => $mysql_user,
+    password => $mysql_user_password,
+    host     => 'localhost',
+    grant    => ['all'],
+  }
+}
