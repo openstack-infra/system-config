ee14010271dc171521506fe849865c9864ba640a
Ananth Suryanarayana <anantha@juniper.net>
2014-06-04 20:56:52 -0700

    add SLAVE_NODE_LABEL env

--- modules/jenkins/files/slave_scripts/ci-infra/slave_auto_run.rb
+++ modules/jenkins/files/slave_scripts/ci-infra/slave_auto_run.rb
@@ -36,8 +36,10 @@ def slave
         Sh.run "wget -o #{jar_file} http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/1.15/swarm-client-1.15-jar-with-dependencies.jar"
     end
 
+    ENV['SLAVE_NODE_LABEL'] ||= "juniper-tests"
+
     # CI-Slaves register to jenkins.opencontrail.org
-    exec %{java -jar #{jar_file} -labels juniper-tests -mode normal -master http://jenkins.opencontrail.org:8080/ -executors 1 -fsroot /home/jenkins -username #{jenkins_user} -password #{jenkins_password} -name #{@hostname} 2>&1 | tee /root/jenkins-slave.log}
+    exec %{java -jar #{jar_file} -labels #{ENV['SLAVE_NODE_LABEL']} -mode normal -master http://jenkins.opencontrail.org:8080/ -executors 1 -fsroot /home/jenkins -username #{jenkins_user} -password #{jenkins_password} -name #{@hostname} 2>&1 | tee /root/jenkins-slave.log}
 end
 
 def subslave
