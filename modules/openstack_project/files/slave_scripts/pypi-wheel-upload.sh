835ab0603aff49258aa56df409378932d7a861ae
Jeremy Stanley <fungi@yuggoth.org>
2014-06-06 14:00:53 +0000

    Determine wheel names for PyPI uploading
    * .../files/slave_scripts/pypi-extract-name.py: Add --tarball and
    --wheel switches to return the proper corresponding name versions,
    since they differ in some cases in the presence of non-alphanumeric
    characters. Make --tarball the default for backward-compatibility.
    This adds a dependency on the wheel module.
    
    * .../files/slave_scripts/pypi-tarball-upload.sh: Request the
    tarball version of the package name prefix.
    
    * .../files/slave_scripts/pypi-wheel-upload.sh: Request the wheel
    version of the package name.
    
    * .../manifests/pypi_slave.pp: Always install the latest versions of
    the wheel and twine packages.
    
    Change-Id: I2ae724f1f4d0a88b602e3581a0b06b999fb4833c

--- modules/openstack_project/files/slave_scripts/pypi-wheel-upload.sh
+++ modules/openstack_project/files/slave_scripts/pypi-wheel-upload.sh
@@ -23,7 +23,7 @@ TAG=`echo $ZUUL_REF | sed 's/^refs.tags.//'`
 
 # Look in the setup.cfg to determine if a package name is specified, but
 # fall back on the project name if necessary
-DISTNAME=`/usr/local/jenkins/slave_scripts/pypi-extract-name.py \
+DISTNAME=`/usr/local/jenkins/slave_scripts/pypi-extract-name.py --wheel \
     || echo $PROJECT`
 # Look in the setup.cfg to see if this is a universal wheel or not
 WHEELTYPE=`/usr/local/jenkins/slave_scripts/pypi-extract-universal.py`
