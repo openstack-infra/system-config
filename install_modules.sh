6a64eb5223c593f6961fd7e95ffa4f07e4ad3891
Michael Krotscheck <krotscheck@gmail.com>
2014-06-06 09:50:20 -0700

    Added RabbitMQ to StoryBoard
    In order to support subscriptions in StoryBoard, we're adding a
    queueing system to support triggered events. The specification in
    question is #95307, which goes into detail on the various different
    options evaluated. I also anticipate that this system can be used
    for report generation.
    
    Change-Id: I81ac598fe0b7e339a714bed8c9173a2dce070e67
    Story: 96

--- install_modules.sh
+++ install_modules.sh
@@ -55,6 +55,8 @@ MODULES["puppetlabs-firewall"]="0.0.4"
 MODULES["puppetlabs-puppetdb"]="3.0.1"
 MODULES["stankevich-python"]="1.6.6"
 MODULES["example42-puppi"]="2.1.9"
+MODULES["puppetlabs-rabbitmq"]="3.1.0"
+MODULES["garethr-erlang"]="0.1.0"
 
 SOURCE_MODULES["https://github.com/nibalizer/puppet-module-puppetboard"]="2.4.0"
 
