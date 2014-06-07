7e99af722d2228c6ce1a318a15e0e72876227496
Ananth Suryanarayana <anantha@juniper.net>
2014-06-07 13:33:42 -0700

    try html font

--- modules/jenkins/files/slave_scripts/ci-infra/util.rb
+++ modules/jenkins/files/slave_scripts/ci-infra/util.rb
@@ -30,7 +30,8 @@ class Sh
             # Do stuff with the output here. Just printing to show it works
             stdin.each { |line|
                 output += line unless ignore_output
-                print "#{COLOR_CYAN}#{cmd}#{COLOR_RESET}: #{line}" if debug
+#               print "#{COLOR_CYAN}#{cmd}#{COLOR_RESET}: #{line}" if debug
+                print %{<b><font color="cyan">#{cmd}:</font></b> #{line}} if debug
             }
             rescue Errno::EIO
             rescue PTY::ChildExited => e
