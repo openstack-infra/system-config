213ee71e6155427f5a468afc4883d308929d641a
Ananth Suryanarayana <anantha@juniper.net>
2014-06-14 11:46:54 -0700

    increase keepalive timeout

--- modules/jenkins/files/slave_scripts/ci-infra/slave_auto_run.rb
+++ modules/jenkins/files/slave_scripts/ci-infra/slave_auto_run.rb
@@ -72,7 +72,7 @@ def subslave
             # If it is not updated within some time, commit suicide!, not if we
             # we want to skip this intentionally (for testing purposes)
             if !File.file? "/root/skip_subslave_keepalive" then
-                fp.puts(Sh.crun "nova delete #{@hostname}") if elapsed > 45
+                fp.puts(Sh.crun "nova delete #{@hostname}") if elapsed > 120
             end
         }
         sleep 10
