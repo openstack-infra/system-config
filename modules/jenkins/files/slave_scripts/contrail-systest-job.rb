9534f3d942841484cd3ed8b86ecfc6c7554a202d
Ananth Suryanarayana <anantha@juniper.net>
2014-06-17 06:56:16 -0700

    support test target list and libvirt driver selection mode

--- modules/jenkins/files/slave_scripts/contrail-systest-job.rb
+++ modules/jenkins/files/slave_scripts/contrail-systest-job.rb
@@ -95,15 +95,29 @@ def setup_contrail(image)
 end
 
 # Update nova libvirt driver.
-def update_nova_libvirt_driver(libvirt_type)
-    return if libvirt_type.nil?
+def update_nova_libvirt_driver(fab_test)
+    return nil if fab_test.nil?
+
+    # Get libvirt type, default to kvm, if support is available
+    # TODO Check vmx in the compute nodes.
+    libvirt_type_wanted = "kvm"
+
+    # Check if the test demands a particular driver type.
+    if fab_test =~ /^(kvm|qemu)_(.*)/ then
+        libvirt_type_wanted = $1
+        fab_test = $2
+    end
 
     @vms.each { |vm|
+        vmx, e = Sh.rrun("ssh #{vm.vmname} \grep -w vmx /proc/cpuinfo | wc -l")
+        libvirt_type = libvirt_type_wanted
+        libvirt_type = "qemu" if vmx == "0"
+
         Sh.run "echo \"perl -ni -e 's/libvirt_type=.*/libvirt_type=#{libvirt_type}/g; print \\$_;' /etc/nova/nova-compute.conf\" | ssh -t #{vm.vmname} \$(< /dev/fd/0)", true
         Sh.run "ssh #{vm.vmname} service nova-compute restart", true
     }
 
-    return
+    return fab_test
 end
 
 
@@ -113,9 +127,7 @@ def install_contrail
     Sh.run "ssh #{vm.vmname} /usr/local/jenkins/slave_scripts/ci-infra/contrail_fab install_contrail"
     Sh.run "echo \"perl -ni -e 's/JVM_OPTS -Xss\\d+/JVM_OPTS -Xss512/g; print \\$_;' /etc/cassandra/cassandra-env.sh\" | ssh -t #{vm.vmname} \$(< /dev/fd/0)"
 
-    Sh.run "ssh #{vm.vmname} /usr/local/jenkins/slave_scripts/ci-infra/contrail_fab setup_all" unless @options.fab_tests.nil?
-
-    update_nova_libvirt_driver(@options.libvirt_type)
+    Sh.run "ssh #{vm.vmname} /usr/local/jenkins/slave_scripts/ci-infra/contrail_fab setup_all" unless @options.fab_tests.empty?
 
     # Reduce number of nova-api and nova-conductors and fix scheduler for
     # even distribution of instances across all compute nodes.
@@ -170,41 +182,45 @@ def verify_contrail
     @vms.each { |vm| Sh.run "ssh #{vm.vmname} /usr/bin/contrail-status" }
 end
 
-def run_sanity
-    return if @options.fab_tests.nil?
+def run_sanity(fab_test)
+    return if fab_test.nil?
 
     # Check if sanities are disabled..
     if @image_built then
-        skip_file = "/root/ci-test/skip_ci_sanity_#{ENV['ZUUL_BRANCH']}"
+        skip_file =
+            "/root/ci-test/skip_ci_systest_#{fab_test}_#{@options.branch}"
         o, e = Sh.rrun("ssh jenkins.opencontrail.org ls -1 #{skip_file} 2>/dev/null", true)
         if o =~ /#{skip_file}/ then
-            puts "SKIPPED: fab run_sanity:ci_sanity due to the presence of the file jenkins.opencontrail.org:#{skip_file}"
+            puts "SKIPPED: fab #{fab_test} due to the presence of the file jenkins.opencontrail.org:#{skip_file}"
             return 0
         end
     end
 
-    o, exit_code = Sh.run("ssh #{@vms.first.vmname} \"TEST_RETRY_FACTOR=10.0 TEST_DELAY_FACTOR=1.2 /usr/local/jenkins/slave_scripts/ci-infra/contrail_fab #{@options.fab_tests}\"", true)
+    fab_test = update_nova_libvirt_driver(fab_test)
+
+    o, exit_code = Sh.run("ssh #{@vms.first.vmname} \"TEST_RETRY_FACTOR=10.0 TEST_DELAY_FACTOR=1.2 /usr/local/jenkins/slave_scripts/ci-infra/contrail_fab #{fab_test}\"", true)
+
 
     # Copy sanity log files, as the sub-slave VMs will go away.
-    Sh.run("scp -r #{@vms.first.vmname}:/root/logs #{ENV['WORKSPACE']}/.", true)
+    Sh.run("scp -r #{@vms.first.vmname}:/root/logs #{ENV['WORKSPACE']}/logs_#{fab_test}", true)
 
     # Get http hyper links to the logs and report summary files.
-    Sh.run("lynx --dump #{ENV['WORKSPACE']}/logs/*/test_report.html", true)
+    Sh.run("lynx --dump #{ENV['WORKSPACE']}/logs_#{fab_test}/*/test_report.html", true)
 
-    puts "Test complete, checking for any failures.."
+    puts "#{fab_test} complete, checking for any failures.."
 
     # Check if any test failed or errored. Number of matches is consisdered
     # as exit-code, and 0 implies success.
     if exit_code == 0 then
         exit_code, e = Sh.rrun(
-            %{lynx --dump #{ENV['WORKSPACE']}/logs/*/test_report.html | } +
+            %{lynx --dump #{ENV['WORKSPACE']}/logs_#{fab_test}/*/test_report.html | } +
             %{\grep Status: | \grep "Fail\\|Error" | wc -l}, true).to_i
     end
 
     if exit_code != 0 then
-        puts "****** run_sanity:ci_sanity FAILED ******"
+        puts "****** #{fab_test} FAILED ******"
     else
-        puts "****** run_sanity:ci_sanity PASSED ******"
+        puts "****** #{fab_test} PASSED ******"
     end
     return exit_code
 end
@@ -215,13 +231,22 @@ def run_test(image = @options.image)
     install_contrail
     setup_sanity
     verify_contrail
-    Sh.exit(run_sanity)
+
+    exit_code = 0
+    @options.fab_tests.each { |fab_test|
+        exit_code = run_sanity(fab_test)
+        break if exit_code
+        exit_code = run_sanity(fab_test)
+    }
+
+    Sh.run("lynx --dump #{ENV['WORKSPACE']}/logs_*/*/test_report.html", true)
+    Sh.exit(exit_code)
 end
 
 @options = OpenStruct.new
 @options.image = nil
 @options.branch = ENV['ZUUL_BRANCH'] || "master"
-@options.fab_tests = "run_sanity:ci_sanity"
+@options.fab_tests = ["run_sanity:ci_sanity", "run_sanity:ci_svc_sanity"]
 
 @options.nodes = 2
 @options.cfgm = ["host1"]
@@ -246,7 +271,7 @@ def parse_options(args = ARGV)
         o.on("-b", "--branch [#{@options.branch}]", "Branch to use ") { |b|
             @options.branch = b
         }
-        o.on("-t", "--test [#{@options.fab_tests}]", "fab test target") { |t|
+        o.on("-t", "--test [#{@options.fab_tests}]", Array, "fab test target") { |t|
             @options.fab_tests = t
         }
         o.on("-l", "--libvirt-type [driver]",
@@ -288,8 +313,8 @@ def parse_options(args = ARGV)
     }
     opt_parser.parse!(args)
     if !args.empty? then
-        @options.fab_tests = ""
-        args.each { |t| @options.fab_tests += "#{t} " }
+        @options.fab_tests = [ ]
+        args.each { |t| @options.fab_tests.push += t }
     end
     if !compute_set then
         if @options.nodes == 1 then
