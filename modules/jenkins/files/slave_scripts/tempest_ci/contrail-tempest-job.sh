97c9af35349684b1d53a71615262724ec900ea69
opencontrail-ci-admin <ci-admin@opencontrail.org>
2014-06-18 08:58:14 -0700

    more changes for CI

--- modules/jenkins/files/slave_scripts/tempest_ci/contrail-tempest-job.sh
+++ modules/jenkins/files/slave_scripts/tempest_ci/contrail-tempest-job.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
+GERRIT_REFSPEC=${GERRIT_REFSPEC-refs/changes/30/96630/20}
+GERRIT_PATCHSET_REVISION=${GERRIT_PATCHSET_REVISION-31e6a6f0fe8260d4222c2b3603df65cafdd90190}
+BUILD_NUMBER=999
+
 #echo "GERRIT_REFSPEC=$GERRIT_REFSPEC"
 #echo "GERRIT_REFSPEC=$GERRIT_PATCHSET_REVISION"
 #echo "BUILD_NUMBER=$BUILD_NUMBER"
 
 # install some required packages
-# bottle==0.11.6, configparser, 
+#   bottle==0.11.6, configparser, netifaces
 pip install bottle==0.11.6
 pip install configparser
+pip install netifaces
 
-scp /usr/local/jenkins/slave_scripts/tempest_ci/contrail-tempest-devstack-job.sh jenkins@localhost:
+# devtack setup
 scp /usr/local/jenkins/slave_scripts/tempest_ci/contrail-devstack-setup.sh jenkins@localhost:
-
+scp /usr/local/jenkins/slave_scripts/tempest_ci/fix_localrc.py jenkins@localhost:
 ssh jenkins@localhost  "bash -x contrail-devstack-setup.sh"
+
+# setup tempest for a run
+scp /usr/local/jenkins/slave_scripts/tempest_ci/tempest_setup.py jenkins@localhost:
+ssh jenkins@localhost  "python tempest_setup.py"
+
+# apply patch and run tempest
+scp /usr/local/jenkins/slave_scripts/tempest_ci/contrail-tempest-devstack-job.sh jenkins@localhost:
 ssh jenkins@localhost  "bash -x contrail-tempest-devstack-job.sh $GERRIT_REFSPEC $GERRIT_PATCHSET_REVISION $BUILD_NUMBER"
 
