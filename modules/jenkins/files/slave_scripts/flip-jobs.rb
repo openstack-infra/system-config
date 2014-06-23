b9b45dc2450995e9d8580eb1d4148e7f7ec2b4fc
Ananth Suryanarayana <anantha@juniper.net>
2014-06-23 11:55:34 -0700

    Add flip-jobs command

--- modules/jenkins/files/slave_scripts/flip-jobs.rb
+++ modules/jenkins/files/slave_scripts/flip-jobs.rb
@@ -13,6 +13,3 @@ o.split(/\r\n/).each { |job|
     Sh.run("#{CLI} disable-job #{job}")
     Sh.run("#{CLI} enable-job #{job}")
 }
-
-#Sh.rrun("java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 list-jobs | \grep -v "Contrail Neutron" | xargs -n 1 java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 disable-job && java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 list-jobs | \grep -v "Contrail Neutron" | xargs -n 1 java -jar /root/jenkins-cli.jar -s http://jenkins.opencontrail.org:8080 enable-job
-
