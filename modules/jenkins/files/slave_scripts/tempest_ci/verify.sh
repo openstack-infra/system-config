684cf12ce31f7dc8991913b23249c2800f3a3697
opencontrail-ci-admin <ci-admin@opencontrail.org>
2014-06-23 23:25:12 -0700

    Fix verify.sh for tempest testcases

--- modules/jenkins/files/slave_scripts/tempest_ci/verify.sh
+++ modules/jenkins/files/slave_scripts/tempest_ci/verify.sh
@@ -1,5 +1,5 @@
 
-testr list-tests tempest.api.network.test_networks tempest.api.network.test_security_groups tempest.api.network.test_floating_ips tempest.api.network.test_routers  tempest.api.network.test_ports | grep -v "ExtendedAttrs\|snat\|extra_route\|gateway" > tests.txt 
+testr list-tests tempest.api.network.test_networks tempest.api.network.test_security_groups tempest.api.network.test_floating_ips tempest.api.network.test_routers  tempest.api.network.test_ports | grep -v "ExtendedAttrs\|snat\|extra_route\|router_set_gateway\|router_unset_gateway" > tests.txt 
 
 testr run --subunit --load-list tests.txt > subunit.log
 /usr/local/bin/subunit2html subunit.log 
