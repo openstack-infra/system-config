a53b0d7e5c1bce8f9ce57d1f4b2910986812e60b
Ananth Suryanarayana <anantha@juniper.net>
2014-06-26 13:15:59 -0700

    Archive failed test logs in /ci-admin/failed_unit_test_logs/.

--- modules/jenkins/files/slave_scripts/contrail-unittests-job.sh
+++ modules/jenkins/files/slave_scripts/contrail-unittests-job.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -o pipefail
-set -ex
+set -x
 
 export WORKSPACE=$PWD
 SKIP_JOBS=$WORKSPACE/skip_jobs
@@ -31,10 +31,27 @@ function build_unittest() {
     unset BUILD_ONLY
 }
 
+function archive_failed_test_logs() {
+    find $WORKSPACE/repo/build -name "*.log" |\grep -w test | xargs \grep -lw FAILED | xargs tar -zvcf --ignore-failed-read $WORKSPACE/failed_unit_test_logs.tgz
+    if [ ! -f $WORKSPACE/failed_unit_test_logs.tgz ]; then
+        return
+    fi
+    if [ -z $BUILD_NUMBER ]; then
+        BUILD_NUMBER=0
+    fi
+    sshpass -p c0ntrail123 ssh ci-admin@ubuntu-build02 \
+        mkdir -p /ci-admin/failed_unit_test_logs/$BUILD_NUMBER
+    sshpass -p c0ntrail123 scp $WORKSPACE/failed_unit_test_logs.tgz \
+        ci-admin@ubuntu-build02:/ci-admin/failed_unit_test_logs/$BUILD_NUMBER/.
+    sshpass -p c0ntrail123 ssh ci-admin@ubuntu-build02 \
+        tar -C /ci-admin/failed_unit_test_logs/$BUILD_NUMBER/ -zxf \
+        /ci-admin/failed_unit_test_logs/$BUILD_NUMBER/failed_unit_test_logs.tgz
+}
+
 function print_test_results() {
 
     # Turn off error check and echo for the rest of the script.
-    set +ex
+    set +x
 
     grep -w FAIL $WORKSPACE/scons_test.log > $WORKSPACE/scons_failed_unittests.log
     FAIL_COUNT=`cat $WORKSPACE/scons_failed_unittests.log | wc -l`
@@ -70,14 +87,20 @@ function print_test_results() {
     echo "*******************************************************************"
 
     # Turn on error check and echo
-    set -ex
+    set -x
 }
 
 # Run unittests
-function run_unittest() {
+function build_and_run_unittest() {
     # Goto the repo top directory.
     cd $WORKSPACE/repo
-    scons -j $SCONS_JOBS test 2>&1 | tee $WORKSPACE/scons_test.log
+    scons -j $SCONS_JOBS test1 2>&1 | tee $WORKSPACE/scons_test.log
+    exit_code=$?
+
+    # Exit in case of error
+    if [ "$exit_code" != "0" ]; then
+        ci_exit $exit_code
+    fi
 
     # Flaky test results are ignored.
     scons -j $SCONS_JOBS -i flaky-test 2>&1 | tee -a $WORKSPACE/scons_test.log
@@ -95,18 +118,28 @@ function test_wait() {
     done
 }
 
-function ci_cleanup() {
+function ci_exit() {
+    exit_code=$1
+    if [ -z $exit_code ]; then
+        exit_code=0
+    fi
     test_wait
-    rm -rf $WORKSPACE/* $WORKSPACE/.* 2>/dev/null || true
-    echo Success
-    exit
+    archive_failed_test_logs
+
+    if [ "$exit_code" == "0" ]; then
+        rm -rf $WORKSPACE/* $WORKSPACE/.* 2>/dev/null
+        echo Success
+    else
+        echo Exit with failed code $exit_code
+        # Leave the workspace intact.
+    fi
+    exit $exit_code
 }
 
 function main() {
-    # build_unittest
-    run_unittest
+    build_and_run_unittest
     print_test_results
-    ci_cleanup
+    ci_exit
 }
 
 env
