764ee8c74fbacf826b55dbead88964095b3a2b87
Monty Taylor <mordred@inaugust.com>
2014-06-27 23:37:48 -0700

    Delete select-mirror and all the args
    It turns out that we pass a lot of args around simply for the
    purpose of doing mirror selection. This goes away if everything
    gets a mirror.
    
    This change should only be merged after the parent change has
    propagated to all puppeted slaves and updated nodepool images.
    
    Co-Authored-By: Jeremy Stanley <fungi@yuggoth.org>
    Change-Id: I4a721f6559d7160c8213268a8c1e35c667a67a4f

--- tools/run-layout.sh
+++ tools/run-layout.sh
@@ -14,26 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-org=$1
-project=$2
-
-if [[ -z "$org" || -z "$project" ]]
-then
-  echo "Usage: $0 ORG PROJECT"
-  echo
-  echo "ORG: The project organization (eg 'openstack')"
-  echo "PROJECT: The project name (eg 'nova')"
-  exit 1
-else
-  if [ -f /usr/local/jenkins/slave_scripts/select-mirror.sh ]
-  then
-      /usr/local/jenkins/slave_scripts/select-mirror.sh $org $project
-  else
-      echo "No mirror script /usr/local/jenkins/slave_scripts/select-mirror.sh,"
-      echo "assuming local environment is configured properly."
-  fi
-fi
-
 mkdir -p .test
 cd .test
 [ -d zuul ] || git clone https://review.openstack.org/p/openstack-infra/zuul --depth 1
