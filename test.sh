0e9da8898da39bc567c6b2ee57e2f12d8166591c
James E. Blair <jeblair@hp.com>
2014-07-03 14:27:20 -0700

    Only run relevant puppet apply tests
    Associate an OS with each node defined in site.pp so that the puppet
    apply test will only test nodes apprapriate to the node the test
    is running on.
    
    Change-Id: I26722dce15589c982af5de36f8ccf42666d29f2b

--- test.sh
+++ test.sh
@@ -12,9 +12,31 @@ sed -i -e 's/^[^][:space:]$]/#&/g' applytest/puppetapplytest*
 sed -i -e 's@hiera(.\([^.]*\).,\([^)]*\))@\2@' applytest/puppetapplytest*
 mv applytest/*00 applytest/head  # These are the top-level variables defined in site.pp
 
+if [ `lsb_release -i -s` == 'CentOS' ]; then
+    if [ `lsb_release -r -s` =~ '6' ]; then
+	CODENAME='centos6'
+    fi
+elif [ `lsb_release -i -s` == 'Ubuntu' ]; then
+    CODENAME=`lsb_release -c -s`
+fi
+
+FOUND=0
 for f in `find applytest -name 'puppetapplytest*' -print` ; do
-    cat applytest/head $f > $f.final
+    if grep "Node-OS: $CODENAME" $f; then
+	cat applytest/head $f > $f.final
+	FOUND=1
+    fi
 done
+
+if [ $FOUND == "0" ]; then
+    echo "No hosts found for node type $CODENAME"
+    exit 1
+fi
+
+grep -v 127.0.1.1 /etc/hosts >/tmp/hosts
+HOST=`echo $HOSTNAME |awk -F. '{ print $1 }'`
+echo "127.0.1.1 $HOST.openstack.org" >> /tmp/hosts
+sudo mv /tmp/hosts /etc/hosts
 
 find applytest -name 'puppetapplytest*.final' -print0 | \
     xargs -0 -P $(nproc) -n 1 -I filearg \
