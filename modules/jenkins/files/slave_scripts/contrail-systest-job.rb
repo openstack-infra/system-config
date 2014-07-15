b33691aa52213cc7d74c6b4ad2bc92e1a9d7c8eb
Ananth Suryanarayana <anantha@juniper.net>
2014-07-15 10:20:45 -0700

    cd to tools/packaging/build before starting packager

--- modules/jenkins/files/slave_scripts/contrail-systest-job.rb
+++ modules/jenkins/files/slave_scripts/contrail-systest-job.rb
@@ -181,7 +181,8 @@ def build_contrail_packages(repo = "#{ENV['WORKSPACE']}/repo")
     Sh.run "scons"
 #   Sh.run "scons #{repo}/build/third_party/log4cplus"
     Sh.run "rm -rf #{repo}/third_party/euca2ools/.git/shallow"
-    Sh.run "#{repo}/tools/packaging/build/packager.py --sku havana"
+    Sh.run "cd #{repo}/tools/packaging/build/"
+    Sh.run "./packager.py --sku havana"
     Sh.run "ls -alh #{repo}/build/artifacts/contrail-install-packages_*_all.deb"
 
     # Return the all-in-one debian/rpm package file path.
