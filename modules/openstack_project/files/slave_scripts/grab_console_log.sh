388c9c3824c6b0192ba3e5b4f1e061800da6deea
Joshua Hesketh <josh@nitrotech.org>
2014-07-17 21:55:55 +1000

    Fix the write arugment for wget
    Download the console log into the correct position instead of trying
    to put it into a dir.
    
    Change-Id: I91d8f37aa6645e94b16cbda8179d679a8ef9d164

--- modules/openstack_project/files/slave_scripts/grab_console_log.sh
+++ modules/openstack_project/files/slave_scripts/grab_console_log.sh
@@ -3,4 +3,4 @@
 echo "Grabbing consoleLog"
 
 console_log_path='consoleText'
-wget -P /tmp/console.txt --no-check-certificate $BUILD_URL$console_log_path
+wget -O /tmp/console.txt --no-check-certificate $BUILD_URL$console_log_path
