92b8981c1c69b079a8ff1c7f8b39ac905ccf853a
Matthew Treinish <mtreinish@kortar.org>
2014-07-18 08:26:52 -0400

    Add subunit2sql gearman workers
    This adds a new gearman worker to process the subunit files from
    the gate job runs. It will use subunit2sql to connect to a sql
    server and process the data from the subunit file. The
    log-gearman-client is modified to allow for pushing subunit jobs
    to gearman, and the worker model for processsing logs is borrowed
    to process the subunit files.
    
    Change-Id: I83103eb6afc22d91f916583c36c0e956c23a64b3

---
+++ modules/subunit2sql/manifests/init.pp
@@ -0,0 +1,52 @@
+# Copyright 2012-2013 Hewlett-Packard Development Company, L.P.
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: subunit2sql
+#
+class subunit2sql (
+) {
+  include pip
+
+  package {'python-mysqldb':
+    ensure   => present,
+  }
+
+  package {'python-psycopg2':
+    ensure   => present,
+  }
+
+  package { 'python-subunit':
+    ensure   => latest,
+    provider => 'pip',
+    require  => Class['pip'],
+  }
+
+  package { 'subunit2sql':
+    ensure   => latest,
+    provider => 'pip',
+    require  => [
+      Class['pip'],
+      Package['python-mysqldb'],
+      Package['python-psycopg2']
+    ],
+  }
+
+  package { 'testtools':
+    ensure   => latest,
+    provider => 'pip',
+    require  => Class['pip'],
+  }
+
+}
