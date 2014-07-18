777c1377328075e837936fcbb618eeb05208ccfb
Matthew Treinish <mtreinish@kortar.org>
2014-07-18 08:26:52 -0400

    Add subunit2sql gearman workers
    This adds a new gearman worker to process the subunit files from
    the gate job runs. It will use subunit2sql to connect to a sql
    server and process the data from the subunit file. The
    log-gearman-client is modified to allow for pushing subunit jobs
    to gearman, and the worker model for processsing logs is borrowed
    to process the subunit files.
    
    Change-Id: I83103eb6afc22d91f916583c36c0e956c23a64b3

---
+++ modules/openstack_project/manifests/subunit_worker.pp
@@ -0,0 +1,47 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Logstash indexer worker glue class.
+#
+class openstack_project::subunit_worker (
+  $elasticsearch_nodes = [],
+  $sysadmins = [],
+  $subunit2sql_db_uri
+) {
+  $iptables_rule = regsubst ($elasticsearch_nodes, '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 9200:9400 -s \1 -j ACCEPT')
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [22],
+    iptables_rules6           => $iptables_rule,
+    iptables_rules4           => $iptables_rule,
+    sysadmins                 => $sysadmins,
+  }
+
+  include subunit2sql
+  subunit2sql::worker { 'A':
+    config_file        => 'puppet:///modules/openstack_project/logstash/jenkins-subunit-worker.yaml',
+    subunit2sql_db_uri => $subunit2sql_db_uri,
+  }
+  subunit2sql::worker { 'B':
+    config_file        => 'puppet:///modules/openstack_project/logstash/jenkins-subunit-worker.yaml',
+    subunit2sql_db_uri => $subunit2sql_db_uri,
+  }
+  subunit2sql::worker { 'C':
+    config_file        => 'puppet:///modules/openstack_project/logstash/jenkins-subunit-worker.yaml',
+    subunit2sql_db_uri => $subunit2sql_db_uri,
+  }
+  subunit2sql::worker { 'D':
+    config_file        => 'puppet:///modules/openstack_project/logstash/jenkins-subunit-worker.yaml',
+    subunit2sql_db_uri => $subunit2sql_db_uri,
+  }
+}
