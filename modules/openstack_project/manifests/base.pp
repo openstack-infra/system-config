e9139eca7a49a49ae425aec7ffe04e79b128d65a
Spencer Krum <spencer.krum@hp.com>
2014-08-26 06:54:07 -0700

    Enable puppet3 master bootstrap
    This enables install_puppet.sh to run successfully on
    trusty with PUPPET_VERSION=3 set.
    
    Add a util directory and a puppetmaster bootstrap
    shell script.
    
    Puppetdb-terminus versions are the same in trusty and
    precise but are different than the puppet version number.
    
    Also puppetdb-terminus isn't present in the puppetlabs
    repositories.
    
    Along with this I have done sufficient testing to use
    puppet version 3.6 with confidence.
    
    Work has been done to enable directory environments as well.
    
    Change-Id: I925fa6ac1ed9fc9c0b5e9e616fb071bf7a815436

--- modules/openstack_project/manifests/base.pp
+++ modules/openstack_project/manifests/base.pp
@@ -1,11 +1,10 @@
 # == Class: openstack_project::base
 #
 class openstack_project::base(
-  $certname      = $::fqdn,
-  $install_users = true,
-  $pin_puppet    = '2.7.',
-  $pin_facter    = '1.',
-  $ca_server     = undef,
+  $certname              = $::fqdn,
+  $install_users         = true,
+  $pin_puppet            = '2.7.',
+  $ca_server             = undef,
 ) {
   if ($::osfamily == 'Debian') {
     include apt
@@ -13,6 +12,17 @@ class openstack_project::base(
   include openstack_project::params
   include openstack_project::users
   include sudoers
+
+  case $pin_puppet {
+    '2.7.': {
+      $pin_facter = '1.'
+      $pin_puppetdb_terminus = '1.6.'
+    }
+    /^3\./: {
+      $pin_facter = '2.'
+      $pin_puppetdb_terminus = '2.2.'
+    }
+  }
 
   file { '/etc/profile.d/Z98-byobu.sh':
     ensure => absent,
@@ -158,6 +168,7 @@ class openstack_project::base(
     }
   }
 
+  $puppet_version = $pin_puppet
   file { '/etc/puppet/puppet.conf':
     ensure  => present,
     owner   => 'root',
