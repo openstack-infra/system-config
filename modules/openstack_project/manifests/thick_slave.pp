a701977ae7f664a2c70a13b87265389161ee3b6e
Romain Chantereau <romain.chantereau@enovance.com>
2014-08-27 16:47:55 +0200

    Be able to generate math formulae with Sphinx mathpng base extension.
    Change-Id: If9f53894d449bf54f7dcefe33c8f7597843a0438

--- modules/openstack_project/manifests/thick_slave.pp
+++ modules/openstack_project/manifests/thick_slave.pp
@@ -12,6 +12,7 @@ class openstack_project::thick_slave(
     $::openstack_project::jenkins_params::awk_package, # for building extract_docs.awk to work correctly
     $::openstack_project::jenkins_params::asciidoc_package, # for building gerrit/building openstack docs
     $::openstack_project::jenkins_params::curl_package,
+    $::openstack_project::jenkins_params::dvipng, # for generating image (ie: math) in docs
     $::openstack_project::jenkins_params::docbook_xml_package, # for building openstack docs
     $::openstack_project::jenkins_params::docbook5_xml_package, # for building openstack docs
     $::openstack_project::jenkins_params::docbook5_xsl_package, # for building openstack docs
