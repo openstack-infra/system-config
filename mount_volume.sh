d0c968bce054c686daea1484dffa444198c0e041
Clark Boylan <clark.boylan@gmail.com>
2014-09-04 10:49:45 -0700

    Add support to launch node for attaching volumes
    We are using cinder block devices more and more. Update launch node so
    that it can attach a preexisting cinder volume against a new nova server
    on first boot. This will allow puppet or other config management to
    format and mount that block device into the VMs filesystem.
    
    Change-Id: Ic121cdc06dcbea0e38e8d0ff8946e999af3d727e

---
+++ mount_volume.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+if [ -b /dev/vdc ]; then
+    DEV='/dev/vdc'
+elif [ -b /dev/xvdf ]; then
+    DEV='/dev/xvdf'
+fi
+if [ `blkid | grep $DEV | grep ext4` -ne 0 ]; then
+    mkfs.ext4 ${DEV}
+    perl -nle "m,${DEV}, || print" -i /etc/fstab
+    if [ ! -d /srv ] ; then
+        mkdir -p /srv
+    fi
+    echo "${DEV}  /srv  ext4  errors=remount-ro,barrier=0  0  2" >> /etc/fstab
+fi
