01731e4b9d1625b47323b062ea73ab485f7ed6d4
Monty Taylor <mordred@inaugust.com>
2014-09-04 09:37:57 -0700

    Split pypi_mirror into its own class
    We want to run more than one of these, and not all on static.o.o,
    so as a first step, split the relevant code out into a class.
    
    Change-Id: I6b3dd294bef3c89ad6353ecdae815ebd6825b0e1

--- modules/openstack_project/manifests/static.pp
+++ modules/openstack_project/manifests/static.pp
@@ -200,84 +200,16 @@ class openstack_project::static (
     require => File['/srv/static/docs-draft'],
   }
 
-  ###########################################################
-  # Pypi Mirror
-
-  file { '/srv/static/mirror':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
+  class { 'openstack_project::pypi_mirror':
+    vhost_name => 'pypi.openstack.org',
   }
 
-  file { '/srv/static/mirror/web':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    require => File['/srv/static/mirror'],
-  }
-
+  # Legacy pypi mirror
   file { '/srv/static/mirror/web/openstack':
     ensure  => directory,
     owner   => 'jenkins',
     group   => 'jenkins',
     require => [User['jenkins'], File['/srv/static/mirror']],
-  }
-
-  apache::vhost { 'pypi.openstack.org':
-    port     => 80,
-    priority => '50',
-    docroot  => '/srv/static/mirror/web',
-    require  => File['/srv/static/mirror/web'],
-  }
-
-  file { '/srv/static/mirror/web/robots.txt':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    source  => 'puppet:///modules/openstack_project/disallow_robots.txt',
-    require => File['/srv/static/mirror/web'],
-  }
-
-  package { 'bandersnatch':
-    ensure   => 'present',
-    provider => 'pip',
-  }
-
-  file { '/etc/bandersnatch.conf':
-    ensure  => present,
-    source  => 'puppet:///modules/openstack_project/bandersnatch.conf',
-  }
-
-  file { '/var/log/bandersnatch':
-    ensure => directory,
-  }
-
-  cron { 'bandersnatch':
-    minute      => '*/5',
-    command     => 'run-bandersnatch >>/var/log/bandersnatch/mirror.log 2>&1',
-    environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-  }
-
-  include logrotate
-  logrotate::file { 'bandersnatch':
-    log     => '/var/log/bandersnatch/mirror.log',
-    options => [
-      'compress',
-      'copytruncate',
-      'missingok',
-      'rotate 7',
-      'daily',
-      'notifempty',
-    ],
-  }
-
-  file { '/usr/local/bin/run-bandersnatch':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    source  => 'puppet:///modules/openstack_project/run_bandersnatch.py',
   }
 
   ###########################################################
