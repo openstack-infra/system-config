33821a2f94bdf74a638ce51fd5c3b441085819c4
Matthew Treinish <mtreinish@kortar.org>
2014-09-17 23:36:56 -0400

    WIP: Add mysql-proxy to subunit2sql server
    This commit (when it's finished) adds a mysql_proxy module which will
    setup a read-only public endpoint for the subunit2sql db.
    
    Change-Id: I478baca354354347fe50074a8e3b9f66ca890d55

---
+++ modules/mysql_proxy/manifests/init.pp
@@ -0,0 +1,52 @@
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to install mysql-proxy
+
+class mysql_proxy {
+    package { 'mysql-proxy':
+      ensure => present,
+    }
+
+    service{ 'mysql-proxy':
+      ensure => running,
+      require => Package['mysql-proxy']
+    }
+
+    file { '/etc/mysql-proxy':
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0644',
+      require  => Package['mysql-proxy'],
+      ensure   => directory,
+    }
+
+    file{ "/etc/mysql-proxy/lua.d":
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0644',
+      require  => Package['mysql-proxy'],
+      ensure   => directory,
+      source   => 'puppet:///modules/mysql_proxy/lua.d',
+    }
+
+    file{ '/etc/mysql-proxy/mysql-proxy.conf':
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0644',
+      content  => template("mysql_proxy/mysql-proxy.conf.erb"),
+      purge    => true,
+      require  => File['/etc/mysql-proxy'],
+    }
+}
