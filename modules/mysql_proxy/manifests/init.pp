87f55447b14c629132d2c752a123f75265f00412
Matthew Treinish <mtreinish@kortar.org>
2014-09-17 23:36:56 -0400

    Add mysql-proxy to enable read-only access to a db
    This commit adds a mysql_proxy module which will setup a read-only
    proxy to a mysql db. The module also adds a dir for adding lua
    scripts which is how you can define custom rules for the proxy. While
    not used now, this will enable future usage if we determine we need
    more complex rules in the proxy configuration.
    
    Change-Id: I478baca354354347fe50074a8e3b9f66ca890d55

---
+++ modules/mysql_proxy/manifests/init.pp
@@ -0,0 +1,53 @@
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to install mysql-proxy
+
+class mysql_proxy {
+
+    package { 'mysql-proxy':
+      ensure => present,
+    }
+
+    service{ 'mysql-proxy':
+      ensure => running,
+      require => Package['mysql-proxy']
+    }
+
+    file { '/etc/mysql-proxy':
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0644',
+      require  => Package['mysql-proxy'],
+      ensure   => directory,
+    }
+
+    file { "/etc/mysql-proxy/lua.d":
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0644',
+      require  => Package['mysql-proxy'],
+      ensure   => directory,
+      source   => 'puppet:///modules/mysql_proxy/lua.d',
+    }
+
+    file { '/etc/default/mysql-proxy':
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0644',
+      source   => 'puppet:///modules/mysql_proxy/mysql_proxy',
+      require  => Package['mysql-proxy'],
+    }
+
+}
