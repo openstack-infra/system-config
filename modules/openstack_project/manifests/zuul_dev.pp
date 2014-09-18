c99ba49bd062279f62a4e5a2fea26687afa88403
James E. Blair <jeblair@hp.com>
2014-09-18 20:47:33 +0000

    Move zuul layout into /etc/zuul/layout
    Move the layout.yaml and functions files into a subdir of /etc/zuul
    to facilitate the separation of project and system configuration.
    
    Change-Id: Ib62063ffef91c370d4626e8590ac856177f6099f

--- modules/openstack_project/manifests/zuul_dev.pp
+++ modules/openstack_project/manifests/zuul_dev.pp
@@ -65,13 +65,13 @@ class openstack_project::zuul_dev(
     }
   }
 
-  file { '/etc/zuul/layout.yaml':
+  file { '/etc/zuul/layout/layout.yaml':
     ensure => present,
     source => 'puppet:///modules/openstack_project/zuul/layout-dev.yaml',
     notify => Exec['zuul-reload'],
   }
 
-  file { '/etc/zuul/openstack_functions.py':
+  file { '/etc/zuul/layout/openstack_functions.py':
     ensure => present,
     source => 'puppet:///modules/openstack_project/zuul/openstack_functions.py',
     notify => Exec['zuul-reload'],
