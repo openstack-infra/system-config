7fff4e7e933643bae05a9d559bf4f7dad594685e
Ian Wienand <iwienand@redhat.com>
2014-10-15 09:52:24 +1100

    Update EPEL7 version
    Update to the lastest EPEL7 release version
    
    Change-Id: I88034b4f5089a14adc1628b8240d8b4c9cb5b067

--- install_puppet.sh
+++ install_puppet.sh
@@ -79,7 +79,7 @@ function setup_puppet_fedora {
 
 function setup_puppet_rhel7 {
 
-    local epel_pkg="http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm"
+    local epel_pkg="http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm"
     local puppet_pkg="https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-10.noarch.rpm"
 
     # install EPEL
