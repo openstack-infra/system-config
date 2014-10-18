c9dcb4c7f27ef76f33dac5335201fd108e48b926
Monty Taylor <mordred@inaugust.com>
2014-10-18 14:56:00 -0700

    Add Kerberos config
    Step one in an AFS cell is getting kerberos working. This does not
    provide end-to-end KDC management - the realm still needs to be
    created by hand.
    
    Change-Id: I891d784d676ab79e7aca9c883dd9e705a30db6e5

---
+++ modules/kerberos/manifests/client.pp
@@ -0,0 +1,17 @@
+class kerberos::client (
+  $realm,
+  $kdcs,
+  $admin_server,
+) {
+
+  package { 'krb5-user':
+    ensure  => present,
+    require => File['/etc/krb5.conf'],
+  }
+
+  file { '/etc/krb5.conf':
+    ensure  => present,
+    replace => true,
+    content => template('kerberos/krb5.conf.erb'),
+  }
+}
