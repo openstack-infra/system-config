a8ea36fd6777afd740a595707cb385cbf37dce2e
Ivan Udovichenko <iudovichenko@mirantis.com>
2014-10-31 15:31:01 +0200

    Remove hardcoded variables from openstack_project module (AFSFS)
    Currently hardcoded variables are set and we should get rid of them
    and use current values as defaults.
    
    Change-Id: I38aa38ffc3f1ea901cee3c202af8ca70e2eaf852

--- modules/openstack_project/manifests/afsfs.pp
+++ modules/openstack_project/manifests/afsfs.pp
@@ -1,27 +1,37 @@
 # AFS Fileserver/BOS
 class openstack_project::afsfs (
-  $cell = 'openstack.org',
-  $sysadmins = [],
+  $cell                      = 'openstack.org',
+  $sysadmins                 = [],
+  $iptables_public_udp_ports = [
+    7000,
+    7002,
+    7003,
+    7004,
+    7005,
+    7006,
+    7007,
+  ],
+  $dbservers                 = [
+    {
+      name     => 'afsdb01.openstack.org',
+      ip       => '104.130.136.20',
+    },
+    {
+      name     => 'afsdb02.openstack.org',
+      ip       => '23.253.200.228',
+    },
+  ]
 ) {
 
   class { 'openstack_project::server':
-    iptables_public_udp_ports => [7000,7002,7003,7004,7005,7006,7007],
+    iptables_public_udp_ports => $iptables_public_udp_ports,
     sysadmins                 => $sysadmins,
     afs                       => true,
   }
 
   class { 'openafs::fileserver':
     cell         => $cell,
-    dbservers    => [
-      {
-        name     => 'afsdb01.openstack.org',
-        ip       => '104.130.136.20',
-      },
-      {
-        name     => 'afsdb02.openstack.org',
-        ip       => '23.253.200.228',
-      },
-    ],
+    dbservers    => $dbservers,
     require      => Class['Openstack_project::Server'],
   }
 }
