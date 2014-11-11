131f12221522e7bdef0dfa8f909b7f46333cf8cc
Ivan Udovichenko <iudovichenko@mirantis.com>
2014-11-11 15:55:58 +0200

    Remove hardcoded variables from openstack_project module (cacti)
    Currently hardcoded variables are set and we should get rid of them
    and use current values as defaults.
    
    Change-Id: Ib13ab747ff1dda603195b9d39b173017c7747fca

--- modules/openstack_project/manifests/cacti.pp
+++ modules/openstack_project/manifests/cacti.pp
@@ -1,75 +1,5 @@
 # Class to configure cacti on a node.
 class openstack_project::cacti (
-  $sysadmins = []
-) {
-
-  if $::osfamily != 'Debian' {
-    fail("${::osfamily} is not supported.")
-  }
-
-  class { 'openstack_project::server':
-    iptables_public_tcp_ports => [80, 443],
-    sysadmins                 => $sysadmins,
-  }
-
-  include apache
-
-  a2mod { 'rewrite':
-    ensure => present,
-  }
-
-  package { 'cacti':
-    ensure => present,
-  }
-
-  file { '/etc/apache2/conf.d/cacti.conf':
-    ensure  => present,
-    source  => 'puppet:///modules/openstack_project/cacti/apache.conf',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['cacti'],
-  }
-
-  file { '/usr/local/share/cacti/resource/snmp_queries':
-    ensure => directory,
-    owner  => 'root',
-  }
-
-  file { '/usr/local/share/cacti/resource/snmp_queries/net-snmp_devio.xml':
-    ensure  => present,
-    source  => 'puppet:///modules/openstack_project/cacti/net-snmp_devio.xml',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => File['/usr/local/share/cacti/resource/snmp_queries'],
-  }
-
-  file { '/var/lib/cacti/linux_host.xml':
-    ensure  => present,
-    source  => 'puppet:///modules/openstack_project/cacti/linux_host.xml',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => File[
-        '/usr/local/share/cacti/resource/snmp_queries/net-snmp_devio.xml'
-      ],
-  }
-
-  file { '/usr/local/bin/create_graphs.sh':
-    ensure => present,
-    source => 'puppet:///modules/openstack_project/cacti/create_graphs.sh',
-    mode   => '0744',
-    owner  => 'root',
-    group  => 'root',
-  }
-
-  exec { 'cacti_import_xml':
-    command => '/usr/bin/php -q /usr/share/cacti/cli/import_template.php --filename=/var/lib/cacti/linux_host.xml --with-template-rras',
-    cwd     => '/usr/share/cacti/cli',
-    require => File['/var/lib/cacti/linux_host.xml'],
-  }
-
   $cacti_hosts = [
     # community is currently not running puppet.
     #'community.openstack.org',
@@ -150,7 +80,76 @@ class openstack_project::cacti (
     'zm07.openstack.org',
     'zm08.openstack.org',
     'zuul.openstack.org',
-  ]
+  ],
+  $sysadmins = [],
+) {
+
+  if $::osfamily != 'Debian' {
+    fail("${::osfamily} is not supported.")
+  }
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [80, 443],
+    sysadmins                 => $sysadmins,
+  }
+
+  include apache
+
+  a2mod { 'rewrite':
+    ensure => present,
+  }
+
+  package { 'cacti':
+    ensure => present,
+  }
+
+  file { '/etc/apache2/conf.d/cacti.conf':
+    ensure  => present,
+    source  => 'puppet:///modules/openstack_project/cacti/apache.conf',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => Package['cacti'],
+  }
+
+  file { '/usr/local/share/cacti/resource/snmp_queries':
+    ensure => directory,
+    owner  => 'root',
+  }
+
+  file { '/usr/local/share/cacti/resource/snmp_queries/net-snmp_devio.xml':
+    ensure  => present,
+    source  => 'puppet:///modules/openstack_project/cacti/net-snmp_devio.xml',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/usr/local/share/cacti/resource/snmp_queries'],
+  }
+
+  file { '/var/lib/cacti/linux_host.xml':
+    ensure  => present,
+    source  => 'puppet:///modules/openstack_project/cacti/linux_host.xml',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File[
+        '/usr/local/share/cacti/resource/snmp_queries/net-snmp_devio.xml'
+      ],
+  }
+
+  file { '/usr/local/bin/create_graphs.sh':
+    ensure => present,
+    source => 'puppet:///modules/openstack_project/cacti/create_graphs.sh',
+    mode   => '0744',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  exec { 'cacti_import_xml':
+    command => '/usr/bin/php -q /usr/share/cacti/cli/import_template.php --filename=/var/lib/cacti/linux_host.xml --with-template-rras',
+    cwd     => '/usr/share/cacti/cli',
+    require => File['/var/lib/cacti/linux_host.xml'],
+  }
 
   openstack_project::cacti_device { $cacti_hosts: }
 }
