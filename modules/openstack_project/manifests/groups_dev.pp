5889d4c0acdd751960cd5f170b233ec835797b59
Ivan Udovichenko <iudovichenko@mirantis.com>
2014-11-11 16:45:09 +0200

    Remove hardcoded variables from openstack_project module (groups_dev)
    Currently hardcoded variables are set and we should get rid of them
    and use current values as defaults.
    
    Change-Id: I1d55ac917af2628e986c095c77dbe0acc807c005
    Counter: 2

--- modules/openstack_project/manifests/groups_dev.pp
+++ modules/openstack_project/manifests/groups_dev.pp
@@ -15,15 +15,33 @@
 # User group management dev server
 #
 class openstack_project::groups_dev (
-  $site_admin_password = '',
-  $site_mysql_host     = '',
-  $site_mysql_password = '',
-  $conf_cron_key = '',
+  $admin_users = [
+    'mkiss',
+  ],
+  $drupal_conf_cron_key = '',
+  $drupal_conf_markdown_directory = '/srv/groups-static-pages',
+  $drupal_conf_openid_provider = 'https://openstackid-dev.openstack.org',
+  $drupal_site_name = 'groups-dev.openstack.org',
+  $drupal_site_root = '/srv/vhosts/groups-dev.openstack.org',
+  $drupal_site_mysql_host = $site_mysql_host,
+  $drupal_site_mysql_user = 'groups',
+  $drupal_site_mysql_password = $site_mysql_password,
+  $drupal_site_mysql_database = 'groups_dev',
+  $drupal_site_vhost_root = '/srv/vhosts',
+  $drupal_site_admin_password = $site_admin_password,
+  $drupal_site_alias = 'groupsdev',
+  $drupal_site_profile = 'groups',
+  $drupal_site_base_url = 'http://groups-dev.openstack.org',
+  $drupal_package_repository = 'http://tarballs.openstack.org/groups/drupal-updates/release-history',
+  $drupal_package_branch = 'dev',
+  $vcsrepo_provider = 'git',
+  $vcsrepo_revision = 'master',
+  $vcsrepo_source = 'https://git.openstack.org/openstack-infra/groups-static-pages',
   $sysadmins = [],
 ) {
 
   realize (
-    User::Virtual::Localuser['mkiss'],
+    User::Virtual::Localuser[$admin_users],
   )
 
 #  include drupal
@@ -35,30 +53,30 @@ class openstack_project::groups_dev (
 
   vcsrepo { '/srv/groups-static-pages':
     ensure   => latest,
-    provider => git,
-    revision => 'master',
-    source   => 'https://git.openstack.org/openstack-infra/groups-static-pages',
+    provider => $vcsrepo_provider,
+    revision => $vcsrepo_revision,
+    source   => $vcsrepo_source,
   }
 
   class { 'drupal':
-    site_name               => 'groups-dev.openstack.org',
-    site_root               => '/srv/vhosts/groups-dev.openstack.org',
-    site_mysql_host         => $site_mysql_host,
-    site_mysql_user         => 'groups',
-    site_mysql_password     => $site_mysql_password,
-    site_mysql_database     => 'groups_dev',
-    site_vhost_root         => '/srv/vhosts',
-    site_admin_password     => $site_admin_password,
-    site_alias              => 'groupsdev',
-    site_profile            => 'groups',
-    site_base_url           => 'http://groups-dev.openstack.org',
-    package_repository      => 'http://tarballs.openstack.org/groups/drupal-updates/release-history',
-    package_branch          => 'dev',
-    conf_cron_key           => $conf_cron_key,
-    conf_markdown_directory => '/srv/groups-static-pages',
-    conf_openid_provider    => 'https://openstackid-dev.openstack.org',
+    site_name               => $drupal_site_name,
+    site_root               => $drupal_site_root,
+    site_mysql_host         => $drupal_site_mysql_host,
+    site_mysql_user         => $drupal_site_mysql_user,
+    site_mysql_password     => $drupal_site_mysql_password,
+    site_mysql_database     => $drupal_site_mysql_database,
+    site_vhost_root         => $drupal_site_vhost_root,
+    site_admin_password     => $drupal_site_admin_password,
+    site_alias              => $drupal_site_alias,
+    site_profile            => $drupal_site_profile,
+    site_base_url           => $drupal_site_base_url,
+    package_repository      => $drupal_package_repository,
+    package_branch          => $drupal_package_branch,
+    conf_cron_key           => $drupal_conf_cron_key,
+    conf_markdown_directory => $drupal_conf_markdown_directory,
+    conf_openid_provider    => $drupal_conf_openid_provider,
     require                 => [ Class['openstack_project::server'],
-      Vcsrepo['/srv/groups-static-pages'] ]
+      Vcsrepo[$drupal_conf_markdown_directory] ]
   }
 
 }
