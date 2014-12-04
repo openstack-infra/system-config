855b072d81dc86b6af7c69dc52ef5a78352870ef
Ivan Udovichenko <iudovichenko@mirantis.com>
2014-12-04 16:52:00 +0200

    Remove hardcoded variables from openstack_project module (review_dev)
    Currently hardcoded variables are set and we should get rid of them
    and use current values as defaults. User can manualy set if bup is
    needed by passing variable "use_bup" with "true" or "false" values.
    
    Change-Id: Ib9f4f4ca9223e64537babe4172895acb2ca4c6ec

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -1,6 +1,9 @@
 # == Class: openstack_project::review_dev
 #
 class openstack_project::review_dev (
+  $admin_users = [
+    'zaro',
+  ],
   $github_oauth_token = '',
   $github_project_username = '',
   $github_project_password = '',
@@ -24,10 +27,39 @@ class openstack_project::review_dev (
   $swift_password = '',
   $project_config_repo = '',
   $projects_config = 'openstack_project/review-dev.projects.ini.erb',
+  $use_bup = true,
+  $bup_backup_user = 'bup-review-dev',
+  $bup_backup_server = 'ci-backup-rs-ord.openstack.org',
+  $gerrit_canonicalweburl = 'https://review-dev.openstack.org/',
+  $gerrit_contactstore_url = 'https://review-dev.openstack.org/fakestore',
+  $gerrit_email = 'review-dev@openstack.org',
+  $gerrit_github_username = 'openstack-gerrit-dev',
+  $gerrit_plugins = {
+    'javamelody' => { 'version' => 'e00d5af' },
+  },
+  $gerrit_vhost_name = 'review-dev.openstack.org',
+  $gerrit_war = 'http://tarballs.openstack.org/ci/test/gerrit-v2.8.4.17.13ac409.war',
+  $gerrit_web_repo_url = 'https://git.openstack.org/cgit/',
+  $gerrit_replication = [
+    {
+      name                 => 'github',
+      url                  => 'git@github.com:',
+      authGroup            => 'Anonymous Users',
+      replicatePermissions => false,
+      mirror               => true,
+    },
+    {
+      name                 => 'local',
+      url                  => 'file:///opt/lib/git/',
+      replicationDelay     => '0',
+      threads              => '4',
+      mirror               => true,
+    },
+  ],
 ) {
 
   realize (
-    User::Virtual::Localuser['zaro'],
+    User::Virtual::Localuser[$admin_users],
   )
 
   class { 'project_config':
@@ -36,8 +68,8 @@ class openstack_project::review_dev (
   }
 
   class { 'openstack_project::gerrit':
-    vhost_name                      => 'review-dev.openstack.org',
-    canonicalweburl                 => 'https://review-dev.openstack.org/',
+    vhost_name                      => $gerrit_vhost_name,
+    canonicalweburl                 => $gerrit_canonicalweburl,
     ssl_cert_file                   => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
     ssl_key_file                    => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_chain_file                  => '',
@@ -47,19 +79,17 @@ class openstack_project::review_dev (
     ssh_rsa_pubkey_contents         => $ssh_rsa_pubkey_contents,
     ssh_project_rsa_key_contents    => $ssh_project_rsa_key_contents,
     ssh_project_rsa_pubkey_contents => $ssh_project_rsa_pubkey_contents,
-    email                           => 'review-dev@openstack.org',
-    war                             =>
-      'http://tarballs.openstack.org/ci/test/gerrit-v2.8.4.17.13ac409.war',
+    email                           => $gerrit_email,
+    war                             => $gerrit_war,
     contactstore                    => $contactstore,
     contactstore_appsec             => $contactstore_appsec,
     contactstore_pubkey             => $contactstore_pubkey,
-    contactstore_url                =>
-      'https://review-dev.openstack.org/fakestore',
+    contactstore_url                => $gerrit_contactstore_url,
     acls_dir                        => $::project_config::gerrit_acls_dir,
     notify_impact_file              => $::project_config::gerrit_notify_impact_file,
     projects_file                   => $::project_config::jeepyb_project_file,
     projects_config                 => $projects_config,
-    github_username                 => 'openstack-gerrit-dev',
+    github_username                 => $gerrit_github_username,
     github_oauth_token              => $github_oauth_token,
     github_project_username         => $github_project_username,
     github_project_password         => $github_project_password,
@@ -69,31 +99,14 @@ class openstack_project::review_dev (
     sysadmins                       => $sysadmins,
     gitweb                          => false,
     cgit                            => true,
-    web_repo_url                    => 'https://git.openstack.org/cgit/',
+    web_repo_url                    => $gerrit_web_repo_url,
     swift_username                  => $swift_username,
     swift_password                  => $swift_password,
-    replication                     => [
-      {
-        name                 => 'github',
-        url                  => 'git@github.com:',
-        authGroup            => 'Anonymous Users',
-        replicatePermissions => false,
-        mirror               => true,
-      },
-      {
-        name                 => 'local',
-        url                  => 'file:///opt/lib/git/',
-        replicationDelay     => '0',
-        threads              => '4',
-        mirror               => true,
-      },
-    ],
+    replication                     => $gerrit_replication,
     require                         => $::project_config::config_dir,
   }
 
-  gerrit::plugin { 'javamelody':
-    version => 'e00d5af',
-  }
+  create_resources('gerrit::plugin',$gerrit_plugins)
 
   file { '/home/gerrit2/.launchpadlib':
     ensure  => directory,
@@ -102,6 +115,7 @@ class openstack_project::review_dev (
     mode    => '0775',
     require => User['gerrit2'],
   }
+
   file { '/home/gerrit2/.launchpadlib/creds':
     ensure  => present,
     owner   => 'gerrit2',
@@ -112,9 +126,11 @@ class openstack_project::review_dev (
     require => User['gerrit2'],
   }
 
-  include bup
-  bup::site { 'rs-ord':
-    backup_user   => 'bup-review-dev',
-    backup_server => 'ci-backup-rs-ord.openstack.org',
+  if $use_bup {
+    include bup
+    bup::site { 'rs-ord':
+      backup_user   => $bup_backup_user,
+      backup_server => $bup_backup_server,
+    }
   }
 }
