96382b860a585eea71a160cb42c12fe380d2fdee
Clark Boylan <clark.boylan@gmail.com>
2014-12-13 10:37:24 -0800

    Run an openstackweb dev site
    We have openstackweb source now \o/ Next step is deploy it on a dev
    site. Change does this by adding a www-dev.openstack.org node to site.pp
    which is deployed using a new openstackweb puppet module.
    
    Note that this puppet module will likely never merge into this repo and
    should instead be split off into its own thing, but this makes initial
    bootstrapping of everything easy by putting it all here.
    
    Change-Id: Iaae265711288046e51d38f982f5f27a83a17025c

---
+++ modules/openstackweb/manifests/init.pp
@@ -0,0 +1,52 @@
+# == Class: openstackweb
+#
+# Installs dependencies of openstack web project deployment.
+# This includes apache, php5, composer, and the source of
+# openstackweb itself.
+#
+class openstackweb (
+  $source = 'https://git.openstack.org/openstack-infra/openstackweb',
+  $revision = 'master',
+) {
+  include apache
+  a2mod { 'rewrite':
+    ensure => present,
+  }
+
+  package { 'php5':
+    ensure => present,
+  }
+
+  # Note that php complains this is deprecated.
+  package { 'php5-mysql':
+    ensure => present,
+  }
+
+  # TODO (clarkb) install git archive of this repo in /var/www
+  # or somewhere else that is not a git repo and use that as proper
+  # install location.
+  vcsrepo { '/opt/openstackweb':
+    ensure   => present,
+    provider => git,
+    source   => $source,
+    revision => $revision,
+    require  => Package['git'],
+  }
+
+  # TODO(clarkb) install composer more sanely
+  exec { 'download_composer':
+    command => '/usr/bin/wget https://getcomposer.org/installer -O composer.installer',
+    cwd     => '/opt/openstackweb',
+    creates => '/opt/openstackweb/composer.installer',
+    require => Package['wget'],
+  }
+  exec { 'install_composer':
+    command => '/usr/bin/php composer.installer',
+    cwd     => '/opt/openstackweb',
+    creates => '/opt/openstackweb/composer.phar',
+    require => [
+      Exec['download_composer'],
+      Package['php5'],
+    ],
+  }
+}
