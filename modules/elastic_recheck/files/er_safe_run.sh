914fbafec385799d42feb4e49e4eeee4f80a3646
Joe Gordon <joe.gordon0@gmail.com>
2014-12-15 14:18:34 -0800

    Improve er_safe_run
    * Nonblocking lock, if cannot acquire lock exit immediately with exit
      code of 1. This is instead of the default behavior of waiting
     indefinitely  for the lock
    * Timeout job after 45 minutes (send KILL signal after 40 minutes). In
      case something goes wrong we don't want to hold the lock for ever.
    
    Every so often something goes wrong (ES hanging etc), so make running of
    elastic-recheck safer.
    
    Change-Id: Ia00a585b6a336ed8f17e828b743858f501b5b8e7

--- modules/elastic_recheck/files/er_safe_run.sh
+++ modules/elastic_recheck/files/er_safe_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-flock /var/lib/elastic-recheck/er_safe_run.lock $@
+# non blocking lock, send KILL signal after 40 minutes, kill after 45 minutes
+flock -n /var/lib/elastic-recheck/er_safe_run.lock timeout -k 40m 45m $@
