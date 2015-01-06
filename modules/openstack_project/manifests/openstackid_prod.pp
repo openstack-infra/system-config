8a0adb483bbe54d61638412f8cef1bda57e2d42c
Marton Kiss <marton.kiss@gmail.com>
2015-01-06 16:46:45 +0100

    Release upgrade of openstackid 1.0.3
    Roll-out v1.0.3 to openstackid.org, this contains the Oauth2 and
    SSL config changes at application side.
    
    Change-Id: I906f1e6b167ffac73543a86ca318d8171a3ff4b7

--- modules/openstack_project/manifests/openstackid_prod.pp
+++ modules/openstack_project/manifests/openstackid_prod.pp
@@ -38,7 +38,7 @@ class openstack_project::openstackid_prod (
   $ssl_cert_file_contents = '',
   $ssl_key_file_contents = '',
   $ssl_chain_file_contents = '',
-  $release = '1.0.2',
+  $release = '1.0.3',
 ) {
 
   class { 'openstack_project::server':
