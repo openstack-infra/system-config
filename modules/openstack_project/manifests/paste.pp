b7e352cc86bb850fa1c4d6b74253e5450165afa2
Jeremy Stanley <fungi@yuggoth.org>
2015-01-08 16:49:58 +0000

    Add a local MySQL database to paste.o.o
    Due to performance problems with the current Trove backend, add a
    MySQL database locally on paste.openstack.org for later use.
    
    Change-Id: Idc04a95ba157e4d3722c3ea8a703e52c5793976c

--- modules/openstack_project/manifests/paste.pp
+++ modules/openstack_project/manifests/paste.pp
@@ -3,6 +3,7 @@
 class openstack_project::paste (
   $db_host,
   $db_password,
+  $mysql_root_password,
   $sysadmins = []
 ) {
   class { 'openstack_project::server':
@@ -15,5 +16,27 @@ class openstack_project::paste (
     db_password => $db_password,
     port        => '5000',
     image       => 'header-bg2.png',
+    require     => mysql::db['openstack'],
+  }
+
+  class { 'mysql::server':
+    config_hash => {
+      'root_password'  => $mysql_root_password,
+      'default_engine' => 'InnoDB',
+      'bind_address'   => '127.0.0.1',
+    }
+  }
+
+  include mysql::server::account_security
+  mysql::db { 'openstack':
+    user     => 'openstack',
+    password => $db_password,
+    host     => 'localhost',
+    grant    => ['all'],
+    charset  => 'utf8',
+    require  => [
+      Class['mysql::server'],
+      Class['mysql::server::account_security'],
+    ],
   }
 }
