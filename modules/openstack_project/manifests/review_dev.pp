dba3b2c5605c4abf5f6c35aeef2eb7198e444ce3
Khai Do <zaro0508@gmail.com>
2015-02-19 22:46:07 -0800

    Upgrade Gerrit from 2.8 to ver 2.9 for review-dev.o.o
    Change-Id: Ie89c02d575de4f9ef82833a2e1b1807bf3856ad1

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -49,7 +49,7 @@ class openstack_project::review_dev (
     ssh_project_rsa_pubkey_contents => $ssh_project_rsa_pubkey_contents,
     email                           => 'review-dev@openstack.org',
     war                             =>
-      'http://tarballs.openstack.org/ci/test/gerrit-v2.8.4.15.6dc8444.war',
+      'http://tarballs.openstack.org/ci/test/gerrit-v2.9.4.3.7adc92a.war',
     contactstore                    => $contactstore,
     contactstore_appsec             => $contactstore_appsec,
     contactstore_pubkey             => $contactstore_pubkey,
