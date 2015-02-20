a38705be6da161bde4248cc554dc4e0962541843
Michael Krotscheck <krotscheck@gmail.com>
2015-02-20 14:38:28 -0800

    Use StoryBoard optional https manifests.
    https://review.openstack.org/#/c/157600/ splits out the apache
    configuration into two optional module manifests, which must
    be explicitly included. This patch updates the infra storyboard
    module to use that module.
    
    Change-Id: I200855cbd9133cfaca6f379d665f29dcda929e30
    Depends-on: If237fbffa644fafa1788987a6eb7a82b5207b03b

--- modules/openstack_project/manifests/storyboard.pp
+++ modules/openstack_project/manifests/storyboard.pp
@@ -55,7 +55,7 @@ class openstack_project::storyboard(
   }
 
   # Install all the things.
-  include ::storyboard::cert
+  include ::storyboard::apache::https
   include ::storyboard::rabbit
   include ::storyboard::mysql
   include ::storyboard::application
