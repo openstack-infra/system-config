cd5a869f43bf346a03e741971721bde078aae123
Ian Cordasco <ian.cordasco@rackspace.com>
2015-03-04 14:47:41 -0600

    Add logging to #openstack-ansible
    When the channel was created, the addition of logging was missed. This
    change will likely conflict with I08dca4193f09b801df468861896f71c71048196e
    
    Change-Id: Ibc714ea0540e46911452b747d281b5fa6b7c72fc

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -42,6 +42,7 @@ class openstack_project::eavesdrop (
         '#magnetodb',
         '#murano',
         '#openstack',
+        '#openstack-ansible',
         '#openstack-barbican',
         '#openstack-blazar',
         '#openstack-ceilometer',
