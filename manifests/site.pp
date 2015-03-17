7c9a23a361e2f6ae806dd02a2702e07be88cc535
Gregory Haynes <greg@greghaynes.net>
2015-03-17 22:37:49 +0000

    Add wheel_mirror_slaves that use pypi-mirror
    We want to start building wheels for as many of our dependencies as
    possible in order to speed up test runs. Resurrecting parts of the old
    pypi-mirror code.
    
    We have pre-seeded this with a group of common packages, taken roughly
    from the bindep output from global-requirements.
    
    Change-Id: Ie33861dbcb413c34012820fda76f8be94fb1d151

--- manifests/site.pp
+++ manifests/site.pp
@@ -1083,4 +1083,11 @@ node 'codesearch.openstack.org' {
   }
 }
 
+node /.*wheel-mirror-.*\.openstack\.org/ {
+    include openstack_project
+    class { 'openstack_project::wheel_mirror_slave':
+        jenkins_ssh_public_key  => $openstack_project::jenkins_ssh_key,
+  }
+}
+
 # vim:sw=2:ts=2:expandtab:textwidth=79
---
+++ modules/openstack_project/manifests/wheel_mirror_slave.pp
@@ -0,0 +1,60 @@
+# Copyright 2015  Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to install dependencies for uploading python packages to pypi and
+# maven repositories
+#
+class openstack_project::wheel_mirror_slave (
+  $jenkins_ssh_public_key,
+) {
+  class { 'openstack_project::slave':
+    ssh_key => $jenkins_ssh_public_key,
+  }
+
+  # below follows a rough list of things required to build binary
+  # wheels.
+
+  # TODO: global-requirements keeps other-requirements that can be
+  # parsed by bindep.  We should have some sort of bindep provider
+  # that can interact with that
+
+  case $::osfamily {
+    'Debian': {
+      ensure_packages(['build-essential',
+                       'python-all-dev', 'python3-all-dev',
+                       'libxml2-dev', 'libxslt-dev',
+                       'libffi-dev', 'libreadline-dev',
+                       'libssl-dev', 'libyaml-dev', 'zlib1g-dev',
+                       'libldap2-dev', 'libmysqlclient-dev',
+                       'libnspr4-dev', 'libpq-dev', 'libsasl2-dev',
+                       'libsqlite3-dev', 'libvirt-dev', 'libzmq-dev',
+                       'pkg-config', 'swig', 'uuid-dev'])
+      }
+     'Redhat': {
+       ensure_packages(['gcc', 'gcc-c++', 'make',
+                        'python-devel', 'python3-devel',
+                        'libxml2-devel', 'libxslt-devel',
+                        'libffi-devel', 'readline-devel',
+                        'openssl-devel', 'libyaml-devel', 'zlib-devel',
+                        'openldap-devel', 'mariadb-devel',
+                        'nspr-devel', 'postgresql-devel', 'cyrus-sasl-devel',
+                        'sqlite-devel', 'libvirt-devel', 'zeromq-devel',
+                        'pkgconfig', 'swig', 'uuid-devel'])
+    }
+    default: {
+      err "${::osfamily} not supported yet"
+    }
+  }
+
+}
