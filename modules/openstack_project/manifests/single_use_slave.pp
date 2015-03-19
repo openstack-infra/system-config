3b3ab22c5a5ffe1e3bdd5b864f2964f34d7ecf3c
Monty Taylor <mordred@inaugust.com>
2015-03-19 12:02:12 -0400

    Get rid of cloud-init on single-use-slaves
    nova metadata service is too flaky. Instead of using it for anything,
    bake the ssh key into the images.
    
    Change-Id: I7c02da1ed6f5c5c3f5a2437f606fc0aab3d3dd3f

--- modules/openstack_project/manifests/single_use_slave.pp
+++ modules/openstack_project/manifests/single_use_slave.pp
@@ -53,4 +53,16 @@ class openstack_project::single_use_slave (
     }
   }
 
+  package { 'cloud-init':
+    ensure => 'absent',
+  }
+
+  ssh_authorized_key { 'nodepool-static-2015-03-19':
+    ensure  => present,
+    user    => 'root',
+    type    => 'ssh-rsa',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6WutNHfM+YdnjeNFeaIpvxqt+9aDn95Ykpmc+fASSjlDZJtOrueH3ch/v08wkE4WQKg03i+t8VonqEwMGmApYA3VzFsURUQbxzlSz5kHlBQSqgz5JTwUmnt1RH5sePL5pkuJ6JgqJ8PxJod6fiD7YDjaKJW/wBzXGnGg2EkgqrkBQXYL4hyaPuSwsQF0Gdwg3QFqXl+R/GrM6FscUkkJzbjqGKI2GhLT8mf2BIMEAiMFhF5Wl4FFrbvhTfPfW+9VdcsiMxCXaxp00n1x1+Y7OqR5AZ/id0Lkz9ZoFVGS901OB/L4xXrvUtI2y+kIYeF6hxfmAl/zhY0eWzwo9lDPz',
+    require => File['/root/.ssh'],
+  }
+
 }
