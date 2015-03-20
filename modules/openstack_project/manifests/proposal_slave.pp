b32ff4ade36818f55999bc13ed3e3fe87cda0549
Steve Kowalik <steven@wedontsleep.org>
2015-03-20 17:06:16 +1100

    Add zanata-cli utility to proposal slave
    Due to the specification to move from Transifex to Zanata, change the
    proposal slave to grab the zanata-cli release tarball from
    maven-central, unpack it and symlink the command line utility into the
    $PATH.
    
    Change-Id: I7b632527d56365364063632fc8506718eada5b86

--- modules/openstack_project/manifests/proposal_slave.pp
+++ modules/openstack_project/manifests/proposal_slave.pp
@@ -30,6 +30,52 @@ class openstack_project::proposal_slave (
     require => User['jenkins'],
   }
 
+  file { '/opt/zanata':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    mode     => '0644',
+    require  => User['jenkins'],
+  }
+
+  exec { 'get_zanata_client_dist_tarball':
+    command => 'wget https://search.maven.org/remotecontent?filepath=org/zanata/zanata-cli/3.4.2/zanata-cli-3.4.2-dist.tar.gz -O /opt/zanata/zanata-cli-3.4.2-dist.tar.gz',
+    path    => '/bin:/usr/bin',
+    creates => '/opt/zanata/zanata-cli-3.4.2-dist.tar.gz',
+    require => File['/opt/zanata'],
+  }
+
+  file { '/opt/zanata/zanata-cli-3.4.2-dist.tar.gz':
+    ensure => present,
+    owner  => 'jenkins',
+    group  => 'jenkins',
+    mode   => '0644',
+    require => Exec['get_zanata_client_dist_tarball'],
+  }
+
+  exec { 'unpack_zanata_client_dist_tarball':
+    command => 'tar zxf zanata-cli-3.4.2-dist.tar.gz',
+    path    => '/bin:/usr/bin',
+    user    => 'jenkins',
+    cwd     => '/opt/zanata',
+    creates => '/opt/zanata/zanata-cli-3.4.2/bin/zanata-cli',
+    require => Exec['get_zanata_client_dist_tarball'],
+  }
+
+  file { '/opt/zanata/zanata-cli-3.4.2/bin/zanata-cli':
+    ensure => present,
+    owner  => 'jenkins',
+    group  => 'jenkins',
+    mode   => '0755',
+    require => Exec['unpack_zanata_client_dist_tarball'],
+  }
+
+  file { '/usr/local/bin/zanata-cli':
+    ensure  => link,
+    target  => '/opt/zanata/zanata-cli-3.4.2/bin/zanata-cli',
+    require => File['/opt/zanata/zanata-cli-3.4.2/bin/zanata-cli'],
+  }
+
   file { '/home/jenkins/.ssh/id_rsa':
     owner   => 'jenkins',
     group   => 'jenkins',
