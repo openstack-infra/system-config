21d0cd28a640141079150ff7f1677be35524b510
Spencer Krum <nibz@spencerkrum.com>
2015-04-07 13:38:30 -0700

    Refactor o_p::template into o_p::server
    Add manage_exim flag so that exim management can be turned off
    or on. This enables all current invocations of o_p::template to
    be duplicated by similar invocations of o_p::server
    
    Change-Id: I7c210b2bf30660e1ce84e8d0c7b0efb8d634b47a
    Spec: http://specs.openstack.org/openstack-infra/infra-specs/specs/server_base_template_refactor.html
    Story: 2000172

--- modules/openstack_project/manifests/backup_server.pp
+++ modules/openstack_project/manifests/backup_server.pp
@@ -1,8 +1,9 @@
 # == Class: openstack_project::backup_server
 #
 class openstack_project::backup_server {
-  class { 'openstack_project::template':
+  class { 'openstack_project::server':
     iptables_public_tcp_ports => [],
+    manage_exim               => false,
   }
   package { 'bup':
     ensure => present,
