853a1afaa7dd1b48a1665b9534e34aeab85bc597
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-04-08 10:58:03 +0200

    Allow configuring all lodgeit parameters on paste
    Lodgeit has the ability to configure vhost, db host,
    user, image... But openstack_project::paste don't
    offer these parameters. Update the manifest to properly
    allow it, and set secure defaults.
    
    Change-Id: Id88d8fb52599b6bd002f7373a6adfad63e21c620

--- modules/openstack_project/manifests/paste.pp
+++ modules/openstack_project/manifests/paste.pp
@@ -3,29 +3,38 @@
 class openstack_project::paste (
   $db_password,
   $mysql_root_password,
+  $db_host            = 'localhost',
+  $db_user            = 'openstack',
+  $mysql_bind_address = '127.0.0,1',
+  $port               = '5000',
+  $image              = 'header-bg2.png',
+  $vhost_name         = 'paste.openstack.org',
+  $default_engine     = 'InnoDB',
 ) {
   include lodgeit
   lodgeit::site { 'openstack':
-    db_host     => 'localhost',
+    port        => $port,
     db_password => $db_password,
-    port        => '5000',
-    image       => 'header-bg2.png',
+    db_host     => $db_host,
+    db_user     => $db_user,
+    vhost_name  => $vhost_name,
+    image       => $image,
     require     => mysql::db['openstack'],
   }
 
   class { 'mysql::server':
     config_hash => {
       'root_password'  => $mysql_root_password,
-      'default_engine' => 'InnoDB',
-      'bind_address'   => '127.0.0.1',
+      'default_engine' => $default_engine,
+      'bind_address'   => $mysql_bind_address,
     }
   }
 
   include mysql::server::account_security
   mysql::db { 'openstack':
-    user     => 'openstack',
+    user     => $db_user,
     password => $db_password,
-    host     => 'localhost',
+    host     => $db_host,
     grant    => ['all'],
     charset  => 'utf8',
     require  => [
