34dbf61df93f52941bef9b85ce9a48653899e131
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-04-08 12:43:45 +0200

    Move manage_projects logic to puppet-jeepyb
    This functionality has been isolated into jeepyb,
    so properly call the class with the right parameters.
    
    Change-Id: Idd5abde6daf67af941d406d5ff09ddb71949a9d7
    Depends-On: I4d90513c3fac6632fcf832564b8763a38274398f

--- modules/openstack_project/manifests/gerrit.pp
+++ modules/openstack_project/manifests/gerrit.pp
@@ -396,6 +396,15 @@ class openstack_project::gerrit (
       }
     }
 
+    if ($testmode == false) {
+      $jeepyb_notify = []
+    }
+    else {
+      $jeepyb_notify = [
+        Exec['jeepyb::manage_projects::jeepyb_manage_projects'],
+      ]
+    }
+
     file { '/home/gerrit2/projects.yaml':
       ensure  => present,
       owner   => 'gerrit2',
@@ -404,6 +413,7 @@ class openstack_project::gerrit (
       source  => $projects_file,
       replace => true,
       require => Class['::gerrit'],
+      notify  => $jeepyb_notify,
     }
 
     file { '/home/gerrit2/projects.ini':
@@ -426,38 +436,17 @@ class openstack_project::gerrit (
       purge   => true,
       force   => true,
       source  => $acls_dir,
-      require => Class['::gerrit']
+      require => Class['::gerrit'],
+      notify  => $jeepyb_notify,
     }
 
     if ($testmode == false) {
-      exec { 'manage_projects':
-        command     => '/usr/local/bin/manage-projects -v >> /var/log/manage_projects.log 2>&1',
-        timeout     => 900, # 15 minutes
-        subscribe   => [
+      class { 'jeepyb::manage_projects':
+        timeout => 900,
+        before  => [
             File['/home/gerrit2/projects.yaml'],
             File['/home/gerrit2/acls'],
           ],
-        refreshonly => true,
-        logoutput   => true,
-        require     => [
-            File['/home/gerrit2/projects.yaml'],
-            File['/home/gerrit2/acls'],
-            Class['jeepyb'],
-          ],
-      }
-
-      include logrotate
-      logrotate::file { 'manage_projects.log':
-        log     => '/var/log/manage_projects.log',
-        options => [
-          'compress',
-          'missingok',
-          'rotate 30',
-          'daily',
-          'notifempty',
-          'copytruncate',
-        ],
-        require => Exec['manage_projects'],
       }
     }
   }
