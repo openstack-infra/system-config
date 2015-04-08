7cb0b537132a02b6e5b912fdf8f8be37528b7687
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-04-08 12:43:45 +0200

    Move manage_projects logic to puppet-jeepyb
    This functionality has been isolated into jeepyb,
    so properly call the class with the right parameters.
    
    Change-Id: Idd5abde6daf67af941d406d5ff09ddb71949a9d7
    Depends-On: I4132646446dfe575c333b981aea9a7778c13bcf0

--- modules/openstack_project/manifests/gerrit.pp
+++ modules/openstack_project/manifests/gerrit.pp
@@ -430,34 +430,17 @@ class openstack_project::gerrit (
     }
 
     if ($testmode == false) {
-      exec { 'manage_projects':
-        command     => '/usr/local/bin/manage-projects -v >> /var/log/manage_projects.log 2>&1',
-        timeout     => 900, # 15 minutes
-        subscribe   => [
+      class { 'jeepyb::manage_projects':
+        timeout => 900,
+        notify  => [
             File['/home/gerrit2/projects.yaml'],
             File['/home/gerrit2/acls'],
           ],
-        refreshonly => true,
-        logoutput   => true,
-        require     => [
+        before  => [
             File['/home/gerrit2/projects.yaml'],
             File['/home/gerrit2/acls'],
-            Class['jeepyb'],
           ],
-      }
-
-      include logrotate
-      logrotate::file { 'manage_projects.log':
-        log     => '/var/log/manage_projects.log',
-        options => [
-          'compress',
-          'missingok',
-          'rotate 30',
-          'daily',
-          'notifempty',
-          'copytruncate',
-        ],
-        require => Exec['manage_projects'],
+        logfile => '/var/log/manage_projects.log',
       }
     }
   }
