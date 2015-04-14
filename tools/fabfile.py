a54d63377be20a61b4a68ad2b510ad79557348fa
Spencer Krum <nibz@spencerkrum.com>
2015-04-14 13:04:38 -0700

    Use envassert for integration tests
    Running envassert_test.sh will apply puppet and run envassert tests.
    We can put more tests in fabfile.py.
    
    Probably should deduplicate code between apply-test and envassert_test.
    
    Change-Id: Ieee22c52e1f606d1c9366c556b6e3c1fcc90bde8

---
+++ tools/fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import env, task
+from envassert import file, process, package, user, group, port, cron, detect, service
+
+env.use_ssh_config = True
+env.user = 'root'
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert package.installed("emacs23-nox")
+    assert service.is_enabled("unbound")
+
