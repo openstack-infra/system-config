138dc23bbc974aaf2e05f5bf67cdf1410522d8ce
Fabien Boucher <fabien.boucher@enovance.com>
2015-04-21 17:19:48 +0000

    Use openstackci module for zuul_merger zuul_prod and zuul_dev
    Depends-On: I48e49c760d5304bd7dd5b0bbbdaa27329b2d8a5f
    
    Change-Id: I98ab36cca49f8784fc93c46db69bc2a777a61938

--- modules/openstack_project/manifests/zuul_dev.pp
+++ modules/openstack_project/manifests/zuul_dev.pp
@@ -6,6 +6,11 @@ class openstack_project::zuul_dev(
   $gerrit_server = '',
   $gerrit_user = '',
   $gerrit_ssh_host_key = '',
+  $gerrit_ssh_host_identity = [
+    'review-dev.openstack.org',
+    '23.253.78.13',
+    '2001:4800:7817:101:be76:4eff:fe04',
+  ],
   $zuul_ssh_private_key = '',
   $url_pattern = '',
   $status_url = 'http://zuul-dev.openstack.org',
@@ -30,49 +35,26 @@ class openstack_project::zuul_dev(
     sysadmins                 => $sysadmins,
   }
 
-  class { 'project_config':
-    url  => $project_config_repo,
-    base => 'dev/',
+  class { 'openstackci::zuul_scheduler':
+    vhost_name               => $vhost_name,
+    gearman_server           => $gearman_server,
+    gerrit_server            => $gerrit_server,
+    gerrit_user              => $gerrit_user,
+    gerrit_ssh_host_key      => $gerrit_ssh_host_key,
+    gerrit_ssh_host_identity => $gerrit_ssh_host_identity,
+    zuul_ssh_private_key     => $zuul_ssh_private_key,
+    url_pattern              => $url_pattern,
+    zuul_url                 => $zuul_url,
+    job_name_in_report       => true,
+    status_url               => $status_url,
+    statsd_host              => $statsd_host,
+    git_email                => 'jenkins@openstack.org',
+    git_name                 => 'OpenStack Jenkins',
+    project_config_repo      => $project_config_repo,
+    project_config_base      => 'dev/',
   }
 
-  class { '::zuul':
-    vhost_name           => $vhost_name,
-    gearman_server       => $gearman_server,
-    gerrit_server        => $gerrit_server,
-    gerrit_user          => $gerrit_user,
-    zuul_ssh_private_key => $zuul_ssh_private_key,
-    url_pattern          => $url_pattern,
-    zuul_url             => $zuul_url,
-    job_name_in_report   => true,
-    status_url           => $status_url,
-    statsd_host          => $statsd_host,
-    git_email            => 'jenkins@openstack.org',
-    git_name             => 'OpenStack Jenkins',
-  }
-
-  class { '::zuul::server':
-    layout_dir      => $::project_config::zuul_layout_dir,
-    require         => $::project_config::config_dir,
-  }
-
-  class { '::zuul::merger': }
-
-  if $gerrit_ssh_host_key != '' {
-    file { '/home/zuul/.ssh':
-      ensure  => directory,
-      owner   => 'zuul',
-      group   => 'zuul',
-      mode    => '0700',
-      require => Class['::zuul'],
-    }
-    file { '/home/zuul/.ssh/known_hosts':
-      ensure  => present,
-      owner   => 'zuul',
-      group   => 'zuul',
-      mode    => '0600',
-      content => "review-dev.openstack.org,23.253.78.13,2001:4800:7817:101:be76:4eff:fe04 ${gerrit_ssh_host_key}",
-      replace => true,
-      require => File['/home/zuul/.ssh'],
-    }
+  class { 'openstackci::zuul_merger':
+    manage_common_zuul => false,
   }
 }
