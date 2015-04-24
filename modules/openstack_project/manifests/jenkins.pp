0fa84392950632cd0b7f5944b764c2acddf8321e
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2015-04-24 18:02:33 +0000

    Use common jenkins master implementation
    Change-Id: Ia18fe6e298caa5a9b017e6c6f5ef6f189f150335
    Depends-On: I635f039021f67828f6d13b13527288aedc9e4390

--- modules/openstack_project/manifests/jenkins.pp
+++ modules/openstack_project/manifests/jenkins.pp
@@ -31,22 +31,7 @@ class openstack_project::jenkins (
     sysadmins                 => $sysadmins,
   }
 
-  # Set defaults here because they evaluate variables which you cannot
-  # do in the class parameter list.
-  if $ssl_cert_file == '' {
-    $prv_ssl_cert_file = "/etc/ssl/certs/${vhost_name}.pem"
-  }
-  else {
-    $prv_ssl_cert_file = $ssl_cert_file
-  }
-  if $ssl_key_file == '' {
-    $prv_ssl_key_file = "/etc/ssl/private/${vhost_name}.key"
-  }
-  else {
-    $prv_ssl_key_file = $ssl_key_file
-  }
-
-  class { '::jenkins::master':
+  class { 'openstackci::jenkins_master':
     vhost_name              => $vhost_name,
     serveradmin             => $serveradmin,
     logo                    => $logo,
@@ -58,65 +43,6 @@ class openstack_project::jenkins (
     ssl_chain_file_contents => $ssl_chain_file_contents,
     jenkins_ssh_private_key => $jenkins_ssh_private_key,
     jenkins_ssh_public_key  => $jenkins_ssh_public_key,
-  }
-
-  jenkins::plugin { 'build-timeout':
-    version => '1.14',
-  }
-  jenkins::plugin { 'copyartifact':
-    version => '1.22',
-  }
-  jenkins::plugin { 'dashboard-view':
-    version => '2.3',
-  }
-  jenkins::plugin { 'gearman-plugin':
-    version => '0.1.1',
-  }
-  jenkins::plugin { 'git':
-    version => '1.1.23',
-  }
-  jenkins::plugin { 'greenballs':
-    version => '1.12',
-  }
-  jenkins::plugin { 'extended-read-permission':
-    version => '1.0',
-  }
-  jenkins::plugin { 'zmq-event-publisher':
-    version => '0.0.3',
-  }
-#  TODO(jeblair): release
-#  jenkins::plugin { 'scp':
-#    version => '1.9',
-#  }
-  jenkins::plugin { 'jobConfigHistory':
-    version => '1.13',
-  }
-  jenkins::plugin { 'monitoring':
-    version => '1.40.0',
-  }
-  jenkins::plugin { 'nodelabelparameter':
-    version => '1.2.1',
-  }
-  jenkins::plugin { 'notification':
-    version => '1.4',
-  }
-  jenkins::plugin { 'openid':
-    version => '1.5',
-  }
-  jenkins::plugin { 'postbuildscript':
-    version => '0.16',
-  }
-  jenkins::plugin { 'publish-over-ftp':
-    version => '1.7',
-  }
-  jenkins::plugin { 'simple-theme-plugin':
-    version => '0.2',
-  }
-  jenkins::plugin { 'timestamper':
-    version => '1.3.1',
-  }
-  jenkins::plugin { 'token-macro':
-    version => '1.5.1',
   }
 
   if $manage_jenkins_jobs == true {
