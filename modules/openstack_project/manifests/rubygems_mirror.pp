5938f794f06e8077d3a0e2bb29b3d31ad3b29085
Emilien Macchi <emilien@redhat.com>
2015-04-27 22:05:49 -0400

    Create rubygems mirror from rubygems.org
    All Puppet OpenStack jobs (lint, syntax, unit and beaker) are quite
    often affected by rubygems.org downtimes and make all jobs failing
    randomly because it can't download some gems during the bootstrap.
    
    This patch aims to:
    * refactorize pypi_mirror.pp to use stdlib for common resources that
      could be colocalized with rubygems mirror resources, so there won't be
      duplicated resource.
    * create rubygems_mirror and rubygems classes to install, configure and
      run rubygems_mirror tools periodicaly.
      It installs rubygems_mirros, create an Apache vhost, manage logs and
      lock, run and run the tool with a new user: 'rubygems'.
    * amend site.pp to match rubygems.openstack.org nodes that could be the
      same IP address of Pypi mirrors.
    
    ** NOTE FOR PyPi MAINTAINERS **
    This change could require some manual maintenance to have both vhost
    working together (we are moving PyPi vhost here).
    
    Change-Id: I8216eed658cc7b471c52ac1d1b6aeb62c296dfbd

---
+++ modules/openstack_project/manifests/rubygems_mirror.pp
@@ -0,0 +1,94 @@
+# == Class: openstack_project::rubygems_mirror
+#
+class openstack_project::rubygems_mirror (
+  $vhost_name,
+  $parallelism      = '10',
+  $destination_path = '/srv/static/mirror/rubygems',
+  $cron_frequency   = '*/5',
+) {
+
+  include ::apache
+
+  ensure_resource('file', '/srv/static', {
+    'ensure' => 'directory',
+  })
+
+  ensure_resource('file', '/srv/static/mirror', {
+    'ensure'  => 'directory',
+    'owner'   => 'root',
+    'group'   => 'root',
+  })
+
+  file { '/srv/static/mirror/rubygems':
+    ensure  => directory,
+    owner   => 'rubygems',
+    group   => 'root',
+    require => [File['/srv/static/mirror'],User['rubygems']],
+  }
+
+  apache::vhost { $vhost_name:
+    port     => 80,
+    priority => '50',
+    docroot  => $destination_path,
+    require  => File[$destination_path],
+  }
+
+  user { 'rubygems':
+    ensure     => 'present',
+    comment    => 'Service used to run rubygems mirror synchronization',
+    managehome => true,
+    require    => Package['rubygems-mirror'],
+  }
+
+  file { '/home/rubygems/.gem/.mirrorrc':
+    ensure  => present,
+    owner   => 'rubygems',
+    group   => 'rubygems',
+    mode    => '0600',
+    content => template('openstack_project/rubygems_mirrorrc.erb'),
+    replace => true,
+    require => [User['rubygems'],Package['rubygems-mirror']],
+  }
+
+  file { '/srv/static/mirror/rubygems/robots.txt':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'puppet:///modules/openstack_project/disallow_robots.txt',
+    require => File['/srv/static/mirror/rubygems'],
+  }
+
+  package { 'rubygems-mirror':
+    ensure   => latest,
+    provider => gem,
+  }
+
+  file { ['/var/run/rubygems','/var/log/rubygems']:
+    ensure  => directory,
+    owner   => 'rubygems',
+    group   => 'root',
+    require => User['rubygems'],
+  }
+
+  cron { 'rubygems-mirror':
+    minute      => $cron_frequency,
+    command     => 'flock -n /var/run/rubygems/mirror.lock timeout -k 2m 30m gem mirror >>/var/log/rubygems-mirror/mirror.log 2>&1',
+    environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
+    user        => 'rubygems',
+  }
+
+  include ::logrotate
+  logrotate::file { 'rubygems-mirror':
+    log     => '/var/log/rubygems-mirror/mirror.log',
+    options => [
+      'compress',
+      'copytruncate',
+      'missingok',
+      'rotate 7',
+      'daily',
+      'notifempty',
+    ],
+  }
+
+}
