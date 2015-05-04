35ce3c58ec8a5961ba9838ca1ad95652552a79be
Joshua Hesketh <josh@nitrotech.org>
2015-05-04 22:23:14 +1000

    Serve up jquery-visibility.js for gerrit
    Grab the latest jquery-visibility, and put it into place in gerrit's
    static folder.
    
    Change-Id: I2a8c5ad638dc4a0d256c06c2c94cde1779b4f0be

--- modules/openstack_project/manifests/gerrit.pp
+++ modules/openstack_project/manifests/gerrit.pp
@@ -285,6 +285,22 @@ class openstack_project::gerrit (
     notify      => Exec['reload_gerrit_header'],
   }
 
+  vcsrepo { '/opt/jquery-visibility':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://github.com/mathiasbynens/jquery-visibility.git',
+  }
+
+  file { '/home/gerrit2/review_site/static/jquery-visibility.js':
+    ensure => present,
+    source => '/opt/jquery-visibility/jquery-visibility.js',
+    subscribe => Vcsrepo['/opt/jquery-visibility'],
+    notify => Exec['reload_gerrit_header'],
+    require => [ File['/home/gerrit2/review_site/static'],
+                 Class['::gerrit'] ]
+  }
+
   file { '/home/gerrit2/review_site/static/hideci.js':
     ensure  => present,
     source  => 'puppet:///modules/openstack_project/gerrit/hideci.js',
