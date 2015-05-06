6351a431bc5c4dcc32a49e4ffed3be2a31f83056
Jeremy Stanley <fungi@yuggoth.org>
2015-05-06 21:18:35 +0000

    Increase JJB timeout to 20min on Jenkins masters
    Our matrix expansion has reached the point where changing a
    parameter in most common devstack-gate job templates takes more than
    the default 10-minute timeout to update completely on a loaded
    Jenkins master. Increasing to 20 minutes leaves a few minutes of
    breathing room for the moment.
    
    Change-Id: Iaca1ec2680179a7579276857ef8659e9dda69749

--- modules/openstack_project/manifests/jenkins.pp
+++ modules/openstack_project/manifests/jenkins.pp
@@ -123,13 +123,15 @@ class openstack_project::jenkins (
     }
 
     class { '::jenkins::job_builder':
-      url          => "https://${vhost_name}/",
-      username     => $jenkins_jobs_username,
-      password     => $jenkins_jobs_password,
-      git_revision => $jenkins_git_revision,
-      git_url      => $jenkins_git_url,
-      config_dir   => $::project_config::jenkins_job_builder_config_dir,
-      require      => $::project_config::config_dir,
+      jenkins_jobs_update_timeout => 1200,
+      url                         => "https://${vhost_name}/",
+      username                    => $jenkins_jobs_username,
+      password                    => $jenkins_jobs_password,
+      git_revision                => $jenkins_git_revision,
+      git_url                     => $jenkins_git_url,
+      config_dir                  =>
+        $::project_config::jenkins_job_builder_config_dir,
+      require                     => $::project_config::config_dir,
     }
 
     file { '/etc/default/jenkins':
