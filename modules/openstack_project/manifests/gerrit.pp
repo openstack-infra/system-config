3fa223863f9e860f6f1636edf61b808fd9df1404
Jeremy Stanley <fungi@yuggoth.org>
2015-05-11 15:46:41 +0000

    Root the Change-Id commentlink explicitly
    Gerrit's query redirects have changed slightly since 2.8, and it's
    possible to end up browsing with a URL like https://hostname// which
    causes relative-path links such as the Change-ID commentlink to 404.
    Prepend a leading / to the commentlink for this and avoid the issue.
    
    Change-Id: I09f0b491e2f54ab10d29f456cd026d90b673447f

--- modules/openstack_project/manifests/gerrit.pp
+++ modules/openstack_project/manifests/gerrit.pp
@@ -173,7 +173,7 @@ class openstack_project::gerrit (
       {
         name  => 'changeid',
         match => '(I[0-9a-f]{8,40})',
-        link  => '#q,$1,n,z',
+        link  => '/#q,$1,n,z',
       },
       {
         name  => 'gitsha',
