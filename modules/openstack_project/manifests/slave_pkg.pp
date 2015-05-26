e223c263f254a3b8953f2a3198b654f190f02166
Paul Belanger <pabelanger@redhat.com>
2015-05-26 17:23:08 +0000

    Jenkins slave for package builds
    This patch is the first step towards a generic package build environment for
    packagers. Right now we are using the zuul-packaging repo to dogfood.
    
    The goal would be to create a new prepare_node_pkg.sh script which nodepool
    would use. Creating a new jenkins slave specifically for packaging.
    
    Change-Id: Id33cad905d332cb4a6af810770fb770f385f3207
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ modules/openstack_project/manifests/slave_pkg.pp
@@ -0,0 +1,33 @@
+# == Class: openstack_project::slave_pkg
+#
+class openstack_project::slave_pkg {
+  $packages = [
+    'build-essential',
+    'debhelper',
+    'debootstrap',
+    'fakeroot',
+    'git-buildpackage',
+    'libwww-perl',
+    'python-sphinx',
+    'sbuild',
+  ]
+
+  package { $packages:
+    ensure  => present,
+  }
+
+  # Jenkins already exists, so we need to append our sbuild group.
+  User <| title == jenkins |> {
+    groups  +> 'mock',
+    require => Package[$packages],
+  }
+
+  $dist = [
+    'sid',
+  ]
+
+  # TODO(pabelanger): We should bootstrap our chroots with disk-builder (nodepool).
+  exec { "sbuild-createchroot --make-sbuild-tarball=/var/lib/sbuild/${dist}-amd64.tar.gz sid `mktemp -d` http://ftp.debian.org/debian":
+    require => Packages[$packages],
+  }
+}
