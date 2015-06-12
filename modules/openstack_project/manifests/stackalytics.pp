ae5aa970edca30f987069925895bdce9afbc051f
Paul Belanger <pabelanger@redhat.com>
2015-06-12 17:09:21 +0000

    WIP: Create stackalytics.o.o under -infra
    Change-Id: I9afa912abee38fb2732e97df06d72788b57d53df
    Depends-On: If62b3f633000cc6380d892cad0a2160136ce8ca4
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ modules/openstack_project/manifests/stackalytics.pp
@@ -0,0 +1,15 @@
+# == Class: openstack_project::stackalytics
+#
+class openstack_project::stackalytics (
+  $gerrit_host = 'review.openstack.org',
+  $gerrit_ssh_user = 'stackalytics',
+  $stackalytics_ssh_private_key = '',
+  $vhost_name = $::fqdn,
+) {
+    class { '::stackalytics':
+      gerrit_host                  => $gerrit_host,
+      gerrit_ssh_user              => $gerrit_ssh_user,
+      stackalytics_ssh_private_key => $stackalytics_ssh_private_key,
+      vhost_name                   => $vhost_name,
+    }
+}
