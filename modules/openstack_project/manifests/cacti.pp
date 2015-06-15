9eac92e123e3542e5867c61b67d2eaf41cda1149
Jeremy Stanley <fungi@yuggoth.org>
2015-06-15 22:12:23 +0000

    Add apps.openstack.org to cacti
    New apps.openstack.org server, we should track/trend its resource
    consumption to make sure we've sized it appropriately.
    
    Change-Id: I303172a4ec16a3b6900920d238d5872f43a5b5e9

--- modules/openstack_project/manifests/cacti.pp
+++ modules/openstack_project/manifests/cacti.pp
@@ -76,6 +76,8 @@ class openstack_project::cacti (
     'afs01.ord.openstack.org',
     'afsdb01.openstack.org',
     'afsdb02.openstack.org',
+    'apps.openstack.org',
+    'ask.openstack.org',
     'eavesdrop.openstack.org',
     'elasticsearch01.openstack.org',
     'elasticsearch02.openstack.org',
