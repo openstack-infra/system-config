a9f7585d11ae2248938be5ea731577ab3ad387c1
Spencer Krum <nibz@spencerkrum.com>
2015-06-23 00:31:56 -0700

    Set the openstack_project::users_install class to run in 'setup' stage
    DO NOT MERGE
    Probably if we want to do this we will move login.defs into its own
    class. I'm somewhat paranoid that stages attached to virtual users
    is gonna have weird behavior.
    
    Change-Id: I129d0dca59a22df63f917fc4c49f1400fcb89b51

--- modules/openstack_project/manifests/template.pp
+++ modules/openstack_project/manifests/template.pp
@@ -58,6 +58,13 @@ class openstack_project::template (
     rules6           => $iptables_rules6,
   }
 
+  ###########################################################
+  # Add Stages
+  # https://docs.puppetlabs.com/puppet/latest/reference/lang_run_stages.html
+
+  stage { 'setup':
+    before => Stage['main'],
+  }
 
   ###########################################################
   # Process if ( $high_level_directive ) blocks
@@ -75,7 +82,8 @@ class openstack_project::template (
   }
 
   class {'openstack_project::users_install':
-    install_users => $install_users
+    install_users => $install_users,
+    stage         => 'setup',
   }
 
   if ($enable_unbound) {
