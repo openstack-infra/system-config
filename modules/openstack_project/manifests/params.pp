93f052372f13b52d448e45254e7bb5d4df0dacaf
Monty Taylor <mordred@inaugust.com>
2015-06-23 13:14:34 -0400

    Trim user specific package a bit
    I'm pretty sure I'm the reason byobu is on any of our servers, and I'm
    also pretty sure I haven't used it in at least 2 years.
    
    Also - starting with 15.04, Ubuntu has a vanilla emacs-nox like redhat
    does. Let's start using it so that we don't have to do patches like this
    in the future.
    
    Change-Id: I37123dd42bdbd6456bb8a8637ab7164af7132642

--- modules/openstack_project/manifests/params.pp
+++ modules/openstack_project/manifests/params.pp
@@ -6,18 +6,18 @@ class openstack_project::params {
   case $::osfamily {
     'RedHat': {
       $packages = ['puppet', 'wget']
-      $user_packages = ['byobu', 'emacs-nox', 'vim-minimal']
+      $user_packages = ['emacs-nox', 'vim-minimal']
       $update_pkg_list_cmd = ''
       $login_defs = 'puppet:///modules/openstack_project/login.defs.redhat'
     }
     'Debian': {
       $packages = ['puppet', 'wget']
       case $::operatingsystemrelease {
-        '15.04': {
-          $user_packages = ['byobu', 'emacs24-nox', 'vim-nox']
+        /^(12|14)\.(04|10)$/: {
+          $user_packages = ['emacs23-nox', 'vim-nox']
         }
         default: {
-          $user_packages = ['byobu', 'emacs23-nox', 'vim-nox']
+          $user_packages = ['emacs-nox', 'vim-nox']
         }
       }
       $update_pkg_list_cmd = 'apt-get update >/dev/null 2>&1;'
