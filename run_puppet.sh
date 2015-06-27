10e066c2214af40591e6e278c7fe863101e9dae2
Clint Byrum <clint@fewbar.com>
2015-06-27 03:16:06 +0000

    Add neutron to single node baremetal0
    Change-Id: I353bfa703a66d8988c7bd63a7be999c4475761ca

--- run_puppet.sh
+++ run_puppet.sh
@@ -11,6 +11,6 @@ MODULE_PATH=${MODULE_DIR}:/etc/puppet/modules
 MANIFEST_LOG=/var/log/manifest.log
 
 cd $BASE_DIR
-/usr/bin/git pull -q && \
+#/usr/bin/git pull -q && \
     /bin/bash install_modules.sh && \
     /usr/bin/puppet apply -l $MANIFEST_LOG --modulepath=$MODULE_PATH manifests/site.pp
