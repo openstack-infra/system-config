8ae922f1ae6e588c802b12929a79594b0f65e0b6
tengqm <tengqim@cn.ibm.com>
2015-06-30 05:16:13 -0400

    Add #senlin to the list of eavesdrop channels
    This will really help contributors from different time zones.
    
    Change-Id: I47d2631edd9465802a95cbc4308bea044d47c211

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -98,6 +98,7 @@ class openstack_project::eavesdrop (
         '#openstack-trove',
         '#openstack-zaqar',
         '#puppet-openstack',
+        '#senlin',
         '#storyboard',
         '#tripleo',
     ],
