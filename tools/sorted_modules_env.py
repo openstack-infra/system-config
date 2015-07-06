5927e3c014e2bc523c6c9c8e65a39234bae029f4
Paul Belanger <pabelanger@redhat.com>
2015-07-06 11:57:19 -0400

    Remove line break (\n) from modulesenv diff
    Strip the trailing line break to reduce whitenoise when outputing
    diff.
    
    Change-Id: I226fc562c3c4df958269e39a446f448c90d673f9
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/sorted_modules_env.py
+++ tools/sorted_modules_env.py
@@ -9,16 +9,16 @@ def assert_sorted(lines):
         return True
     else:
         print "Modules not sorted:"
-        for line in difflib.context_diff(lines, sorted(lines)):
-            print line
+        diff = difflib.context_diff(lines, sorted(lines))
+        print '\n'.join(list(diff))
         sys.exit(1)
 
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-integration = [i for i in lines if i.startswith('INTEGRATION')]
-source = [i for i in lines if i.startswith('SOURCE')]
+integration = [i.rstrip() for i in lines if i.startswith('INTEGRATION')]
+source = [i.rstrip() for i in lines if i.startswith('SOURCE')]
 
 assert_sorted(integration)
 assert_sorted(source)
