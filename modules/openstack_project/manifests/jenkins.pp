e8cfd3e61ccecf10a672c17cfd2057c10bc2669b
Khai Do <zaro0508@gmail.com>
2015-07-07 15:40:33 -0700

    Use third party ci manifest to setup jenkins job builder.
    Change Ibc7a46a8cb36 sets up a jjb manifest for third party.  Use
    that manifest.
    
    Change-Id: I4e27ec9e47f55799443b5ebad19ebfbaebe061c4
    depends-on: Ibc7a46a8cb364b9e64b1fddb6b4f9df5a9e1fdd5

--- modules/openstack_project/manifests/jenkins.pp
+++ modules/openstack_project/manifests/jenkins.pp
@@ -55,13 +55,13 @@ class openstack_project::jenkins (
       url  => $project_config_repo,
     }
 
-    class { '::jenkins::job_builder':
-      jenkins_jobs_update_timeout => 1200,
-      url                         => "https://${vhost_name}/",
-      username                    => $jenkins_jobs_username,
-      password                    => $jenkins_jobs_password,
-      git_revision                => $jenkins_git_revision,
-      git_url                     => $jenkins_git_url,
+    class { '::openstackci::jenkins_job_builder':
+      jenkins_url                 => "https://${vhost_name}/",
+      jenkins_username            => $jenkins_jobs_username,
+      jenkins_password            => $jenkins_jobs_password,
+      jjb_git_revision            => $jenkins_git_url,
+      jjb_git_url                 => $jenkins_git_revision,
+      jjb_update_timeout          => 1200,
       config_dir                  =>
         $::project_config::jenkins_job_builder_config_dir,
       require                     => $::project_config::config_dir,
