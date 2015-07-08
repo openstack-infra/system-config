f0ffcab9d76b2da97740be1488bc112d3d8935af
K Jonathan Harker <k.jonathan.harker@hp.com>
2015-07-08 13:58:36 -0700

    Use new api for jenkins masters credentials
    In Ie9381740e3644feaee1f1b201499e3a253677f39 we created a new parameter
    for the jenkins masters credentials as part of a move to put credentials
    in their own config file.
    
    Depends-On: Ie9381740e3644feaee1f1b201499e3a253677f39
    Change-Id: I774b03ad49b232a11c0046dda5e0364bb1dcaede

--- modules/openstack_project/manifests/nodepool_prod.pp
+++ modules/openstack_project/manifests/nodepool_prod.pp
@@ -9,9 +9,7 @@ class openstack_project::nodepool_prod(
   $vhost_name = 'nodepool.openstack.org',
   $sysadmins = [],
   $statsd_host = '',
-  $jenkins_api_user ='',
-  $jenkins_api_key ='',
-  $jenkins_credentials_id ='',
+  $jenkins_masters = [],
   $rackspace_username ='',
   $rackspace_password ='',
   $rackspace_project ='',
@@ -47,6 +45,7 @@ class openstack_project::nodepool_prod(
     elements_dir              => $::project_config::nodepool_elements_dir,
     require                   => $::project_config::config_dir,
     logging_conf_template     => $nodepool_logging_template,
+    jenkins_masters           => $jenkins_masters,
   }
 
   file { '/etc/nodepool/nodepool.yaml':
