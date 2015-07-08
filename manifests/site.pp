3e3d87c0aafb267338d33fbbf03864def069825c
Khai Do <zaro0508@gmail.com>
2015-07-08 16:40:25 -0700

    use updated jenkins_master.pp manifest
    Jenkins job builder setup was moved into openstackci::jenkins_master
    and some of the params names were changes so need to update usage of
    the params accordingly.
    
    Change-Id: Id10d63745fff43f3188e630df38c8c9ba97c3e17
    depends-on: I50d4d2dd4209250e14ceeb7ff8ddf5b6ba3e2ad4

--- manifests/site.pp
+++ manifests/site.pp
@@ -132,7 +132,7 @@ node 'jenkins.openstack.org' {
   }
   class { 'openstack_project::jenkins':
     project_config_repo     => 'https://git.openstack.org/openstack-infra/project-config',
-    jenkins_jobs_password   => hiera('jenkins_jobs_password', 'XXX'),
+    jenkins_password        => hiera('jenkins_jobs_password', 'XXX'),
     jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents', 'XXX'),
     ssl_cert_file_contents  => hiera('jenkins_ssl_cert_file_contents', 'XXX'),
     ssl_key_file_contents   => hiera('jenkins_ssl_key_file_contents', 'XXX'),
