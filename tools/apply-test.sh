8f7c6ab79f8a079dc5c9f193bf558a83c870d09b
Spencer Krum <nibz@spencerkrum.com>
2015-07-28 18:05:08 -0700

    Make the apply-test public-hiera aware
    Change-Id: I761bf49ed6279a492cabeca878ec5e7c0fac3d0e

--- tools/apply-test.sh
+++ tools/apply-test.sh
@@ -97,8 +97,25 @@ HOST=`echo $HOSTNAME |awk -F. '{ print $1 }'`
 echo "127.0.1.1 $HOST.openstack.org $HOST" >> /tmp/hosts
 sudo mv /tmp/hosts /etc/hosts
 
-sudo mkdir -p /var/run/puppet
+# manage hiera
+mkdir -p /opt/system-config
+cp -r hiera /opt/
+cat > /etc/hiera.yaml <<EOF
+---
+:hierarchy:
+  # Public Hieradata Only
+  - "hiera/%{::environment}/fqdn/%{::fqdn}"
+  - "hiera/%{::environment}/group/%{group}" # no :: because group is set at nodescope
+  - "hiera/%{::environment}/common"
+:backends:
+  - yaml
+:yaml:
+  :datadir: "/opt/"
+EOF
 sudo cp /etc/hiera.yaml /etc/puppet/hiera.yaml
+
+
+sudo mkdir -p /var/run/puppet
 sudo -E bash -x ./install_modules.sh
 echo "Running apply test on these hosts:"
 find applytest -name 'puppetapplytest*.final' -print0
