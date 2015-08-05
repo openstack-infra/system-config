fbda8b12c0db804e555c10c1d2cb3b902db30372
Colleen Murphy <colleen@gazlene.net>
2015-08-05 11:04:13 -0700

    Add bifrost manifest
    http://paste.openstack.org/show/406921/
    
    This patch adds an openstack_project class to install bifrost from
    source and add static config files, plus a script to run ansible to
    complete the ironic deployment. Still figuring out exact data values
    to plug in.
    
    Change-Id: I947add7e8e8aa88fe6e881d77fd3278910b3b903
    Co-Authored-By: Clint Byrum <clint@fewbar.com>

---
+++ modules/openstack_project/manifests/infracloud/bifrost.pp
@@ -0,0 +1,49 @@
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+class openstack_project::infracloud::bifrost (
+  $ironic_db_password,
+  $mysql_password,
+) {
+
+  include ::ansible
+
+  class { '::mysql::server':
+    root_password => $mysql_password,
+  }
+
+  vcsrepo { '/opt/stack/bifrost':
+    ensure   => 'latest',
+    provider => 'git',
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack/bifrost',
+  }
+
+  file { '/etc/bifrost':
+    ensure => directory,
+  }
+
+  file { '/etc/bifrost/bifrost_global_vars':
+    ensure  => present,
+    source  => 'puppet://modules/openstack_project/bifrost/bifrost_global_vars',
+    require => File['/etc/bifrost'],
+  }
+
+  file { '/opt/stack/baremetal.json':
+    ensure  => file,
+    source  => 'puppet:///modules/openstack_project/bifrost/baremetal.json', # contains dummy data at the moment
+    require => Class['::ironic::bifrost'],
+  }
+
+}
