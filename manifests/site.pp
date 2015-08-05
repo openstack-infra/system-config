630ef5c6fbbc605eb664e8ffed730f761023b891
Colleen Murphy <colleen@gazlene.net>
2015-08-05 11:04:13 -0700

    First stab at making things bifrosty
    http://paste.openstack.org/show/406921/
    
    This patch adds an openstack_project class to use the new
    ironic::bifrost class to install bifrost, plus a script to run ansible
    to complete the ironic deployment. Still figuring out exact data values
    to plug in.
    
    Change-Id: I947add7e8e8aa88fe6e881d77fd3278910b3b903

--- manifests/site.pp
+++ manifests/site.pp
@@ -919,4 +919,11 @@ node 'apps.openstack.org' {
   }
 }
 
+# Node-OS: trusty
+node /bifrost/ {
+  class { '::openstack_project::infracloud::bifrost':
+    ironic_db_password   => hiera('ironic_db_password', 'XXX'),
+    mysql_password       => hiera('bifrost_mysql_password', 'XXX'),
+  }
+}
 # vim:sw=2:ts=2:expandtab:textwidth=79
