8ea81f87d4f051910796b5e9f73e78ca14475f18
Colleen Murphy <colleen@gazlene.net>
2015-08-17 19:52:50 -0700

    (WIP) Add compute class for Infra Cloud
    Configures compute node using the ml2 linuxbridge agent.
    
    todo:
     - openstack_project::server / iptables rules
    
    Change-Id: I9bce2a37d427558ba7a59fc6bc9edfd62edeac45

---
+++ modules/openstack_project/manifests/infracloud/compute.pp
@@ -0,0 +1,62 @@
+class openstack_project::infracloud::compute(
+  $nova_rabbit_password,
+  $neutron_rabbit_password,
+  $neutron_admin_password,
+  $controller_public_address,
+  $controller_management_address,
+) {
+
+  ### Networking ###
+
+  include ::openstack_project::infracloud::veth
+
+  ### Repos ###
+  include ::apt
+
+  class { 'openstack_extras::repo::debian::ubuntu':
+    release         => 'kilo',
+    package_require => true,
+  }
+
+  ### Nova ###
+
+  # nova.conf
+  class { '::nova':
+    rabbit_userid       => 'nova',
+    rabbit_password     => $nova_rabbit_password,
+    rabbit_host         => $controller_management_address,
+    glance_api_servers  => "https://${controller_address}:9292",
+  }
+
+  # nova-compute service
+  class { '::nova::compute':
+    enabled => true,
+  }
+
+  # nova.conf neutron credentials
+  class { '::nova::network::neutron':
+    neutron_url            => "https://${controller_address}:9696",
+    neutron_admin_auth_url => "https://${controller_address}:35357/v2.0",
+    neutron_admin_password => $neutron_admin_password,
+  }
+
+  ### Neutron ###
+
+  # neutron.conf
+  class { '::neutron':
+    rabbit_user     => 'neutron',
+    rabbit_password => $neutron_rabbit_password,
+    rabbit_host     => $controller_management_address,
+  }
+
+  # ML2
+  class { '::neutron::agents::ml2::linuxbridge':
+    physical_interface_mappings => ['provider:veth2'],
+  }
+  # Fix for https://bugs.launchpad.net/ubuntu/+source/neutron/+bug/1453188
+  file { '/usr/bin/neutron-plugin-linuxbridge-agent':
+    ensure => link,
+    target => '/usr/bin/neutron-linuxbridge-agent',
+    before => Package['neutron-plugin-linuxbridge-agent'],
+  }
+}
