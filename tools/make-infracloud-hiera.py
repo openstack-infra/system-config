d02e172b815a6d11b37098392babbb9ac5de2fae
Clint Byrum <clint@fewbar.com>
2015-08-18 09:59:37 -0700

    Add tools to make infra-cloud inventory templates
    The infra-cloud Bifrost inventory needs to have secrets moved into erb,
    and may be populated with random names instead of assigned hostnames in
    the infracloud scheme.
    
    Change-Id: I2f0c3f856d3bd5885755670845653d2e001f9081

---
+++ tools/make-infracloud-hiera.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import os
+
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('inventory', help="Path to inventory json file")
+parser.add_argument('hiera_path', help="Output hiera yaml here")
+args = parser.parse_args()
+
+inventory = json.loads(open(args.inventory).read())
+new_inventory = dict(inventory)
+hiera = {'ipmi_passwords': {}}
+if os.path.exists(args.hiera_path):
+    with open(args.hiera_path, 'r') as hiera_file:
+        hiera = yaml.safe_load(hiera_file.read())
+
+for name, info in inventory.items():
+    password = info['driver_info']['power']['ipmi_password']
+    if name not in hiera['ipmi_passwords']:
+        hiera['ipmi_passwords'][name] = password
+    info = dict(info)
+    template = '<%= @ipmi_passwords[{}] %>'.format(name)
+    info['driver_info']['power']['ipmi_password'] = template
+    new_inventory[name] = info
+
+print(json.dumps(new_inventory, indent=2))
+with open(args.hiera_path, 'w') as hiera_file:
+    yaml.safe_dump(hiera, hiera_file)
