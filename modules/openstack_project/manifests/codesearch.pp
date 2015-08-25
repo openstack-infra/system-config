4041df2232ec987319dfbdc0b740aaf37630f81e
Emma Barber <me@taron.io>
2015-08-24 23:23:06 -0700

    Add codesearch.o.o node and module
    puppet-hound is in working order. This adds puppet-hound to
    INTEGRATION_MODULES, creates a new codesearch.o.o node,
    and installs hound on it.
    
    Depends-On: Ie4b6509947f58407c4cc6f5a2c7c2bc84c619ce9
    Change-Id: I238b05e8b25229d7dcf840441cd9d3af09f3e249

---
+++ modules/openstack_project/manifests/codesearch.pp
@@ -0,0 +1,32 @@
+# Class to configure hound on a node.
+class openstack_project::codesearch (
+  $project_config_repo,
+) {
+
+  class { 'project_config':
+    url => $project_config_repo,
+  }
+
+  class { 'hound':
+    manage_config => false,
+    require       => Exec['create-hound-config'],
+  }
+
+  include jeepyb
+  include logrotate
+  include pip
+
+  exec { 'create-hound-config':
+    command     => 'create-hound-config',
+    path        => '/bin:/usr/bin:/usr/local/bin',
+    environment => "PROJECTS_YAML=${::project_config::jeepyb_project_file}",
+    user        => 'hound',
+    cwd         => '/home/hound',
+    require     => [
+      User['hound'],
+      $::project_config::config_dir,
+    ],
+    notify      => Class['hound']
+  }
+
+}
