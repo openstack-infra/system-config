d8924acd1323405b5dba4efb146e9bff209907e5
Emma Barber <me@taron.io>
2015-08-24 23:23:06 -0700

    Add codesearch module and node
    Change-Id: I238b05e8b25229d7dcf840441cd9d3af09f3e249

---
+++ modules/openstack_project/manifests/codesearch.pp
@@ -0,0 +1,34 @@
+# Class to configure hound on a node.
+class openstack_project::codesearch (
+  $project_config_repo = 'https://git.openstack.org/openstack-infra/project-config',
+) {
+
+  class { 'project_config':
+    url => $project_config_repo,
+  }
+
+  class { 'hound':
+    manage_config => false,
+  }
+
+  include jeepyb
+  include logrotate
+  include pip
+
+  $local_git_dir = '/var/lib/git'
+  $ssh_project_key = ''
+
+  exec { 'create-hound-config':
+    command     => 'create-hound-config',
+    path        => '/bin:/usr/bin:/usr/local/bin',
+    environment => "PROJECTS_YAML=${::project_config::jeepyb_project_file}",
+    user        => 'hound',
+    cwd         => '/home/hound',
+    require     => [
+      User['hound'],
+      $::project_config::config_dir,
+    ],
+    refreshonly => true,
+  }
+
+}
