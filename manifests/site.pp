fd200382b4e20319e3a831b2cefeb7539ed2a92b
Thierry Carrez <thierry@openstack.org>
2015-09-17 16:43:37 +0200

    Enable the #success feature in statusbot
    This adds the configuration bits to enable the #success command
    in statusbot.
    
    Depends-On: I66b577732d1fec271a42f9229a8b5af2e52a58f4
    Change-Id: Id26fb1a9dc27874040d2f5dd05bf20140d07512b

--- manifests/site.pp
+++ manifests/site.pp
@@ -321,7 +321,11 @@ node 'eavesdrop.openstack.org' {
     statusbot_wiki_user     => hiera('statusbot_wiki_username', 'username'),
     statusbot_wiki_password => hiera('statusbot_wiki_password', 'XXX'),
     statusbot_wiki_url      => 'https://wiki.openstack.org/w/api.php',
+    # https://wiki.openstack.org/wiki/Infrastructure_Status
     statusbot_wiki_pageid   => '1781',
+    # https://wiki.openstack.org/wiki/Successes
+    statusbot_wiki_successpageid => '7717',
+    statusbot_irclogs_url   => 'http://eavesdrop.openstack.org/irclogs/%(chan)s/%(chan)s.%(date)s.log.html',
     accessbot_nick          => hiera('accessbot_nick', 'username'),
     accessbot_password      => hiera('accessbot_nick_password', 'XXX'),
   }
