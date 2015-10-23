b47e6dfdd4b6b7f630ff090883aef9f460ce1932
Michael Krotscheck <krotscheck@gmail.com>
2015-10-22 19:42:38 -0700

    Renamed pypi.<>.openstack.org to mirror.<>.openstack.org
    This patch repurposes the pypi manifest, and rewrites it as a
    more general 'mirror' manifest. Apache setup has been pulled
    from  pypi_mirror into mirror, and mod_alias has been added
    to make our pypi mirror available under the <host>/pypi (rather
    than the <host>/root. Additional aliases were necessary in
    order to ensure that the subdirectory tree still resolves.
    
    Additionally, a new docroot was created for the mirror's vhost,
    in anticipation of symlinks for various other static mirrors,
    and the openstack::server module was moved into the node's
    manifest in accordance with the openstack::server spec.
    
    WARNING: This patch requires a manual dns change. Additional
    A records should be added for mirror.<region>.openstack.org
    
    Change-Id: I9a2bf596cf47bffad5d6a5fd0da3c571fa266013

---
+++ modules/openstack_project/manifests/mirror.pp
@@ -0,0 +1,40 @@
+# == Class: openstack_project::mirror
+#
+class openstack_project::mirror (
+  $vhost_name = $::fqdn,
+) {
+
+  include ::httpd
+
+  ::httpd::mod { ['version', 'alias']: }
+
+  $base = '/srv/static'
+  $docroot = "${base}/docroot"
+  $pypi_docroot = "${base}/mirror"
+
+  if ! defined(File[$base]) {
+    file { $base:
+      ensure => directory,
+    }
+  }
+
+  file { $docroot:
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    require => File[$base],
+  }
+
+  ::httpd::vhost { $vhost_name:
+    port     => 80,
+    priority => '50',
+    docroot  => $docroot,
+    template => 'openstack_project/mirror.vhost.erb',
+    require  => File[$docroot],
+  }
+
+  class { 'openstack_project::pypi_mirror':
+    data_directory => $pypi_docroot,
+    require        => File[$base],
+  }
+}
