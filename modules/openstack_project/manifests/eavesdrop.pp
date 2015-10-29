c9666424a17ecf2e6870c09fa32024f534c292f9
Dina Belova <dbelova@mirantis.com>
2015-10-29 23:10:49 +0900

    Add #openstack-performance channel to the channels logging
    Change-Id: I459e03861bbd61b110a32c652303e10d99375c12

--- modules/openstack_project/manifests/eavesdrop.pp
+++ modules/openstack_project/manifests/eavesdrop.pp
@@ -84,6 +84,7 @@ class openstack_project::eavesdrop (
         '#openstack-neutron-release',
         '#openstack-nova',
         '#openstack-operators',
+        '#openstack-performance',
         '#openstack-opw',
         '#openstack-oslo',
         '#openstack-qa',
