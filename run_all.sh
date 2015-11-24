927db9a0a72529a0a0f048a7acf6468da547b99f
Monty Taylor <mordred@inaugust.com>
2015-11-24 16:23:06 -0500

    Update system-config and puppet modules everywhere
    If we're going to run puppet apply on all of our nodes, they need
    the puppet modules installed on them first.
    
    Change-Id: Ie14f2cb50bf3c786bf94fb2d0c7609e46507cb87

--- run_all.sh
+++ run_all.sh
@@ -22,19 +22,20 @@ export ANSIBLE_LOG_PATH=/var/log/puppet_run_all.log
 SYSTEM_CONFIG=/opt/system-config/production
 ANSIBLE_PLAYBOOKS=$SYSTEM_CONFIG/playbooks
 
-cd $SYSTEM_CONFIG
-git fetch -a && git reset -q --hard @{u}
-./install_modules.sh
-ansible-galaxy install --force -r roles.yaml
-
-# One must touch manifests/site.pp to trick puppet into re-loading modules
-# some times
-touch manifests/site.pp
 
 # It's possible for connectivity to a server or manifest application to break
 # for indeterminate periods of time, so the playbooks should be run without
 # errexit
 set +e
+
+# Update system-config repo on all of the nodes and here on the master. Also,
+# update the ansible roles here on the master
+ansible-playbook -f 20 ${ANSIBLE_PLAYBOOKS}/update_puppet.yaml
+
+cd $SYSTEM_CONFIG
+# One must touch manifests/site.pp to trick puppet into re-loading modules
+# some times
+touch manifests/site.pp
 
 # First run the git/gerrit sequence, since it's important that they all work
 # together
