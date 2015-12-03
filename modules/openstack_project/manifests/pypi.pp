5cccc20619d44ed613750d1d8b9fef7e7333b13e
Michael Krotscheck <krotscheck@gmail.com>
2015-12-03 14:04:50 -0800

    Move Bandersnatch Directory
    In order to 'make room' for other mirrors, we would like to move
    the /srv/static/mirror directory, containing the bandersnatch pypi
    mirror assets, to /srv/static/pypi. This will make room for other
    directories such as /src/static/wheel, /src/static/rubygems,
    and more.
    
    To circumvent a possible bandersnatch rebuild, the puppet agent
    should be temporarily disabled on the mirrors when this patch lands.
    Then a core will have to manually disable bandersnatch, disable apache
    (to prevent mirror access), move the data directories, and follow
    up by applying the puppet module to restart everything.
    
    Change-Id: I55286df53d8dd84ea2377035b830cd92d378ad39

--- modules/openstack_project/manifests/pypi.pp
+++ modules/openstack_project/manifests/pypi.pp
@@ -3,7 +3,14 @@
 class openstack_project::pypi (
   $vhost_name = $::fqdn,
   $sysadmins = [],
+  $root_data_directory = '/srv/static'
 ) {
+
+  if ! defined(File[$root_data_directory]) {
+    file { $root_data_directory:
+      ensure => directory,
+    }
+  }
 
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [22, 80],
@@ -12,5 +19,6 @@ class openstack_project::pypi (
 
   class { 'openstack_project::pypi_mirror':
     vhost_name => $vhost_name,
+    data_directory => "${root_data_directory}/pypi"
   }
 }
