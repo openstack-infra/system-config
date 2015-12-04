cebebdebe8e8a6c6e5d27540aa51133f9a75052c
Michael Krotscheck <krotscheck@gmail.com>
2015-12-04 06:05:31 -0800

    Removed manifests for legacy mirrors
    This patch removes the legacy pypi.<region>.openstack.org manifests.
    Please only merge this after the servers, and their respective DNS
    entries, have been cleaned up.
    
    Change-Id: I167381e4e2963fc0ab81fc99a24078c9db63234b

--- manifests/site.pp
+++ manifests/site.pp
@@ -525,12 +525,18 @@ node /^git\d+\.openstack\.org$/ {
   }
 }
 
-# Machines in each region to run PyPI mirrors.
-# Node-OS: precise
-node /^pypi\..*\.openstack\.org$/ {
-  $group = "pypi"
-  class { 'openstack_project::pypi':
-    sysadmins               => hiera('sysadmins', []),
+# Machines in each region to run unified package mirrors.
+# Node-OS: trusty
+node /^mirror\..*\.openstack\.org$/ {
+  $group = "mirror"
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [22, 80],
+    sysadmins                 => hiera('sysadmins', []),
+  }
+
+  class { 'openstack_project::mirror':
+    vhost_name => $::fqdn,
   }
 }
 
