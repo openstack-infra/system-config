a6a8629bfc9d4a0aba53b49ac5aefe1d61ac69ba
Michael Krotscheck <krotscheck@gmail.com>
2015-12-03 21:55:22 -0800

    Separate concerns in pypi_mirror.pp
    This patch separates the creation of the bandersnatch pypi mirror
    from the creation of the apache vhost, and parameterizes the data
    directory in which bandersnatch places its packages.
    
    This is done so that we may reuse the pypi_mirror.pp module when
    building our new unified mirrors, which host pypi assets in a
    different directory and under a different hostname and URI.
    
    This patch should not trigger any changes on the existing mirrors.
    
    Change-Id: I55286df53d8dd84ea2377035b830cd92d378ad39

--- modules/openstack_project/manifests/pypi.pp
+++ modules/openstack_project/manifests/pypi.pp
@@ -10,7 +10,35 @@ class openstack_project::pypi (
     sysadmins                 => $sysadmins,
   }
 
+  $mirror_root = '/srv/static'
+  $pypi_root = "${mirror_root}/mirror"
+
+  if ! defined(File[$mirror_root]) {
+    file { $mirror_root:
+      ensure => directory,
+    }
+  }
+
   class { 'openstack_project::pypi_mirror':
-    vhost_name => $vhost_name,
+    data_directory => "${pypi_root}",
+    require        => File[$mirror_root]
+  }
+
+  include ::httpd
+
+  ::httpd::vhost { $vhost_name:
+    port     => 80,
+    priority => '50',
+    docroot  => "${pypi_root}/web",
+    require  => Class['Openstack_project::Pypi_mirror'],
+  }
+
+  file { "${pypi_root}/web/robots.txt":
+    ensure   => present,
+    owner    => 'root',
+    group    => 'root',
+    mode     => '0444',
+    source   => 'puppet:///modules/openstack_project/disallow_robots.txt',
+    require  => Class['Openstack_project::Pypi_mirror'],
   }
 }
