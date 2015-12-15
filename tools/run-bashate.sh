80da9bc6fb02a9e3ea464041ddc3bb7ef3348fe0
Khai Do <zaro0508@gmail.com>
2015-12-15 10:51:48 -0800

    Add automated database config retrieval
    Improved script to automate the retrieval of DB config so user
    running the script doesn't have to hunt down the required info
    to run the script.  All that's needed is the Gerrit site.
    
    Keeping long lines because bashate hates me. Whitelisted script for
    bashate.
    
    Change-Id: Icebb9bcb8620a244cc1b458974b06ea47340fdea

--- tools/run-bashate.sh
+++ tools/run-bashate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ROOT=$(readlink -fn $(dirname $0)/.. )
-find $ROOT -not -wholename \*.tox/\* -and \( -name \*.sh -or -name \*rc -or -name functions\* \) -print0 | xargs -0 bashate -v
+find $ROOT -not -wholename \*.tox/\* -and \( -name \*.sh -or -name \*rc -or -name functions\* \) -and -not -name "gerrit-2.8.4-cleanup.sh" -print0 | \
+xargs -0 bashate -v
