c11e48325ee830da26961f88addcb7f948c00926
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-12-21 15:00:32 +0100

    Add Infra Cloud compute node definition
    Add definition for compute nodes in the hpuseast region.
    
    Change-Id: I75b0abcd0401588033f681e77bcc07a442422fa4

--- manifests/site.pp
+++ manifests/site.pp
@@ -1188,4 +1188,20 @@ node /^compute\d{3}\.hpuswest\.ic\.openstack\.org$/ {
   }
 }
 
+node /^compute\d{3}\.hpuseast\.ic\.openstack\.org$/ {
+  $group = 'infracloud'
+  class { '::openstack_project::server':
+    sysadmins                 => hiera('sysadmins', []),
+    enable_unbound            => false,
+  }
+  class { '::openstack_project::infracloud::compute':
+    nova_rabbit_password             => hiera('nova_rabbit_password'),
+    neutron_rabbit_password          => hiera('neutron_rabbit_password'),
+    neutron_admin_password           => hiera('neutron_admin_password'),
+    ssl_cert_file_contents           => hiera('infracloud_hpuseast_ssl_cert_file_contents'),
+    br_name                          => 'br-vlan1598',
+    controller_public_address        => 'controller00.hpuseast.ic.openstack.org',
+  }
+}
+
 # vim:sw=2:ts=2:expandtab:textwidth=79
