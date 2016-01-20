23d08cb934df25cb765b7691a6088aaa686faf2d
James E. Blair <jeblair@linux.vnet.ibm.com>
2016-01-20 15:11:24 -0800

    Add mirror_update.openstack.org
    This is a machine that will drive centralized mirror updates for
    mirrors that are in AFS.
    
    Change-Id: I33eed90c21a2e57a26349be83ee69e968b18d2f8
    Depends-On: I56bb3ce2237be1179724f7a2bdcf9d5b04bdecd2
    Depends-On: I704c8bf395d83adea0f9a0db6fd3d3814ad7660a

---
+++ modules/openstack_project/files/bandersnatch-mirror-update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2016 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+echo "Obtaining bandersnatch tokens and running bandersnatch."
+k5start -t -f /etc/bandersnatch.keytab service/bandersnatch -- timeout -k 2m 30m run-bandersnatch
+
+RET=$?
+
+if [ $RET ]; then
+    echo "Bandersnatch completed successfully, running vos release."
+    k5start -t -f /etc/afsadmin.keytab service/afsafdmin -- vos release mirror.pypi
+fi
+
+echo "Done."
