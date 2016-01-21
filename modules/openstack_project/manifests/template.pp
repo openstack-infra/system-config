7d7ec9be6ab8e510803969a574b241e3cb9575ea
Monty Taylor <mordred@inaugust.com>
2016-01-21 13:28:49 -0500

    Move pypi config into public hiera
    With automatic parameter lookup:
    
      https://docs.puppetlabs.com/hiera/3.0/puppet.html#automatic-parameter-lookup
    
    we do not have to plumb things through from top to bottom. We can
    actually simply set parameters for inner classes directly in hiera.
    
    Change-Id: Id250622d98b67d255c8f7e4eda6e48f87e195b84

--- modules/openstack_project/manifests/template.pp
+++ modules/openstack_project/manifests/template.pp
@@ -18,25 +18,6 @@ class openstack_project::template (
   $puppetmaster_server       = 'puppetmaster.openstack.org',
   $manage_exim               = false,
   $sysadmins                 = [],
-  $pypi_index_url            = 'https://pypi.python.org/simple',
-  $pypi_trusted_hosts        = [
-    'pypi.bhs1.openstack.org',
-    'pypi.dfw.openstack.org',
-    'pypi.gra1.openstack.org',
-    'pypi.iad.openstack.org',
-    'pypi.nyj01.openstack.org',
-    'pypi.ord.openstack.org',
-    'pypi.region-b.geo-1.openstack.org',
-    'pypi.regionone.openstack.org',
-    'mirror.bhs1.ovh.openstack.org',
-    'mirror.dfw.rackspace.openstack.org',
-    'mirror.gra1.ovh.openstack.org',
-    'mirror.iad.rax.openstack.org',
-    'mirror.nyj01.internap.openstack.org',
-    'mirror.ord.rax.openstack.org',
-    'mirror.region-b.geo-1.hpcloud.openstack.org',
-    'mirror.regionone.bluebox-sjc1.openstack.org',
-  ],
   $purge_apt_sources         = false,
 ) {
 
@@ -238,8 +219,6 @@ class openstack_project::template (
 
   $desired_virtualenv = '13.1.0'
   class { '::pip':
-    index_url       => $pypi_index_url,
-    trusted_hosts   => $pypi_trusted_hosts,
     manage_pip_conf => true,
   }
 
