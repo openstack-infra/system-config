991a695aaf8daa8c4b995995859cd98ba5a63657
Michael Krotscheck <krotscheck@gmail.com>
2016-01-21 10:53:20 -0800

    Added pypi hosting for AFS mirror slave
    This patch adds a vhost that exposes /afs/openstack.org/mirror/pypi/web
    to the internet under <hostname>/pypi. It also adds a do-not-follow
    robots.txt file.
    
    Change-Id: I3d7629d80fe1e8762344852fab4a942bc476b706

--- modules/openstack_project/manifests/mirror.pp
+++ modules/openstack_project/manifests/mirror.pp
@@ -5,5 +5,48 @@ class openstack_project::mirror (
 ) {
 
   $mirror_root = '/afs/openstack.org/mirror'
+  $pypi_root = "${mirror_root}/pypi"
+  $www_root = '/var/www/mirror'
 
+  #####################################################
+  # Build Apache Webroot
+  file { "${www_root}":
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+  }
+
+  # Create the symlink to pypi.
+  file { "${www_root}/pypi":
+    ensure  => link,
+    target  => "${pypi_root}/web",
+    owner   => root,
+    group   => root,
+    require => [
+      File["${www_root}"],
+    ]
+  }
+
+  file { "${www_root}/robots.txt":
+    ensure   => present,
+    owner    => 'root',
+    group    => 'root',
+    mode     => '0444',
+    source   => 'puppet:///modules/openstack_project/disallow_robots.txt',
+    require  => File["${www_root}"],
+  }
+
+  #####################################################
+  # Build VHost
+  include ::httpd
+
+  ::httpd::vhost { $vhost_name:
+    port     => 80,
+    priority => '50',
+    docroot  => "${www_root}",
+    template => 'openstack_project/mirror.vhost.erb',
+    require  => [
+      File["${www_root}"],
+    ]
+  }
 }
