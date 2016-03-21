ce9097d8d1927f95fd4deaf4e4dd759d928f5fd8
Paul Belanger <pabelanger@redhat.com>
2016-03-21 18:59:19 -0400

    Store puppetlabs gpg key local
    This will reduce pressure on gpg keyserver and stop puppet from
    failing when a keyserver is down.
    
    Change-Id: I1dbb1331941f1fef7e59615f2b71d282fe36b3f9
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- modules/openstack_project/manifests/template.pp
+++ modules/openstack_project/manifests/template.pp
@@ -328,8 +328,8 @@ class openstack_project::template (
         location => 'http://apt.puppetlabs.com',
         repos    => 'main',
         key      => {
-          'id'     =>'47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-          'server' => 'pgp.mit.edu',
+          'id'      => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
+          'content' => file('openstack_project/puppetlabs.gpg'),
         },
       }
     }
