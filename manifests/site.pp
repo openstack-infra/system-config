de6a1127d9b1594c1af6608720cd0b28ba25fd9c
Paul Belanger <pabelanger@redhat.com>
2016-04-05 09:50:07 -0400

    More groups(-dev).o.o to trusty
    Both servers are running ubuntu-trusty, so lets test our puppet
    modules against that.
    
    Change-Id: I0924721ea80c7e4bb9fd14bc0a5710ef9c25370d
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -265,7 +265,7 @@ node 'graphite.openstack.org' {
   }
 }
 
-# Node-OS: precise
+# Node-OS: trusty
 node 'groups.openstack.org' {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [22, 80, 443],
@@ -282,7 +282,7 @@ node 'groups.openstack.org' {
   }
 }
 
-# Node-OS: precise
+# Node-OS: trusty
 node 'groups-dev.openstack.org' {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [22, 80, 443],
