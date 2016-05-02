e504a4051106a74d091d530075cc47597948e0e0
Ben Kero <ben.kero@gmail.com>
2016-05-02 10:45:14 -0700

    add trusty support for paste.openstack.org
    This commit adds support for ubuntu trusty for a new pastebin host named
    paste01.openstack.org.
    
    Change-Id: Ib7073b2e50cb8c545491d98c17251ca79a1575cc

--- manifests/site.pp
+++ manifests/site.pp
@@ -337,6 +337,19 @@ node 'paste.openstack.org' {
   class { 'openstack_project::paste':
     db_password         => hiera('paste_db_password'),
     db_host             => hiera('paste_db_host'),
+    vhost               => 'paste.openstack.org',
+  }
+}
+
+# Node-OS: trusty
+node /^paste\d+\.openstack\.org$/ {
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [80],
+    sysadmins                 => hiera('sysadmins', []),
+  }
+  class { 'openstack_project::paste':
+    db_password         => hiera('paste_db_password'),
+    db_host             => hiera('paste_db_host'),
   }
 }
 
