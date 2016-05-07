c85859ca2d740244424a1ac7e290da9495c3310a
Paul Belanger <pabelanger@redhat.com>
2016-05-07 18:37:27 -0400

    Add debian-ceph-jewel mirror to AFS
    As more teams will be switching to jewel from hammer, we also need to
    provide a debian mirror for it.
    
    Change-Id: I0f5097672c0afd092380d841c665c3fdfa25f280
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- modules/openstack_project/manifests/mirror.pp
+++ modules/openstack_project/manifests/mirror.pp
@@ -9,6 +9,7 @@ class openstack_project::mirror (
   $wheel_root = "${mirror_root}/wheel"
   $npm_root = "${mirror_root}/npm"
   $ceph_deb_hammer_root = "${mirror_root}/ceph-deb-hammer"
+  $ceph_deb_jewel_root = "${mirror_root}/ceph-deb-jewel"
 
   $www_base = '/var/www'
   $www_root = "${www_base}/mirror"
@@ -110,6 +111,17 @@ class openstack_project::mirror (
   file { "${www_root}/ceph-deb-hammer":
     ensure  => link,
     target  => "${ceph_deb_hammer_root}",
+    owner   => root,
+    group   => root,
+    require => [
+      File["${www_root}"],
+    ]
+  }
+
+  # Create the symlink to ceph-deb-jewel.
+  file { "${www_root}/ceph-deb-jewel":
+    ensure  => link,
+    target  => "${ceph_deb_jewel_root}",
     owner   => root,
     group   => root,
     require => [
