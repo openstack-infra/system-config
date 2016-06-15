5ea1a43d56f78822cbfc3808ad91c247efa8d93b
James E. Blair <jeblair@redhat.com>
2016-06-15 13:17:57 -0700

    Decomission jenkins.openstack.org
    Change-Id: Ibb6115ca391b76f9be26660c073447931dc283b0

--- manifests/site.pp
+++ manifests/site.pp
@@ -137,32 +137,6 @@ node 'stackalytics.openstack.org' {
 }
 
 # Node-OS: precise
-node 'jenkins.openstack.org' {
-  $group = "jenkins"
-  $zmq_event_receivers = ['logstash.openstack.org',
-                          'nodepool.openstack.org']
-  $zmq_iptables_rule = regsubst($zmq_event_receivers,
-                                '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 8888 -s \1 -j ACCEPT')
-  $http_iptables_rule = '-m state --state NEW -m tcp -p tcp --dport 80 -s nodepool.openstack.org -j ACCEPT'
-  $https_iptables_rule = '-m state --state NEW -m tcp -p tcp --dport 443 -s nodepool.openstack.org -j ACCEPT'
-  $iptables_rule = flatten([$zmq_iptables_rule, $http_iptables_rule, $https_iptables_rule])
-  class { 'openstack_project::server':
-    iptables_rules6     => $iptables_rule,
-    iptables_rules4     => $iptables_rule,
-    sysadmins           => hiera('sysadmins', []),
-    puppetmaster_server => 'puppetmaster.openstack.org',
-  }
-  class { 'openstack_project::jenkins':
-    project_config_repo     => 'https://git.openstack.org/openstack-infra/project-config',
-    jenkins_password        => hiera('jenkins_jobs_password'),
-    jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents'),
-    ssl_cert_file           => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-    ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
-    ssl_chain_file          => '',
-  }
-}
-
-# Node-OS: precise
 node /^jenkins\d+\.openstack\.org$/ {
   $group = "jenkins"
   $zmq_event_receivers = ['logstash.openstack.org',
@@ -840,7 +814,6 @@ node 'zuul.openstack.org' {
     statsd_host                    => 'graphite.openstack.org',
     gearman_workers                => [
       'nodepool.openstack.org',
-      'jenkins.openstack.org',
       'jenkins05.openstack.org',
       'jenkins06.openstack.org',
       'jenkins07.openstack.org',
@@ -942,7 +915,6 @@ node 'zuul-dev.openstack.org' {
     sysadmins            => hiera('sysadmins', []),
     statsd_host          => 'graphite.openstack.org',
     gearman_workers      => [
-      'jenkins.openstack.org',
       'jenkins05.openstack.org',
       'jenkins06.openstack.org',
       'jenkins07.openstack.org',
