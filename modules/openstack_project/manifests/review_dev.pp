169496396b13753b93de7279e906bc657f2359f4
Khai Do <zaro0508@gmail.com>
2016-06-16 23:14:34 -0700

    Refactor gerrit commentlinks parameter
    Refactor the commentlinks to allow review.o.o and review-dev.o.o to
    define seperate comment links.  We essentially want to point
    review.o.o story links to storyboard.o.o and review-dev.o.o links to
    storyboard-dev.o.o
    
    Change-Id: I70e5791a76ca97756613c393e598978ec13c8271

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -77,6 +77,48 @@ class openstack_project::review_dev (
     swift_username                      => $swift_username,
     swift_password                      => $swift_password,
     replication_force_update            => true,
+    commentlinks                        => [
+      {
+        name  => 'bugheader',
+        match => '([Cc]loses|[Pp]artial|[Rr]elated)-[Bb]ug:\\s*#?(\\d+)',
+        link  => 'https://launchpad.net/bugs/$2',
+      },
+      {
+        name  => 'bug',
+        match => '\\b[Bb]ug:? #?(\\d+)',
+        link  => 'https://launchpad.net/bugs/$1',
+      },
+      {
+        name  => 'story',
+        match => '\\b[Ss]tory:? #?(\\d+)',
+        link  => 'https://storyboard-dev.openstack.org/#!/story/$1',
+      },
+      {
+        name  => 'blueprint',
+        match => '(\\b[Bb]lue[Pp]rint\\b|\\b[Bb][Pp]\\b)[ \\t#:]*([A-Za-z0-9\\-]+)',
+        link  => 'https://blueprints.launchpad.net/openstack/?searchtext=$2',
+      },
+      {
+        name  => 'testresult',
+        match => '<li>([^ ]+) <a href=\"[^\"]+\" target=\"_blank\">([^<]+)</a> : ([^ ]+)([^<]*)</li>',
+        html  => '<li class=\"comment_test\"><span class=\"comment_test_name\"><a href=\"$2\">$1</a></span> <span class=\"comment_test_result\"><span class=\"result_$3\">$3</span>$4</span></li>',
+      },
+      {
+        name  => 'launchpadbug',
+        match => '<a href=\"(https://bugs\\.launchpad\\.net/[a-zA-Z0-9\\-]+/\\+bug/(\\d+))[^\"]*\">[^<]+</a>',
+        html  => '<a href=\"$1\">$1</a>'
+      },
+      {
+        name  => 'changeid',
+        match => '(I[0-9a-f]{8,40})',
+        link  => '/#q,$1,n,z',
+      },
+      {
+        name  => 'gitsha',
+        match => '(<p>|[\\s(])([0-9a-f]{40})(</p>|[\\s.,;:)])',
+        html  => '$1<a href=\"/#q,$2,n,z\">$2</a>$3',
+      },
+    ],
     replication                         => [
       {
         name                 => 'github',
