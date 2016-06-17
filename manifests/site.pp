0f6ca74131543b81fc921316e09964338661798b
Khai Do <zaro0508@gmail.com>
2016-06-17 00:17:10 -0700

    Configure its-storyboard plugin
    Add configurations for the its-storyboard plugin:
    1. Move java_home variable to a higher level module so it can be used
       to install certificate on review-dev.o.o
    2. Configure its-storyboard for review.o.o and review-dev.o.o
    3. Associate review.o.o with storyboard.o.o and review-dev.o.o with
       storyboard-dev.o.o
    4. Import ssl certificate to java on review-dev.o.o so that the plugin
       can POST updates to storyboard-dev.o.o using storyboard REST APIs.
    5. Configure rules (or conditions) telling its-plugin to update
       storyboard stories on specific stream events.
    
    Change-Id: Id6ec16e267fca5fbbc42b1d3547fc5d2fa4c671b
    depends-on: I9f47a2ed88ffbe827e494a478c0dc89a08bbe370
    depends-on: I5e817fab8a8973b688fd44dd819e3616df171321

--- manifests/site.pp
+++ manifests/site.pp
@@ -59,6 +59,7 @@ node 'review.openstack.org' {
     contactstore_pubkey                 => hiera('gerrit_contactstore_pubkey'),
     swift_username                      => hiera('swift_store_user', 'username'),
     swift_password                      => hiera('swift_store_key'),
+    storyboard_password                 => hiera('gerrit_storyboard_token'),
   }
 }
 
@@ -91,6 +92,8 @@ node 'review-dev.openstack.org' {
     lp_sync_consumer_key                => hiera('gerrit_dev_lp_consumer_key'),
     lp_sync_token                       => hiera('gerrit_dev_lp_access_token'),
     lp_sync_secret                      => hiera('gerrit_dev_lp_access_secret'),
+    storyboard_password                 => hiera('gerrit_dev_storyboard_token'),
+    storyboard_ssl_cert                 => hiera('gerrit_dev_storyboard_ssl_crt'),
   }
 }
 
