0f2c8101c98d14b1a5b0fc6be97f4cd046a5d7ef
Paul Belanger <pabelanger@redhat.com>
2016-06-18 19:04:53 -0400

    Remove jenkins from site.pp
    We still depend on the manifests so we cannot remember them at this
    point in time.
    
    Change-Id: I1f472db79d3c797a43be9bcc07d5fd1c449cffb5
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -136,31 +136,6 @@ node 'stackalytics.openstack.org' {
   }
 }
 
-# Node-OS: precise
-node /^jenkins\d+\.openstack\.org$/ {
-  $group = "jenkins"
-  $zmq_event_receivers = ['logstash.openstack.org',
-                          'nodepool.openstack.org']
-  $zmq_iptables_rule = regsubst($zmq_event_receivers,
-                                '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 8888 -s \1 -j ACCEPT')
-  $http_iptables_rule = '-m state --state NEW -m tcp -p tcp --dport 80 -s nodepool.openstack.org -j ACCEPT'
-  $https_iptables_rule = '-m state --state NEW -m tcp -p tcp --dport 443 -s nodepool.openstack.org -j ACCEPT'
-  $iptables_rule = flatten([$zmq_iptables_rule, $http_iptables_rule, $https_iptables_rule])
-  class { 'openstack_project::server':
-    iptables_rules6     => $iptables_rule,
-    iptables_rules4     => $iptables_rule,
-    sysadmins           => hiera('sysadmins', []),
-    puppetmaster_server => 'puppetmaster.openstack.org',
-  }
-  class { 'openstack_project::jenkins':
-    jenkins_password        => hiera('jenkins_jobs_password'),
-    jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents'),
-    ssl_cert_file           => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-    ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
-    ssl_chain_file          => '',
-  }
-}
-
 # Node-OS: trusty
 node 'cacti.openstack.org' {
   include openstack_project::ssl_cert_check
