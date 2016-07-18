f370ca6621f69a1969c1d0aeb5b6ee6b1cfa3186
Ramy Asselin <ramy.asselin@hpe.com>
2016-07-18 04:48:11 -0700

    Stop Jenkin master restarts
    Jenkin masters are no longer in use. Stop restarting them.
    
    Change-Id: Idc66665e341f2be36f3ec269a535333cb5db868c

--- manifests/site.pp
+++ manifests/site.pp
@@ -180,8 +180,6 @@ node 'puppetmaster.openstack.org' {
   }
   class { 'openstack_project::puppetmaster':
     root_rsa_key                               => hiera('puppetmaster_root_rsa_key'),
-    jenkins_api_user                           => hiera('jenkins_api_user', 'username'),
-    jenkins_api_key                            => hiera('jenkins_api_key'),
     puppetmaster_clouds                        => hiera('puppetmaster_clouds'),
   }
 }
