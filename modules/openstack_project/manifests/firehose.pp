8b288f82382199c39bf298d92c19e6f01b5b1b2e
Matthew Treinish <mtreinish@kortar.org>
2016-07-26 19:03:55 -0400

    Add firehose.o.o config to system-config
    This commit adds the policy to configure firehose nodes which will run
    mosquitto and a germqtt daemon.
    
    Depends-On: Ibec91fd0abc637ea7087872cab1ec8487c73acae
    Depends-On: I24a0cdb6a41f6e440db8e68216b19ca61b4cba31
    Change-Id: Ie3be71e16c42c32a9f479da468db6c53ebae52ac

---
+++ modules/openstack_project/manifests/firehose.pp
@@ -0,0 +1,42 @@
+# Copyright 2016 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# firehose glue class.
+#
+class openstack_project::firehose (
+  $gerrit_username = 'germqtt',
+  $gerrit_public_key,
+  $gerrit_private_key,
+  $gerrit_ssh_host_key,
+  $mqtt_hostname = 'firehose01.openstack.org',
+  $mqtt_password,
+  $mqtt_username = 'infra',
+) {
+  include mosquitto
+  class {'mosquitto::server':
+    log_file => '/var/log/mosquitto.log',
+    infra_service_username => $mqtt_username,
+    infra_service_password => $mqtt_password,
+  }
+
+  include germqtt
+  class {'germqtt::server':
+    gerrit_username    => $gerrit_username,
+    gerrit_public_key  => $gerrit_public_key,
+    gerrit_private_key => $gerrit_private_key,
+    gerrit_host_key    => $gerrit_host_key,
+    mqtt_username      => $mqtt_username,
+    mqtt_password      => $mqtt_password,
+  }
+}
