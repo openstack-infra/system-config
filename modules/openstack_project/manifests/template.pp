f0def4bef09b05851b4082af683a0346f35856e7
Paul Belanger <pabelanger@redhat.com>
2016-08-01 12:30:42 -0400

    Use IPv6 address for puppetmaster.o.o
    We are switching to the IPv6 address because ubuntu-xenial has
    disabled DNS lookup in openssh-server by default.  As a results, root
    can no longer SSH into ubuntu-xenail servers.
    
    Change-Id: Ic796bee28bc04d35785647b003749860f6b26730
    Depends-On: I28c5d71e62a62bd27f289a8bd70b235eac213e5c
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- modules/openstack_project/manifests/template.pp
+++ modules/openstack_project/manifests/template.pp
@@ -34,8 +34,9 @@ class openstack_project::template (
   include openstack_project::users
 
   class { 'ssh':
-    trusted_ssh_source => $puppetmaster_server,
-    permit_root_login => $permit_root_login,
+    trusted_ssh_type   => 'address',
+    trusted_ssh_source => '23.253.245.198,2001:4800:7818:101:3c21:a454:23ed:4072',
+    permit_root_login  => $permit_root_login,
   }
 
   if ( $afs ) {
