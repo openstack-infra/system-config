07b18d6c2a4e3d66fe93ce2cdc32c54623f0a607
Khai Do <zaro0508@gmail.com>
2016-08-08 10:53:10 -0700

    Upgrade gerrit on review.o.o
    This upgrade does not require a reindex. It will pull in the following
    changes:
    
    277538  Append submitted info to the change REST endpoint.
    267927  Remove required access permission to list account emails
    340157  Update Google gson library to 2.3.1
    345540  Add rel=nofollow parameter to links in comments
    311903  Use maven repos for bouncy castle resources
    260324  Add syntax highlighting support for puppet
    
    Change-Id: I1c2e0884ed9ded757c9e001d27b54349afdf5670

--- modules/openstack_project/manifests/review.pp
+++ modules/openstack_project/manifests/review.pp
@@ -128,7 +128,7 @@ class openstack_project::review (
     index_threads                       => 4,
     httpd_maxqueued                     => '200',
     war                                 =>
-      'http://tarballs.openstack.org/ci/gerrit/gerrit-v2.11.4.11.a14450f.war',
+      'http://tarballs.openstack.org/ci/gerrit/gerrit-v2.11.4.22.e0c0f29.war',
     contactstore                        => $contactstore,
     contactstore_appsec                 => $contactstore_appsec,
     contactstore_pubkey                 => $contactstore_pubkey,
