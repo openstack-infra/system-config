da6513a87fbe5db2fe06cf2f582a0c97f995de0a
Jeremy Stanley <fungi@yuggoth.org>
2016-08-13 16:02:15 +0000

    Test Gerrit CI comment regex for rel="nofollow"
    Now that we're running with a backport of
    https://gerrit-review.googlesource.com/80320 from upstream, the
    hyperlinks in comments include a rel="nofollow" parameter. Our
    regular expression for these entries needs to take that into
    account, so give it a try on review-dev.
    
    Change-Id: Ida090f45cfdfb41c43c4ba1157f0478fc0936a20

--- modules/openstack_project/manifests/review_dev.pp
+++ modules/openstack_project/manifests/review_dev.pp
@@ -112,8 +112,8 @@ class openstack_project::review_dev (
       },
       {
         name  => 'testresult',
-        match => '<li>([^ ]+) <a href=\"[^\"]+\" target=\"_blank\">([^<]+)</a> : ([^ ]+)([^<]*)</li>',
-        html  => '<li class=\"comment_test\"><span class=\"comment_test_name\"><a href=\"$2\">$1</a></span> <span class=\"comment_test_result\"><span class=\"result_$3\">$3</span>$4</span></li>',
+        match => '<li>([^ ]+) <a href=\"[^\"]+\" target=\"_blank\" rel=\"nofollow\">([^<]+)</a> : ([^ ]+)([^<]*)</li>',
+        html  => '<li class=\"comment_test\"><span class=\"comment_test_name\"><a href=\"$2\" rel=\"nofollow\">$1</a></span> <span class=\"comment_test_result\"><span class=\"result_$3\">$3</span>$4</span></li>',
       },
       {
         name  => 'launchpadbug',
