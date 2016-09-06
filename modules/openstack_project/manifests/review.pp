dad610bfc636e0375dd6f8803cbad654afbaa566
Khai Do <zaro0508@gmail.com>
2016-09-06 14:23:37 -0700

    upgrade Gerrit's javamelody plugin on review.o.o
    upgrade Gerrit's javamelody plugin[1] on review.o.o
    This brings in an upstream change[2] to update javamelody to version
    1.60.0
    
    NOTE: Approving this change will kill Gerrit due to a bug, the change
    to fix[3] this bug is uncommited atm.  The way to avoid the bug is
    to stop Gerrit, approve this change, allow puppet to propogate
    the update then start Gerrit.
    
    [1] http://tarballs.openstack.org/ci/gerrit/plugins/javamelody/javamelody-1de5d37.jar
    [2] https://gerrit.googlesource.com/plugins/javamelody/+/1de5d373afb1b1edf0647c804a92dc1ce8e96ec2
    [3] https://review.openstack.org/#/c/268689/
    
    Change-Id: I2d5a81a763da6e65a831360a9b4412f79c08945e

--- modules/openstack_project/manifests/review.pp
+++ modules/openstack_project/manifests/review.pp
@@ -302,7 +302,7 @@ class openstack_project::review (
     require                             => $::project_config::config_dir,
   }
 
-  gerrit::plugin { 'javamelody': version       => '3fefa35' }
+  gerrit::plugin { 'javamelody': version       => '1de5d37' }
   gerrit::plugin { 'its-storyboard': version   => 'a9cb131' }
 
   class { 'gerritbot':
