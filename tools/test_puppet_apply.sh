a74e4d4a6fe9595a43ca676130a4767488cea2b5
James E. Blair <jeblair@redhat.com>
2016-09-16 11:35:02 -0700

    Use ansible for puppet apply test
    This is needed since the hiera configuration is in the ansible
    role.
    
    Change-Id: Ie2a028b9e0179168d279bcf33de7f997cdfdbc8d

--- tools/test_puppet_apply.sh
+++ tools/test_puppet_apply.sh
@@ -18,7 +18,7 @@ file=$1
 fileout=${file}.out
 echo "##" > $fileout
 cat $file > $fileout
-sudo puppet apply --modulepath=${MODULE_PATH} --color=false --noop --verbose --debug $file >/dev/null 2>> $fileout
+sudo /tmp/ansible/bin/ansible-playbook -f1 --limit localhost --check playbooks/remote_puppet_adhoc.yaml -e puppet_environment=production -e manifest=$file -e puppet_logdest=/tmp/puppet-out
 ret=$?
 cat $fileout
 exit $ret
