3d175807bb2623a5550c48a36a20a9b7424d4d9e
Paul Belanger <pabelanger@redhat.com>
2016-09-30 14:34:55 -0400

    Add docs.o.o vhost for files.o.o
    Now that we are publishing docs to AFS, add a new vhost for apache.
    
    Change-Id: I8bc1bfefc5190f3c607905bbdc91c1cf2e47c83b
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- modules/openstack_project/manifests/files.pp
+++ modules/openstack_project/manifests/files.pp
@@ -49,4 +49,17 @@ class openstack_project::files (
       'create 640 root adm',
     ],
   }
+
+  ###########################################################
+  # docs.openstack.org
+
+  ::httpd::vhost { 'docs.openstack.org':
+    port       => 80,
+    docroot    => "${afs_root}/docs",
+    priority   => '50',
+    template   => 'openstack_project/docs.vhost.erb',
+    require    => [
+      File["${afs_root}/docs"],
+    ],
+  }
 }
