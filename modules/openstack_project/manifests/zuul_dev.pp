da9e2d11c65c6101d23acec687f408d6f9f32203
Khai Do <zaro0508@gmail.com>
2016-10-17 10:23:07 -0700

    Add a nodepool to zuul-dev.o.o
    Setup nodepool on zuul-dev.o.o.  This allows simulating production
    gerrit, zuul and nodepool integration.
    
    Change-Id: Id3ea81dea5f6a429fb2e04dc675702ecf689be1d

--- modules/openstack_project/manifests/zuul_dev.pp
+++ modules/openstack_project/manifests/zuul_dev.pp
@@ -15,6 +15,48 @@ class openstack_project::zuul_dev(
   $project_config_repo = 'https://git.openstack.org/openstack-infra/project-config',
   $gearman_workers = [],
   $zuul_launcher_keytab = '',
+  $mysql_password = '',
+  $mysql_root_password = '',
+  $nodepool_ssh_public_key = '',
+  $nodepool_ssh_private_key = '',
+  $bluebox_username    = '',
+  $bluebox_password    = 'password',
+  $bluebox_project     = 'project',
+  $rackspace_username  = '',
+  $rackspace_password  = 'password',
+  $rackspace_project   = 'project',
+  $hpcloud_username    = '',
+  $hpcloud_password    = 'password',
+  $hpcloud_project     = 'project',
+  $internap_username   = '',
+  $internap_password   = 'password',
+  $internap_project    = 'project',
+  $ovh_username        = '',
+  $ovh_password        = 'password',
+  $ovh_project         = 'project',
+  $tripleo_username    = '',
+  $tripleo_password    = 'password',
+  $tripleo_project     = 'project',
+  $infracloud_vanilla_username    = '',
+  $infracloud_vanilla_password    = 'password',
+  $infracloud_vanilla_project     = 'project',
+  $infracloud_chocolate_username  = '',
+  $infracloud_chocolate_password  = 'password',
+  $infracloud_chocolate_project   = 'project',
+  $osic_cloud1_username           = '',
+  $osic_cloud1_password           = 'password',
+  $osic_cloud1_project            = 'project',
+  $osic_cloud8_username           = '',
+  $osic_cloud8_password           = '',
+  $osic_cloud8_project            = '',
+  $vexxhost_username   = '',
+  $vexxhost_password   = 'password',
+  $vexxhost_project    = 'project',
+  $datacentred_username   = '',
+  $datacentred_password   = 'password',
+  $datacentred_project    = 'project',
+  $citycloud_username = '',
+  $citycloud_password = 'password',
 ) {
 
   realize (
@@ -60,5 +102,42 @@ class openstack_project::zuul_dev(
     project_config_repo      => $project_config_repo,
     project_config_base      => 'dev/',
     zuul_launcher_keytab     => $zuul_launcher_keytab,
+  }
+
+  include openstack_project
+
+  class { '::openstackci::nodepool':
+    vhost_name                    => '127.0.0.1',
+    project_config_repo           => $project_config_repo,
+    mysql_password                => $mysql_password,
+    mysql_root_password           => $mysql_root_password,
+    nodepool_ssh_public_key       => $nodepool_ssh_public_key,
+    # TODO(pabelanger): Switch out private key with zuul_worker once we are
+    # ready.
+    nodepool_ssh_private_key      => $nodepool_ssh_private_key,
+    oscc_file_contents            => template("openstack_project/nodepool/clouds.yaml.erb"),
+    image_log_document_root       => '/var/log/nodepool/image',
+    statsd_host                   => 'graphite.openstack.org',
+    logging_conf_template         => 'openstack_project/nodepool/nodepool.logging.conf.erb',
+    builder_logging_conf_template => 'openstack_project/nodepool/nodepool-builder.logging.conf.erb',
+    upload_workers                => '16',
+    jenkins_masters               => [],
+    split_daemon                  => true,
+  }
+  file { '/home/nodepool/.config/openstack/infracloud_vanilla_cacert.pem':
+    ensure  => present,
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    mode    => '0600',
+    content => hiera('infracloud_vanilla_ssl_cert_file_contents'),
+    require => Class['::openstackci::nodepool'],
+  }
+  file { '/home/nodepool/.config/openstack/infracloud_chocolate_cacert.pem':
+    ensure  => present,
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    mode    => '0600',
+    content => hiera('infracloud_chocolate_ssl_cert_file_contents'),
+    require => Class['::openstackci::nodepool'],
   }
 }
