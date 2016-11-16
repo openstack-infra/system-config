af19cdc7c70bb864c27fcf76727497bc0ab8d72a
Ramy Asselin <ramy.asselin@hpe.com>
2016-11-15 17:12:46 -0800

    Add a test node for puppet-openstackci/single-node-ci
    Ensure changes to single-node-ci dependencies are always
    co-installable.
    
    Change-Id: I6638bc477a63f1da8fc70dc048691d1667e8319f

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,6 +64,51 @@ node 'review.openstack.org' {
 }
 
 # Node-OS: trusty
+# Used for testing all-in-one deployments
+node 'single-node-ci.openstack.org' {
+  # If the fqdn is not resolvable, use its ip address
+  $vhost_name = hiera('vhost_name', $::fqdn)
+
+  class { '::openstackci::single_node_ci':
+    vhost_name                  => $vhost_name,
+    project_config_repo         => hiera('project_config_repo'),
+    serveradmin                 => hiera('serveradmin', "webmaster@${vhost_name}"),
+    jenkins_vhost_name          => hiera('jenkins_vhost_name', 'jenkins'),
+    jenkins_username            => hiera('jenkins_username', 'jenkins'),
+    jenkins_password            => hiera('jenkins_password', 'XXX'),
+    jenkins_ssh_private_key     => hiera('jenkins_ssh_private_key'),
+    jenkins_ssh_public_key      => hiera('jenkins_ssh_public_key'),
+    gerrit_server               => hiera('gerrit_server', 'review.openstack.org'),
+    gerrit_user                 => hiera('gerrit_user'),
+    gerrit_user_ssh_public_key  => hiera('gerrit_user_ssh_public_key'),
+    gerrit_user_ssh_private_key => hiera('gerrit_user_ssh_private_key'),
+    gerrit_ssh_host_key         => hiera('gerrit_ssh_host_key',
+      'review.openstack.org,23.253.232.87,2001:4800:7815:104:3bc3:d7f6:ff03:bf5d b8:3c:72:82:d5:9e:59:43:54:11:ef:93:40:1f:6d:a5'),
+    git_email                   => hiera('git_email'),
+    git_name                    => hiera('git_name'),
+    log_server                  => hiera('log_server'),
+    smtp_host                   => hiera('smtp_host', 'localhost'),
+    smtp_default_from           => hiera('smtp_default_from', "zuul@${vhost_name}"),
+    smtp_default_to             => hiera('smtp_default_to', "zuul.reports@${vhost_name}"),
+    zuul_revision               => hiera('zuul_revision', 'master'),
+    zuul_git_source_repo        => hiera('zuul_git_source_repo',
+      'https://git.openstack.org/openstack-infra/zuul'),
+    oscc_file_contents          => hiera('oscc_file_contents', ''),
+    mysql_root_password         => hiera('mysql_root_password'),
+    mysql_nodepool_password     => hiera('mysql_nodepool_password'),
+    nodepool_jenkins_target     => hiera('nodepool_jenkins_target', 'jenkins1'),
+    jenkins_api_key             => hiera('jenkins_api_key', 'XXX'),
+    jenkins_credentials_id      => hiera('jenkins_credentials_id', 'XXX'),
+    nodepool_revision           => hiera('nodepool_revision', 'master'),
+    nodepool_git_source_repo    => hiera('nodepool_git_source_repo',
+      'https://git.openstack.org/openstack-infra/nodepool'),
+    jjb_git_revision            => hiera('jjb_git_revision', '1.6.1'),
+    jjb_git_url                 => hiera('jjb_git_url',
+      'https://git.openstack.org/openstack-infra/jenkins-job-builder'),
+  }
+}
+
+# Node-OS: trusty
 node 'review-dev.openstack.org' {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80, 443, 29418],
