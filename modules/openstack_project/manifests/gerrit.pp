b8b261deb6b4a77551cd8c874b6b89c6d42201f7
Khai Do <zaro0508@gmail.com>
2016-11-18 22:01:54 -0800

    Setup known_hosts file on gerrit servers
    Setup known_hosts file on review.o.o and review-dev.o.o so that
    known services connecting to it won't be blocked.
    
    Change-Id: If0b082867df73322a9115b94ea5be18d0631acc0
    depends-on: I0bb6638f5362696cd9a9a7401cb2e96ab69eea06

--- modules/openstack_project/manifests/gerrit.pp
+++ modules/openstack_project/manifests/gerrit.pp
@@ -10,6 +10,7 @@ class openstack_project::gerrit (
   $canonicalweburl = "https://${::fqdn}/",
   $git_http_url = '',
   $canonical_git_url = '',
+  $gerrit_ssh_known_hosts = '',
   $serveradmin = 'webmaster@openstack.org',
   $ssh_host_key = '/home/gerrit2/review_site/etc/ssh_host_rsa_key',
   $ssh_project_key = '/home/gerrit2/review_site/etc/ssh_project_rsa_key',
@@ -122,6 +123,7 @@ class openstack_project::gerrit (
     enable_javamelody_top_menu          => false,
     # passthrough
     java_home                           => $java_home,
+    gerrit_ssh_known_hosts              => $gerrit_ssh_known_hosts,
     ssl_cert_file                       => $ssl_cert_file,
     ssl_key_file                        => $ssl_key_file,
     ssl_chain_file                      => $ssl_chain_file,
