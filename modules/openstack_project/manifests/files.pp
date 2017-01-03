44daf3ee5eeffb97090fd7086b152306a8fcdaf6
Andreas Jaeger <aj@suse.com>
2017-01-03 07:57:56 +0100

    Remove docs-beta.openstack.org
    Remove the alias, we do not need this anymore.
    
    Note to reviewers: An infra-root needs to remove the DNS entry for
    docs-beta as well.
    
    Change-Id: Ia4b5af8fc6838e94b8ca784edacca3c0c961ff98

--- modules/openstack_project/manifests/files.pp
+++ modules/openstack_project/manifests/files.pp
@@ -63,7 +63,6 @@ class openstack_project::files (
     docroot    => "${afs_root}docs",
     priority   => '50',
     template   => 'openstack_project/docs.vhost.erb',
-    serveraliases => 'docs-beta.openstack.org',
   }
 
   ###########################################################
