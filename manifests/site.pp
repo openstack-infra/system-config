a1093f7faabe4d2c21065dd0cf3eeec0b7803475
Clark Boylan <clark.boylan@gmail.com>
2017-01-24 11:00:31 -0800

    Deploy simple ethercalc server
    This is a simple first deployment of an ethercalc service. It does not
    come with authenticated redis or redis backups. It will however have
    working ssl.
    
    Change-Id: I8c434a6bff42bce75e67fb37665d213f3cc018c8
    Depends-On: Id10247211d9643e81bb1b6e8fb67377ba6de873a

--- manifests/site.pp
+++ manifests/site.pp
@@ -356,6 +356,21 @@ node 'eavesdrop.openstack.org' {
 }
 
 # Node-OS: trusty
+node /^ethercalc\d+\.openstack\.org$/ {
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [22, 80, 443],
+    sysadmins                 => hiera('sysadmins', []),
+  }
+
+  class { 'openstack_project::ethercalc':
+    vhost_name              => 'ethercalc.openstack.org',
+    ssl_cert_file_contents  => hiera('ethercalc_ssl_cert_file_contents'),
+    ssl_key_file_contents   => hiera('ethercalc_ssl_key_file_contents'),
+    ssl_chain_file_contents => hiera('ethercalc_ssl_chain_file_contents'),
+  }
+}
+
+# Node-OS: trusty
 node 'etherpad.openstack.org' {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [22, 80, 443],
