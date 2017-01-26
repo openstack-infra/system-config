0076bb4ece9f64b19d805873e9bb6d6ecb1bafe6
Clark Boylan <clark.boylan@gmail.com>
2017-01-26 08:55:26 -0800

    Add bup backups to ethercalc.openstack.org
    This will backup the redis snapshots (among other things) on the
    ethercalc server. This should be the last major step before putting this
    service into production.
    
    Change-Id: I12d7809f3a0cea560662491ceff1ff7b1e07ce9a

--- modules/openstack_project/manifests/ethercalc.pp
+++ modules/openstack_project/manifests/ethercalc.pp
@@ -19,6 +19,16 @@ class openstack_project::ethercalc (
     ssl_chain_file_contents => $ssl_chain_file_contents,
   }
 
-  # TODO(clarkb) Redis backups
   include ethercalc::redis
+
+  # Redis creates a snapshot at /var/lib/redis/dump.rdb periodically
+  # (at worst every 15 minutes if at least one change is made to redis)
+  # which can be used to recover the Redis DB. Bup will automagically
+  # pick this file up during its normal operation so no other DB dumping
+  # is required like with mysql.
+  include bup
+  bup::site { 'rs-ord':
+    backup_user   => 'bup-etherpad',
+    backup_server => 'ci-backup-rs-ord.openstack.org',
+  }
 }
