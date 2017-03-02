7e7956bd66c77a1e203eec01518f0d60c9c5132c
Paul Belanger <pabelanger@redhat.com>
2017-03-02 13:39:51 -0500

    Fixes to ubuntu-mariadb mirror
    Changes that are needed to make reprepro function properly.
    
    Change-Id: I344f529e8edf7a9f272774db8dd42330919c4e65
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- modules/openstack_project/manifests/mariadb_mirror.pp
+++ modules/openstack_project/manifests/mariadb_mirror.pp
@@ -5,7 +5,7 @@ define openstack_project::mariadb_mirror (
   ### MariaDB mirror ###
   ::openstack_project::reprepro { "ubuntu-mariadb-$name-reprepro-mirror":
     confdir       => "/etc/reprepro/ubuntu-mariadb-$name",
-    basedir       => "/afs/.openstack.org/mirror/ubuntu-mariadb-$name",
+    basedir       => "/afs/.openstack.org/mirror/ubuntu-mariadb/$name",
     distributions => 'openstack_project/reprepro/distributions.ubuntu-mariadb.erb',
     updates_file  => "puppet:///modules/openstack_project/reprepro/ubuntu-mariadb-$name-updates",
     releases      => [ 'trusty', 'xenial' ],
@@ -15,7 +15,7 @@ define openstack_project::mariadb_mirror (
     user        => $user,
     hour        => '*/2',
     minute      => '0',
-    command     => "flock -n /var/run/reprepro/ubuntu-mariadb-$name.lock reprepro-mirror-update /etc/reprepro/ubuntu-mariadab-$name mirror.ubuntu-mariadb-$name >>/var/log/reprepro/ubuntu-mariadb-$name-mirror.log 2>&1",
+    command     => "flock -n /var/run/reprepro/ubuntu-mariadb-$name.lock reprepro-mirror-update /etc/reprepro/ubuntu-mariadb-$name mirror.ubuntu-mariadb-$name >>/var/log/reprepro/ubuntu-mariadb-$name-mirror.log 2>&1",
     environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
   }
 }
