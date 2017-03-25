9248d340f2460b076ce5f048d9f7c5d14b7b33a6
Colleen Murphy <colleen@gazlene.net>
2017-03-25 18:48:55 +0100

    Move package resources to o_p::server
    Once nodepool is building worker images with these packages already
    installed, we don't need to manage them in the common
    openstack_project::template class. This patch moves them over to
    openstack_project::server so that it is obvious that puppet doesn't need
    to manage them for both single-use workers and long-lived servers.
    Eventually the openstack_project::template class should be empty and we
    can remove it.
    
    Depends-On: Ie1a0aba57390c9c0b269b4cbb076090ae1de73a9
    Change-Id: I31295cdf12941e4adcf87a73418df1d17d9ec3d2

--- modules/openstack_project/manifests/params.pp
+++ modules/openstack_project/manifests/params.pp
@@ -3,15 +3,26 @@
 # This class holds parameters that need to be
 # accessed by other classes.
 class openstack_project::params {
+  $cross_platform_packages = [
+    'at',
+    'git',
+    'lvm2',
+    'parted',
+    'puppet',
+    'rsync',
+    'strace',
+    'tcpdump',
+    'wget',
+  ]
   case $::osfamily {
     'RedHat': {
-      $packages = ['parted', 'puppet', 'wget', 'iputils']
+      $packages = concat($cross_platform_packages, ['iputils', 'bind-utils'])
       $user_packages = ['emacs-nox', 'vim-enhanced']
       $update_pkg_list_cmd = ''
       $login_defs = 'puppet:///modules/openstack_project/login.defs.redhat'
     }
     'Debian': {
-      $packages = ['parted', 'puppet', 'wget', 'iputils-ping']
+      $packages = concat($cross_platform_packages, ['iputils-ping', 'dnsutils'])
       case $::operatingsystemrelease {
         /^(12|14)\.(04|10)$/: {
           $user_packages = ['emacs23-nox', 'vim-nox', 'iftop',
