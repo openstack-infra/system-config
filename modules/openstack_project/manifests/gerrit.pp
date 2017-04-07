bf456b38d1f393b012c6df291399f672e63b6585
Monty Taylor <mordred@inaugust.com>
2017-04-07 05:39:56 -0500

    Revert "Setup known_hosts file on gerrit servers"
    The key is not in heira.
    
    This reverts commit 956372cd395d0c7050464078a3a861e7e97faf35.
    
    Change-Id: I19bc0f418318edc839c4eca168cd3c05ac7655f0

--- modules/openstack_project/manifests/gerrit.pp
+++ modules/openstack_project/manifests/gerrit.pp
@@ -10,7 +10,6 @@ class openstack_project::gerrit (
   $canonicalweburl = "https://${::fqdn}/",
   $git_http_url = '',
   $canonical_git_url = '',
-  $gerrit_ssh_known_hosts = '',
   $serveradmin = 'webmaster@openstack.org',
   $ssh_host_key = '/home/gerrit2/review_site/etc/ssh_host_rsa_key',
   $ssh_project_key = '/home/gerrit2/review_site/etc/ssh_project_rsa_key',
@@ -124,7 +123,6 @@ class openstack_project::gerrit (
     enable_javamelody_top_menu          => false,
     # passthrough
     java_home                           => $java_home,
-    known_hosts_content                 => $gerrit_ssh_known_hosts,
     ssl_cert_file                       => $ssl_cert_file,
     ssl_key_file                        => $ssl_key_file,
     ssl_chain_file                      => $ssl_chain_file,
