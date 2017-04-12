4d1564635ab6ac40e6c50e01dc34e05b8ec1da91
Paul Belanger <pabelanger@redhat.com>
2017-04-12 14:32:54 -0400

    Remove /etc/sudoers.d/jenkins-sudo support
    We can move /etc/sudoers.d/jenkins-sudo into a DIB element, and
    properly clean up slave_common.pp of our other sudoers.d settings.
    
    Change-Id: I548b22f68b4f288fe1c4658abf9b42a47dc7fa68
    Depends-On: Icaceeefbf6c3765d15d0e1488006f9f52661fba3
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- modules/openstack_project/manifests/slave_common.pp
+++ modules/openstack_project/manifests/slave_common.pp
@@ -3,23 +3,5 @@
 # Common configuration between openstack_project::slave and
 # openstack_project::single_use_slave
 class openstack_project::slave_common(
-  $sudo         = false,
 ){
-  if ($sudo == true) {
-    file { '/etc/sudoers.d/jenkins-sudo':
-      ensure => present,
-      source => 'puppet:///modules/openstack_project/jenkins-sudo.sudo',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0440',
-    }
-  }
-
-  file { '/etc/sudoers.d/jenkins-sudo-grep':
-    ensure => present,
-    source => 'puppet:///modules/openstack_project/jenkins-sudo-grep.sudo',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0440',
-  }
 }
