5d20402d29ab7645f605a1c032e8010d315038eb
Paul Belanger <pabelanger@redhat.com>
2017-06-01 16:06:22 -0400

    Create ze01.openstack.org
    This server will be used to run zuul-executors on.  We'll only launch
    a single service first so we can ensure it works before bringing more
    online.
    
    Change-Id: If0bdb26aeca68f992dce0dbed83008d6fa0bc5b9
    Depends-On: Iddac03a7187df47d2c72d957a50610f8b6a1dff5
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -1088,6 +1088,37 @@ node /^nb\d+\.openstack\.org$/ {
   }
 }
 
+# Node-OS: xenial
+node /^ze\d+\.openstack\.org$/ {
+  $gerrit_server        = 'review.openstack.org'
+  $gerrit_user          = 'zuul'
+  $zuul_ssh_private_key = hiera('zuul_ssh_private_key_contents')
+  $git_email            = 'zuul@openstack.org'
+  $git_name             = 'OpenStack Zuul'
+  $revision             = 'feature/zuulv3'
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [79],
+    sysadmins                 => hiera('sysadmins', []),
+  }
+
+  # NOTE(pabelanger): We call ::zuul directly, so we can override all in one
+  # settings.
+  class { '::zuul':
+    gerrit_server        => $gerrit_server,
+    gerrit_user          => $gerrit_user,
+    zuul_ssh_private_key => $zuul_ssh_private_key,
+    git_email            => $git_email,
+    git_name             => $git_name,
+    revision             => $revision,
+    python_version       => 3,
+    zookeeper_hosts      => 'nodepool.openstack.org:2181',
+    zuulv3               => true,
+  }
+
+  class { '::zuul::executor': }
+}
+
 # Node-OS: trusty
 node 'zuulv3-dev.openstack.org' {
   $gerrit_server        = 'review.openstack.org'
