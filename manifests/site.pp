982b831a55eabbaf97e414967ef96fe2fb51298a
Paul Belanger <pabelanger@redhat.com>
2017-06-06 15:29:37 -0400

    Allow ze01.o.o access to gearman / add to cacti.o.o
    We also need to add gearman configuration to ze01.o.o
    
    Change-Id: I1ed1f7b41fb85ab831d2752d1de4a2d1dad00ec0
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -1107,6 +1107,8 @@ node /^ze\d+\.openstack\.org$/ {
   # NOTE(pabelanger): We call ::zuul directly, so we can override all in one
   # settings.
   class { '::zuul':
+    gearman_server          => 'zuulv3.openstack.org',
+    internal_gearman        => false,
     gerrit_server           => $gerrit_server,
     gerrit_user             => $gerrit_user,
     zuul_ssh_private_key    => $zuul_ssh_private_key,
@@ -1176,6 +1178,18 @@ node 'zuulv3.openstack.org' {
   $git_email            = 'zuul@openstack.org'
   $git_name             = 'OpenStack Zuul'
   $revision             = 'feature/zuulv3'
+
+  $gearman_workers = [
+    'ze01.openstack.org',
+  ]
+  $iptables_rules = regsubst ($gearman_workers, '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 4730 -s \1 -j ACCEPT')
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [80],
+    iptables_rules6           => $iptables_rules,
+    iptables_rules4           => $iptables_rules,
+    sysadmins                 => hiera('sysadmins', []),
+  }
 
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80],
