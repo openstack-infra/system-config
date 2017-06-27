5997f15c713fa3bee9b5b184a4183b89bc3d6a48
James E. Blair <jeblair@redhat.com>
2017-06-27 13:48:47 -0700

    Map zuul ssh keys into trusted environment
    This makes the ssh key files available to trusted zuul jobs running
    on the executors.  We will use this to scp files to static.o.o.
    
    Change-Id: I717d9371efb16640d267cf03620d7e3367e1f79e
    Depends-On: I2e457e05ccee8da33cddf11deb47144c556a024f

--- manifests/site.pp
+++ manifests/site.pp
@@ -1125,6 +1125,7 @@ node /^ze\d+\.openstack\.org$/ {
     gearman_client_ssl_cert => hiera('gearman_client_ssl_cert'),
     gearman_client_ssl_key  => hiera('gearman_client_ssl_key'),
     gearman_ssl_ca          => hiera('gearman_ssl_ca'),
+    trusted_ro_dirs         => ['/var/lib/zuul/ssh'],
   }
 
   class { '::zuul::executor': }
