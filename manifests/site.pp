0820e290035378daef2460e48c38c5050c6537de
Paul Belanger <pabelanger@redhat.com>
2017-08-31 19:08:23 -0400

    Add /afs to trusted_rw_paths
    AFS jobs for zuulv3 will need write access to this directory.
    
    Change-Id: Ia852a7c26ed3de76259b66d559c25ae64d54531a
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -1102,6 +1102,7 @@ node /^ze\d+\.openstack\.org$/ {
     #TODO(pabelanger): Add openafs role for zuul-jobs to setup /etc/openafs
     # properly. We need to revisting this post Queens PTG.
     trusted_ro_paths         => ['/etc/openafs', '/var/lib/zuul/ssh'],
+    trusted_rw_paths         => ['/afs'],
     disk_limit_per_job       => 500,  # Megabytes
     site_variables_yaml_file => $::project_config::zuul_site_variables_yaml,
     require                  => $::project_config::config_dir,
