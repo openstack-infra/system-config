8e4d15906f001b880290ae71de40e38dde2f2118
Matthew Treinish <mtreinish@kortar.org>
2017-11-15 15:39:59 -0500

    WIP: Add subunit2sql check database server
    This commit adds a new server for running another subunit2sql database
    for storing check data. Unlike the gate/periodic server this will be
    done by manually installing and running mysql on a node. This will give
    us improved control of the database (rather than what Rackspace Trove
    provides)
    
    Change-Id: I6af06f209f71a6e9664ac114f9e27769824157a2

---
+++ modules/openstack_project/manifests/subunit_db_server.pp
@@ -0,0 +1,73 @@
+# Copyright 2017 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# subunit2sql db server glue class.
+#
+class openstack_project::subunit_worker (
+  $expire_age = '186',
+  $expire_cron_minute = '0',
+  $expire_cron_hour = '3',
+  $expire_cron_weekday = '7',
+  $subunit2sql_db_host = 'localhost',
+  $subunit2sql_db_user = 'subunit2sql',
+  $subunit2sql_db_name = 'subunit2sql',
+  $subunit2sql_db_pass,
+  $root_mysql_pass,
+) {
+
+  class { '::mysql:server':
+    package_name     => 'mariadb-server',
+    service_name     => 'mysql',
+    root_password    => $root_mysql_pass,
+    override_options => {
+      mysqld => {
+        'max_connections' => '1024',
+      }
+    }
+  }
+
+  mysql::db { $subunit2sql_db_name:
+    charset  => 'utf8',
+    user     => $subunit2sql_db_user,
+    password => $subunit2sql_db_pass,
+    grant    => ['ALL'],
+  }
+
+
+  mysql::mysql_user { 'query':
+    ensure   => 'present',
+    password => 'query',
+    require  => Db[$subunit2sql_db_name],
+  }
+
+  mysql::mysql_grant {
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['SELECT'],
+    table      => "$subunit2sql_db_name.*",
+    user       => 'query',
+    require    => Mysql_user['query'],
+  }
+
+  include subunit2sql
+  class { 'subunit2sql::server':
+    db_host             => $subunit2sql_db_host,
+    db_pass             => $subunit2sql_db_pass,
+    expire_age          => $expire_age
+    expire_cron_minute  => $expire_cron_minute,
+    expire_cron_hour    => $expire_cron_hour,
+    expire_cron_weekday => $expire_cron_weekday,
+    require             => Db[$subunit2sql_db_name],
+  }
+}
