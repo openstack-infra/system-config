c41a891a10cedc537fa8cd40bd682cd06232694b
Ian Wienand <iwienand@redhat.com>
2017-12-15 10:03:50 +1100

    xenial update for codesearch.o.o
    Change-Id: I7859eb26168e5ee11ab2290e55409ff6b86aceab

--- manifests/site.pp
+++ manifests/site.pp
@@ -1782,7 +1782,9 @@ node 'odsreg.openstack.org' {
 }
 
 # Node-OS: trusty
-node 'codesearch.openstack.org' {
+# Node-OS: xenial
+node /^codesearch\d*\.openstack\.org$/ {
+  group = "codesearch"
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80],
     sysadmins                 => hiera('sysadmins', []),
