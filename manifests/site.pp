e7a9af1052e003ac44cc4fec62a2cb4a44ced66c
James E. Blair <jeblair@redhat.com>
2017-12-15 16:20:56 -0800

    Add an authoritative hidden master
    This runs bind as a hidden master nameserver so we can do all the
    keysigning there, and then use nsd (or bind) as public authoritative
    slaves.
    
    Change-Id: Ifb2ad109103051fa13c4af1c7be1ca0ae98bb1a1

--- manifests/site.pp
+++ manifests/site.pp
@@ -825,6 +825,26 @@ node /^status\d*\.openstack\.org$/ {
   }
 }
 
+# This is a hidden authoritative master nameserver, not publicly
+# accessible.
+# Node-OS: xenial
+node /^adns\d+\.openstack\.org$/ {
+  $group = 'adns'
+
+  class { 'openstack_project::server':
+    sysadmins                 => hiera('sysadmins', []),
+    iptables_allowed_hosts    => [
+      {protocol => 'tcp', port => '53', hostname => 'ns1.openstack.org'},
+    ],
+  }
+
+  class { 'openstack_project::master_nameserver':
+    tsig_key => hiera('tsig_key', {}),
+    notifies => dns_a('ns1.openstack.org'),
+  }
+}
+
+# These are publicly accessible authoritative slave nameservers.
 # Node-OS: xenial
 node /^ns\d+\.openstack\.org$/ {
   $group = 'ns'
