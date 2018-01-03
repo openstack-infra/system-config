5ba850cba2e2bde7e4409d9e8d918b64383eb67f
Emilien Macchi <emilien@redhat.com>
2018-01-03 14:50:16 -0800

    Add Puppetlabs mirror for Ubuntu Xenial
    Change-Id: Ibf5bd302b121abd64b3c74a6ec0cf8fecc455e6b

--- modules/openstack_project/manifests/mirror_update.pp
+++ modules/openstack_project/manifests/mirror_update.pp
@@ -266,6 +266,37 @@ class openstack_project::mirror_update (
     key_source => 'puppet:///modules/openstack_project/reprepro/docker-mirror-gpg-key.asc',
   }
 
+  ## Puppetlabs APT mirror
+  ::openstack_project::reprepro { 'ubuntu-puppetlabs-reprepro-mirror':
+    confdir       => '/etc/reprepro/ubuntu-puppetlabs',
+    basedir       => '/afs/.openstack.org/mirror/ubuntu-puppetlabs',
+    distributions => 'openstack_project/reprepro/distributions.ubuntu-puppetlabs.erb',
+    updates_file  => 'puppet:///modules/openstack_project/reprepro/xenial-pc1',
+    releases      => ['xenial'],
+  }
+
+  cron { 'reprepro ubuntu puppetlabs':
+    user        => $user,
+    hour        => '*/2',
+    minute      => '0',
+    command     => 'flock -n /var/run/reprepro/ubuntu-puppetlabs.lock reprepro-mirror-update /etc/reprepro/ubuntu-puppetlabs mirror.ubuntu-puppetlabs >>/var/log/reprepro/ubuntu-puppetlabs-mirror.log 2>&1',
+    environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
+    require     => [
+       File['/usr/local/bin/reprepro-mirror-update'],
+       File['/etc/afsadmin.keytab'],
+       File['/etc/reprepro.keytab'],
+       ::Openstack_project::Reprepro['ubuntu-puppeltabs-reprepro-mirror'],
+    ]
+  }
+
+  gnupg_key { 'Puppetlabs Archive':
+    ensure     => present,
+    key_id     => 'EF8D349F',
+    user       => 'root',
+    key_type   => 'public',
+    key_source => 'puppet:///modules/openstack_project/reprepro/puppetlabs-mirror-gpg-key.asc',
+  }
+
   ### CentOS mirror ###
   file { '/etc/centos.keytab':
     owner   => 'root',
