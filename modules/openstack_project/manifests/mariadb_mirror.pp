a5f12069b9f8073816088b81799e2e59a9e4840d
Ian Wienand <iwienand@redhat.com>
2018-03-15 09:23:15 +1100

    Fix mariadb sync
    These two jobs share the same volume, so use the common name as the
    argument, otherwise we fail to release.
    
     reprepro completed successfully, running vos release.
     VLDB: no such entry
    
    Change-Id: Ied4feaec7fe5f0bfd459fa080b55b452e21416d4

--- modules/openstack_project/manifests/mariadb_mirror.pp
+++ modules/openstack_project/manifests/mariadb_mirror.pp
@@ -11,11 +11,13 @@ define openstack_project::mariadb_mirror (
     releases      => [ 'trusty', 'xenial' ],
   }
 
+  # NOTE(ianw) we have several versions, but they all share the same
+  # mirror.ubuntu-mariadb volume.  Should we offset the run somehow?
   cron { "reprepro ubuntu mariadb $name":
     user        => $user,
     hour        => '*/2',
     minute      => '0',
-    command     => "flock -n /var/run/reprepro/ubuntu-mariadb-$name.lock reprepro-mirror-update /etc/reprepro/ubuntu-mariadb-$name mirror.ubuntu-mariadb-$name >>/var/log/reprepro/ubuntu-mariadb-$name-mirror.log 2>&1",
+    command     => "flock -n /var/run/reprepro/ubuntu-mariadb-$name.lock reprepro-mirror-update /etc/reprepro/ubuntu-mariadb-$name mirror.ubuntu-mariadb >>/var/log/reprepro/ubuntu-mariadb-$name-mirror.log 2>&1",
     environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
   }
 }
