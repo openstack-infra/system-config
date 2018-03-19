400dac5f04741b650778d8f3ddb2ec35dc615383
Paul Belanger <pabelanger@redhat.com>
2018-03-19 12:27:22 -0400

    Disable gerrit_initial_init on review-dev01.o.o
    Allow us to first attach git volume before configuring gerrit.
    
    Change-Id: I4e20bbe167ee0b9b0f5e566b3ad4eef40d0ac98a
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -81,6 +81,9 @@ node /^review-dev\d*\.openstack\.org$/ {
   }
 
   class { 'openstack_project::review_dev':
+    # TODO(pabelanger): Remove once we've attached volume to server with git
+    # repos for gerrit.
+    gerrit_initial_init                 => false,
     project_config_repo                 => 'https://git.openstack.org/openstack-infra/project-config',
     github_oauth_token                  => hiera('gerrit_dev_github_token'),
     github_project_username             => hiera('github_dev_project_username', 'username'),
