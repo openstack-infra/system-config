45f422de477596ff8bd850fd9085411c3dad4887
Jeremy Stanley <fungi@yuggoth.org>
2018-04-12 20:05:32 +0000

    Update to nodejs 6.x on etherpad.o.o
    In order to upgrade to current versions of the Etherpad service, we
    need newer nodejs than is available on Ubuntu Trusty. Switch to 6.x
    which is the current default for the puppet-etherpad_lite module and
    is also what we've tested on etherpad-dev.openstack.org. Switching
    between Ubuntu and Nodesource package sources does not result in an
    immediate package upgrade, so this must be performed manually before
    we change the eplite_version parameter to something newer.
    
    Change-Id: Ied9e5bd7ffa16f1832d3e1e26d0886de67f98f72

--- modules/openstack_project/manifests/etherpad.pp
+++ modules/openstack_project/manifests/etherpad.pp
@@ -13,7 +13,7 @@ class openstack_project::etherpad (
   class { 'etherpad_lite':
     ep_ensure      => 'latest',
     eplite_version => 'cc9f88e7ed4858b72feb64c99beb3e13445ab6d9',
-    nodejs_version => 'system',
+    nodejs_version => '6.x',
   }
 
   class { 'etherpad_lite::apache':
