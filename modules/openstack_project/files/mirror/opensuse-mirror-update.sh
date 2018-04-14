f3c4d506a0f7cffa63ea7ca57bd843669b4f70aa
Dirk Mueller <dirk@dmllr.de>
2018-04-14 22:00:56 +0200

    Mirror opensuse emergency update channel
    With $DIB_DISTRIBUTION_MIRROR being set, it expects an update
    channel for tumbleweed distribution as well. Given that there
    is an emergency update channel (that is usually empty), we
    can use that one to resolve issues with the nodepool image
    building.
    
    Change-Id: I5f786d16f26bfb836d37a740f8490d81076cdfbb

--- modules/openstack_project/files/mirror/opensuse-mirror-update.sh
+++ modules/openstack_project/files/mirror/opensuse-mirror-update.sh
@@ -61,6 +61,20 @@ $K5START rsync -rlptDvz \
     --exclude="i586" \
     $MIRROR/$REPO/repo/oss/ $BASE/$REPO/repo/oss/
 
+REPO=update/tumbleweed
+if ! [ -f $BASE/$REPO ]; then
+    $K5START mkdir -p $BASE/$REPO
+fi
+
+date --iso-8601=ns
+echo "Running rsync distribution $REPO ..."
+$K5START rsync -rlptDvz \
+    --delete --stats \
+    --delete-excluded \
+    --exclude="i586" \
+    rsync://rsync.opensuse.org/buildservice-repos-main/openSUSE:/Factory:/Update/standard/ \
+    $BASE/$REPO
+
 date --iso-8601=ns | $K5START tee $BASE/timestamp.txt
 echo "rsync completed successfully, running vos release."
 k5start -t -f /etc/afsadmin.keytab service/afsadmin -- vos release -v $MIRROR_VOLUME
