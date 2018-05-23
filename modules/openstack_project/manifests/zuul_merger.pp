6402f98d12054fcc734e3108963aa257a568e477
James E. Blair <jeblair@redhat.com>
2018-05-22 18:08:18 -0700

    Add opendaylight connection to zuul
    This adds a connection to the opendaylight Gerrit server to zuul
    so that we can perform cross-community testing.
    
    Change-Id: I846983e873072c2235a4c49e36f602a47d06cfce

--- modules/openstack_project/manifests/zuul_merger.pp
+++ modules/openstack_project/manifests/zuul_merger.pp
@@ -18,7 +18,7 @@ class openstack_project::zuul_merger(
     gearman_server           => $gearman_server,
     gerrit_server            => $gerrit_server,
     gerrit_user              => $gerrit_user,
-    known_hosts_content      => "review.openstack.org,104.130.246.32,2001:4800:7819:103:be76:4eff:fe04:9229 ${gerrit_ssh_host_key}",
+    known_hosts_content      => "review.openstack.org,104.130.246.32,2001:4800:7819:103:be76:4eff:fe04:9229 ${gerrit_ssh_host_key}\ngit.opendaylight.org,52.35.122.251,2600:1f14:421:f500:7b21:2a58:ab0a:2d17 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyRXyHEw/P1iZr/fFFzbodT5orVV/ftnNRW59Zh9rnSY5Rmbc9aygsZHdtiWBERVVv8atrJSdZool75AglPDDYtPICUGWLR91YBSDcZwReh5S9es1dlQ6fyWTnv9QggSZ98KTQEuE3t/b5SfH0T6tXWmrNydv4J2/mejKRRLU2+oumbeVN1yB+8Uau/3w9/K5F5LgsDDzLkW35djLhPV8r0OfmxV/cAnLl7AaZlaqcJMA+2rGKqM3m3Yu+pQw4pxOfCSpejlAwL6c8tA9naOvBkuJk+hYpg5tDEq2QFGRX5y1F9xQpwpdzZROc5hdGYntM79VMMXTj+95dwVv/8yTsw==",
     zuul_ssh_private_key     => $zuul_ssh_private_key,
     zuul_url                 => $zuul_url,
     git_email                => $git_email,
