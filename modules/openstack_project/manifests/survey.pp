2002c87e3cc99451d2cd57154cd34f1033140f19
Anita Kuno <anteaya@anteaya.info>
2018-05-31 13:30:41 -0400

    Survey Documentation
    This patch creates the documentation for the survey service.
    
    Co-Authored-By: Jeremy Stanley <fungi@yuggoth.org>
    
    Change-Id: Ie602a952b58c5f5200518cb31218097bddd5b747
    Story: 2000691

--- modules/openstack_project/manifests/survey.pp
+++ modules/openstack_project/manifests/survey.pp
@@ -1,3 +1,23 @@
+# Copyright 2016 Markus Opolka <markus@martiablog.de>
+# Copyright 2018 Anita Kuno <anteaya@anteaya.info>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Inspiration for this patch as well as portions of it
+# come from the work of Markus Opolka and his
+# LimeSurvey Puppet module:
+# https://github.com/martialblog/puppet-limesurvey
+#
 class openstack_project::survey (
   $vhost_name = $::fqdn,
   $ssl_cert_file = '/etc/ssl/certs/survey.openstack.org.pem',
