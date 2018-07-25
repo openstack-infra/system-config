e5480fc3279bb13cbd35f389ed0b1480628c4eaa
Monty Taylor <mordred@inaugust.com>
2018-07-25 12:36:26 -0500

    Remove base.yaml things from openstack_project::server
    Now that we've got base server stuff rewritten in ansible, remove the
    old puppet versions.
    
    Update the update_puppet_version playbook to do both 3 and 4 and set an
    ansible variable to control which hosts get which version.
    
    Depends-On: https://review.openstack.org/588326
    Change-Id: I5c82fe6fd25b9ddaa77747db377ffa7e8bf23c7b

--- modules/openstack_project/manifests/params.pp
+++ modules/openstack_project/manifests/params.pp
@@ -3,24 +3,12 @@
 # This class holds parameters that need to be
 # accessed by other classes.
 class openstack_project::params {
-  $cross_platform_packages = [
-    'at',
-    'git',
-    'lvm2',
-    'parted',
-    'rsync',
-    'strace',
-    'tcpdump',
-    'wget',
-  ]
   case $::osfamily {
     'RedHat': {
-      $packages = concat($cross_platform_packages, ['iputils', 'bind-utils'])
       $user_packages = ['emacs-nox', 'vim-enhanced']
       $login_defs = 'puppet:///modules/openstack_project/login.defs.redhat'
     }
     'Debian': {
-      $packages = concat($cross_platform_packages, ['iputils-ping', 'dnsutils'])
       case $::operatingsystemrelease {
         /^(12|14)\.(04|10)$/: {
           $user_packages = ['emacs23-nox', 'vim-nox', 'iftop',
