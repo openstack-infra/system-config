b7351fe43c3228b04f8313bdac55601b7fb798e3
Monty Taylor <mordred@inaugust.com>
2018-08-17 06:17:16 -0500

    Install limestone CA on hosts using openstacksdk
    In order to talk to limestone clouds we need to configure a custom CA.
    Do this in ansible instead of puppet.
    
    A followup should add writing out clouds.yaml files.
    
    Change-Id: I355df1efb31feb31e039040da4ca6088ea632b7e
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- testinfra/test_base.py
+++ testinfra/test_base.py
@@ -147,3 +147,20 @@ def test_unattended_upgrades(host):
         cfg_file = host.file("/etc/yum/yum-cron.conf")
         assert cfg_file.exists
         assert cfg_file.contains('apply_updates = yes')
+
+
+def test_openstacksdk_config(host):
+    f = host.file('/etc/openstack')
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o750
+    del f
+
+    f = host.file('/etc/openstack/limestone_cacert.pem')
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o640
