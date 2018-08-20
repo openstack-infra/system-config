2e2f91260e39fdaf01a76547552f26fcec45dc22
Ian Wienand <iwienand@redhat.com>
2018-08-20 13:32:23 +1000

    Generate roles documentation
    Since we're building out roles in system-config now, generate
    documentation.  We look in roles/* and playbook/roles/* (follow-on
    changes will split things up between the two).
    
    Correct the reference names in the exim documentation to avoid
    warnings and failure.
    
    This also revealed a single unicode character in the exim readme
    (which caused prior versions of zuul-sphinx to barf).  For fun, see if
    you can find it!
    
    Depends-On: https://review.openstack.org/#/c/579474/
    Change-Id: I243a96bbd6d09560f8aa80b6345b90039422547a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,7 +15,15 @@ sys.path.insert(0, os.path.abspath('.'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['custom_roles', 'openstackdocstheme']
+extensions = ['custom_roles',
+              'openstackdocstheme',
+              'zuul_sphinx']
+
+# We have roles split between zuul-suitable roles at top level roles/*
+# (automatically detected by zuul-sphinx) and playbook-specific roles
+# (might have plugins, etc that make them unsuitable as potential zuul
+# roles).  Document both.
+zuul_role_paths = ['playbooks/roles']
 
 # openstackdocstheme options
 repository_name = 'openstack-infra/system-config'
