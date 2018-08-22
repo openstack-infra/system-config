360e0107a740a2dcdca98e75cb12cd9af39eebb1
David Shrewsbury <shrewsbury.dave@gmail.com>
2018-08-22 14:41:11 -0400

    Add NTP service to base-server
    Removes puppet-side ntp setup.
    
    Change-Id: I75a87ca55fd4b75c5715cc3fd2b708e0b0ae2c20

--- modules/openstack_project/manifests/server.pp
+++ modules/openstack_project/manifests/server.pp
@@ -20,11 +20,6 @@ class openstack_project::server (
   }
 
   ###########################################################
-  # Manage  ntp
-
-  include '::ntp'
-
-  ###########################################################
   # Process if ( $high_level_directive ) blocks
 
   if ($enable_unbound) {
