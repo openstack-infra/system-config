a6c2fb53e7ddf1bd2925c30de2a6d851c56e27c3
Ian Wienand <iwienand@redhat.com>
2018-08-24 17:59:28 +1000

    Use zuul-sphinx README.rst detection
    With the dependent change, zuul-sphinx will raise a warning when the
    autoroles matcher finds a role without a README.rst.  Since we
    error-on-warnings this will stop the docs build.  Thus we don't need
    this explicit linters check.
    
    Change-Id: I02eefce2448152505bacba3b2a12021515b4a31a
    Depends-On: https://review.openstack.org/596014

--- tools/check_readme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Trivial script run from tox -e linters to ensure roles have a
-# README.rst attached
-
-if ! [ -f $1/README.rst ]; then
-    echo "*** Missing role documentation: $1/README.rst"
-    exit 1
-fi
