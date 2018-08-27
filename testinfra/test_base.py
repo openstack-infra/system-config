744c7dcff5745da31360f0f36fef5ec16208b60b
Ian Wienand <iwienand@redhat.com>
2018-08-27 12:09:59 +1000

    [wip] create an ansible group for bridge.o.o
    Change-Id: Id182a653b22af4c3fcb5333548d5974594005622

--- testinfra/test_base.py
+++ testinfra/test_base.py
@@ -21,3 +21,25 @@ def test_exim_is_installed(host):
 
     cmd = host.run("exim -bt root")
     assert cmd.rc == 0
+
+def test_ansible_group_on_bridge(host):
+    '''Check for "ansible" group
+
+    There should be an "ansible" group on bridge.o.o, but not on other
+    hosts where ansible is not installed.  Ansible running directories
+    should be in this group.
+    '''
+    ansible_vars = host.ansible.get_variables()
+    if ansible_vars['inventory_hostname'] == 'bridge.openstack.org':
+        assert host.group("ansible").exists
+        ansible_cache = host.file('/var/cache/ansible')
+        assert ansible_cache.user == 'root'
+        assert ansible_cache.group == 'ansible'
+        assert ansible_cache.mode == 0o770
+
+        ansible_log = host.file('/var/cache/ansible')
+        assert ansible_log.user == 'root'
+        assert ansible_log.group == 'ansible'
+        assert ansible_log.mode == 0o775
+    else:
+        assert not host.group("ansible").exists
