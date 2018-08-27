bfc06f5586bfec40236c25a4a7593dd48f47bce6
David Shrewsbury <shrewsbury.dave@gmail.com>
2018-08-27 13:39:59 -0400

    Enable unattended upgrades
    Debian gets this from 'unattended-upgrades' and Red Hat family
    gets it from yum-cron.
    
    Change-Id: Ie00f6073ad1127b11a89e69816baf47011480dbf

--- testinfra/test_base.py
+++ testinfra/test_base.py
@@ -91,3 +91,19 @@ def test_snmp(host):
 def test_timezone(host):
     tz = host.check_output('date +%Z')
     assert tz == "UTC"
+
+
+def test_unattended_upgrades(host):
+    if host.system_info.distribution in ['ubuntu', 'debian']:
+        package = host.package("unattended-upgrades")
+        cfg_file = host.file("/etc/apt/apt.conf.d/20auto-upgrades")
+        assert package.is_installed
+        assert cfg_file.exists
+        assert cfg_file.contains('APT::Periodic::Update-Package-Lists "1"')
+        assert cfg_file.contains('APT::Periodic::Unattended-Upgrade "1"')
+    else:
+        package = host.package("yum-cron")
+        cfg_file = host.file("/etc/yum/yum-cron.conf")
+        assert package.is_installed
+        assert cfg_file.exists
+        assert cfg_file.contains('apply_updates = yes')
