9bc7f96c7c1ecf76b94b7f477b1228a41a4e47b1
James E. Blair <jeblair@redhat.com>
2018-08-30 16:05:06 -0700

    Add testinfra tests for bridge
    Change-Id: I4df79669c9daa3eb998ee666be6c53c957467748

---
+++ testinfra/test_bridge.py
@@ -0,0 +1,45 @@
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+testinfra_hosts = ['bridge.openstack.org']
+
+
+def test_clouds_yaml(host):
+    clouds_yaml = host.file('/etc/openstack/clouds.yaml')
+    assert clouds_yaml.exists
+
+    assert b'password' in clouds_yaml.content
+
+    all_clouds_yaml = host.file('/etc/openstack/all-clouds.yaml')
+    assert all_clouds_yaml.exists
+
+    assert b'password' in all_clouds_yaml.content
+
+
+def test_openstacksdk_config(host):
+    f = host.file('/etc/openstack')
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o750
+    del f
+
+    f = host.file('/etc/openstack/limestone_cacert.pem')
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o640
