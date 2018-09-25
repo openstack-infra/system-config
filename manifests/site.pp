eb11f1984f273cbf6563dd1f765966b658d18199
James E. Blair <jeblair@redhat.com>
2018-09-25 07:56:23 -0700

    Add opendev nameservers
    This will allow us to create new nameservers in the opendev.org
    domain.  We will replace the existing servers once these are
    bootstrapped.
    
    Change-Id: If71e3f87a9d7a83d80cff053874c84411b248515

--- manifests/site.pp
+++ manifests/site.pp
@@ -733,6 +733,21 @@ node /^adns\d+\.openstack\.org$/ {
   }
 }
 
+# This is a hidden authoritative master nameserver, not publicly
+# accessible.
+# Node-OS: bionic
+node /^adns\d+\.opendev\.org$/ {
+  $group = 'adns'
+
+  class { 'openstack_project::server': }
+
+  class { 'openstack_project::master_nameserver':
+    tsig_key => hiera('tsig_key', {}),
+    dnssec_keys => hiera_hash('dnssec_keys', {}),
+    notifies => concat(dns_a('ns1.opendev.org'), dns_a('ns2.opendev.org')),
+  }
+}
+
 # These are publicly accessible authoritative slave nameservers.
 # Node-OS: xenial
 node /^ns\d+\.openstack\.org$/ {
@@ -758,6 +773,37 @@ node /^ns\d+\.openstack\.org$/ {
         allow_notify => dns_a('adns1.openstack.org'),
         masters => dns_a('adns1.openstack.org'),
         zones => ['zuul-ci.org', 'zuulci.org'],
+        tsig_name => $tsig_name,
+      }
+    }
+  }
+}
+
+# These are publicly accessible authoritative slave nameservers.
+# Node-OS: bionic
+node /^ns\d+\.opendev\.org$/ {
+  $group = 'ns'
+
+  class { 'openstack_project::server': }
+
+  $tsig_key = hiera('tsig_key', {})
+  if $tsig_key != {} {
+    $tsig_name = 'tsig'
+    nsd::tsig { 'tsig':
+      algo => $tsig_key[algorithm],
+      data => $tsig_key[secret],
+    }
+  } else {
+    $tsig_name = undef
+  }
+
+  class { '::nsd':
+    ip_addresses => [ $::ipaddress, $::ipaddress6 ],
+    zones => {
+      'adns1_zones' => {
+        allow_notify => dns_a('adns1.opendev.org'),
+        masters => dns_a('adns1.opendev.org'),
+        zones => ['opendev.org'],
         tsig_name => $tsig_name,
       }
     }
