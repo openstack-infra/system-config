5be872b0d9d4ba42635a41d7f02c1281ce2d220c
Ian Wienand <iwienand@redhat.com>
2018-09-27 15:15:58 +1000

    Import install-docker role
    This is a role for installing docker on our control-plane servers.
    
    It is based on install-docker from zuul-jobs.
    
    Basic testinfra tests are added; because docker fiddles the iptables
    rules in magic ways, the firewall testing is moved out of the base
    tests and skipped for now on the docker host.
    
    Change-Id: Ia4de5032789ff0f2b07d4f93c0c52cf94aa9c25c

--- testinfra/test_base.py
+++ testinfra/test_base.py
@@ -62,7 +62,7 @@ def test_iptables(host):
     rules = host.iptables.rules()
     rules = [x.strip() for x in rules]
 
-    start = [
+    needed_rules = [
         '-P INPUT ACCEPT',
         '-P FORWARD ACCEPT',
         '-P OUTPUT ACCEPT',
@@ -72,11 +72,11 @@ def test_iptables(host):
         '-A openstack-INPUT -p icmp -m icmp --icmp-type any -j ACCEPT',
         '-A openstack-INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT',
         '-A openstack-INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT',
+        '-A openstack-INPUT -j REJECT --reject-with icmp-host-prohibited'
     ]
+    for rule in needed_rules:
+        assert rule in rules
     assert rules[:len(start)] == start
-
-    reject = '-A openstack-INPUT -j REJECT --reject-with icmp-host-prohibited'
-    assert reject in rules
 
     # Make sure that the zuul console stream rule is still present
     zuul = ('-A openstack-INPUT -p tcp -m state --state NEW'
