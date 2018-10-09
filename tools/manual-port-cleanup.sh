07e20fb48354a705561ef0ba1fa4b91368298393
Ian Wienand <iwienand@redhat.com>
2018-10-10 07:59:41 +1100

    Add a helper for manual port cleanup
    If ports leak on an active cloud/region, you may need to clean them
    up.  This should make it easier.
    
    Change-Id: Iee9a076468e105c7a4931620b0c2c5edab003dc9

---
+++ tools/manual-port-cleanup.sh
@@ -0,0 +1,41 @@
+#
+# Script to help with manual removal of leaked ports on bridge.o.o.
+#
+# Removes any DOWN ports in the CLOUD/REGION that have remained after
+# sleeping for a few minutes
+#
+
+CLOUD=openstackjenkins-ovh
+REGION=BHS1
+
+export OS_CLIENT_CONFIG_FILE=/etc/openstack/all-clouds.yaml
+OSC="openstack --os-cloud=$CLOUD --os-region=$REGION"
+
+first=$(mktemp)
+second=$(mktemp)
+
+echo "Getting initial port status"
+$OSC port list -c ID -c Status -f value | grep DOWN | awk '{print $1}' > $first
+echo "Done"
+
+echo "Pausing"
+sleep 180
+
+echo "Getting updated port status"
+$OSC port list -c ID -c Status -f value | grep DOWN | awk '{print $1}' > $second
+echo "Done"
+
+ports=$(comm -12 <(sort $first) <(sort $second))
+
+port_count=$(wc -l <<< $ports)
+echo $port_count
+
+i=1
+for port in $ports; do
+    echo "Remove port $port [$i/$port_count]"
+    $OSC port delete $port
+    i=$((i+1))
+done
+
+rm $first
+rm $second
