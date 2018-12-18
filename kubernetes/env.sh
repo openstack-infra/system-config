e0d7024080a7db0fab596dfe31d58c9c26392ad3
Monty Taylor <mordred@inaugust.com>
2018-12-18 23:20:00 +0000

    WIP Deploy a kubernetes and a gitea
    This is what was used for the POC - but it is NOT in shape to land and
    use for real.
    
    Change-Id: Ie019c2e24c3780cec2468a00987dba4ac34ed570

---
+++ kubernetes/env.sh
@@ -0,0 +1,25 @@
+# TODO(mordred) Update the ansible to not need env vars
+# This has to be done via env vars due to how the
+# ansible writes out some config values.
+export OS_AUTH_URL="https://auth.vexxhost.net/v3"
+export OS_REGION_NAME="sjc1"
+export OS_USERNAME="openstackci"
+export OS_USER_DOMAIN_NAME="default"
+export OS_PROJECT_NAME="openstackci"
+export OS_PROJECT_DOMAIN_NAME="default"
+export OS_PASSWORD=TODO-FIX-ME
+
+export KEY="infra-root-keys"
+export NAME="opendev-k8s"
+export IMAGE="Ubuntu 16.04 LTS (x86_64) [2018-08-24]"
+export MASTER_FLAVOR="v2-highcpu-4"
+export MASTER_BOOT_FROM_VOLUME="True"
+export FLOATING_IP_NETWORK_UUID="0048fce6-c715-4106-a810-473620326cb0"
+export NODE_FLAVOR="v2-highcpu-8"
+export NODE_AUTO_IP="True"
+export NODE_BOOT_FROM_VOLUME="True"
+export NODE_VOLUME_SIZE="64"
+export NODE_EXTRA_VOLUME="True"
+export NODE_EXTRA_VOLUME_SIZE="80"
+export USE_OCTAVIA="True"
+export BLOCK_STORAGE_VERSION='v3'
---
+++ kubernetes/run.sh
@@ -0,0 +1,15 @@
+# TODO(mordred) Turn this in to a playbook. This file is here to record
+# the general sequence needed.
+kubectl apply -f rook-operator.yaml
+kubectl apply -f rook-cluster.yaml
+kubectl apply -f rook-toolbox.yaml
+kubectl apply -f rook-filesystem.yaml
+kubectl apply -f gitea-namespace.yaml
+kubectl apply -f storage-class.yaml
+kubectl apply -f mariadb-secret.yaml
+kubectl apply -f mariadb-pv.yaml
+kubectl apply -f mariadb.yaml
+kubectl apply -f gitea.yaml
+
+source env.sh
+ansible-playbook site.yaml
