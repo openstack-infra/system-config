8c4c9da94499048e2984e05e502ae13c684d8117
James E. Blair <jeblair@redhat.com>
2019-01-11 15:04:16 -0800

    WIP: run a gerrit container
    Change-Id: I399d3cf8471bc8063022b0db0ff81718b2ee2941

---
+++ testinfra/test_gerrit.py
@@ -0,0 +1,21 @@
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+testinfra_hosts = ['review.opendev.org']
+
+
+def test_gerrit_listening(host):
+    gerrit_web = host.socket("tcp://0.0.0.0:8080")
+    assert gerrit_web.is_listening
