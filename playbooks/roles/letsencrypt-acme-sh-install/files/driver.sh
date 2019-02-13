61064a2bdf9ee96329347075b32b6f0e12063585
Ian Wienand <iwienand@redhat.com>
2019-02-14 08:10:51 +1100

    [WIP] letsencrypt support
    See the proposed spec at
    
     https://review.openstack.org/#/c/587283/
    
    This change illustrates:
    
     - defining the certificates required
     - running the inital issue and getting the authentication tokens
     - updating the TXT entries on adns
     - running the authentication check (which would create the certificates)
    
    What is not done/to come:
    
     - actually creating the certificates
     - idempotentency for valid certificates; i.e. handling certifcates that
       currently don't need renweal
    
    Depends-On: https://review.openstack.org/644701
    Change-Id: I1f66da614751a29cc565b37cdc9ff34d70fdfd3f

---
+++ playbooks/roles/letsencrypt-acme-sh-install/files/driver.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Take output like:
+#  [Thu Feb 14 13:44:37 AEDT 2019] Domain: '_acme-challenge.test.opendev.org'
+#  [Thu Feb 14 13:44:37 AEDT 2019] TXT value: 'QjkChGcuqD7rl0jN8FNWkWNAISX1Zry_vE-9RxWF2pE'
+#
+# and turn it into:
+#
+# _acme-challenge.test.opendev.org:QjkChGcuqD7rl0jN8FNWkWNAISX1Zry_vE-9RxWF2pE
+#
+# Ansible then parses this back to a dict
+
+ACME_SH=${ACME_SH:-/opt/acme.sh/acme.sh}
+CERT_HOME=${CERT_HOME:-/etc/letsencrypt-certs}
+CHALLENGE_ALIAS_DOMAIN=${CHALLENGE_ALIAS_DOMAIN:-acme.opendev.org}
+# default to staging to avoid production errors
+LETSENCRYPT_STAGING=${LETSENCRYPT_STAGING:--staging}
+LOG_FILE=${LOG_FILE:-/var/log/acme.sh/acme.sh.log}
+
+if [[ ${1} == "issue" ]]; then
+    shift;
+    for arg in "$@"; do
+
+        $ACME_SH ${STAGING} \
+            --cert-home ${CERT_HOME} \
+            --no-color \
+            --yes-I-know-dns-manual-mode-enough-go-ahead-please \
+            --issue \
+            --dns \
+            --challenge-alias ${CHALLENGE_ALIAS_DOMAIN} \
+            $arg 2>&1 | tee -a ${LOG_FILE} | \
+                egrep 'Domain:|TXT value:' | cut -d"'" -f2 | paste -d':' - -
+                # shell magic ^ is
+                #  - extract everything between ' '
+                #  - stick every two lines together, separated by a :
+    done
+elif [[ ${1} == "renew" ]]; then
+    shift;
+    for arg in "$@"; do
+        $ACME_SH \
+            --cert-home ${CERT_HOME} \
+            --no-color \
+            --yes-I-know-dns-manual-mode-enough-go-ahead-please \
+            --renew \
+            --staging \
+            $arg 2>&1 | tee -a ${LOG_FILE}
+    done
+else
+    echo "Unknown driver arg: $1"
+    exit 1
+fi
+
