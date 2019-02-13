2fb3dfe6e472ff97876ff75ce8a46c6b018027f2
Ian Wienand <iwienand@redhat.com>
2019-02-14 08:10:51 +1100

    [WIP] letsencrypt support
    See the proposed spec at
    
     https://review.openstack.org/#/c/587283/
    
    This change illustrates:
    
     - defining the certificates required
     - running the inital issue and getting the authentication tokens
     - updating the TXT entries on adns
     - running the authentication check (which would create the certificates)
    
    What is not done/to come:
    
     - actually creating the certificates
     - idempotentency for valid certificates; i.e. handling certifcates that
       currently don't need renweal
    
    Change-Id: I1f66da614751a29cc565b37cdc9ff34d70fdfd3f

---
+++ testinfra/test_letsencrypt.py
@@ -0,0 +1,60 @@
+# Copyright 2019 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import pytest
+
+testinfra_hosts = ['adns-letsencrypt.opendev.org',
+                   'letsencrypt01.opendev.org',
+                   'letsencrypt02.opendev.org']
+
+
+def test_acme_zone(host):
+    if host.backend.get_hostname() != 'adns-letsencrypt.opendev.org':
+        pytest.skip()
+    acme_opendev_zone = host.file('/var/lib/bind/zones/acme.opendev.org/zone.db')
+    assert acme_opendev_zone.exists
+
+    # On our test nodes, unbound is listening on 127.0.0.1:53; this
+    # ensures the query hits bind
+    query_addr = host.ansible("setup")["ansible_facts"]["ansible_default_ipv4"]["address"]
+    cmd = host.run("dig -t txt _acme-challenge.acme.opendev.org @" + query_addr)
+    count = 0
+    for line in cmd.stdout.split('\n'):
+        if line.startswith('_acme-challenge.acme.opendev.org.'):
+            count = count + 1
+    if count != 6:
+        # NOTE(ianw): I'm sure there's more pytest-y ways to save this
+        # for debugging ...
+        print(cmd.stdout)
+    assert count == 6, "Did not see required number of TXT records!"
+
+def test_certs_created(host):
+    if host.backend.get_hostname() == 'letsencrypt01.opendev.org':
+        domain_one = host.file(
+            '/etc/letsencrypt-certs/'
+            'letsencrypt01.opendev.org/letsencrypt01.opendev.org.key')
+        assert domain_one.exists
+        domain_two = host.file(
+            '/etc/letsencrypt-certs/'
+            'someotherservice.opendev.org/someotherservice.opendev.org.key')
+        assert domain_two.exists
+
+    elif host.backend.get_hostname() == 'letsencrypt02.opendev.org':
+        domain_one = host.file(
+            '/etc/letsencrypt-certs/'
+            'letsencrypt02.opendev.org/letsencrypt02.opendev.org.key')
+        assert domain_one.exists
+
+    else:
+        pytest.skip()
