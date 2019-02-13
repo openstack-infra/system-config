c2190c44fd0010d3a3ca12405a5d6152471b01f9
Ian Wienand <iwienand@redhat.com>
2019-02-14 08:10:51 +1100

    letsencrypt support
    This change contains the roles and testing for deploying certificates
    on hosts using letsencrypt with domain authentication.
    
    From a top level, the process is implemented in the roles as follows:
    
    1) letsencrypt-acme-sh-install
    
       This role installs the acme.sh tool on hosts in the letsencrypt
       group, along with a small custom driver script to help parse output
       that is used by later roles.
    
    2) letsencrypt-request-certs
    
       This role runs on each host, and reads a host variable describing
       the certificates required.  It uses the acme.sh tool (via the
       driver) to request the certificates from letsencrypt.  It populates
       a global Ansible variable with the authentication TXT records
       required.
    
       If the certificate exists on the host and is not within the renewal
       period, it should do nothing.
    
    3) letsencrypt-install-txt-record
    
       This role runs on the adns server.  It installs the TXT records
       generated in step 2 to the acme.opendev.org domain and then
       refreshes the server.  Hosts wanting certificates will have
       pre-provisioned CNAME records for _acme-challenge.host.opendev.org
       pointing to acme.opendev.org.
    
    4) letsencrypt-create-certs
    
       This role runs on each host, reading the same variable as in step
       2.  However this time the acme.sh tool is run to authenticate and
       create the certificates, which should now work correctly via the
       TXT records from step 3.  After this, the host will have the
       full certificate material.
    
    Testing is added via testinfra.  For testing purposes requests are
    made to the staging letsencrypt servers and a self-signed certificate
    is provisioned in step 4 (as the authentication is not available
    during CI).  We test that the DNS TXT records are created locally on
    the CI adns server, however.
    
    Related-Spec: https://review.openstack.org/587283
    
    Change-Id: I1f66da614751a29cc565b37cdc9ff34d70fdfd3f

---
+++ playbooks/roles/letsencrypt-acme-sh-install/files/driver.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+ACME_SH=${ACME_SH:-/opt/acme.sh/acme.sh}
+CERT_HOME=${CERT_HOME:-/etc/letsencrypt-certs}
+CHALLENGE_ALIAS_DOMAIN=${CHALLENGE_ALIAS_DOMAIN:-acme.opendev.org.}
+# Set to !0 to use letsencrypt staging rather than production requests
+LETSENCRYPT_STAGING=${LETSENCRYPT_STAGING:-0}
+LOG_FILE=${LOG_FILE:-/var/log/acme.sh/acme.sh.log}
+
+STAGING=""
+if [[ ${LETSENCRYPT_STAGING} != 0 ]]; then
+    STAGING="--staging"
+fi
+
+echo -e  "\n--- start --- $(date -u '+%Y-%m-%dT%k:%M:%S%z') ---" >> ${LOG_FILE}
+
+if [[ ${1} == "issue" ]]; then
+    # Take output like:
+    #  [Thu Feb 14 13:44:37 AEDT 2019] Domain: '_acme-challenge.test.opendev.org'
+    #  [Thu Feb 14 13:44:37 AEDT 2019] TXT value: 'QjkChGcuqD7rl0jN8FNWkWNAISX1Zry_vE-9RxWF2pE'
+    #
+    # and turn it into:
+    #
+    # _acme-challenge.test.opendev.org:QjkChGcuqD7rl0jN8FNWkWNAISX1Zry_vE-9RxWF2pE
+    #
+    # Ansible then parses this back to a dict.
+    shift;
+    for arg in "$@"; do
+        $ACME_SH ${STAGING} \
+            --cert-home ${CERT_HOME} \
+            --no-color \
+            --yes-I-know-dns-manual-mode-enough-go-ahead-please \
+            --issue \
+            --dns \
+            --challenge-alias ${CHALLENGE_ALIAS_DOMAIN} \
+            $arg 2>&1 | tee -a ${LOG_FILE} | \
+                egrep 'Domain:|TXT value:' | cut -d"'" -f2 | paste -d':' - -
+                # shell magic ^ is
+                #  - extract everything between ' '
+                #  - stick every two lines together, separated by a :
+    done
+elif [[ ${1} == "renew" ]]; then
+    shift;
+    for arg in "$@"; do
+        $ACME_SH ${STAGING} \
+            --cert-home ${CERT_HOME} \
+            --no-color \
+            --yes-I-know-dns-manual-mode-enough-go-ahead-please \
+            --renew \
+            $arg 2>&1 | tee -a ${LOG_FILE}
+    done
+elif [[ ${1} == "selfsign" ]]; then
+    # For testing, simulate the key generation
+    shift;
+    for arg in "$@"; do
+        # TODO(ianw): Set SAN names from the other "-d" arguments?;
+        # it's a pita to parse.
+        read -r -a domain_array <<< "$arg"
+        domain=${domain_array[1]}
+        mkdir -p ${CERT_HOME}/${domain}
+        cd ${CERT_HOME}/${domain}
+        echo "Creating certs in ${CERT_HOME}/${domain}"
+        openssl genrsa -out ${domain}.key 2048
+        openssl rsa -in ${domain}.key -out ${domain}.key
+        openssl req -sha256 -new -key ${domain}.key -out ${domain}.csr -subj '/CN=localhost'
+        openssl x509 -req -sha256 -days 365 -in ${domain}.csr -signkey ${domain}.key -out ${domain}.cer
+        cp ${domain}.cer fullchain.cer
+    done
+else
+    echo "Unknown driver arg: $1"
+    exit 1
+fi
+
+echo "--- end   --- $(date -u '+%Y-%m-%dT%k:%M:%S%z') ---" >> ${LOG_FILE}
