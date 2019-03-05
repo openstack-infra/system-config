54a09c0e50fac18a84d0d96eee8d292d833ffb1b
James E. Blair <jeblair@redhat.com>
2019-03-04 16:31:09 -0800

    DNM: test broken zuul-preview dockerfile
    Change-Id: Ib4acd288f0b344eb11e5634f554c9f0276a9bfba
    Depends-On: https://review.openstack.org/640901

--- testinfra/test_zuul_preview.py
+++ testinfra/test_zuul_preview.py
@@ -11,7 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+# test
 
 testinfra_hosts = ['zp01.opendev.org']
 
