8d406568b7d8b8ac3b86ba9550308a46335087d1
Ian Wienand <iwienand@redhat.com>
2019-03-11 02:50:11 +0000

    [dmn]
    stashing some scripts to make git:// -> https:// changes
    
    Change-Id: Ibd44ca8f3444c960e0b2cd3e40a5d99d327d1577

---
+++ tools/mass-git-change/all-repos.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+import functools
+import logging
+import json
+import requests
+import sys
+
+logging.basicConfig(level=logging.ERROR)#logging.DEBUG)
+
+url = 'https://review.openstack.org/projects/'
+
+# This is what a project looks like
+'''
+  "openstack-attic/akanda": {
+    "id": "openstack-attic%2Fakanda",
+    "state": "READ_ONLY"
+  },
+'''
+
+# Check if this project has a plugin file
+def has_devstack_plugin(proj):
+    # Don't link in the deb packaging repos
+    if "openstack/deb-" in proj:
+        return False
+    r = requests.get("https://git.openstack.org/cgit/%s/plain/devstack/plugin.sh" % proj)
+    return r.status_code == 200
+
+def is_in_openstack_namespace(proj):
+    # only interested in openstack namespace (e.g. not retired
+    # stackforge, etc)
+    return proj.startswith('openstack')
+
+# Check if this project has a plugin file
+def has_devstack_plugin(session, proj):
+    # Don't link in the deb packaging repos
+    if "openstack/deb-" in proj:
+        return False
+    r = session.get("https://git.openstack.org/cgit/%s/plain/devstack/plugin.sh" % proj)
+    return r.status_code == 200
+
+
+logging.debug("Getting project list from %s" % url)
+r = requests.get(url)
+with requests.Session() as session:
+    has_devstack_plugin = functools.partial(has_devstack_plugin, session)
+    projects = sorted(filter(is_in_openstack_namespace, json.loads(r.text[4:])))
+    logging.debug("Found %d projects" % len(projects))
+    found_plugins = filter(has_devstack_plugin, projects)
+
+    for project in found_plugins:
+        print('https://git.openstack.org/'+project)
