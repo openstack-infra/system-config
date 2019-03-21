fa8276a3e2e0a6e40f3be2d23bea863cb16f6d35
Clark Boylan <clark.boylan@gmail.com>
2019-03-21 13:36:04 -0700

    Install latest grafana
    With puppet3 we were install 5.4.2 even though we wanted 5.1.3. Puppet4
    is now installing the version we specify (5.1.3); however, if newer
    versions just work works (which experience says they do) maybe we should
    just install latest.
    
    Change-Id: I63e6bf9cdc1c95e8abdf79492dcc1258ab90bc88

--- modules/openstack_project/manifests/grafana.pp
+++ modules/openstack_project/manifests/grafana.pp
@@ -53,7 +53,7 @@ class openstack_project::grafana (
 
   $grafana_cfg_merged = merge($grafana_cfg_defaults, $grafana_cfg)
 
-  $version = '5.1.3'
+  $version = 'latest'
 
   class { '::grafana':
     cfg            => $grafana_cfg_merged,
